bc1d7e0888082f8bc223462aa7808fac899748f4
Guillaume Marquilly <guillaume@blockchain.com>
2017-05-23 13:40:19 +0100

    added Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Dream Wallet Front-end
+
+# Commands
+
+1. `npm install`: restore the npm dependencies
+2. `npm run build`: build the project
+3. `npm run start`: start the website on http://localhost:3000
---
+++ src/components/Transactions/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const TransactionsPage = () => {
+
+  return (
+      <section className="transactions">
+        <span>TransactionsPage</span>
+      </section>
+    );
+  };
+
+export default TransactionsPage;
