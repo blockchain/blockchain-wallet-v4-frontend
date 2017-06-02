ffcea39543c38177b6c854a49309baa60441304c
Guillaume Marquilly <guillaume@blockchain.com>
2017-06-02 17:21:36 +0100

    fix - missing files

--- src/scenes/SecuritySettings/template.js
+++ src/scenes/SecuritySettings/template.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Basic from './Basic'
-import Advanced from './Advanced'
 import ActivityLogging from './ActivityLogging'
 import APIAccess from './APIAccess'
 import IPWhitelist from './IPWhitelist'
@@ -20,17 +18,8 @@ import SettingRow from 'components/Shared/SettingRow'
 import style from './style.scss'
 
 const SecuritySettings = ({ advanced, toggleAdvanced }) => {
-  let advancedButtonClass = 'button-default' + (advanced ? ' active' : '')
   return (
     <section className={style.securitySettings}>
-      {/*
-      <Basic />
-      <div style={{ paddingTop: 16, paddingBottom: 40 }}>
-        <button className={advancedButtonClass} onClick={toggleAdvanced}>Advanced Settings</button>
-      </div>
-      */}
-      {/*advanced && (<Advanced />)*/}
-
       <InfoWell>
         Basic security: Make sure your details are accurate and up to date to keep
         your wallet safe from unauthorized access and to help you restore access to
