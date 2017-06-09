c72383568d660bab1a5d2f6994970ec368b0d002
Guillaume Marquilly <guillaume@blockchain.com>
2017-06-09 16:03:46 +0100

    Changes to implement the new imports from the dream-wallet

--- src/services/walletApi.js
+++ src/services/walletApi.js
@@ -1,7 +1,7 @@
-import { createWalletApi } from 'dream-wallet/lib/network'
+import { Network } from 'dream-wallet/lib'
 import settings from 'config'
 
-export const api = createWalletApi({
+export const api = Network.createWalletApi({
   rootUrl: settings.ROOT_URL,
   apiUrl: settings.API_BLOCKCHAIN_INFO,
   apiCode: settings.API_CODE
