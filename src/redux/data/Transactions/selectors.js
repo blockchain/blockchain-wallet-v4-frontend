dd899eb2b74a3a7efdc8c503f3dc67c52afc02b6
Jaume Pernas <jaume@pernas.cat>
2017-06-11 17:08:47 +0100

    transactions selector

--- src/redux/data/Transactions/selectors.js
+++ src/redux/data/Transactions/selectors.js
@@ -1,3 +1,5 @@
+import { prop } from 'ramda'
+
 // // context is a single address/xpub for now
 // export const getAddrInfo = dpath => context => state =>
 //   state[dpath].get('addressesInfo').get(context)
@@ -7,3 +9,5 @@
 //   const info = getAddrInfo(dpath)(context)(state)
 //   return info ? info.get('transactions') : List([])
 // }
+
+export const getTransactions = prop('txs')
