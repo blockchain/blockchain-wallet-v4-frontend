dd6cfcba8d4ca4d59d9adcfdc3b9e76a5a050883
Jaume Pernas <jaume@pernas.cat>
2017-06-11 15:43:01 +0100

    Balance Summary component

--- src/scenes/Home/BalanceSummary/index.js
+++ src/scenes/Home/BalanceSummary/index.js
@@ -1,54 +1,24 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import { propEq, filter, map, prop, has } from 'ramda'
-import {bindActionCreators} from 'redux'
-import { core } from 'data/rootSelectors.js'
+import { prop, mapAccum, head } from 'ramda'
+import { core, ui } from 'data/rootSelectors.js'
 
 import BalanceSummary from './template.js'
 
 class BalanceSummaryContainer extends React.Component {
   render () {
-    let balances = [{
-      title: 'My Bitcoin Wallet',
-      amount: 0.00199132
-    }, {
-      title: 'beer budget',
-      amount: 0
-    }, {
-      title: 'my single legacy address',
-      amount: 0
-    }]
-    let total = 0.00199132
-
+    const adder = (acc, value) => [acc + prop('amount', value), acc + prop('amount', value)]
+    const total = head(mapAccum(adder, 0, this.props.balances)) 
     return (
-      <BalanceSummary balances={balances} total={total} />
+      <BalanceSummary balances={this.props.balances} total={total} bitcoinDisplayed={this.props.bitcoinDisplayed} />
     )
   }
 }
 
 function mapStateToProps (state, ownProps) {
-  const legacyAddresses = core.wallet.getAddresses(state)
-  const hdAccounts = core.wallet.getHDAccounts(state)
-  const addresses = core.addresses.getBalances(state)
-  // console.log(core.common.wallet(state))
-  // console.log(map(myfun, addresses))
-  // let balances = []
-  // for(let i =0; i< addresses; i++) {
-
-  // }
-
-  // filter(propEq('address', address), prop('addresses', data))
-  // const a = legacyAddresses[0].addr
-
-  // const elementL = filter(propEq('addr', a), legacyAddresses)
-
-
-  // console.log(elementL)
-  // console.log(elementL[0].label)
-  // const balances = core.addresses.
-
   return {
-    balances: undefined
+    balances: core.common.getLabelBalances(state),
+    bitcoinDisplayed: ui.getBitcoinDisplayed(state)
   }
 }
 
--- src/scenes/Home/BalanceSummary/template.js
+++ src/scenes/Home/BalanceSummary/template.js
@@ -4,6 +4,9 @@
 import React from 'react'
 import CSSModules from 'react-css-modules'
 
+import BitcoinDisplay from 'components/Shared/BitcoinDisplay'
+import CurrencyDisplay from 'components/Shared/CurrencyDisplay'
+
 import style from './style.scss'
 
 const BalanceSummary = (props) => {
@@ -17,13 +20,21 @@ const BalanceSummary = (props) => {
           return (
             <div className='row flex-row flex-between border-bottom padding-vertical-10' key={index}>
               <span className='f-14'>{balance.title}</span>
-              <span className='f-14'>{balance.amount} BTC</span>
+              {props.bitcoinDisplayed ? (
+                <BitcoinDisplay className='f-14' amount={balance.amount} />
+              ) : (
+                <CurrencyDisplay className='f-14' amount={balance.amount} />
+              )}
             </div>
           )
         })}
         <div className='row flex-row flex-between padding-vertical-10'>
           <span className='f-14 em-500 capitalize'>Total</span>
-          <span className='f-14 em-500 capitalize'>{props.total} BTC</span>
+          {props.bitcoinDisplayed ? (
+            <BitcoinDisplay className='f-14 em-500 capitalize' amount={props.total} />
+          ) : (
+            <CurrencyDisplay className='f-14 em-500 capitalize' amount={props.total} />
+          )}
         </div>
       </div>
     </div>
