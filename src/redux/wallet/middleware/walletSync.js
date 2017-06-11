d466960bbe275f93fc50acaa9c836397d7baf7bf
Jaume Pernas <jaume@pernas.cat>
2017-06-11 18:28:56 +0100

    sync middleware fixed

--- src/redux/wallet/middleware/walletSync.js
+++ src/redux/wallet/middleware/walletSync.js
@@ -1,42 +1,41 @@
-import { WALLET_INITIAL_STATE } from '../reducers'
 import * as A from '../actions'
 
-const walletSync = ({ wpath, api } = {}) => (store) => (next) => (action) => {
-  let prevWallet = store.getState()[wpath]
-  let result = next(action)
-  let nextWallet = store.getState()[wpath]
+const walletSync = ({ isAuthenticated, walletPath, api } = {}) => (store) => (next) => (action) => {
+  const prevWallet = store.getState()[walletPath]
+  const wasAuth = isAuthenticated(store.getState())
+  const result = next(action)
+  const nextWallet = store.getState()[walletPath]
+  const isAuth = isAuthenticated(store.getState())
+
   // Easily know when to sync, because of ✨immutable✨ data
   // the initial_state check could be done against full payload state
 
-  // TODO :: refactor/simplify this middleware
-  if (action.type !== A.PAYLOAD_CHECKSUM_CHANGE &&
-      prevWallet.get('password') !== '' &&
-      nextWallet.get('password') !== '' && // we need a logged in control here
-      prevWallet.get('walletImmutable') !== WALLET_INITIAL_STATE &&
+  if ((wasAuth && isAuth) &&
+      // action.type !== A.PAYLOAD_CHECKSUM_CHANGE &&
       prevWallet !== nextWallet) {
-    api.saveWallet(nextWallet).then(checksum => {
-      store.dispatch(A.syncStart())
-      store.dispatch(A.changePayloadChecksum(checksum))
-      return checksum
-    }).then(
-      (cs) => store.dispatch(A.syncSuccess(cs))
-    ).catch(
-      (error) => store.dispatch(A.syncError(error))
-    )
+      api.saveWallet(nextWallet).then(checksum => {
+        store.dispatch(A.syncStart())
+        // store.dispatch(A.changePayloadChecksum(checksum))
+        return checksum
+      }).then(
+        (cs) => store.dispatch(A.syncSuccess(cs))
+      ).catch(
+        (error) => store.dispatch(A.syncError(error))
+      )
   }
 
-  if (action.type === A.WALLET_NEW_SET && prevWallet !== nextWallet) { // wallet signup
-    const { email } = action.payload
-    api.createWallet(email)(nextWallet).then(checksum => {
-      store.dispatch(A.syncStart())
-      store.dispatch(A.changePayloadChecksum(checksum))
-      return checksum
-    }).then(
-      (cs) => store.dispatch(A.syncSuccess(cs))
-    ).catch(
-      (error) => store.dispatch(A.syncError(error))
-    )
-  }
+  // if (action.type === A.WALLET_NEW_SET && prevWallet !== nextWallet) { // wallet signup
+  //   const { email } = action.payload
+  //   api.createWallet(email)(nextWallet).then(checksum => {
+  //     store.dispatch(A.syncStart())
+  //     store.dispatch(A.changePayloadChecksum(checksum))
+  //     return checksum
+  //   }).then(
+  //     (cs) => store.dispatch(A.syncSuccess(cs))
+  //   ).catch(
+  //     (error) => store.dispatch(A.syncError(error))
+  //   )
+  // }
 
   return result
 }
