665275a2f21701e9374fb5bedcec61ac0626fa16
Guillaume Marquilly <guillaume@blockchain.com>
2017-06-13 13:12:26 +0100

    fix - reference

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import persistState from 'redux-localstorage'
 import { createBrowserHistory } from 'history'
-import { connectRouter, routerMiddleware } from 'react-router-redux'
+import { routerMiddleware } from 'react-router-redux'
 
 import Immutable from 'immutable-ext'
 import { is } from 'ramda'
