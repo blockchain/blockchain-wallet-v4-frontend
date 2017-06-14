88fcd478cbc93f30c3e143c58f3eb0d97a56cbc4
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2017-06-14 21:34:04 +0100

    refactoring - ticker

--- src/components/Layouts/Wallet/Header/Ticker/index.js
+++ src/components/Layouts/Wallet/Header/Ticker/index.js
@@ -2,43 +2,22 @@ import React from 'react'
 import {connect} from 'react-redux'
 import { path } from 'ramda'
 
+import { convertToBitcoin, convertToCurrency } from 'services/conversionService'
 import { selectors } from 'data'
 
 import Ticker from './template.js'
 
 class TickerContainer extends React.Component {
   render () {
-    let bitcoin = 100000000
-    let bitcoinAmount = 0
-    let currencyAmount = 0
-    let bitcoinUnit = this.props.unit
-    let currencyUnit = this.props.currency
-    let rates = this.props.rates
-
-    switch (bitcoinUnit) {
-      case 'UBC':
-        bitcoinAmount = parseFloat((bitcoin / 100).toFixed(2))
-        bitcoinUnit = 'bits'
-        break
-      case 'MBC':
-        bitcoinAmount = parseFloat((bitcoin / 100000).toFixed(5))
-        bitcoinUnit = 'mBTC'
-        break
-      case 'BTC':
-        bitcoinAmount = parseFloat((bitcoin / 100000000).toFixed(8))
-        bitcoinUnit = 'BTC'
-        break
-    }
-
-    let ratio = path([currencyUnit, 'last'], rates)
-    currencyAmount = parseFloat((bitcoin * ratio / 100000000).toFixed(2))
+    let satoshiAmount = 100000000
+    let conversionBitcoin = convertToBitcoin(satoshiAmount, this.props.unit)
+    let conversionCurrency = convertToCurrency(satoshiAmount, this.props.currency, this.props.rates)
 
     return (
       <Ticker
-        bitcoinAmount={bitcoinAmount}
-        bitcoinUnit={bitcoinUnit}
-        currencyAmount={currencyAmount}
-        currencyUnit={currencyUnit} />
+        bitcoinValue={conversionBitcoin.success ? conversionBitcoin.value : 'N/A'}
+        currencyValue={conversionCurrency.success ? conversionCurrency.value : 'N/A'}
+      />
     )
   }
 }
--- src/components/Layouts/Wallet/Header/Ticker/template.js
+++ src/components/Layouts/Wallet/Header/Ticker/template.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import CSSModules from 'react-css-modules'
 
 import style from './style.scss'
 
 const Ticker = (props) => {
   return (
-    <a href='https://markets.blockchain.info' className='navigation'>{`${props.bitcoinAmount} ${props.bitcoinUnit} - ${props.currencyAmount} ${props.currencyUnit}`}</a>
+    <a href='https://markets.blockchain.info' className='navigation'>{`${props.bitcoinValue} - ${props.currencyValue}`}</a>
   )
+}
+
+Ticker.propTypes = {
+  bitcoinValue: PropTypes.string.isRequired,
+  currencyValue: PropTypes.string.isRequired
 }
 
 export default CSSModules(Ticker, style)
