b53bc8920b0c90ff8bdab4f655336022c745ed9f
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2017-06-15 22:34:58 +0100

    fix - landing (responsive)

--- src/scenes/Landing/Main/index.js
+++ src/scenes/Landing/Main/index.js
@@ -8,65 +8,65 @@ const Main = () => {
   return (
     <section styleName='main'>
       <div className='container h-100'>
-        <div className='row h-25 justify-content-center align-items-center'>
-          <div className='col center-align'>
+        <div className='row justify-content-center align-items-center'>
+          <div className='col-12 padding-vertical-50 center-align'>
             <span className='f-52 white'>The World's Most Popular Bitcoin Wallet</span>
           </div>
         </div>
-        <div className='row h-25 justify-content-around align-items-center'>
-          <div className='col col-md-3'>
+        <div className='row h-25 justify-content-around align-items-center hidden-sm-down'>
+          <div className='col-12 col-md-3'>
             <div className='row justify-content-center'>
               <div className='col center-align'>
                 <span className='f-32 white'>100 Million+</span>
               </div>
             </div>
              <div className='row justify-content-center'>
-              <div className='col center-align'>
+              <div className='col-12 center-align'>
                 <span className='f-20 white'>Transactions</span>
               </div>
             </div>
           </div>
           <div className='col col-md-3'>
             <div className='row justify-content-center'>
-              <div className='col center-align'>
+              <div className='col-12 center-align'>
                 <span className='f-32 white'>13 Million+</span>
               </div>
             </div>
              <div className='row justify-content-center'>
-              <div className='col center-align'>
+              <div className='col-12 center-align'>
                <span className='f-20 white'>Wallets</span>
               </div>
             </div>
           </div>
           <div className='col-12 col-md-3'>
             <div className='row justify-content-center'>
-              <div className='col center-align'>
+              <div className='col-12 center-align'>
                 <span className='f-32 white'>140+</span>
               </div>
             </div>
              <div className='row justify-content-center'>
-              <div className='col center-align'>
+              <div className='col-12 center-align'>
                 <span className='f-20 white'>Countries Served</span>
               </div>
             </div>
           </div>
         </div>
         <div className='row h-25 justify-content-center align-items-center'>
-          <div className='col'>
+          <div className='col-12'>
             <div className='row justify-content-center'>
-              <div className='col-3 center-align'>
+              <div className='col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3 center-align'>
                 <NavLink to='/register'>
                   <button className='button-primary full-width heavy'>Create your wallet</button>
                 </NavLink>
               </div>
             </div>
             <div className='row justify-content-center'>
-              <div className='col-3 center-align'>
+              <div className='col-12 center-align'>
                 <span className='f-20 white'>or</span>
               </div>
             </div>
             <div className='row justify-content-center'>
-              <div className='col-3 center-align'>
+              <div className='col-12 col-sm-8 col-md-6 col-lg-4 col-xl-3 center-align'>
                 <NavLink to='/login'>
                   <button className='button-purple full-width heavy'>Login</button>
                 </NavLink>
