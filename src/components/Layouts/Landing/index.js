eb816aa165b28bf4514b7a484b0f0407f0f9999c
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2017-06-15 22:26:41 +0100

    refacoring - landing page

--- src/components/Layouts/Landing/index.js
+++ src/components/Layouts/Landing/index.js
@@ -7,22 +7,10 @@ import Footer from './Footer'
 const LandingLayout = ({component: Component, ...rest}) => {
   return (
     <Route {...rest} render={matchProps => (
-      <div className='container-fluid bg-blue'>
-        <div className='row'>
-          <div className='col'>
-            <Header />  
-          </div>
-        </div>  
-        <div className='row h-100 justify-content-center align-items-center'>
-          <div className='col'>
-            <Component {...matchProps} />
-          </div>
-        </div>
-        <div className='row align-items-end'>
-          <div className='col'>
-            <Footer />
-          </div>
-        </div>  
+      <div className='bg-blue'>
+        <Header />  
+        <Component {...matchProps} />
+        <Footer />
       </div>
     )} />
   )
