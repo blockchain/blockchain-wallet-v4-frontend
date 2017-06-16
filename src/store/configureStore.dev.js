090e780a6cc528fccd5da8bf9b11faf0c33592f0
Jaume Pernas <jaume@pernas.cat>
2017-06-16 18:49:46 +0100

    serialization devtools

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -15,11 +15,27 @@ import { rootSaga, rootReducer } from 'data'
 // import { auth } from 'data/rootSelectors.js'
 import { serializer } from 'dream-wallet/lib/types'
 
+const devToolsConfig = {
+  // features: {
+  //   pause: true, // start/pause recording of dispatched actions
+  //   lock: true, // lock/unlock dispatching actions and side effects    
+  //   persist: true, // persist states on page reloading
+  //   export: true, // export history of actions in a file
+  //   import: true, // import history of actions from a file
+  //   jump: true, // jump back and forth (time travelling)
+  //   skip: true, // skip (cancel) actions
+  //   reorder: true, // drag and drop actions in the history list 
+  //   dispatch: true, // dispatch custom actions or action creators
+  //   test: true // generate tests for the selected actions
+  // },
+  serialize: serializer
+}
+
 const configureStore = () => {
   const history = createBrowserHistory()
   const sagaMiddleware = createSagaMiddleware()
   // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ serialize: { immutable: Immutable } }) : compose
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(serializer) : compose
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(devToolsConfig) : compose
   // const walletPath = settings.WALLET_IMMUTABLE_PATH
 
   const store = createStore(
