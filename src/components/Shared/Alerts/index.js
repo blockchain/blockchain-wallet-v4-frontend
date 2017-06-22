4453c186f70de1a7946dc04b80e65e143f080a53
Justin Tormey <justin@blockchain.com>
2017-06-22 14:44:03 -0400

    add selector function for alerts

--- src/components/Shared/Alerts/index.js
+++ src/components/Shared/Alerts/index.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { actions } from 'data'
+import { selectAlerts } from 'data/Alerts/selectors'
+import { clearAlerts, dismissAlert } from 'data/Alerts/actions'
 import Alerts from './template.js'
 
 const AlertsContainer = (props) => <Alerts {...props} />
 
-let mapStateToProps = (state) => ({
-  alerts: state.applicationState.alerts
-})
-
-let mapDispatchToProps = (dispatch) => bindActionCreators({
-  clearAlerts: actions.alerts.clearAlerts,
-  dismissAlert: actions.alerts.dismissAlert
-}, dispatch)
+let mapStateToProps = (state) => ({ alerts: selectAlerts(state) })
+let mapDispatchToProps = (dispatch) => bindActionCreators({ clearAlerts, dismissAlert }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(AlertsContainer)
