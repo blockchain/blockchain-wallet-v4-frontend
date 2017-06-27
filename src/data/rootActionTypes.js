d1b8186296ca72f7284e8e8ee5ef8670763b9c92
Jaume Pernas <jaume@pernas.cat>
2017-06-28 00:39:22 +0100

    actionTypes split

--- src/data/rootActionTypes.js
+++ src/data/rootActionTypes.js
@@ -3,8 +3,10 @@ import * as auth from './Auth/actionTypes.js'
 import * as log from './Log/actionTypes.js'
 import * as preferences from './Preferences/actionTypes.js'
 import * as ui from './UI/actionTypes.js'
+import { coreActionsTypes as core } from 'dream-wallet/lib'
 
 export {
+  core,
   activity,
   auth,
   log,
