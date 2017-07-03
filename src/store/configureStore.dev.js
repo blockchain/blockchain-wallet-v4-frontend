c757e312262500f99f62dcec99484cdbdac3cc57
Jaume Pernas <jaume@pernas.cat>
2017-07-03 21:40:20 +0100

    action standarization

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -4,13 +4,13 @@ import createSagaMiddleware from 'redux-saga'
 import persistState from 'redux-localstorage'
 import { createBrowserHistory } from 'history'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
-// import { coreMiddleware } from 'dream-wallet/lib'
+import { coreMiddleware } from 'dream-wallet/lib'
 import autoDisconnection from 'middleware/autoDisconnection.js'
 import { rootSaga, rootReducer } from 'data'
-// import settings from 'config'
-// import { api } from 'services/walletApi.js'
+import settings from 'config'
+import { api } from 'services/walletApi.js'
 // import { Socket } from 'dream-wallet/lib/network'
-// import { auth } from 'data/rootSelectors.js'
+import { auth } from 'data/rootSelectors.js'
 import { serializer } from 'dream-wallet/lib/types'
 
 const devToolsConfig = { serialize: serializer }
@@ -19,18 +19,18 @@ const configureStore = () => {
   const history = createBrowserHistory()
   const sagaMiddleware = createSagaMiddleware()
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(devToolsConfig) : compose
-  // const walletPath = settings.WALLET_IMMUTABLE_PATH
+  const walletPath = settings.WALLET_IMMUTABLE_PATH
   const reduxRouterMiddleware = routerMiddleware(history)
 
   const store = createStore(
     connectRouter(history)(rootReducer),
     composeEnhancers(
-      // persistState(['session', 'preferences']),
-      persistState(undefined, {deserialize: string => JSON.parse(string, serializer.reviver)}),
+      persistState(['session', 'preferences']),
+      // persistState(undefined, {deserialize: string => JSON.parse(string, serializer.reviver)}),
       applyMiddleware(
         reduxRouterMiddleware,
         autoDisconnection,
-        // coreMiddleware.walletSync({isAuthenticated: auth.isAuthenticated, api, walletPath}),
+        // coreMiddleware.walletSync({isAuthenticated: auth.getIsAuthenticated, api, walletPath}),
         // coreMiddleware.socket({ socket }),
         sagaMiddleware,
         logger
