410f8d6ce67e87b7e0c40d273eee9c47a1c2742d
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2017-08-18 21:00:29 +0100

    fix - wallet menu left

--- src/layouts/Wallet/MenuLeft/Navigation/template.js
+++ src/layouts/Wallet/MenuLeft/Navigation/template.js
@@ -13,111 +13,108 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
 `
-const NavList = styled.ul`
+const Menu = styled.ul`
   list-style: none;
   padding: 0;
 `
-const SubNavList = styled.ul`
+const MenuItem = styled.li`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 8px 16px;
+  color: #757575;
+  cursor: pointer;
+
+  & > :first-child { width: 30px; }
+  & > :last-child { text-transform: uppercase; }
+  &.active {  & > * { color: #10ADE4; } }
+`
+const SubMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
   list-style: none;
   text-transform: none;
-  padding-left: 38px;
-  margin-top: 0.3125em;
+  padding-left: 40px;
 `
-const NavHeader = styled.li`
-  margin: 0 0 10px 0;
-  position: relative;
-  display: flex;
-  justify-content: start;
-  align-items: center;
-  color: ${props => props.theme.basicGrey};
-  padding: 0.4em 0.8em;
-
-  &.active {
-    & > * {
-      color: ${props => props.theme.iris};
-    }
-  }
-`
-const SubNavHeader = NavHeader.extend`
-  padding: 0.3125em;
+const SubMenuItem = styled.li`
+  padding: 5px 10px;
+  margin-right: 10px;
   text-transform: none;
-  margin: 0px;
+  color: #757575;
+  cursor: pointer;
+
+  &.active {  & > * { color: #10ADE4; } }
 `
-// const NavIcon = styled(Icon)`
-//   font-size: 20px;
-//   margin-right: .55em;
-//   text-align: center;
-//   line-height: 0;
-//   width: 20px;
-// `
 
 const Navigation = (props) => {
   const { settingsToggled, handleOpenSettings, handleCloseSettings, handleCloseMenuLeft, ...rest } = props
 
   return (
     <Wrapper {...rest}>
-      <NavList>
+      <Menu>
         <LinkContainer to='/wallet' activeClassName='active' onClick={handleCloseSettings}>
-          <NavHeader>
+          <MenuItem>
             <Icon name='home' />
             <FormattedMessage id='components.layouts.wallet.menuleft.navigation.home' defaultMessage='Home' />
-          </NavHeader>
+          </MenuItem>
         </LinkContainer>
         <LinkContainer to='/transactions' activeClassName='active' onClick={handleCloseSettings}>
-          <NavHeader>
+          <MenuItem>
             <Icon name='tx' />
             <FormattedMessage id='components.layouts.wallet.menuleft.navigation.transactions' defaultMessage='Transactions' smaller uppercase />
-          </NavHeader>
+          </MenuItem>
         </LinkContainer>
         <LinkContainer to='/buy-sell' activeClassName='active' onClick={handleCloseSettings}>
-          <NavHeader>
+          <MenuItem>
             <Icon name='bitcoin' />
             <FormattedMessage id='components.layouts.wallet.menuleft.navigation.buybitcoin' defaultMessage='Buy bitcoin' smaller uppercase />
-          </NavHeader>
+          </MenuItem>
         </LinkContainer>
         <LinkContainer to='/security-center' activeClassName='active' onClick={handleCloseSettings}>
-          <NavHeader>
+          <MenuItem>
             <Icon name='lock' />
             <FormattedMessage id='components.layouts.wallet.menuleft.navigation.securitycenter' defaultMessage='Security center' smaller uppercase />
-          </NavHeader>
+          </MenuItem>
         </LinkContainer>
         <LinkContainer to='/settings' activeClassName='active' onClick={handleOpenSettings}>
-          <NavHeader>
+          <MenuItem>
             <Icon name='settings' />
             <FormattedMessage id='components.layouts.wallet.menuleft.navigation.settings' defaultMessage='Settings' smaller uppercase />
-          </NavHeader>
+          </MenuItem>
         </LinkContainer>
         {settingsToggled && (
-          <SubNavList>
+          <SubMenu>
             <LinkContainer to='/settings/info' activeClassName='active' onClick={handleCloseMenuLeft}>
-              <SubNavHeader>
+              <SubMenuItem>
                 <FormattedMessage id='components.layouts.wallet.menuleft.navigation.walletinfo' defaultMessage='Wallet information' smaller />
-              </SubNavHeader>
+              </SubMenuItem>
             </LinkContainer>
             <LinkContainer to='/settings/preferences' activeClassName='active' onClick={handleCloseMenuLeft}>
-              <SubNavHeader>
+              <SubMenuItem>
                 <FormattedMessage id='components.layouts.wallet.menuleft.navigation.preferences' defaultMessage='Preferences' smaller />
-              </SubNavHeader>
+              </SubMenuItem>
             </LinkContainer>
             <LinkContainer to='/settings/security' activeClassName='active' onClick={handleCloseMenuLeft}>
-              <SubNavHeader>
+              <SubMenuItem>
                 <FormattedMessage id='components.layouts.wallet.menuleft.navigation.security' defaultMessage='Security' smaller />
-              </SubNavHeader>
+              </SubMenuItem>
             </LinkContainer>
             <LinkContainer to='/settings/addresses' activeClassName='active' onClick={handleCloseMenuLeft}>
-              <SubNavHeader>
+              <SubMenuItem>
                 <FormattedMessage id='components.layouts.wallet.menuleft.navigation.addresses' defaultMessage='Addresses' smaller />
-              </SubNavHeader>
+              </SubMenuItem>
             </LinkContainer>
-          </SubNavList>
+          </SubMenu>
         )}
         <LinkContainer to='/faq' activeClassName='active' onClick={handleCloseSettings}>
-          <NavHeader>
+          <MenuItem>
             <Icon name='help' />
             <FormattedMessage id='components.layouts.wallet.menuleft.navigation.faq' defaultMessage='Faq' smaller uppercase />
-          </NavHeader>
+          </MenuItem>
         </LinkContainer>
-      </NavList>
+      </Menu>
     </Wrapper>
   )
 }
