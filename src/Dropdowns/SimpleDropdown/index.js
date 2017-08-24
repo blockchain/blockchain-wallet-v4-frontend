239ffefc0e11509fb6002b544858bfaf501018b0
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-08-24 10:41:34 +0100

    Fix: Dropdown and link story color, textgroup inline

--- src/Dropdowns/SimpleDropdown/index.js
+++ src/Dropdowns/SimpleDropdown/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import Dropdown from './template.js'
 import { DefaultColor } from '../../'
+import { keysIn } from 'ramda'
 
 class SimpleDropdown extends React.Component {
   constructor (props) {
@@ -38,7 +39,7 @@ class SimpleDropdown extends React.Component {
 }
 
 SimpleDropdown.defaultProps = {
-  color: DefaultColor.iris,
+  color: 'iris',
   opened: false,
   selectedValue: 0,
   uppercase: true,
@@ -53,7 +54,7 @@ SimpleDropdown.PropTypes = {
   })),
   callback: PropTypes.func.isRequired,
   opened: PropTypes.bool,
-  color: PropTypes.oneOf([DefaultColor.iris, DefaultColor.white]),
+  color: PropTypes.oneOf(keysIn(DefaultColor)),
   uppercase: PropTypes.bool,
   down: PropTypes.bool
 }
