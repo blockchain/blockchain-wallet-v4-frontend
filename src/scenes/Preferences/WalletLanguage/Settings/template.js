6ab9424254d9e16440f8ab51208d51fbbc04f017
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-08-24 17:11:42 +0100

    Simplify template. Extend language service.

--- src/scenes/Preferences/WalletLanguage/Settings/template.js
+++ src/scenes/Preferences/WalletLanguage/Settings/template.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { FormattedMessage } from 'react-intl'
 import { Field } from 'redux-form'
 
-import { Button, Text } from 'blockchain-info-components'
-import { Form, SelectBox } from 'components/Form'
-import { languages, languagesSortedByName, getLanguageName, convertCultureCodeToLanguage, convertLanguageToCultureCode } from 'services/LanguageService'
+import { SelectBox } from 'components/Form'
+import { languagesSortedByName, convertLanguageToLanguageName } from 'services/LanguageService'
 
 const Wrapper = styled.div`
   display: flex;
@@ -15,25 +13,14 @@ const Wrapper = styled.div`
   align-items: flex-end;
 `
 const Settings = (props) => {
-  const { toggled, handleToggle, handleClick, language, submitting, invalid } = props
-
-  if (toggled) {
-    return (
-      <Wrapper>
-        <Form>
-          <SelectBox />
-        </Form>
-      </Wrapper>
-    )
-  } else {
-    return (
-      <Wrapper>
-        <Field name='language' component={SelectBox} elements={[{group: '', items: languagesSortedByName.map(x => ({text: x.name, value: x.language}))}]}
-          input={{onBlur: function () {}, onChange: function () {}, onFocus: function () {}, value: 1}}
-          label={languages.find(lang => lang.language === language).name} />
-      </Wrapper>
-    )
-  }
+  const { handleClick, language } = props
+  return (
+    <Wrapper>
+      <Field name='language' component={SelectBox} elements={[{ group: '', items: languagesSortedByName.map(x => ({ text: x.name, value: x.language })) }]}
+        input={{ onBlur: function () { }, onChange: handleClick, onFocus: function () { }, value: 1 }}
+        label={convertLanguageToLanguageName(language).value} />
+    </Wrapper>
+  )
 }
 
 Settings.propTypes = {
