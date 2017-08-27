33be5c6aae77dbd87e6b67677abcb8a382359d3a
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2017-08-27 14:12:03 +0100

    refactoring to support translations

--- src/Banners/Banner.js
+++ src/Banners/Banner.js
@@ -1,69 +1,49 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Icon } from '../Icons'
 import { Text } from '../Text'
 
-class Banner extends React.Component {
-  constructor(props) {
-    super(props);
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background: rgba(${props => props.color}, .1);
+  border: 1px solid rgba(${props => props.color}, .2);
+  border-radius: 4px;
+  padding: 5px 10px;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+
+  & > :first-child { margin-right: 8px; }
+`
+
+const selectStyle = type => {
+  switch (type) {
+    case 'success': return { color1: 'jade', color2: '0, 167, 111', uppercase: false, icon: 'success' }
+    case 'warning': return { color1: 'mahogany', color2: '202, 58, 60', uppercase: true, icon: 'alert' }
+    case 'alert': return { color1: 'iris', color2: '16, 173, 228', uppercase: false, icon: 'bell' }
+    default: return { color1: 'iris', color2: '16, 173, 228', uppercase: false, icon: null }
   }
+}
 
-  // TODO: Update color strings to use new naming style
-  textColor() {
-    switch (this.props.type) {
-      case "success": return "jade";
-      case "warning": return "mahogany";
-      default: return "iris";
-    }
-  }
+const Banner = props => {
+  const { type, children } = props
+  const style = selectStyle(type)
+  const { color1, color2, uppercase, icon } = style
 
-  textTransform() {
-    return this.props.type === "warning" ? true : false;
-  }
+  return (
+    <Container color={color2}>
+      { icon && <Icon name={icon} size='12px' weight={400} color={color1} /> }
+      <Text size='12px' weight={400} color={color1} uppercase={uppercase}>
+        { children }
+      </Text>
+    </Container>
+  )
+}
 
-  // -- Helper Functions -- //
-
-  getRGBFor(type) {
-    switch (type) {
-      case "success": return "0, 167, 111";
-      case "warning": return "202, 58, 60";
-      default: return "16, 173, 228";
-    }
-  }
-
-  // TODO: Update icons
-  getIconFor(type) {
-    switch (type) {
-      case "alert": return <Icon name="bell" color="iris" size="12px" />;
-      case "warning": return <Icon name="alert" color="mahogany" size="12px" />;
-      case "success": return <Icon name="success" color="jade" size="12px" />;
-      default: return null;
-    }
-  }
-
-  render() {
-    const Icon = this.getIconFor(this.props.type);
-    const Message = this.props.text;
-    const RGBVal = this.getRGBFor(this.props.type);
-    const Container = styled.div`
-      background: rgba(${RGBVal}, .1);
-      border: 1px solid rgba(${RGBVal}, .2);
-      border-radius: 4px;
-      margin-top: 10px;
-      padding: 5px 10px;
-      -moz-osx-font-smoothing: grayscale;
-      -webkit-font-smoothing: antialiased;
-      & div > span { padding-right: 8px; }
-    `
-
-    return(
-      <Container>
-        <Text color={this.textColor()} size="12px" weight={400} uppercase={this.textTransform()}>
-          {Icon}{Message}
-        </Text>
-      </Container>
-    );
-  }
+Banner.propTypes = {
+  type: PropTypes.oneOf(['success', 'warning', 'error'])
 }
 
 export default Banner
