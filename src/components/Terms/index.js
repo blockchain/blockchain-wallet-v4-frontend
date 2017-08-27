0684e8591315ca01aa369e73610086572e8137d2
plondon <philip@blockchain.com>
2017-08-27 16:46:26 -0400

    fix: terms label styled

--- src/components/Terms/index.js
+++ src/components/Terms/index.js
@@ -4,20 +4,18 @@ import { FormattedMessage } from 'react-intl'
 
 import { Link, Text } from 'blockchain-info-components'
 
-const TermsContainer = styled.div`
+const TermsContainer = styled.label.attrs({ htmlFor: 'terms' })`
   & > * { display: inline-block; margin-right: 5px; }
 `
 const Terms = (props) => (
-  <label htmlFor='terms'>
-    <TermsContainer >
-      <Text size='12px' weight={300}>
-        <FormattedMessage id='scenes.register.registerform.read' defaultMessage='I have read and agree to the' />
-      </Text>
-      <Link href='https://blockchain.info/Resources/TermsofServicePolicy.pdf' target='_blank' size='12px' weight={300}>
-        <FormattedMessage id='scenes.register.registerform.terms' defaultMessage='Terms of Service' />
-      </Link>
-    </TermsContainer>
-  </label>
+  <TermsContainer >
+    <Text size='12px' weight={300}>
+      <FormattedMessage id='scenes.register.registerform.read' defaultMessage='I have read and agree to the' />
+    </Text>
+    <Link href='https://blockchain.info/Resources/TermsofServicePolicy.pdf' target='_blank' size='12px' weight={300}>
+      <FormattedMessage id='scenes.register.registerform.terms' defaultMessage='Terms of Service' />
+    </Link>
+  </TermsContainer>
 )
 
 export default Terms
