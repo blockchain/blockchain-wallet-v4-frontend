b4e3a23b5795fb548cb1ffd585444e5dfe48491d
Guillaume Marquilly <guillaume@blockchain.com>
2017-08-30 18:09:02 +0100

    Fixes badges / gauges

--- src/Form/CheckBoxInput.js
+++ src/Form/CheckBoxInput.js
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 import { DefaultColor } from '../Colors'
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
   width: 100%;
 `
 
@@ -41,12 +45,12 @@ const Label = styled.label`
 
 const CheckBoxInput = props => {
   const { name, errorState, children, ...rest } = props
-
+  console.log(props)
   return (
     <Wrapper>
       <BaseCheckBoxInput id={name} {...rest} />
       <Label htmlFor={name}>
-        {children}
+        { children }
       </Label>
     </Wrapper>
   )
--- stories/chapters/gauges.js
+++ stories/chapters/gauges.js
@@ -3,12 +3,24 @@ import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 
 import Layout from '../components/layout'
-import { ConfirmationGauge } from '../../src'
+import { ConfirmationGauge, PasswordGauge } from '../../src'
 
 storiesOf('Gauges', module)
-  .addDecorator(story => (<Layout>{story()}</Layout>))
   .addDecorator((story, context) => withInfo({ text: 'Documentation', inline: true })(story)(context))
-  .add('ConfirmationGauge empty', () => <ConfirmationGauge />)
-  .add('ConfirmationGauge orange', () => <ConfirmationGauge nbConfirmations={1} />)
-  .add('ConfirmationGauge yellow', () => <ConfirmationGauge nbConfirmations={2} />)
-  .add('ConfirmationGauge green', () => <ConfirmationGauge nbConfirmations={3} />)
+  .add('ConfirmationGauge', () => (
+    <Layout>
+      <ConfirmationGauge />
+      <ConfirmationGauge nbConfirmations={1} />
+      <ConfirmationGauge nbConfirmations={2} />
+      <ConfirmationGauge nbConfirmations={3} />
+    </Layout>
+  ))
+  .add('PasswordGauge', () => (
+    <Layout>
+      <PasswordGauge score={1} />
+      <PasswordGauge score={2} />
+      <PasswordGauge score={3} />
+      <PasswordGauge score={4} />
+      <PasswordGauge score={5} />
+    </Layout>
+  ))
