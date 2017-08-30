d985f7d7606019d3d002883f00aa001e3caf8f70
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-08-30 14:03:10 +0100

    Changing email sends a confirmation email. Add resend feature.

--- src/scenes/Preferences/EmailAddress/Settings/index.js
+++ src/scenes/Preferences/EmailAddress/Settings/index.js
@@ -1,48 +1,84 @@
+
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators, compose } from 'redux'
-import { actions as reduxFormActions } from 'redux-form'
+import { actions as reduxFormActions, formValueSelector } from 'redux-form'
 import { singleForm } from 'providers/FormProvider'
 import ui from 'redux-ui'
-import { equals } from 'ramda'
+import { equals, isEmpty } from 'ramda'
 
 import { actions, selectors } from 'data'
 import Settings from './template.js'
 
-class SettingsContainer extends React.Component {
+class SettingContainer extends React.Component {
   constructor (props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
     this.handleToggle = this.handleToggle.bind(this)
+    this.handleResend = this.handleResend.bind(this)
+    // this.handleVerify = this.handleVerify.bind(this)
+  }
+
+  componentWillMount () {
+    if (!isEmpty(this.props.email)) {
+      this.props.reduxFormActions.initialize('settingEmailAddress', { emailAddress: this.props.email })
+      this.props.updateUI({ verifyToggled: !this.props.emailVerified })
+    }
   }
 
   componentWillReceiveProps (nextProps) {
-    if (!equals(nextProps.emailAddress, this.props.emailAddress)) {
-      this.props.reduxFormActions.change('settingEmailAddress', this.props.emailAddress)
+    if (!equals(this.props.email, nextProps.email)) {
+      this.props.updateUI({ updateToggled: false, verifyToggled: true })
+    }
+    if (equals(this.props.email, nextProps.email) && !equals(this.props.emailVerified, nextProps.emailVerified)) {
+      this.props.updateUI({ updateToggled: false, verifyToggled: false })
     }
   }
 
   handleClick () {
-    console.log('click')
+    const { guid, sharedKey, emailAddress } = this.props
+    this.props.settingsActions.updateEmail(guid, sharedKey, emailAddress)
+    this.handleToggle()
+    this.props.updateUI({ verifyToggled: !this.props.emailVerified })
   }
 
   handleToggle () {
-    this.props.updateUI({ toggled: !this.props.ui.toggled })
+    this.props.updateUI({ updateToggled: !this.props.ui.updateToggled })
   }
+
+  handleResend () {
+    const { guid, sharedKey, emailAddress } = this.props
+    this.props.settingsActions.updateEmail(guid, sharedKey, emailAddress)
+  }
+
+  // handleVerify () {
+  //   const { guid, sharedKey, code } = this.props
+  //   this.props.settingsActions.verifyEmail(guid, sharedKey, code)
+  // }
 
   render () {
     const { ui, uiUpdate, ...rest } = this.props
+
     return <Settings
       {...rest}
-      toggled={ui.toggled}
+      updateToggled={ui.updateToggled}
+      verifyToggled={ui.verifyToggled}
       handleToggle={this.handleToggle}
       handleClick={this.handleClick}
-        />
+      handleResend={this.handleResend}
+      // handleVerify={this.handleVerify}
+    />
   }
 }
 
 const mapStateToProps = (state) => ({
-  emailAddress: selectors.core.settings.getEmail(state)
+  guid: selectors.core.wallet.getGuid(state),
+  sharedKey: selectors.core.wallet.getSharedKey(state),
+  email: selectors.core.settings.getEmail(state),
+  emailVerified: selectors.core.settings.getEmailVerified(state),
+  emailAddress: formValueSelector('settingEmailAddress')(state, 'emailAddress'),
+  code: formValueSelector('settingEmailAddress')(state, 'code')
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -51,9 +87,13 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 const enhance = compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    ui({ key: 'Setting_emailAddress', state: { toggled: false } }),
-    singleForm('settingEmailAddress')
+  connect(mapStateToProps, mapDispatchToProps),
+  ui({ key: 'Setting_EmailAddress', state: { updateToggled: false, verifyToggled: false } }),
+  singleForm('settingEmailAddress')
 )
 
-export default enhance(SettingsContainer)
+SettingContainer.propTypes = {
+  email: PropTypes.string
+}
+
+export default enhance(SettingContainer)
