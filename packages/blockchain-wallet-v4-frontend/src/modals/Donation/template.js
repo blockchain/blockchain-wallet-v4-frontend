ab2e1a5bd96b1449f9fbe2fbeece3785cde77156
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-09-07 16:50:01 +0100

    Improve donation modal

--- packages/blockchain-wallet-v4-frontend/src/modals/Donation/template.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/Donation/template.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
 
-import { Link, Modal, Text, Tooltip } from 'blockchain-info-components'
+import { Button, Link, Modal, Text } from 'blockchain-info-components'
 import modalEnhancer from 'providers/ModalEnhancer'
 
 const DonationContainer = styled.div`
@@ -11,35 +11,32 @@ const DonationContainer = styled.div`
   justify-content: center;
   align-content: center;
   width: 100;
-  padding: 30px 0;
 `
 const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
   padding: 5px 0;
-`
-const Aligned = styled.div`
-& > * { display: inline-block; margin-right: 5px; }
+
+  & > * { justify-content: center; }
 `
 
 const Donation = (props) => {
   const { handleBack, ...rest } = props
 
   return (
-    <Modal {...rest} icon='send' title='Donation' size='large'>
-      <Aligned>
-        <Text size='14px' weight={500} capitalize>
-          <FormattedMessage id='modals.Donation.donate' defaultMessage='Donate to a charity' />
-        </Text>
-        <Tooltip>
-          <FormattedMessage id='modals.Donation.tooltip' defaultMessage='The charity will receive this amount from you.' />
-        </Tooltip>
-      </Aligned>
+    <Modal {...rest} icon='send' title='Donate' size='large' nature='primary'>
       <DonationContainer>
-        <Text size='14px' weight={500} capitalize>
-          <FormattedMessage id='modals.Donation.donate' defaultMessage='Donate to a charity' />
+        <Text size='14px' color='white' weight={500}>
+          <FormattedMessage id='modals.Donation.donate' defaultMessage='See what adding a portion of your transaction will do for those in need.' />
         </Text>
       </DonationContainer>
       <Footer>
-        <Link onClick={handleBack} size='13px' weight={300}>
+        <Button nature='primary' fullwidth uppercase>
+          <FormattedMessage id='modals.Donation.send' defaultMessage='Add this donation to my transaction' />
+        </Button>
+        <Link onClick={handleBack} uppercase size='13px' color='white' weight={300}>
           <FormattedMessage id='modals.Donation.back' defaultMessage='Go back' />
         </Link>
       </Footer>
