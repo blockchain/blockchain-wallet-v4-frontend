991be4e7278fff1e3fa8401c4e40abe91e82f1eb
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-09-20 16:16:46 +0100

    Move config to template. Get currency. Use highcharts.

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/template.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import ReactHighcharts from 'react-highcharts'
 import ReactHighstock from 'react-highcharts/reactHighstock'
 
 const Wrapper = styled.div`
@@ -16,11 +17,67 @@ const Wrapper = styled.div`
 `
 
 const Chart = (props) => {
-  const { config } = props
+  const { currency, data } = props
+
+  const config = {
+    chart: {
+      height: 300
+    },
+    title: {
+      text: null
+    },
+    yAxis: {
+      title: {
+        text: null
+      },
+      labels: {
+        formatter: function () {
+          return (currency + this.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
+        }
+      },
+      lineWidth: 1,
+      gridLineWidth: 0
+    },
+    xAxis: {
+      type: 'datetime',
+      tickWidth: 0,
+      labels: {
+        style: {
+          color: 'gray'
+        }
+      }
+    },
+    plotOptions: {
+      series: {
+        color: '#10ADE4'
+      },
+      line: {
+        marker: {
+          enabled: false
+        }
+      }
+    },
+    tooltip: {
+      pointFormat: '{series.name}(' + 'GBP' + '): {point.y}'
+    },
+    credits: {
+      enabled: false
+    },
+    legend: {
+      enabled: false
+    },
+
+    series: [
+      {
+        name: 'Price',
+        data: data
+      }
+    ]
+  }
 
   return (
     <Wrapper>
-      <ReactHighstock config={config} />
+      <ReactHighcharts config={config} />
     </Wrapper>
   )
 }
