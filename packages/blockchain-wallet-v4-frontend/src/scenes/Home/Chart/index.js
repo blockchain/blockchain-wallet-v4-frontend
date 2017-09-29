21482494df7f9fef2fdc99b3292815dd46c5b59d
Guillaume Marquilly <guillaume@blockchain.com>
2017-09-29 11:12:33 +0100

    fix - config

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-
+import moment from 'moment'
 import { equals } from 'ramda'
+
 import { actions, selectors } from 'data'
+import ChartConfig from './chart.config.js'
 import Chart from './template.js'
 
 const intervals = {
-  hour: 3600 * 1000,
-  day: 24 * 3600 * 1000
+  hour: 60 * 60 * 1000,
+  day: 24 * 60 * 60 * 1000
 }
-
 const scales = {
-  FIFTEENMIN: 900,
-  HOUR: 3600,
-  TWOHOUR: 7200,
-  DAY: 86400,
-  FIVEDAY: 432000
+  FIFTEENMIN: 15 * 60,
+  HOUR: 60 * 60,
+  TWOHOUR: 2 * 60 * 60,
+  DAY: 24 * 60 * 60,
+  FIVEDAY: 5 * 24 * 60 * 60
 }
 const BTCSTART = 1282089600
 const ETHSTART = 1438992000
@@ -35,11 +36,11 @@ class ChartContainer extends React.Component {
     this.selectBitcoin = this.selectBitcoin.bind(this)
     this.selectEthereum = this.selectEthereum.bind(this)
     this.selectTimeframe = this.selectTimeframe.bind(this)
-    this.configureChart = this.configureChart.bind(this)
   }
 
   componentWillMount () {
-    const { coin, currency, start, scale } = this.state
+    const { currency } = this.props
+    const { coin, start, scale } = this.state
     this.props.chartActions.fetchPriceIndexSeries(coin, currency, start, scale)
   }
 
@@ -64,97 +65,60 @@ class ChartContainer extends React.Component {
   }
 
   selectTimeframe (timeframe) {
-    const date = new Date()
-
     switch (timeframe) {
       case 'all':
-        this.setState({ start: this.state.coin === 'BTC' ? BTCSTART : ETHSTART, scale: scales.FIVEDAY })
+        this.setState({
+          start: this.state.coin === 'BTC' ? BTCSTART : ETHSTART,
+          scale: scales.FIVEDAY,
+          timeframe,
+          interval: intervals.day
+        })
         break
       case 'year':
-        this.setState({ start: Math.round(date.setFullYear(date.getFullYear() - 1) / 1000 | 0), scale: scales.DAY })
+        this.setState({
+          start: moment().subtract(1, 'year').format('X'),
+          scale: scales.DAY,
+          timeframe,
+          interval: intervals.day
+        })
         break
       case 'month':
-        this.setState({ start: Math.round(date.setMonth(date.getMonth() - 1) / 1000 | 0), scale: scales.TWOHOUR })
+        this.setState({
+          start: moment().subtract(1, 'month').format('X'),
+          scale: scales.TWOHOUR,
+          timeframe,
+          interval: intervals.day
+        })
         break
       case 'week':
-        this.setState({ start: Math.round(date.setDate(date.getDate() - 7) / 1000 | 0), scale: scales.HOUR, interval: intervals.hour })
+        this.setState({
+          start: moment().subtract(7, 'month').format('X'),
+          scale: scales.HOUR,
+          timeframe,
+          interval: intervals.hour
+        })
         break
       case 'day':
-        this.setState({ start: Math.round(date.setDate(date.getDate() - 1) / 1000 | 0), scale: scales.FIFTEENMIN, interval: intervals.hour })
+        this.setState({
+          start: moment().subtract(1, 'day').format('X'),
+          scale: scales.FIFTEENMIN,
+          timeframe,
+          interval: intervals.hour
+        })
         break
       default: break
-    }
-    this.setState({ timeframe })
-  }
-
-  configureChart () {
-    const { currency, data } = this.props
-
-    return {
-      chart: {
-        height: 300
-      },
-      title: {
-        text: null
-      },
-      yAxis: {
-        title: {
-          text: null
-        },
-        labels: {
-          formatter: function () {
-            return (currency + this.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
-          }
-        },
-        lineWidth: 1,
-        gridLineWidth: 0
-      },
-      xAxis: {
-        type: 'datetime',
-        tickWidth: 0,
-        labels: {
-          style: {
-            color: 'gray'
-          }
-        }
-      },
-      plotOptions: {
-        series: {
-          color: '#10ADE4',
-          pointStart: this.state.start,
-          pointInterval: this.state.interval
-        },
-        line: {
-          marker: {
-            enabled: false
-          }
-        }
-      },
-      tooltip: {
-        pointFormat: '{series.name}(' + currency + '): {point.y}'
-      },
-      credits: {
-        enabled: false
-      },
-      legend: {
-        enabled: false
-      },
-      series: [
-        {
-          name: 'Price',
-          data: data
-        }
-      ]
     }
   }
 
   render () {
-    const config = this.configureChart()
-
+    const { coin, timeframe, start, interval } = this.state
+    const { currency, data } = this.props
+    const config = ChartConfig(start, interval, currency, data)
+    console.log(config)
     return (
       <Chart
-        coin={this.state.coin}
-        timeframe={this.state.timeframe}
+        coin={coin}
+        timeframe={timeframe}
         config={config}
         selectBitcoin={this.selectBitcoin}
         selectEthereum={this.selectEthereum}
--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/template.js
@@ -39,32 +39,31 @@ const Filter = styled(Link)`
 
 const Chart = (props) => {
   const { start, interval, currency, data, ...rest } = props
-  const { config, coin, selectBitcoin, selectEthereum, selectTimeframe } = rest
-  const bitcoinSelected = coin === 'BTC'
+  const { config, coin, timeframe, selectBitcoin, selectEthereum, selectTimeframe } = rest
 
   return (
     <Wrapper>
       <Ticker>
-        <BitcoinTicker selected={bitcoinSelected} onClick={selectBitcoin} />
-        <EthereumTicker selected={!bitcoinSelected} onClick={selectEthereum} />
+        <BitcoinTicker selected={coin === 'BTC'} onClick={selectBitcoin} />
+        <EthereumTicker selected={coin === 'ETH'} onClick={selectEthereum} />
       </Ticker>
       <Highchart>
         <ReactHighcharts config={config} />
       </Highchart>
       <Filters>
-        <Filter size='14px' weight={300} color='brand-primary' onClick={() => selectTimeframe('all')}>
+        <Filter size='14px' weight={300} color={timeframe === 'all' ? 'brand-secondary' : 'brand-primary'} onClick={() => selectTimeframe('all')}>
           <FormattedMessage id='scenes.home.chart.alltime' defaultMessage='All time' />
         </Filter>
-        <Filter size='14px' weight={300} color='brand-primary' onClick={() => selectTimeframe('year')}>
+        <Filter size='14px' weight={300} color={timeframe === 'year' ? 'brand-secondary' : 'brand-primary'} onClick={() => selectTimeframe('year')}>
           <FormattedMessage id='scenes.home.chart.year' defaultMessage='Year' />
         </Filter>
-        <Filter size='14px' weight={300} color='brand-primary' onClick={() => selectTimeframe('month')}>
+        <Filter size='14px' weight={300} color={timeframe === 'month' ? 'brand-secondary' : 'brand-primary'} onClick={() => selectTimeframe('month')}>
           <FormattedMessage id='scenes.home.chart.month' defaultMessage='Month' />
         </Filter>
-        <Filter size='14px' weight={300} color='brand-primary' onClick={() => selectTimeframe('week')}>
+        <Filter size='14px' weight={300} color={timeframe === 'week' ? 'brand-secondary' : 'brand-primary'} onClick={() => selectTimeframe('week')}>
           <FormattedMessage id='scenes.home.chart.week' defaultMessage='Week' />
         </Filter>
-        <Filter size='14px' weight={300} color='brand-primary' onClick={() => selectTimeframe('day')}>
+        <Filter size='14px' weight={300} color={timeframe === 'day' ? 'brand-secondary' : 'brand-primary'} onClick={() => selectTimeframe('day')}>
           <FormattedMessage id='scenes.home.chart.day' defaultMessage='Day' />
         </Filter>
       </Filters>
