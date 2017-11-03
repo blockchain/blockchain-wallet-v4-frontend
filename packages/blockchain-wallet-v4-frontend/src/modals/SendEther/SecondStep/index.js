b4aab79853bc66cf9f82c5e840c76dd8603c83f2
Lyncee <guillaume@blockchain.com>
2017-11-03 15:53:56 +0000

    fix - send ethereum > effective balance calculation

--- packages/blockchain-wallet-v4-frontend/src/modals/SendEther/SecondStep/index.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/SendEther/SecondStep/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { formValueSelector } from 'redux-form'
+import { Exchange } from 'blockchain-wallet-v4/src'
 
 import { actions } from 'data'
 import SecondStep from './template.js'
@@ -14,25 +15,27 @@ class SecondStepContainer extends React.Component {
 
   onSubmit (e) {
     e.preventDefault()
-    // this.props.paymentActions.sendBitcoin(this.props.selection)
   }
 
   render () {
-    return <SecondStep {...this.props} onSubmit={this.onSubmit} />
+    const { amount, ...rest } = this.props
+    const amountWei = Exchange.convertEtherToEther({ value: amount, fromUnit: 'ETH', toUnit: 'WEI' }).value
+
+    return <SecondStep {...rest} amount={amountWei} onSubmit={this.onSubmit} />
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const to = formValueSelector('sendBitcoin')(state, 'to')
-  const message = formValueSelector('sendBitcoin')(state, 'message')
-  const wei = 100
-  const fee = 0.000411
+  const to = formValueSelector('sendEther')(state, 'to')
+  const message = formValueSelector('sendEther')(state, 'message')
+  const amount = formValueSelector('sendEther')(state, 'amount')
+  const fee = formValueSelector('sendEther')(state, 'fee')
 
   return {
     fee,
     message,
     to,
-    wei
+    amount
   }
 }
 
