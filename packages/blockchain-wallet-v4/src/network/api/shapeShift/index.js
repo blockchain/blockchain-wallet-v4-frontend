36d84b5005264e3c1731f743ef08727b1072fed7
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-11-08 15:59:42 +0000

    Create get function in shapeshiftto simplify requests to shapeShift

--- packages/blockchain-wallet-v4/src/network/api/shapeShift/index.js
+++ packages/blockchain-wallet-v4/src/network/api/shapeShift/index.js
@@ -1,11 +1,42 @@
-export default ({ shapeShiftRootUrl, get }) => {
+export default ({ shapeShiftRootUrl }) => {
+  // checkStatus :: Response -> Promise Response
+  const checkStatus = (r) => r.ok ? Promise.resolve(r) : r.text().then(j => Promise.reject(j))
+
+  // extractData :: Response -> Promise (JSON | BLOB | TEXT)
+  const extractData = (r) => {
+    const responseOfType = (t) => r.headers.get('content-type') && r.headers.get('content-type').indexOf(t) > -1
+    switch (true) {
+      case responseOfType('application/json'): return r.json()
+      case responseOfType('image/jpeg'): return r.blob()
+      default: return r.text()
+    }
+  }
+
+  const request = (method, endpoint, data) => {
+    console.log('Making shapeshift request ' + endpoint)
+
+    let url = shapeShiftRootUrl + endpoint
+    let options = {
+      method,
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'omit'
+    }
+
+    if (method !== 'GET') {
+      options.body = JSON.stringify(data)
+    }
+
+    return fetch(url, options).then(checkStatus).then(extractData)
+  }
+
+  // Get request
+  const get = ({ endPoint }) => request('GET', endPoint, {})
+
   const getBtcEth = () => get({
-    url: shapeShiftRootUrl,
     endPoint: `marketinfo/btc_eth`
   })
 
   const getEthBtc = () => get({
-    url: shapeShiftRootUrl,
     endPoint: `marketinfo/eth_btc`
   })
 
