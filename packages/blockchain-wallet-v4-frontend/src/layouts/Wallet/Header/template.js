c6b2e3a643599d24611484cee853adb4b81f2e07
Jaume Pernas <jaume@pernas.cat>
2017-11-14 14:25:32 +0000

    remove ticker reference

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/Header/template.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/Header/template.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
-import Ticker from './Ticker'
 import WhatsNew from './WhatsNew'
 import Refresh from './Refresh'
 import Logout from './Logout'
