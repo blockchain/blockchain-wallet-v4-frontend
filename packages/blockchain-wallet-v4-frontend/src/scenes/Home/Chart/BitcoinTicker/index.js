766fa9c0aca7fc2cf341aae9fa4969ee1ea370cf
Lyncee <guillaume@blockchain.com>
2017-11-14 12:14:36 +0000

    fix - validation message on FiatConvertor main input (rates disabled)

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/BitcoinTicker/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Chart/BitcoinTicker/index.js
@@ -3,20 +3,24 @@ import { connect } from 'react-redux'
 
 import { Exchange } from 'blockchain-wallet-v4/src'
 import { selectors } from 'data'
+import { isBitcoinFiatAvailable } from 'services/ValidationHelper'
 import BitcoinTicker from './template.js'
 
 class BitcoinTickerContainer extends React.Component {
   render () {
-    const { coin, currency, bitcoinRates } = this.props
+    const { coin, country, currency, bitcoinRates, bitcoinOptions } = this.props
     const rate = Exchange.displayBitcoinToFiat({ value: '1', fromUnit: 'BTC', toCurrency: currency, rates: bitcoinRates })
-
-    return <BitcoinTicker rate={rate} selected={coin === 'BTC'} {...this.props} />
+    return isBitcoinFiatAvailable(country, currency, bitcoinRates, bitcoinOptions)
+      ? <BitcoinTicker selected={coin === 'BTC'} {...this.props}>{rate}</BitcoinTicker>
+      : <BitcoinTicker selected={coin === 'BTC'} {...this.props}>N/A</BitcoinTicker>
   }
 }
 
 const mapStateToProps = (state) => ({
+  country: selectors.core.settings.getCountryCode(state),
   currency: selectors.core.settings.getCurrency(state),
-  bitcoinRates: selectors.core.data.bitcoin.getRates(state)
+  bitcoinRates: selectors.core.data.bitcoin.getRates(state),
+  bitcoinOptions: selectors.core.walletOptions.selectBitcoin(state)
 })
 
 export default connect(mapStateToProps)(BitcoinTickerContainer)
