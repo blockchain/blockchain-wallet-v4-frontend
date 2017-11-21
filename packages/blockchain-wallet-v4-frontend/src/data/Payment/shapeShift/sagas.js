24f328624ac7867a820c71806608e15f8bcb960c
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-11-21 18:25:07 +0000

    Create frontend saga initShapeShift

--- packages/blockchain-wallet-v4-frontend/src/data/Payment/shapeShift/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/Payment/shapeShift/sagas.js
@@ -1,7 +1,22 @@
-import { takeEvery, put, call } from 'redux-saga/effects'
+import { takeEvery, put, call, select } from 'redux-saga/effects'
 import * as AT from './actionTypes'
 import * as actions from '../../actions.js'
 import * as sagas from '../../sagas.js'
+import * as selectors from '../../selectors.js'
+
+export const initShapeShift = function * (action) {
+  try {
+    yield call(sagas.core.data.bitcoin.fetchFee)
+    const index = yield select(selectors.core.wallet.getDefaultAccountIndex)
+    yield call(sagas.core.data.bitcoin.fetchUnspent, index, undefined)
+    const feePerByte = yield select(selectors.core.data.bitcoin.getFeeRegular)
+    yield call(sagas.core.data.bitcoin.refreshEffectiveBalance, { feePerByte })
+  } catch (e) {
+    if (e !== 'No free outputs to spend') {
+      yield put(actions.alerts.displayError('Could not init shapeshift.'))
+    }
+  }
+}
 
 export const createOrder = function * (action) {
   try {
@@ -12,5 +27,6 @@ export const createOrder = function * (action) {
 }
 
 export default function * () {
+  yield takeEvery(AT.INIT_SHAPESHIFT, initShapeShift)
   yield takeEvery(AT.CREATE_ORDER, createOrder)
 }
