7968dd5804896a6f4ef54f2fbe9b265f085c9fbf
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2017-11-27 13:27:48 +0000

    Derive bitcoin address for shapeshift sendamount request

--- packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Shapeshift/SecondStep/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Shapeshift/SecondStep/index.js
@@ -3,8 +3,9 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Exchange } from 'blockchain-wallet-v4/src'
 import { formValueSelector } from 'redux-form'
-
 import { prop, toLower } from 'ramda'
+
+import settings from 'config'
 import { actions, selectors } from 'data'
 import SecondStep from './template.js'
 
@@ -17,17 +18,15 @@ class SecondStepContainer extends React.Component {
   componentWillMount () {
     // Make request to shapeShift to create order
     console.log(this.props.exchangeAccounts)
-    const { source, target } = this.props.exchangeAccounts
+    const { exchangeAccounts, amount, sourceAddress, targetAddress } = this.props
+    const { source, target } = exchangeAccounts
     const sourceCoin = source.coin
     const targetCoin = target.coin
     const pair = toLower(sourceCoin + '_' + targetCoin)
     console.log(source, target)
-
-    const sourceAddress = source.addr || source.xpub
-    const targetAddress = target.addr || target.xpub
-    console.log(this.props.amount)
+    console.log(amount)
     console.log({
-      depositAmount: this.props.amount,
+      depositAmount: amount,
       pair,
       returnAddress: sourceAddress,
       withdrawal: targetAddress
@@ -46,11 +45,12 @@ class SecondStepContainer extends React.Component {
   }
 
   render () {
-    const { exchangeAccounts, amount, ...rest } = this.props
+    const { exchangeAccounts, amount, sourceAddress, targetAddress, ...rest } = this.props
     const { source, target } = exchangeAccounts
-    const sourceAddress = source.address || source.xpub
-    const targetAddress = target.address || target.xpub
     const { minerFee, quotedRate } = this.props.order
+    console.log(this.props.order)
+    console.log('Miner fee is ', minerFee)
+    console.log('Quoted rate is ', quotedRate)
     const txFee = 0 // To be computed
     const received = prop('value', Exchange.convertCoinToCoin({ value: amount * quotedRate - minerFee, coin: target.coin, baseToStandard: false }))
     const sourceAmount = prop('value', Exchange.convertCoinToCoin({ value: amount, coin: source.coin, baseToStandard: false }))
@@ -74,11 +74,29 @@ class SecondStepContainer extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  exchangeAccounts: formValueSelector('exchange')(state, 'accounts'),
-  amount: formValueSelector('exchange')(state, 'amount'),
-  order: selectors.core.data.shapeShift.getOrder(state)
-})
+const extractAddress = (value, selectorFunction) =>
+  value
+    ? value.addr
+      ? value.addr
+      : selectorFunction(value.index)
+    : undefined
+
+const mapStateToProps = (state, ownProps) => {
+  const getReceive = index => selectors.core.common.getNextAvailableReceiveAddress(settings.NETWORK, index, state)
+  const exchangeAccounts = formValueSelector('exchange')(state, 'accounts')
+  const f = selectors.core.wallet.getAccountLabel(state)
+  const g = selectors.core.wallet.getLegacyAddressLabel(state)
+  const { source, target } = exchangeAccounts
+
+  return {
+    exchangeAccounts,
+    amount: formValueSelector('exchange')(state, 'amount'),
+    order: selectors.core.data.shapeShift.getOrder(state),
+    targetAddress: prop('addr', target) || extractAddress(exchangeAccounts.target, getReceive),
+    sourceAddress: prop('addr', source) || extractAddress(exchangeAccounts.source, getReceive)
+
+  }
+}
 
 const mapDispatchToProps = (dispatch) => ({
   shapeShiftActions: bindActionCreators(actions.payment.shapeShift, dispatch)
