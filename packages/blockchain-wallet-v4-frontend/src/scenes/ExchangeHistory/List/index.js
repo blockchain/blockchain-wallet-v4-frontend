c4df5c7b6ad40d77f3b99cf078efe9c87f9f4677
Lyncee <guillaume@blockchain.com>
2017-11-28 13:33:35 +0000

    feat - Pagination component + Table component

--- packages/blockchain-wallet-v4-frontend/src/scenes/ExchangeHistory/List/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/ExchangeHistory/List/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { map, assoc } from 'ramda'
+import { compose, bindActionCreators } from 'redux'
+import ui from 'redux-ui'
+import { assoc, head, map, repeat, slice } from 'ramda'
 import moment from 'moment'
 
 import { actions } from 'data'
@@ -11,16 +12,34 @@ class ListContainer extends React.Component {
   constructor (props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
+    this.handleClickPage = this.handleClickPage.bind(this)
   }
 
   handleClick (trade) {
     this.props.modalActions.showModal('ExchangeDetails', { trade })
   }
 
+  handleClickPage (value) {
+    this.props.updateUI({ page: value })
+  }
+
   render () {
     const trades = map(x => assoc('date', moment(x.date).format('DD MMMM YYYY, HH:mm'), x), this.props.trades)
-    console.log(trades)
-    return <List trades={trades} handleClick={this.handleClick} />
+    // const temp = map(x => assoc('date', moment(x.date).format('DD MMMM YYYY, HH:mm'), x), this.props.trades)
+    // const trades = repeat(head(temp), 105)
+    const pageSize = 10
+    const pageTotal = Math.trunc((trades.length / pageSize)) + 1
+    const pageNumber = this.props.ui.page
+    const paginatedTrades = slice((pageNumber - 1) * pageSize, pageNumber * pageSize, trades)
+
+    return <List
+      trades={paginatedTrades}
+      pageNumber={pageNumber}
+      pageSize={pageSize}
+      pageTotal={pageTotal}
+      handleClick={this.handleClick}
+      handleClickPage={this.handleClickPage}
+     />
   }
 }
 
@@ -28,4 +47,9 @@ const mapDispatchToProps = dispatch => ({
   modalActions: bindActionCreators(actions.modals, dispatch)
 })
 
-export default connect(undefined, mapDispatchToProps)(ListContainer)
+const enhance = compose(
+  ui({ state: { page: 1 } }),
+  connect(undefined, mapDispatchToProps)
+)
+
+export default enhance(ListContainer)
