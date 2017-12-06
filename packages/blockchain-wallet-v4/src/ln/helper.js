6f917e48211d3cec97a2c421a2bb8ca77a8c3c26
Mats Jerratsch <matsjj@gmail.com>
2017-12-06 11:16:06 +0000

    chore(Cleanup): Rename test methods to asserts

--- packages/blockchain-wallet-v4/src/ln/helper.js
+++ packages/blockchain-wallet-v4/src/ln/helper.js
@@ -1,16 +1,21 @@
 const assert = require('assert')
 const secp = require('bcoin/lib/bcoin-browser').secp256k1
+const Long = require('long')
 
-export let testPubKey = (pubKey) => {
+export let assertPubKey = (pubKey) => {
   assert.equal(pubKey.length, 33)
 }
 
-export let testSignature = (sig) => {
+export let assertSignature = (sig) => {
   assert.equal(sig.length, 64)
 }
 
-export let testNumber = (num) => {
+export let assertNumber = (num) => {
   assert.equal(typeof num, 'number')
+}
+
+export let assertLong = (num) => {
+  assert(Long.isLong(num))
 }
 
 export let addSighash = (sig) => Buffer.concat([sig, wrapHex('01')])
--- packages/blockchain-wallet-v4/src/ln/scripts.js
+++ packages/blockchain-wallet-v4/src/ln/scripts.js
@@ -2,14 +2,14 @@ import * as SCRIPT from 'bitcoinjs-lib/src/script'
 import * as hash from 'bcoin/lib/crypto/digest'
 
 import * as OPS from 'bitcoin-ops'
-import {addSighash, sigToBitcoin, testNumber, testPubKey, testSignature} from './helper';
+import {addSighash, sigToBitcoin, assertNumber, assertPubKey, assertSignature} from './helper';
 
 const assert = require('assert')
 
 let OP_CSV = 178
 
 let intToNum = num => {
-  testNumber(num)
+  assertNumber(num)
   let b = null
 
   if (num < 65536) {
@@ -26,7 +26,7 @@ let intToNum = num => {
 }
 
 export let getP2PKHScript = pubkey => {
-  testPubKey(pubkey)
+  assertPubKey(pubkey)
   let chunks = []
   chunks.push(OPS.OP_DUP)
   chunks.push(OPS.OP_HASH160)
@@ -37,7 +37,7 @@ export let getP2PKHScript = pubkey => {
 }
 
 export let getP2WPKHScript = pubkey => {
-  testPubKey(pubkey)
+  assertPubKey(pubkey)
 
   let chunks = []
   chunks.push(OPS.OP_0)
@@ -46,16 +46,16 @@ export let getP2WPKHScript = pubkey => {
 }
 
 export let getP2WPKHRedeemScript = (pubkey, signature) => {
-  testPubKey(pubkey)
-  testSignature(signature)
+  assertPubKey(pubkey)
+  assertSignature(signature)
 
   return [sigToBitcoin(signature), pubkey]
 }
 
 export let getFundingOutputScript = (fundingKeyLocal, fundingKeyRemote) => {
   // 2 <key1> <key2> 2 OP_CHECKMULTISIG
-  testPubKey(fundingKeyLocal)
-  testPubKey(fundingKeyRemote)
+  assertPubKey(fundingKeyLocal)
+  assertPubKey(fundingKeyRemote)
 
   let chunks = []
   chunks.push(OPS.OP_2)
@@ -74,10 +74,10 @@ export let getFundingOutputScript = (fundingKeyLocal, fundingKeyRemote) => {
 }
 
 export let getFundingRedeemScript = (key1, key2, sig1, sig2) => {
-  testPubKey(key1)
-  testPubKey(key2)
-  testSignature(sig1)
-  testSignature(sig2)
+  assertPubKey(key1)
+  assertPubKey(key2)
+  assertSignature(sig1)
+  assertSignature(sig2)
 
   sig1 = sigToBitcoin(sig1)
   sig2 = sigToBitcoin(sig2)
@@ -108,9 +108,9 @@ export let getToLocalOutputScript = (revocationKey, toSelfDelay, localDelayedKey
   //   <local_delayedkey>
   // OP_ENDIF
   // OP_CHECKSIG
-  testPubKey(revocationKey)
-  testPubKey(localDelayedKey)
-  testNumber(toSelfDelay)
+  assertPubKey(revocationKey)
+  assertPubKey(localDelayedKey)
+  assertNumber(toSelfDelay)
 
   let chunks = []
 
@@ -129,7 +129,7 @@ export let getToLocalOutputScript = (revocationKey, toSelfDelay, localDelayedKey
 
 export let getToRemoteOutputScript = (remoteKey) => {
   // This output sends funds to the other peer, thus is a simple P2WPKH to remotekey.
-  testPubKey(remoteKey)
+  assertPubKey(remoteKey)
 
   let chunks = []
 
@@ -163,9 +163,9 @@ export let getOfferedHTLCOutput = (revocationKey, remoteKey, localKey, paymentHa
   //       OP_CHECKSIG
   //     OP_ENDIF
   //   OP_ENDIF
-  testPubKey(remoteKey)
-  testPubKey(localKey)
-  testPubKey(revocationKey)
+  assertPubKey(remoteKey)
+  assertPubKey(localKey)
+  assertPubKey(revocationKey)
   assert.equal(paymentHash.length, 20)
 
   let chunks = []
@@ -227,10 +227,10 @@ export let getReceivedHTLCOutput = (revocationKey, remoteKey, localKey, paymentH
   //     OP_CHECKSIG
   //   OP_ENDIF
   // OP_ENDIF
-  testPubKey(remoteKey)
-  testPubKey(localKey)
-  testPubKey(revocationKey)
-  testNumber(cltvTimeout)
+  assertPubKey(remoteKey)
+  assertPubKey(localKey)
+  assertPubKey(revocationKey)
+  assertNumber(cltvTimeout)
   assert.equal(paymentHash.length, 20)
 
   let chunks = []
@@ -288,9 +288,9 @@ export let getHTLCFollowUpTx = (revocationKey, toSelfDelay, delayedKeyLocal) => 
   //   <local_delayedkey>
   // OP_ENDIF
   // OP_CHECKSIG
-  testPubKey(revocationKey)
-  testPubKey(delayedKeyLocal)
-  testNumber(toSelfDelay)
+  assertPubKey(revocationKey)
+  assertPubKey(delayedKeyLocal)
+  assertNumber(toSelfDelay)
 
   let chunks = []
 
