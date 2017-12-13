cda807e524ac4aec5cc882f928258aa70ff376f2
Mats Jerratsch <matsjj@gmail.com>
2017-12-13 11:43:10 +0000

    fix(LN): Only load LN on starting up

--- packages/blockchain-wallet-v4-frontend/src/index.js
+++ packages/blockchain-wallet-v4-frontend/src/index.js
@@ -1,21 +1,9 @@
-const index = (process.env.NODE_ENV === 'production')
-  ? require('./index.prod')
-  : require('./index.dev')
-
-// Import React & JS
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import App from 'scenes/app.js'
-import configureStore from 'store'
-import configureLocales from 'services/LocalesService'
 import * as LN from 'blockchain-wallet-v4/src/ln/index'
 import * as random from 'crypto'
 
-import * as script from 'blockchain-wallet-v4/src/ln/scripts'
 import * as Long from 'long'
 
-var ec = require('bcoin/lib/crypto/secp256k1-browser')
+const ec = require('bcoin/lib/crypto/secp256k1-browser')
 
 let options = {
   chainHash: Buffer.from('06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f', 'hex'),
@@ -36,26 +24,5 @@ options.staticLocal = staticLocal
 
 console.info('Starting! \nNode ID: ' + staticLocal.pub.toString('hex'))
 
+
 LN.startUp(options)
-
-// Register store
-const { store, history } = configureStore()
-
-// Register locales
-const { messages } = configureLocales(store)
-
-// Documentation: https://github.com/gaearon/react-hot-loader/tree/master/docs
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer key={Math.random()}>
-      <Component store={store} history={history} messages={messages} />
-    </AppContainer>,
-    document.getElementById('app')
-  )
-}
-
-render(App)
-
-if (module.hot) {
-  module.hot.accept('scenes/app.js', () => render(require('scenes/app.js').default))
-}
