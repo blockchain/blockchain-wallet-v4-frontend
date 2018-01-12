79d97634061bd51218613e8b0753972f125cb373
Mats Jerratsch <matsjj@gmail.com>
2018-01-12 16:36:35 +0000

    WIP correctly create peer state

--- packages/blockchain-wallet-v4/src/ln/peers/reducers.js
+++ packages/blockchain-wallet-v4/src/ln/peers/reducers.js
@@ -1,4 +1,5 @@
 import {peerReducer} from './peer/reducers.js'
+import {ADD} from './peer/actionTypes'
 
 // The state is a mapping of public key to connection objects
 const INITIAL_STATE = {}
@@ -7,20 +8,24 @@ const peersReducer = (state = INITIAL_STATE, action) => {
   const { type, publicKey } = action
 
   let oldState = state[publicKey]
-  switch (type) {
-    default: return state
+
+  if (oldState === undefined && type !== ADD) {
+    return state
+  } else {
+
+    let newState = peerReducer(oldState, action)
+    console.info(oldState)
+    console.info(newState)
+
+    if (oldState === newState) {
+
+      return state
+    } else {
+      let copy = Object.assign({}, state)
+      copy[publicKey] = newState
+      return copy
+    }
   }
-
-  // let newState = peerReducer(oldState, action)
-  //
-  // if (oldState === newState) {
-  //   return state
-  // } else {
-  //   let copy = Object.assign({}, state)
-  //   copy[publicKey] = oldState
-  //   return copy
-  // }
-
 }
 
 export default peersReducer
