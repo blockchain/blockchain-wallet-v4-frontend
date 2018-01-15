d6f938d24bf6f95bac67af5398419b582bfb0408
Mats Jerratsch <matsjj@gmail.com>
2018-01-15 17:39:23 +0000

    WIP cleanup state file

--- packages/blockchain-wallet-v4/src/ln/channel/state.js
+++ packages/blockchain-wallet-v4/src/ln/channel/state.js
@@ -1,57 +1,8 @@
 import {fromJS} from 'immutable'
 import {wrapHex} from '../helper'
-import {sendOutAllMessages} from "../peers/connection";
+import {sendOutAllMessages} from '../peers/connection'
 
-var Long = require('long')
-
-export let StateHolder = function (state) {
-  this.state = state
-
-  this.get = () => state
-  this.set = (s) => {
-    // TODO not sure where's the best place to do this
-    s = sendOutAllMessages(s)
-    state = s
-  }
-  this.update = (s) => {
-    this.set(s(state))
-  }
-}
-
-export let State = () => fromJS({
-  channels: {},
-  connections: {},
-
-  connected: false
-})
-
-export let Connection = () => fromJS({
-  conn: undefined,
-  connected: true,
-  initSent: true,
-  initReceived: false,
-
-  gfRemote: [],
-  lfRemote: [],
-
-  channels: [],
-
-  lastPing: 0,
-
-  error: null
-})
-
-export let Wallet = () => ({
-  unspents: [
-    {privKey: wrapHex('92fc477d4310b63613c84cbb17ce39f7bb16b5a55daeb3462cb4aabbccddeeff'), hash: wrapHex('41935e3f9332ea8e2da79c3cc926ac1df66307809b6cb93a27ac62518ea6122d'), n: 0, value: 900000}
-  ]
-})
-
-export let TransactionOutpoint = (hash, n) =>
-                          fromJS({hash, n})
-
-export let ChannelParams = (fundingKey, dustLimitSatoshis, maxHtlcValueInFlightMsat, channelReserveSatoshis, htlcMinimumMsat, feeRatePerKw, toSelfDelay, maxAcceptedHtlcs, revocationBasepoint, paymentBasepoint, delayedPaymentBasepoint, htlcBasepoint, shutdownScriptpubkey, globalFeatures, localFeatures) =>
-                          ({fundingKey, dustLimitSatoshis, maxHtlcValueInFlightMsat, channelReserveSatoshis, htlcMinimumMsat, feeRatePerKw, toSelfDelay, maxAcceptedHtlcs, revocationBasepoint, paymentBasepoint, delayedPaymentBasepoint, htlcBasepoint, shutdownScriptpubkey, globalFeatures, localFeatures})
+const Long = require('long')
 
 export let ChannelUpdateTypes = {
   ADD: 1,
@@ -67,7 +18,24 @@ export let Funded = {
   LOCAL_FUNDED: 1,
   REMOTE_FUNDED: 2
 }
+
+// TODO we shouldn't need this, but instead wire up with the actual wallet
+export let Wallet = () => ({
+  unspents: [
+    {privKey: wrapHex('92fc477d4310b63613c84cbb17ce39f7bb16b5a55daeb3462cb4aabbccddeeff'), hash: wrapHex('41935e3f9332ea8e2da79c3cc926ac1df66307809b6cb93a27ac62518ea6122d'), n: 0, value: 900000}
+  ]
+})
+
 export let ChannelUpdateWrapper = (type, direction, index, msg) => ({type, direction, index, msg})
+
+export let ChannelParams = (fundingKey, dustLimitSatoshis, maxHtlcValueInFlightMsat, channelReserveSatoshis, htlcMinimumMsat, feeRatePerKw, toSelfDelay, maxAcceptedHtlcs, revocationBasepoint, paymentBasepoint, delayedPaymentBasepoint, htlcBasepoint, shutdownScriptpubkey, globalFeatures, localFeatures) =>
+                          ({fundingKey, dustLimitSatoshis, maxHtlcValueInFlightMsat, channelReserveSatoshis, htlcMinimumMsat, feeRatePerKw, toSelfDelay, maxAcceptedHtlcs, revocationBasepoint, paymentBasepoint, delayedPaymentBasepoint, htlcBasepoint, shutdownScriptpubkey, globalFeatures, localFeatures})
+
+export let PaymentWrapper = (direction, id, payment) =>
+                     fromJS({direction, id, payment})
+
+export let Payment = (amount, paymentHash, onionRoutingPackage, cltvTimeout, paymentPreImage) =>
+              fromJS({amount, paymentHash, onionRoutingPackage, cltvTimeout, paymentPreImage})
 
 export let ChannelState = () => ({
   amountMsatLocal: null,
@@ -116,9 +84,3 @@ export let Channel = () => ({
 
   messageOut: []
 })
-
-export let PaymentWrapper = (direction, id, payment) =>
-                     fromJS({direction, id, payment})
-
-export let Payment = (amount, paymentHash, onionRoutingPackage, cltvTimeout, paymentPreImage) =>
-              fromJS({amount, paymentHash, onionRoutingPackage, cltvTimeout, paymentPreImage})
