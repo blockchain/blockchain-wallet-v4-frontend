68207163ea6ae549830553eed0f010300ef183a0
Mats Jerratsch <matsjj@gmail.com>
2018-01-15 18:03:14 +0000

    WIP clean up index

--- packages/blockchain-wallet-v4-frontend/src/index.dev.js
+++ packages/blockchain-wallet-v4-frontend/src/index.dev.js
@@ -29,32 +29,13 @@ if (module.hot) {
   module.hot.accept('scenes/app.js', () => render(require('scenes/app.js').default))
 }
 
-
-store.dispatch(addPeer(wrapHex('02c39955c1579afe4824dc0ef4493fdf7f3660b158cf6d367d8570b9f19683afb5')))
+const peer = '02c39955c1579afe4824dc0ef4493fdf7f3660b158cf6d367d8570b9f19683afb5'
+store.dispatch(addPeer(wrapHex(peer)))
 
 // TODO move to bootstrap section
-//store.dispatch(startSocket())
-
-let options = {
-  chainHash: Buffer.from('06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f', 'hex'),
-  dustLimitSatoshis: Long.fromNumber(546),
-  maxHtlcValueInFlightMsat: Long.fromNumber(100000),
-  channelReserveSatoshis: Long.fromNumber(1000),
-  feeRatePerKw: 10000,
-  htlcMinimumMsat: 1,
-  toSelfDelay: 60,
-  maxAcceptedHtlcs: 100,
-  staticRemote: wrapHex('02c39955c1579afe4824dc0ef4493fdf7f3660b158cf6d367d8570b9f19683afb5'),
-  value: Long.fromNumber(1)
-}
+store.dispatch(startSocket())
 
 // TODO this is simulating the user opening a payment channel
 setTimeout(() => {
-  store.dispatch(startSocket())
-  // store.dispatch(open(options))
-  // setTimeout(() => {
-  //   console.log('create payment channel')
-  //   store.dispatch(open(options))
-  // }, 3000)
-
+  store.dispatch(open(peer, Long.fromNumber(1000)))
 }, 100)
