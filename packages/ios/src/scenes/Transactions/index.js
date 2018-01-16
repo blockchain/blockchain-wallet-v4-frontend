87d325bab0229ae5329a3a0b330c0abe292053b8
Maurice Achtenhagen <maurice@blockchain.com>
2018-01-16 09:50:28 -0500

    Add partial scenes

---
+++ packages/ios/src/scenes/Transactions/index.js
@@ -0,0 +1,37 @@
+
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+// import { Icon } from 'blockchain-info-components/src'
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
+
+export default class Transactions extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Transactions</Text>
+      </View>
+    )
+  }
+}
---
+++ packages/ios/src/scenes/index.js
@@ -0,0 +1,6 @@
+
+export { default as Dashboard } from './Dashboard'
+export { default as Pin } from './Pin'
+export { default as Request } from './Request'
+export { default as Send } from './Send'
+export { default as Transactions } from './Transactions'
