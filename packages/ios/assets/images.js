a2c920dabb815d5858761e01d2c4dc61a0bbd640
Maurice Achtenhagen <maurice@blockchain.com>
2018-01-16 09:50:54 -0500

    Add assets

---
+++ packages/ios/assets/images.js
@@ -0,0 +1,14 @@
+
+const images = {
+  bc_logo: require('../assets/img/bc_logo.png'),
+  delete: require('../assets/img/delete.png'),
+  home: require('../assets/img/home.png'),
+  home_selected: require('../assets/img/home_selected.png'),
+	menu: require('../assets/img/menu.png'),
+	QRCode: require('../assets/img/qr_code.png'),
+  swipeToRequestArrow: require('../assets/img/swipeToRequestArrow.png'),
+	transactions: require('../assets/img/transactions.png'),
+	transactions_selected: require('../assets/img/transactions_selected.png'),
+}
+
+export default images
