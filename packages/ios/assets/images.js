b274b6aabe203b09b0f8d9886e7171d8a10b2a3e
Maurice Achtenhagen <maurice@blockchain.com>
2018-01-17 16:44:14 -0500

    Update Pin screen back button assets

--- packages/ios/assets/images.js
+++ packages/ios/assets/images.js
@@ -1,12 +1,14 @@
 
 const images = {
+  back: require('../assets/img/back.png'),
   bc_logo: require('../assets/img/bc_logo.png'),
   blockchain_blue: require('../assets/img/blockchain-blue.png'),
-  delete: require('../assets/img/delete.png'),
   home: require('../assets/img/home.png'),
   home_selected: require('../assets/img/home_selected.png'),
 	menu: require('../assets/img/menu.png'),
 	QRCode: require('../assets/img/qr_code.png'),
+  request: require('../assets/img/request.png'),
+  send: require('../assets/img/send.png'),
   swipeToRequestArrow: require('../assets/img/swipeToRequestArrow.png'),
 	transactions: require('../assets/img/transactions.png'),
 	transactions_selected: require('../assets/img/transactions_selected.png'),
