33e1f80e823e766267daedd34d26a6f1ac38e25a
Pernas <jaume@pernas.cat>
2018-01-18 23:32:35 +0000

    fixed selector for tx list

--- packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bitcoin/Content/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bitcoin/Content/selectors.js
@@ -10,20 +10,18 @@ const filterTransactions = curry((status, criteria, transactions) => {
   const fullPredicate = allPass([isOfType(status), searchPredicate])
   return filter(fullPredicate, transactions)
 })
-// getData :: state -> Remote(things)
+// getData :: state -> {filtered: Remote(), total: Remote(Int)}
 export const getData = state => {
   const transactions = selectors.core.common.bitcoin.getWalletTransactions(state)
-  console.log(transactions)
-  // [Remotes([])]
   const status = formValueSelector('bitcoinTransaction')(state, 'status') || ''
   const search = formValueSelector('bitcoinTransaction')(state, 'search') || ''
   const source = formValueSelector('bitcoinTransaction')(state, 'source') || {}
-  // const filtered = transactions.map(map(filterTransactions(status, search)))
+  const filtered = transactions.map(map(filterTransactions(status, search)))
   // console.log(filtered)
-  // const total = transactions.map(map(length))
+  const total = transactions.map(map(length)) // Remote [ Int ]
   // console.log(total)
   // return lift((transactions, total) => ({ transactions, total, source: source.address || source.xpub }))(filtered, total)
-  return {filtered: [Remote.Success([]), Remote.Success([]), Remote.Loading], total: Remote.of(10)}
+  return {filtered: [Remote.Success([]), Remote.Success([]), Remote.Loading], total: Remote.of(10), source: source.address || source.xpub}
 }
 
 export const getContext = selectors.core.wallet.getWalletContext
