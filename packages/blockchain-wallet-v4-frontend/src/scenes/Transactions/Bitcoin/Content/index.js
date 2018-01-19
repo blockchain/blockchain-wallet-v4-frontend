9196be48fb377c518c8972f2b5d4f535d25aabc6
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-01-19 16:51:05 +0000

    fix - conflict

--- packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bitcoin/Content/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bitcoin/Content/index.js
@@ -2,53 +2,34 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
+import { equals } from 'ramda'
 import { getContext, getData } from './selectors'
 import { selectors, actions } from 'data'
-// import Error from './template.error'
-// import Loading from './template.loading'
-// import Success from './template.success'
 import Content from './template'
 
 const threshold = 250
 
 class ContentContainer extends React.Component {
   componentWillMount () {
-    // if (Remote.NotAsked.is(this.props.data)) {
-    this.props.dataBitcoinActions.fetchTransactions(this.props.context, true, 0)
-    // }
+    this.props.dataBitcoinActions.fetchTransactions('', true)
   }
 
   componentWillReceiveProps (nextProps) {
+    // Refresh the list if we change the source
     if (this.props.data.source !== nextProps.data.source) {
-      this.props.dataBitcoinActions.fetchTransactions(nextProps.data.source, true, 0)
+      this.props.dataBitcoinActions.fetchTransactions(nextProps.data.source, true)
     }
-    
-    // const fetchTransactions = curry((reset, ntx, currentData, nextData) =>
-    //   this.props.dataBitcoinActions.fetchTransactions(nextData.source, reset, ntx)
-    // )
 
     // Appends more transactions depending on the scroll position
-    // if (!equals(this.props.scroll.yOffset, nextProps.scroll.yOffset)) {
-    //   if (nextProps.scroll.yMax - nextProps.scroll.yOffset < threshold) {
-    //     console.log('ask for more')
-    //     const nbTransactions = nextProps.data.map(compose(length, prop('transactions')))
-    //     nbTransactions.map(console.log)
-    //     lift(fetchTransactions(false))(nbTransactions, this.props.data, nextProps.data)
-    //   }
-    // }
+    if (!equals(this.props.scroll.yOffset, nextProps.scroll.yOffset)) {
+      if (nextProps.scroll.yMax - nextProps.scroll.yOffset < threshold) {
+        this.props.dataBitcoinActions.fetchTransactions(nextProps.data.source, false)
+      }
+    }
   }
 
   render () {
-    const { data } = this.props
-    console.log(data)
-    // return data.cata({
-    //   Success: (value) => <div />, // <Success isEmpty={equals(value.total, 0)} transactions={value.transactions} />,
-    //   Failure: (message) => <Error>{message}</Error>,
-    //   Loading: () => <Loading />,
-    //   NotAsked: () => <Loading />
-    // })
-    return <Content {...data} />
-    // return <div />
+    return <Content {...this.props.data} />
   }
 }
 
