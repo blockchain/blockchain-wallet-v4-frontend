ef9e303b9f4108db66ed8008d9d69e76d4dd5ff1
plondon <philip@blockchain.com>
2018-01-23 18:43:57 -0500

    feat(Addresses): Display and bitcoin addresses and wallets

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Bitcoin/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Bitcoin/template.success.js
@@ -1,36 +1,99 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
+import SwitchableDisplay from 'components/Display/SwitchableDisplay'
 import { SettingDescription, SettingHeader } from 'components/Setting'
+import { Table, TableHeader, TableCell, TableRow, Text } from 'blockchain-info-components'
 
 const Wrapper = styled.section`
   padding: 30px;
   box-sizing: border-box;
 `
-const AddressesSettingHeader = SettingHeader.extend`
+const BitcoinWalletsAddressesSettingHeader = SettingHeader.extend`
   justify-content: flex-start;
+`
+const AddressesSettingDescription = SettingDescription.extend`
+  margin-bottom: 10px;
+`
+const ImportedAddressesSettingHeader = SettingHeader.extend`
+  justify-content: flex-start;
+  margin-top: 30px;
 `
 
 const Success = (props) => {
-  const { wallets } = props
+  const { wallets, importedAddresses } = props.data
+
+  const walletTableRows = wallets.map((wallet, i) => {
+    return (
+      <TableRow key={i}>
+        <TableCell width='30%'>
+          <Text size='13px'>{wallet.label}</Text>
+        </TableCell>
+        <TableCell width='30%'>
+          <Text size='13px'><SwitchableDisplay coin='BTC'>{wallet.value.balance}</SwitchableDisplay></Text>
+        </TableCell>
+      </TableRow>
+    )
+  })
+
+  const importedAddressesTableRows = importedAddresses.map((address, i) => {
+    return (
+      <TableRow key={i}>
+        <TableCell width='30%'>
+          <Text size='13px'>{address.addr}</Text>
+        </TableCell>
+        <TableCell width='30%'>
+          <Text size='13px'><SwitchableDisplay coin='BTC'>{address.info.final_balance}</SwitchableDisplay></Text>
+        </TableCell>
+      </TableRow>
+    )
+  })
 
   return (
-    wallets.map((wallet, i) => {
-      return (
-        <Wrapper>
-          <AddressesSettingHeader>
-            <FormattedMessage id='scenes.settings.addresses.header' defaultMessage='Bitcoin Wallets' />
-          </AddressesSettingHeader>
-          <SettingDescription>
-            <FormattedMessage id='scenes.settings.addresses.header' defaultMessage='Wallets are a way of organizing your funds. Common ways to organize your funds include dividing them up into categories like spending, savings, or business related expenses. Your wallet automatically manages your bitcoin addresses for you by generating a new one each time you need one to receive a payment. You can click on Manage to the right of a wallet to see all of the individual addresses that have been generated for that specific wallet.' />
-          </SettingDescription>
-          <div key={i}>
-            <div>{ wallet.label }</div>
-            <div>{ wallet.value.balance }</div>
-          </div>
-        </Wrapper>
-      )
-    })
+    <Wrapper>
+      <BitcoinWalletsAddressesSettingHeader>
+        <FormattedMessage id='scenes.settings.addresses.bitcoin_wallets' defaultMessage='Bitcoin Wallets' />
+      </BitcoinWalletsAddressesSettingHeader>
+      <AddressesSettingDescription>
+        <FormattedMessage id='scenes.settings.addresses.bitcoin_wallets_description' defaultMessage='Wallets are a way of organizing your funds. Common ways to organize your funds include dividing them up into categories like spending, savings, or business related expenses. Your wallet automatically manages your bitcoin addresses for you by generating a new one each time you need one to receive a payment. You can click on Manage to the right of a wallet to see all of the individual addresses that have been generated for that specific wallet.' />
+      </AddressesSettingDescription>
+      <Table>
+        <TableHeader>
+          <TableCell width='30%'>
+            <Text size='13px' weight={500} capitalize>
+              <FormattedMessage id='scenes.settings.addresses.wallet_name' defaultMessage='Wallet Name' />
+            </Text>
+          </TableCell>
+          <TableCell width='30%'>
+            <Text size='13px' weight={500} capitalize>
+              <FormattedMessage id='scenes.settings.addresses.wallet_description' defaultMessage='Balance' />
+            </Text>
+          </TableCell>
+        </TableHeader>
+        { walletTableRows }
+      </Table>
+      <ImportedAddressesSettingHeader>
+        <FormattedMessage id='scenes.settings.addresses.imported_bitcoin_addrs' defaultMessage='Imported Bitcoin Addresses' />
+      </ImportedAddressesSettingHeader>
+      <AddressesSettingDescription>
+        <FormattedMessage id='scenes.settings.addresses.imported_bitcoin_addrs_desc' defaultMessage='⚠️ Not backed up by your Recovery Phrase. Transfer into a wallet to secure funds.' />
+      </AddressesSettingDescription>
+      <Table>
+        <TableHeader>
+          <TableCell width='30%'>
+            <Text size='13px' weight={500} capitalize>
+              <FormattedMessage id='scenes.settings.imported_addresses.address' defaultMessage='Address' />
+            </Text>
+          </TableCell>
+          <TableCell width='30%'>
+            <Text size='13px' weight={500} capitalize>
+              <FormattedMessage id='scenes.settings.imported_addresses.wallet_description' defaultMessage='Balance' />
+            </Text>
+          </TableCell>
+        </TableHeader>
+        { importedAddressesTableRows }
+      </Table>
+    </Wrapper>
   )
 }
 
