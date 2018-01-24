1190ed469f3e4f973bb8fcdfadbc8cd93a76a102
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-01-24 16:39:25 +0000

    Start implementation of balance dropdown

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/index.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/index.js
@@ -1,31 +1,40 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { equals } from 'ramda'
 
+import { Remote } from 'blockchain-wallet-v4/src'
 import { actions } from 'data'
-import { getData } from './selectors'
+import { getContext, getData } from './selectors'
 import Error from './template.error'
 import Loading from './template.loading'
 import Success from './template.success'
 
 class Balance extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleCoinDisplay = this.handleCoinDisplay.bind(this)
-  }
-
   componentWillMount () {
     // this.props.actions.fetchMetadataEthereum()
+
+    const { context, data } = this.props
+    if (Remote.Success.is(context) && Remote.NotAsked.is(data)) {
+      context.map(x => this.props.ethereumActions.fetchData(x))
+    }
+    if (Remote.NotAsked.is(this.props.data)) {
+      this.props.bitcoinActions.fetchData()
+      this.props.bitcoinActions.fetchRates()
+      this.props.ethereumActions.fetchRates()
+    }
   }
 
-  handleCoinDisplay () {
-    this.props.preferencesActions.toggleCoinDisplayed()
-  }
+  // componentWillReceiveProps (nextProps) {
+  //   if (!equals(this.props.context, nextProps.context)) {
+  //     nextProps.context.map(x => this.props.ethereumActions.fetchData(x))
+  //   }
+  // }
 
   render () {
     const { data } = this.props
     return data.cata({
-      Success: (value) => <Success bitcoinContext={value.bitcoinContext} etherContext={value.etherContext} handleCoinDisplay={this.handleCoinDisplay} />,
+      Success: (value) => <Success balances={value} />,
       Failure: (message) => <Error>{message}</Error>,
       Loading: () => <Loading />,
       NotAsked: () => <Loading />
@@ -34,12 +43,15 @@ class Balance extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  data: getData(state)
+  data: getData(state),
+  context: getContext(state)
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(actions.core.kvStore.ethereum, dispatch),
-  preferencesActions: bindActionCreators(actions.preferences, dispatch)
+  // actions: bindActionCreators(actions.core.kvStore.ethereum, dispatch),
+  bitcoinActions: bindActionCreators(actions.core.data.bitcoin, dispatch),
+  ethereumActions: bindActionCreators(actions.core.data.ethereum, dispatch),
+  settingsActions: bindActionCreators(actions.core.settings, dispatch)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Balance)
--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/template.success.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { FormattedMessage } from 'react-intl'
 
 import BitcoinBalance from './BitcoinBalance'
 import EtherBalance from './EtherBalance'
-import { Text } from 'blockchain-info-components'
+import { SimpleDropdown, Text } from 'blockchain-info-components'
+import CoinDisplay from 'components/Display/CoinDisplay'
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   cursor: pointer;
@@ -19,13 +21,21 @@ const Wrapper = styled.div`
 `
 
 const Success = props => {
-  const { bitcoinContext, etherContext, handleCoinDisplay } = props
+  const { balances } = props
+  const { bitcoinBalance, etherBalance, totalFiatBalance, symbol } = balances
 
   return (
-    <Wrapper onClick={handleCoinDisplay}>
-      <BitcoinBalance context={bitcoinContext} />
-      <Text weight={200} size='24px' >|</Text>
-      <EtherBalance context={etherContext} />
+    <Wrapper>
+      <Text>
+        <FormattedMessage id='layouts.wallet.menutop.balance.total' defaultMessage='Total Balance' />
+      </Text>
+      <SimpleDropdown
+        down
+        selectedValue={0}
+        items={[{ value: 0, text: `${symbol}${totalFiatBalance}` },
+        { value: 1, text: <div><CoinDisplay coin='BTC'>bitcoinBalance</CoinDisplay></div> },
+        { value: 2, text: 'ho' }]}
+        callback={this.handleClick} />
     </Wrapper>
   )
 }
