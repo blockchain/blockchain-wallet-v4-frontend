6912d4107c27225e266ad27d6923970340ecfa87
Salome <salomejumala@gmail.com>
2018-01-24 16:00:16 +0400

    delete blockchain poller

--- packages/blockchain-wallet-v4/src/ln/blockchain/actions.js
+++
@@ -1,9 +0,0 @@
-import {PUSH_TRANSACTION, ADD_TRANSACTION, TRANSACTION_CONFIRMED, TRANSACTION_PUSHED} from './actionTypes'
-
-// sagas
-export const pushTransaction = (tx, requiredConfirmations) => ({type: PUSH_TRANSACTION, txHex, requiredConfirmations})
-
-// reducers
-export const addTransaction = (tx, requiredConfirmations) => ({type: ADD_TRANSACTION, txHex, requiredConfirmations})
-export const transactionPushed = (tx) => ({type: TRANSACTION_PUSHED, txHex})
-export const transactionConfirmed = (tx) => ({type: TRANSACTION_CONFIRMED, txHex})
--- packages/blockchain-wallet-v4/src/ln/blockchain/selectors.js
+++
@@ -1,3 +0,0 @@
-import { path } from 'ramda'
-
-export const transactionsPath = path(['ln', 'transactions'])
