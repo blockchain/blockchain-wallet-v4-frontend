d8c3fbda088cd5e9b7743790d0615ca027b8fabd
plondon <philip@blockchain.com>
2018-01-26 07:31:37 -0500

    feat(Top View): finish layout

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/BitcoinBalance/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/BitcoinBalance/template.success.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import CoinDisplay from 'components/Display/CoinDisplay'
+import FiatDisplay from 'components/Display/FiatDisplay'
 
-import SwitchableDisplay from 'components/Display/SwitchableDisplay'
+const Wrapper = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  padding-right: 25px;
+  padding-left: 5px;
+  > div:last-child {
+    margin-left: 10px;
+    > div {
+      color: ${props => props.theme['gray-3']}
+    }
+  }
+`
 
-const Success = props => <SwitchableDisplay coin='BTC' size='24px' weight={100} showIcon>{props.balance}</SwitchableDisplay>
+const Success = props => {
+  return (
+    <Wrapper>
+      <CoinDisplay coin='BTC' size='12px' weight={300}>{props.balance}</CoinDisplay>
+      <FiatDisplay coin='BTC' size='12px' weight={300}>{props.balance}</FiatDisplay>
+    </Wrapper>
+  )
+}
 
 Success.propTypes = {
   balance: PropTypes.number.isRequired
