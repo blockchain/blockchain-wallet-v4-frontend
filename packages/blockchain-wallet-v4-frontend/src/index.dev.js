8dacbfb931e91e0afb36205ba0dce9b21c3375ae
Mats Jerratsch <matsjj@gmail.com>
2018-01-29 11:40:24 +0000

    WIP cleanup

--- packages/blockchain-wallet-v4-frontend/src/index.dev.js
+++ packages/blockchain-wallet-v4-frontend/src/index.dev.js
@@ -5,8 +5,6 @@ import App from 'scenes/app.js'
 import configureStore from 'store'
 import configureLocales from 'services/LocalesService'
 import {startSocket} from '../../blockchain-wallet-v4/src/ln/tcprelay/actions'
-import {wrapHex} from '../../blockchain-wallet-v4/lib/ln/helper'
-import {addPeer, disconnect, connect} from '../../blockchain-wallet-v4/src/ln/peers/actions'
 import {startUp} from '../../blockchain-wallet-v4/src/ln/root/actions'
 import {open} from '../../blockchain-wallet-v4/src/ln/channel/actions'
 import Long from 'long'
@@ -34,8 +32,6 @@ store.dispatch(startUp())
 
 const peer = '02c39955c1579afe4824dc0ef4493fdf7f3660b158cf6d367d8570b9f19683afb5'
 setTimeout(() => {
-  store.dispatch(addPeer(wrapHex(peer)))
-
 // TODO move to bootstrap section
   store.dispatch(startSocket())
   store.dispatch({type: 'LOGIN', payload: {guid: '2d2e974b-c148-404a-a704-99e51f0bf36d', password: '1234567890a'}})
