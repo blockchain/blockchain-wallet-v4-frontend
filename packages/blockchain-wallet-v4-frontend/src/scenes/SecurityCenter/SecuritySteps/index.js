931cc526dc1331651185d2bdc53127a5797c8218
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-01-31 14:06:13 +0000

    Start security center. Move email, 2fa and backup phrase settings to security center.

---
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/SecuritySteps/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SecuritySteps from './template.js'
+
+class SecurityStepsContainer extends React.Component {
+  render () {
+    return (
+      <SecuritySteps />
+    )
+  }
+}
+
+export default SecurityStepsContainer
