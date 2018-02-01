56f9138576adc6e42146fb04c55fed8de70e631d
plondon <philip@blockchain.com>
2018-02-01 15:03:29 -0500

    feat(Address): get wallet context after adding address, fetchData

--- packages/blockchain-wallet-v4/src/redux/wallet/sagas.js
+++ packages/blockchain-wallet-v4/src/redux/wallet/sagas.js
@@ -8,7 +8,7 @@ import Task from 'data.task'
 import Either from 'data.either'
 import * as A from './actions'
 import * as S from './selectors'
-import { fetchDataSuccess } from '../data/bitcoin/actions'
+import { fetchData } from '../data/bitcoin/actions'
 
 import { Wrapper, Wallet, Address, HDWalletList } from '../../types'
 
@@ -38,14 +38,9 @@ export const walletSaga = ({ api } = {}) => {
     const a = Address.fromJS(address)
     const addAddress = wallet => Wallet.addAddress(wallet, a, password)
     const task = eitherToTask(Wrapper.traverseWallet(Either.of, addAddress, wrapper))
-    const data = yield call(api.fetchBlockchainData, a.addr)
-    const addressData = {
-      addresses: indexBy(prop('address'), prop('addresses', data)),
-      info: path(['wallet'], data),
-      latest_block: path(['info', 'latest_block'], data)
-    }
     yield call(runTask, task, A.setWrapper)
-    yield put(fetchDataSuccess(addressData))
+    const walletContext = yield select(S.getWalletContext)
+    yield put(fetchData(walletContext))
   }
 
   const createWalletSaga = function * ({ password, email }) {
