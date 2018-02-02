9e9075767b6822a2bce12f403fef81dfd33df664
plondon <philip@blockchain.com>
2018-02-02 09:30:09 -0500

    feat(Addresses): dont update bitcoin and imported addr components when loading

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Bitcoin/Wallets/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Bitcoin/Wallets/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getData } from './selectors'
 import Success from './template.success'
+import { Remote } from 'blockchain-wallet-v4/src'
 
 class BitcoinWalletsContainer extends React.Component {
+  shouldComponentUpdate (nextProps) {
+    return !Remote.Loading.is(nextProps.data)
+  }
+
   render () {
     const { data, ...rest } = this.props
     return (
