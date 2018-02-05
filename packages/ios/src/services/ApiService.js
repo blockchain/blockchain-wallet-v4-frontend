ebe6c2cf73d185f931aa9e138f68711f943827f1
Maurice Achtenhagen <maurice@blockchain.com>
2018-02-05 10:26:18 -0500

    Add API service

--- packages/ios/src/services/ApiService.js
+++ packages/ios/src/services/ApiService.js
@@ -1,9 +1,9 @@
 import { createWalletApi } from 'blockchain-wallet-v4/src/network'
 
 const api = createWalletApi({
-   rootUrl: 'https://blockchain.info/',
-   apiUrl: 'https://api.blockchain.info/',
-   apiCode: '1770d5d9-bcea-4d28-ad21-6cbd5be018a8'
+   rootUrl: '',
+   apiUrl: '',
+   apiCode: ''
 })
 
 export { api }
