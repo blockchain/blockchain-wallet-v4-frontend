d90757fb5c54497d1e05e8b1a1b0eb62c23a0dce
Philip Welber <welber@blockchain.com>
2018-02-05 10:59:14 -0500

    continuing security center email verification

--- packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/EmailAddress/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/EmailAddress/index.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators, compose } from 'redux'
+import { actions } from 'data'
+import ui from 'redux-ui'
 
+import { Remote } from 'blockchain-wallet-v4/src'
 import { getData } from './selectors'
 import Error from './template.error'
 import Loading from './template.loading'
 import Success from './template.success'
 
 class EmailAddressContainer extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.handleVerifyClick = this.handleVerifyClick.bind(this)
+  }
+
+  handleVerifyClick () {
+    this.props.updateUI({ verifyToggled: !this.props.ui.verifyToggled })
+  }
+
   render () {
     const { data, ...rest } = this.props
 
     return data.cata({
       Success: (value) => <Success {...rest}
-        data={value} />,
+        data={value}
+        handleVerifyClick={this.handleVerifyClick} />,
       Failure: (message) => <Error {...rest}
         message={message} />,
       Loading: () => <Loading {...rest} />,
@@ -25,4 +40,14 @@ const mapStateToProps = (state) => ({
   data: getData(state)
 })
 
-export default connect(mapStateToProps)(EmailAddressContainer)
+const mapDispatchToProps = (dispatch) => ({
+  settingsActions: bindActionCreators(actions.modules.settings, dispatch),
+  formActions: bindActionCreators(actions.form, dispatch)
+})
+
+const enhance = compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  ui({ key: 'Security_EmailAddress', state: { updateToggled: false, verifyToggled: false } })
+)
+
+export default enhance(EmailAddressContainer)
