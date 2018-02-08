5e92ac788b0997c2a2af728d437ebecc2ea6a01e
Maurice Achtenhagen <maurice@blockchain.com>
2018-02-08 14:17:26 -0500

    Read web socket url from config

--- packages/ios/src/services/SocketService.js
+++ packages/ios/src/services/SocketService.js
@@ -1,3 +1,4 @@
 import { Socket } from 'blockchain-wallet-v4/src/network'
+import Config from 'react-native-config'
 
-export const socket = new Socket({ wsUrl: 'wss://ws.blockchain.info/inv' })
+export const socket = new Socket({ wsUrl: Config.WEB_SOCKET_URL })
