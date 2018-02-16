5505edd3624f03d56d946a94ad182f4c6e323d99
Philip Welber <welber@blockchain.com>
2018-02-16 13:33:31 -0500

    more emailVerification to Components

--- packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/EmailAddress/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/EmailAddress/template.success.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import styled from 'styled-components'
-import { Button, Text, Link } from 'blockchain-info-components'
-import { TextBox } from 'components/Form'
+import { Button, Text } from 'blockchain-info-components'
 
-import { validEmailCode } from 'services/FormHelper'
-import { Field, reduxForm } from 'redux-form'
+import { reduxForm } from 'redux-form'
 import { SecurityComponent, SecurityContainer, SecurityDescription, SecurityHeader, SecurityIcon, SecuritySummary } from 'components/Security'
 
 import ChangeEmailSteps from '../Components/ChangeEmailSteps'
+import EmailVerificationSteps from '../Components/EmailVerificationSteps'
 
 const EmailExplanation = styled.div`
 `
@@ -16,68 +15,16 @@ const ChangeEmailText = styled(Text)`
   cursor: pointer;
   margin-top: 5px;
 `
-const EmailCodeWrapper = styled.form`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  button {
-    margin-left: 100px;
-  }
-`
 const IconContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 25px;
 `
-const EmailChangeWarning = styled(Text)`
-  margin-top: 25px;
-  background: #FFCF62;
-  padding: 5px;
-  width: 130%;
-  span:first-of-type {
-    padding-right: 5px;
-  }
-`
 
 const EmailAddress = (props) => {
-  const { data, ui } = props
+  const { data, ui, handleSubmitVerification, handleResend } = props
   const { email, verified, failed } = data
   const isVerified = verified === 1
-
-  const renderVerificationSteps = () => {
-    return (
-      <SecuritySummary>
-        <SecurityHeader>
-          <FormattedMessage id='scenes.security.email.unverifiedtitle' defaultMessage='Verify email address' />
-        </SecurityHeader>
-        <SecurityDescription>
-          <FormattedMessage id='scenes.security.email.verifyemailaddress' defaultMessage='We have sent a verification code to ' />
-          {email}
-          <FormattedMessage id='scenes.security.email.verifyemailaddress2' defaultMessage='. Please open the email and enter the code below to complete the verification process.' />
-        </SecurityDescription>
-        <EmailCodeWrapper onSubmit={props.handleSubmitVerification}>
-          <Field name='emailCode' validate={[validEmailCode]} component={TextBox} placeholder='123AB' onPaste={props.handleEmailCodePaste} />
-          <Button nature='primary' type='submit'>
-            <FormattedMessage id='scenes.preferences.email.settings.updateform.verify' defaultMessage='Verify Code' />
-          </Button>
-        </EmailCodeWrapper>
-        {
-          failed
-            ? <EmailChangeWarning size='12px' color='error'>
-              <FormattedMessage id='scenes.security.email.verifyemailaddress2' defaultMessage='Your verification code is incorrect. Please double check your email and try again.' />
-              <Link size='12px' onClick={props.handleResend}>Get a new verification code</Link>
-            </EmailChangeWarning>
-            : null
-        }
-      </SecuritySummary>
-    )
-  }
-
-  const renderChangeEmailSteps = () => {
-    return (
-      <ChangeEmailSteps handleEmailChangeCancel={props.handleEmailChangeCancel} handleEmailChangeSubmit={props.handleEmailChangeSubmit} />
-    )
-  }
 
   const renderInitial = () => {
     return (
@@ -120,8 +67,8 @@ const EmailAddress = (props) => {
         (!ui.verifyToggled && !ui.changeEmailToggled) && !props.alone
           ? renderInitial()
           : ui.changeEmailToggled
-            ? renderChangeEmailSteps()
-            : renderVerificationSteps()
+            ? <ChangeEmailSteps handleEmailChangeCancel={props.handleEmailChangeCancel} handleEmailChangeSubmit={props.handleEmailChangeSubmit} />
+            : <EmailVerificationSteps failed={failed} email={email} handleSubmitVerification={handleSubmitVerification} handleResend={handleResend} />
       }
       <SecurityComponent>
         {
