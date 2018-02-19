6e7db3d980c50bbce8ac233c48c457b1e03ae949
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-02-19 18:29:31 +0000

    fix - update unspent

--- packages/blockchain-wallet-v4-frontend/src/components/Form/SelectBoxAccounts/Accounts/services.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Form/SelectBoxAccounts/Accounts/services.js
@@ -1,4 +1,4 @@
-import { equals, head, isNil, lift, path, prop } from 'ramda'
+import { equals, head, path, prop } from 'ramda'
 
 export const changeSource = (currentSource, newSource, currentTarget, btcBalances, ethBalances) => {
   const currentCoin = prop('coin', currentSource)
@@ -20,44 +20,39 @@ export const changeTarget = (currentTarget, newTarget, currentSource, btcBalance
   return { source: currentSource, target: newTarget }
 }
 
-export const changeCoin = (prevProps, nextProps) => {
-  console.log('changeCoin', nextProps)
-  const { btcBalances, ethBalances } = nextProps
-  const prevSource = path(['accounts', 'source'], prevProps)
-  const nextSource = path(['accounts', 'source'], nextProps)
-  const prevSourceCoin = path(['accounts', 'source', 'coin'], prevProps)
-  const nextSourceCoin = path(['accounts', 'source', 'coin'], nextProps)
-  const prevTargetCoin = path(['accounts', 'target', 'coin'], prevProps)
-  const nextTargetCoin = path(['accounts', 'target', 'coin'], nextProps)
-
-  console.log('changeCoin', prevSourceCoin, nextSourceCoin)
-  console.log('changeCoin', prevTargetCoin, nextTargetCoin)
-  // If the source coin has changed, we select a new target coin
-  if (!isNil(prevSourceCoin) && !equals(prevSourceCoin, nextSourceCoin) && equals(prevTargetCoin, nextTargetCoin)) {
-    const nextTarget = selectDefaultAccount(prevSourceCoin, btcBalances, ethBalances)
-    nextProps.formActions.change('exchange', 'target', nextTarget)
+export const initUnspent = (props) => {
+  const source = path(['value', 'source'], props)
+  const sourceCoin = prop('coin', source)
+  if (equals(sourceCoin, 'BTC')) {
+    props.dataBitcoinActions.fetchUnspent(prop('index', source) || prop('address', source))
   }
+}
 
-  // If the target coin has changed, we select a new source coin
-  if (!isNil(prevTargetCoin) && !equals(prevTargetCoin, nextTargetCoin) && equals(prevSourceCoin, nextSourceCoin)) {
-    const nextTarget = selectDefaultAccount(prevTargetCoin, btcBalances, ethBalances)
-    nextProps.formActions.change('exchange', 'source', nextTarget)
-  }
-
-  // Update unspent if needed (BTC)
-  if (!equals(prevSource, nextSource) && !isNil(prevSourceCoin) && equals(nextSourceCoin, 'BTC')) {
+export const updateUnspent = (prevProps, nextProps) => {
+  const prevSource = path(['value', 'source'], prevProps)
+  const nextSource = path(['value', 'source'], nextProps)
+  const nextSourceCoin = prop('coin', nextSource)
+  if (!equals(prevSource, nextSource) && equals(nextSourceCoin, 'BTC')) {
     nextProps.dataBitcoinActions.fetchUnspent(prop('index', prevSource) || prop('address', prevSource))
   }
 }
 
-export const updateEffectiveBalance = (props) => {
-  const coins = prop('btcFee', props)
-  const updateBalance = (data) => {
-    const feePerBytes = prop('btcFee', data)
-    props.dataBitcoinActions.refreshEffectiveBalance(coins, feePerBytes)
-  }
-  props.data.map(x => updateBalance(x))
-}
+// export const initEffectiveBalance = (props) => {
+//   const updateBalance = (data) => {
+//     const feePerBytes = prop('btcFee', data)
+//     props.dataBitcoinActions.refreshEffectiveBalance(coins, feePerBytes)
+//   }
+//   props.data.map(x => updateBalance(x))
+// }
+
+// export const updateEffectiveBalance = (prevProps, nextProps) => {
+//   const coins = prop('btcFee', props)
+//   const updateBalance = (data) => {
+//     const feePerBytes = prop('btcFee', data)
+//     props.dataBitcoinActions.refreshEffectiveBalance(coins, feePerBytes)
+//   }
+//   props.data.map(x => updateBalance(x))
+// }
 
 export const selectDefaultAccount = (coin, btcBalances, ethBalances) => {
   switch (coin) {
