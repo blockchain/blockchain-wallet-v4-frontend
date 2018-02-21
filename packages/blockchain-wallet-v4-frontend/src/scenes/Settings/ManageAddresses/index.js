7e6c9faa1dc5ddbb0d6732718cf67e6eb3813fd7
Justin Tormey <justin@blockchain.com>
2018-02-21 12:04:25 -0500

    refactor(AddressLabels): clean up template

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/ManageAddresses/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/ManageAddresses/index.js
@@ -24,7 +24,7 @@ class ManageAddressesContainer extends React.Component {
 const mapStateToProps = (state, props) => {
   let index = props.computedMatch.params.index
   let account = Types.Wallet.selectHDAccounts(state.walletPath.wallet).get(index)
-  let labels = Types.HDAccount.selectAddressLabels(account).toArray()
+  let labels = Types.HDAccount.selectAddressLabels(account).reverse().toArray()
   let nextReceiveIndex = selectors.core.data.bitcoin.getReceiveIndex(account.xpub, state)
   let lastLabeledIndex = labels.reduce((acc, l) => Math.max(acc, l.index), 0)
   let receiveIndex = nextReceiveIndex.map(i => Math.max(i, lastLabeledIndex + 1))
--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/ManageAddresses/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/ManageAddresses/template.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { LinkContainer } from 'react-router-bootstrap'
 import styled from 'styled-components'
-import settings from 'config'
-import { Icon, TabMenu, TabMenuItem, Text, Table, TableHeader, TableCell, TableRow, Button, Link } from 'blockchain-info-components'
+import { Icon, TabMenu, TabMenuItem, Text, IconButton } from 'blockchain-info-components'
 import HorizontalMenu from 'components/HorizontalMenu'
+import AddressesTable from './AddressesTable'
+import AddressesTableEntry from './AddressesTableEntry'
 
 const Wrapper = styled.div`
   width: 100%;
@@ -31,51 +32,26 @@ const ManageAddressesTemplate = ({ account, labels, receiveIndex, deriveAddress,
       <Text weight={400} size='small' style={{ marginTop: 25 }}>
         <FormattedMessage id='scenes.settings.manage_addresses.unused_addresses' defaultMessage='Unused Addresses' />
       </Text>
-      <Text weight={200} size='small' style={{ marginTop: 10 }}>
+      <Text weight={200} size='small' style={{ marginTop: 10, marginBottom: 15 }}>
         <FormattedMessage id='scenes.settings.manage_addresses.unused_addresses.message' defaultMessage='Your Blockchain Wallet contains an unlimited collection of bitcoin addresses that you can use to receive funds from anybody, globally. Your wallet will automatically manage your bitcoin addresses for you. The addresses below are the subset of addresses that are labeled.' />
       </Text>
+      {labels.length === 0 ? null : (
+        <AddressesTable>
+          {labels.map(entry => (
+            <AddressesTableEntry {...{ entry, deriveAddress, onEditLabel, onDeleteLabel }} />
+          ))}
+        </AddressesTable>
+      )}
       {receiveIndex.cata({
         Success: (index) => (
-          <Button onClick={() => onSetLabel(index, 'New Address')}>
+          <IconButton style={{ marginTop: 15 }} name='build' onClick={() => onSetLabel(index, 'New Address')}>
             <FormattedMessage id='scenes.settings.manage_addresses.add_label' defaultMessage='Add Next Address' />
-          </Button>
+          </IconButton>
         ),
         Failure: () => null,
         Loading: () => null,
         NotAsked: () => null
       })}
-      <Table>
-        <TableHeader>
-          <TableCell width='30%'>
-            <Text size='13px' weight={500} capitalize>
-              <FormattedMessage id='scenes.settings.addresses.address' defaultMessage='Address' />
-            </Text>
-          </TableCell>
-          <TableCell width='30%'>
-            <Text size='13px' weight={500} capitalize>
-              <FormattedMessage id='scenes.settings.addresses.address_label' defaultMessage='Label' />
-            </Text>
-          </TableCell>
-        </TableHeader>
-        {labels.map(entry => (
-          <TableRow key={entry.index}>
-            <TableCell width='30%'>
-              <Link href={`${settings.ROOT_URL}address/${deriveAddress(entry.index)}`} size='small' weight={300} target='_blank'>
-                {deriveAddress(entry.index)}
-              </Link>
-            </TableCell>
-            <TableCell width='30%'>
-              <Text size='13px'>{entry.label}</Text>
-            </TableCell>
-            <TableCell width='30%'>
-              <Icon name='pencil' onClick={() => onEditLabel(entry.index)} />
-            </TableCell>
-            <TableCell width='30%'>
-              <Icon name='trash' onClick={() => onDeleteLabel(entry.index)} />
-            </TableCell>
-          </TableRow>
-        ))}
-      </Table>
     </InnerWrapper>
   </Wrapper>
 )
