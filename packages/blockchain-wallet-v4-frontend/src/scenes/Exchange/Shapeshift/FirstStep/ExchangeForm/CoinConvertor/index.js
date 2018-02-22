d97ef805c63b54990ec74b1215aaf4e54b09b117
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-02-22 18:14:41 +0000

    added - links minimum/maximum

--- packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Shapeshift/FirstStep/ExchangeForm/CoinConvertor/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Shapeshift/FirstStep/ExchangeForm/CoinConvertor/index.js
@@ -17,54 +17,34 @@ class CoinConvertorContainer extends React.Component {
     this.state = this.props.input.value
     this.handleChangeSource = this.handleChangeSource.bind(this)
     this.handleChangeTarget = this.handleChangeTarget.bind(this)
+    this.handleClickMinimum = this.handleClickMinimum.bind(this)
+    this.handleClickMaximum = this.handleClickMaximum.bind(this)
   }
 
   componentWillReceiveProps (nextProps) {
     // Props have been updated
-    console.log('componentWillReceiveProps', this.props, nextProps)
     if (!equals(this.props.input.value, nextProps.input.value)) {
       console.log('input.value has changed: ', nextProps.input.value)
       this.setState(nextProps.input.value)
     }
 
-     // // Reset coin values when source coin has changed
-    // if (!equals(this.props.sourceCoin, nextProps.sourceCoin)) {
-    //   this.setState({ coinSourceValue: '', coinTargetValue: '' })
-    // }
-
+     // Reset coin values when source coin has changed
     if (Remote.Success.is(nextProps.data)) {
       const depositAmount = nextProps.data.map(x => path(['success', 'depositAmount'], x)).getOrElse(this.state.source)
       const withdrawalAmount = nextProps.data.map(x => path(['success', 'withdrawalAmount'], x)).getOrElse(this.state.target)
       console.log('remote success', depositAmount, withdrawalAmount)
-      if (!isNil(depositAmount) && !isNil(withdrawalAmount)) {
-        nextProps.formActions.change('exchange', 'amount', { source: depositAmount, target: withdrawalAmount })
-      }
-
-    //   if (!equals(this.coinSourceValue, depositAmount) || !equals(this.coinTargetValue, withdrawalAmount)) {
-    //     // this.update({ coinSourceValue: depositAmount, coinTargetValue: withdrawalAmount })
-    //   }
-
-      // if (!equals(this.state.source, depositAmount)) {
-      //   console.log('depositAmount update', depositAmount)
-      //   nextProps.formActions.change('exchange', '', depositAmount)
-      //   this.setState({ source: depositAmount })
-      // }
-
-      // if (!equals(this.state.target, withdrawalAmount)) {
-      //   console.log('withdrawalAmount update', withdrawalAmount)
-      //   this.setState({ target: withdrawalAmount })
+      // if (!isNil(depositAmount) && !isNil(withdrawalAmount)) {
+      nextProps.formActions.change('exchange', 'amount', { source: depositAmount || 0, target: withdrawalAmount || 0 })
       // }
     }
   }
 
   update (data) {
-    console.log('updateee', data)
     this.setState(data)
     this.props.input.onChange(data.coinSourceValue)
   }
 
   handleChangeSource (value) {
-    console.log('handleChangeSource', value)
     if (!isNil(value)) {
       if (this.timeout) clearTimeout(this.timeout)
       this.timeout = setTimeout(() => this.props.shapeshiftDataActions.fetchShapeshiftQuotation(value, this.props.pair, true), 1000)
@@ -72,21 +52,29 @@ class CoinConvertorContainer extends React.Component {
   }
 
   handleChangeTarget (value) {
-    console.log('handleChangeTarget', value)
     if (!isNil(value)) {
       if (this.timeout) clearTimeout(this.timeout)
       this.timeout = setTimeout(() => this.props.shapeshiftDataActions.fetchShapeshiftQuotation(value, this.props.pair, false), 1000)
     }
   }
 
+  handleClickMinimum () {
+    console.log('handleClickMinium')
+  }
+
+  handleClickMaximum () {
+    console.log('handleClickMaximum')
+  }
+
   render () {
     const { source, target } = this.state
 
     return this.props.data.cata({
-      Success: (value) => <Success {...value} {...this.props} source={source} target={target} handleChangeSource={this.handleChangeSource} handleChangeTarget={this.handleChangeTarget} loading={false} />,
-      Failure: (message) => <Success {...this.props} source={source} target={target} handleChangeSource={this.handleChangeSource} handleChangeTarget={this.handleChangeTarget} loading={false} />,
-      Loading: () => <Success {...this.props} source={source} target={target} handleChangeSource={this.handleChangeSource} handleChangeTarget={this.handleChangeTarget} loading />,
-      NotAsked: () => <Success {...this.props} source={source} target={target} handleChangeSource={this.handleChangeSource} handleChangeTarget={this.handleChangeTarget} loading={false} />
+      Success: (value) => <Success {...value} {...this.props} source={source} target={target} handleChangeSource={this.handleChangeSource}
+        handleChangeTarget={this.handleChangeTarget} handleClickMinimum={this.handleClickMinimum} handleClickMaximum={this.handleClickMaximum} loading={false} />,
+      Failure: (message) => <Success {...this.props} source={source} target={target} loading={false} />,
+      Loading: () => <Success {...this.props} source={source} target={target} loading />,
+      NotAsked: () => <Success {...this.props} source={source} target={target} loading={false} />
     })
   }
 }
