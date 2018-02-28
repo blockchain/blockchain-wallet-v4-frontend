cb45c49f77382bdcfeba35f5177bcfcd167e4067
schnogz <andrew@blockchain.com>
2018-02-28 15:21:43 -0500

    setup jest and lerna, add some unit tests

--- packages/blockchain-wallet-v4-frontend/src/services/FormHelper/index.js
+++ packages/blockchain-wallet-v4-frontend/src/services/FormHelper/index.js
@@ -6,31 +6,18 @@ import zxcvbn from 'zxcvbn'
 import { utils } from 'blockchain-wallet-v4/src'
 
 const required = value => value ? undefined : 'Required'
-
 const validNumber = value => isNumeric(value) ? undefined : 'Invalid number'
-
 const requiredNumber = value => isNumeric(value) && value > 0 ? undefined : 'Invalid number'
-
 const validEmail = value => isEmail(value) ? undefined : 'Invalid email address'
-
 const validateMnemonic = value => bip39.validateMnemonic(value) ? undefined : 'Invalid passphrase'
-
 const validWalletId = value => isGuid(value) ? undefined : 'Invalid wallet identifier'
-
 const validMobileNumber = value => !isEmpty(parse(value)) ? undefined : 'Invalid mobile number'
-
 const validStrongPassword = value => (value !== undefined && zxcvbn(value).score > 1) ? undefined : 'Your password is not strong enough'
-
 const validIpList = value => isIpList(value) ? undefined : 'Invalid IP list'
-
 const validPasswordStretchingNumber = value => (value > 1 && value <= 20000) ? undefined : 'Please ensure 1 < PBKDF2 <= 20000'
-
 const validEtherAddress = value => utils.ethereum.isValidAddress(value) ? undefined : 'Invalid Ethereum Address'
-
 const validBitcoinAddress = value => utils.bitcoin.isValidBitcoinAddress(value) ? undefined : 'Invalid Bitcoin Address'
-
 const validEmailCode = value => isAlphaNumeric(value) ? undefined : 'Invalid Email Code'
-
 const validBitcoinPrivateKey = value => utils.bitcoin.isValidBitcoinPrivateKey(value) ? undefined : 'Invalid Bitcoin Private Key'
 
 export {
