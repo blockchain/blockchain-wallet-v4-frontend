787a973399a2730e9882316f4ff13c5a111a91a2
Salome <salomejumala@gmail.com>
2018-03-01 22:55:16 +0400

    refresh saga

--- packages/blockchain-wallet-v4/src/redux/refresh/sagas.js
+++ packages/blockchain-wallet-v4/src/redux/refresh/sagas.js
@@ -1,32 +1,28 @@
 
-import { call, put, select, takeLatest } from 'redux-saga/effects'
+import {put, select, takeLatest, throttle } from 'redux-saga/effects'
 import { set } from 'ramda-lens'
-import * as A from './actions'
 import * as AT from './actionTypes'
 import * as btcActions from '../data/bitcoin/actions'
 import * as bchActions from '../data/bch/actions'
 import * as ethActions from '../data/ethereum/actions'
 import * as S from '../wallet/selectors'
+import * as ethSelectors from '../kvStore/ethereum/selectors'
 
 export const refreshSaga = ({ api } = {}) => {
   const refresh = function * () {
-    try {
-      const walletContext = yield select(S.getWalletContext)
-      yield put(btcActions.fetchData(walletContext))
-
-      //  context?
-      yield put(bchActions.fetchData(walletContext))
-      yield put(btcActions.fetchRates())
-      yield put(bchActions.fetchRates())
-      yield put(ethActions.fetchRates())
-      // ethereum balance
-      // accounts?
-      yield put(ethActions.fetchLatestBlock())
-    } catch (e) {
-    }
+    const walletContext = yield select(S.getWalletContext)
+    yield put(btcActions.fetchData(walletContext))
+    yield put(bchActions.fetchData(walletContext))
+    const ethContext = yield select(ethSelectors.getContext)
+    yield put(ethActions.fetchData(ethContext.data))
+    yield put(btcActions.fetchRates())
+    yield put(bchActions.fetchRates())
+    yield put(ethActions.fetchRates())
+    yield put(ethActions.fetchLatestBlock())
+    console.log('refresh done')
   }
 
   return function * () {
-    yield takeLatest(AT.REFRESH, refresh)
+    yield throttle(5000, AT.REFRESH, refresh)
   }
 }
