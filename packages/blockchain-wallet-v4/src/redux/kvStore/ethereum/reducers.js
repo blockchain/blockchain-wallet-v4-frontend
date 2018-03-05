f76f06462605cc6df0c062c8c9dc54e35278481c
Salome <salomejumala@gmail.com>
2018-03-06 01:38:38 +0400

    ethereum transaction notes

--- packages/blockchain-wallet-v4/src/redux/kvStore/ethereum/reducers.js
+++ packages/blockchain-wallet-v4/src/redux/kvStore/ethereum/reducers.js
@@ -1,5 +1,5 @@
-import { set, mapped } from 'ramda-lens'
-import { compose } from 'ramda'
+import { set, mapped, over } from 'ramda-lens'
+import { assocPath, compose } from 'ramda'
 import { KVStoreEntry } from '../../../types'
 import * as AT from './actionTypes'
 import Remote from '../../../remote'
@@ -22,6 +22,11 @@ export default (state = INITIAL_STATE, action) => {
     }
     case AT.FETCH_METADATA_ETHEREUM_FAILURE: {
       return Remote.Failure(payload)
+    }
+    case AT.SET_TRANSACTION_NOTE_ETHEREUM: {
+      let valueLens = compose(mapped, KVStoreEntry.value)
+      let setTrades = assocPath(['ethereum', 'tx_notes', payload.txHash], payload.txNote)
+      return over(valueLens, setTrades, state)
     }
     default:
       return state
