cd9709314ae6af33da9432591e7b21844b57c22b
plondon <philip@blockchain.com>
2018-03-10 09:09:18 -0500

    fix(Accept): busy state

--- packages/blockchain-wallet-v4-frontend/src/modals/SfoxExchangeData/Create/index.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/SfoxExchangeData/Create/index.js
@@ -2,44 +2,25 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators, compose } from 'redux'
-// import { formValueSelector } from 'redux-form'
 import Create from './template'
 import { actions, selectors } from 'data'
 import ui from 'redux-ui'
 
 class CreateContainer extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = { busy: false }
-
-    this.handleSignup = this.handleSignup.bind(this)
-  }
-
   componentDidMount () {
     if (this.props.emailVerified && this.props.smsVerified) this.props.updateUI({ create: 'create_account' })
     else if (this.props.emailVerified) this.props.updateUI({ create: 'change_mobile' })
     else this.props.updateUI({ create: 'enter_email_code' })
   }
 
-  handleSignup (e) {
-    e.preventDefault()
-    this.props.sfoxFrontendActions.sfoxSignup()
-  }
-
   render () {
-    return <Create
-      {...this.props}
-      setBusyOff={this.setBusyOff}
-      handleSignup={this.handleSignup}
-    />
+    return <Create {...this.props} />
   }
 }
 
 CreateContainer.propTypes = {
   ui: PropTypes.object,
   updateUI: PropTypes.function,
-  sfoxFrontendActions: PropTypes.object,
   smsVerified: PropTypes.number.isRequired,
   emailVerified: PropTypes.number.isRequired
 }
