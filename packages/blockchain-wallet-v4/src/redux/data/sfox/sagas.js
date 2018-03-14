bf82b60e190d5ad3044b78a33c2516a78648ad35
Philip Welber <welber@blockchain.com>
2018-03-14 17:12:33 -0400

    feat(BuySell): fix sagas for adding bank manually

--- packages/blockchain-wallet-v4/src/redux/data/sfox/sagas.js
+++ packages/blockchain-wallet-v4/src/redux/data/sfox/sagas.js
@@ -19,12 +19,9 @@ export const sfoxSaga = ({ api, sfoxService } = {}) => {
     try {
       const sfox = yield call(getSfox)
       const methods = yield apply(sfox, sfox.getBuyMethods)
-      // console.log('setbankmanually core saga', methods.ach, sfox)
-      const addedBankAccount = yield apply(sfox, methods.ach.addAccount, [routing, account, name, type])
-      // console.log('setbankmanually core saga2', addedBankAccount)
+      const addedBankAccount = yield apply(methods.ach, methods.ach.addAccount, [routing, account, name, type])
       yield put(A.setBankManuallySuccess(addedBankAccount))
     } catch (e) {
-      console.warn('setBankManually core saga', e)
       yield put(A.setBankAccountFailure(e))
     }
   }
