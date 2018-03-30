f6e0eef4bb061d081c611d7d9f6ed9302c979783
Philip Welber <welber@blockchain.com>
2018-03-30 13:20:28 -0400

    fix(Modals): remove sfox add bank modal

--- packages/blockchain-wallet-v4-frontend/src/modals/SfoxExchangeData/index.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/SfoxExchangeData/index.js
@@ -57,7 +57,7 @@ class SfoxExchangeData extends React.Component {
 
     return (
       <Tray position={this.props.position} total={this.props.total} in={show} class='tray' onClose={this.handleClose.bind(this)}>
-        <ModalHeader onClose={this.handleClose.bind(this)}>
+        <ModalHeader tray center onClose={this.handleClose.bind(this)}>
           <StepIndicator step={step} stepMap={this.stepMap} />
         </ModalHeader>
         <ModalBody>
