e03bf85163d4758b11d7df886717fdf6bf1b486a
plondon <philip@blockchain.com>
2018-04-08 16:32:21 -0400

    Api network rootUrl fix

--- packages/blockchain-wallet-v4/src/network/api/wallet/index.js
+++ packages/blockchain-wallet-v4/src/network/api/wallet/index.js
@@ -123,25 +123,25 @@ export default ({ get, post }) => {
   })
 
   const incrementStat = (eventName) => get({
-    url: rootUrl,
+    url: global.domains.root,
     endPoint: 'event',
     data: {name: 'wallet_web_login_via', mode: 'no-cors'}
   })
 
   const incrementSecPasswordStats = (secondPassActive) => get({
-    url: rootUrl,
+    url: global.domains.root,
     endPoint: 'event',
     data: {name: `wallet_login_second_password_${secondPassActive ? 1 : 0}`}
   })
 
   const incrementLoginViaQrStats = () => get({
-    url: rootUrl,
+    url: global.domains.root,
     endPoint: 'event',
     data: {name: 'wallet_login_second_password_wallet_web_login_via_qr'}
   })
 
   const incrementCurrencyUsageStats = (btcBalance, ethBalance, bchBalance) => get({
-    url: rootUrl,
+    url: global.domains.root,
     endPoint: 'event',
     data: {name: `wallet_login_balance_btc_${btcBalance > 0 ? 1 : 0}_eth_${ethBalance > 0 ? 1 : 0}_bch_${bchBalance > 0 ? 1 : 0}`}
   })
