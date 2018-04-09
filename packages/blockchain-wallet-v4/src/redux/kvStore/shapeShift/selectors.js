d6b724ee016b3789ab7160e304801ecdcc0c3d41
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-04-09 18:08:46 +0100

    ShapeShift exchange history improvements

--- packages/blockchain-wallet-v4/src/redux/kvStore/shapeShift/selectors.js
+++ packages/blockchain-wallet-v4/src/redux/kvStore/shapeShift/selectors.js
@@ -10,13 +10,13 @@
 //   getTrades
 // )(state)
 
-import { compose, curry, filter, head, path } from 'ramda'
+import { compose, curry, filter, head, path, reverse } from 'ramda'
 import { SHAPESHIFT } from '../config'
 import { kvStorePath } from '../../paths'
 
 export const getMetadata = path([kvStorePath, SHAPESHIFT])
 
-export const getTrades = state => getMetadata(state).map(path(['value', 'trades']))
+export const getTrades = state => getMetadata(state).map(path(['value', 'trades'])).map(trades => reverse(trades))
 
 export const getTrade = curry((address, state) => getTrades(state)
   .map(compose(head, filter(x => path(['quote', 'deposit'], x) === address))))
