a64f90d862501cc8c7b81f48d7f5f960a55c67cd
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-04-09 18:08:46 +0100

    ShapeShift exchange history improvements

--- packages/blockchain-wallet-v4-frontend/src/scenes/ExchangeHistory/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/ExchangeHistory/index.js
@@ -1,29 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { compose, bindActionCreators } from 'redux'
-import ui from 'redux-ui'
-import { equals } from 'ramda'
 
-import { actions, selectors } from 'data'
+import { actions } from 'data'
 import { getData } from './selectors'
 import Error from './template.error'
 import Loading from './template.loading'
 import Success from './template.success'
 
-const threshold = 250
-
 class ExchangeHistoryContainer extends React.Component {
   componentWillMount () {
     this.props.dataShapeshiftActions.refreshShapeshiftTrades()
-  }
-
-  componentWillReceiveProps (nextProps) {
-    // Appends more transactions depending on the scroll position
-    if (!equals(this.props.scroll.yOffset, nextProps.scroll.yOffset)) {
-      if (nextProps.scroll.yMax - nextProps.scroll.yOffset < threshold) {
-        this.props.updateUI({ total: this.props.ui.total + 10 })
-      }
-    }
   }
 
   componentWillUnmount () {
@@ -43,8 +30,7 @@ class ExchangeHistoryContainer extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  data: getData(state, ownProps.ui.total),
-  scroll: selectors.scroll.selectScroll(state)
+  data: getData(state)
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -52,7 +38,6 @@ const mapDispatchToProps = dispatch => ({
 })
 
 const enhance = compose(
-  ui({ key: 'EtherTransactions', state: { total: 10 } }),
   connect(mapStateToProps, mapDispatchToProps)
 )
 
