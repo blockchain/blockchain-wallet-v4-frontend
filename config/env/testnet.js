0cf95b122657d5acc717230cec24bdfd6acac5cc
plondon <philip@blockchain.com>
2018-04-09 15:28:08 -0400

    Fix - use wallet options from before call locally

--- config/env/testnet.js
+++ config/env/testnet.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BLOCKCHAIN_INFO: 'https://testnet.blockchain.info',
+  BLOCKCHAIN_INFO: 'https://testnet.blockchain.info/',
   API_BLOCKCHAIN_INFO: 'https://api-testnet.blockchain.info/',
   WEB_SOCKET_URL: 'wss://ws.blockchain.info/testnet3/inv'
 }
--- packages/blockchain-wallet-v4/src/network/api/index.js
+++ packages/blockchain-wallet-v4/src/network/api/index.js
@@ -11,14 +11,10 @@ import sfox from './sfox'
 import wallet from './wallet'
 import fetchService from './fetch'
 
-export const BLOCKCHAIN_INFO = 'https://blockchain.info/'
-export const API_BLOCKCHAIN_INFO = 'https://api.blockchain.info/'
-export const API_CODE = '1770d5d9-bcea-4d28-ad21-6cbd5be018a8'
-
 export const SHAPESHIFT_IO = 'https://shapeshift.io/'
 export const SHAPESHIFT_API_KEY = 'b7a7c320c19ea3a8e276c8921bc3ff79ec064d2cd9d98ab969acc648246b4be5ab2379af704c5d3a3021c0ddf82b3e479590718847c1301e1a85331d2d2a8370'
 
-export default ({ rootUrl = BLOCKCHAIN_INFO, apiUrl = API_BLOCKCHAIN_INFO, apiCode = API_CODE, shapeShiftRootUrl = SHAPESHIFT_IO, shapeShiftApiKey = SHAPESHIFT_API_KEY } = {}) => {
+export default ({ rootUrl, apiUrl, apiCode, shapeShiftRootUrl = SHAPESHIFT_IO, shapeShiftApiKey = SHAPESHIFT_API_KEY } = {}) => {
   const { get, post } = fetchService({ apiCode })
 
   return {
