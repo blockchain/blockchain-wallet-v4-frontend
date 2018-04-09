62d7bedea306215dc37d0b83a560eb2dfee63241
plondon <philip@blockchain.com>
2018-04-09 15:29:25 -0400

    Remove testnet/stging urls

--- packages/blockchain-wallet-v4-frontend/src/config/config.dev.js
+++ packages/blockchain-wallet-v4-frontend/src/config/config.dev.js
@@ -19,32 +19,3 @@ export default {
   NETWORK_ETHEREUM: 1, // MAINNET
   NETWORK_BCH: BitcoinCash.networks.bitcoin
 }
-
-// ======================= TESTNET ==========================
-
-// export default {
-//   ROOT_URL: 'https://testnet5.blockchain.info/',
-//   API_BLOCKCHAIN_INFO: 'https://api.testnet.blockchain.info/',
-//   API_CODE: '1770d5d9-bcea-4d28-ad21-6cbd5be018a8',
-//   WEB_SOCKET_URL: 'wss://ws.testnet.blockchain.info/inv',
-//   WALLET_DATA_PATH: 'wallet.data',
-//   WALLET_PAYLOAD_PATH: 'wallet.payload',
-//   WALLET_SETTINGS_PATH: 'wallet.settings',
-//   WALLET_OPTIONS_PATH: 'wallet.options',
-//   WALLET_KVSTORE_PATH: 'wallet.kvstore',
-//   NETWORK_BITCOIN: Bitcoin.networks.testnet
-// }
-
-// ======================= STAGING ==========================
-
-// export default {
-//   ROOT_URL: 'https://explorer.staging.blockchain.info/',
-//   API_BLOCKCHAIN_INFO: 'https://api.staging.blockchain.info/',
-//   API_CODE: '1770d5d9-bcea-4d28-ad21-6cbd5be018a8',
-//   WALLET_DATA_PATH: 'wallet.data',
-//   WALLET_PAYLOAD_PATH: 'wallet.payload',
-//   WALLET_SETTINGS_PATH: 'wallet.settings',
-//   WALLET_OPTIONS_PATH: 'wallet.options',
-//   WALLET_KVSTORE_PATH: 'wallet.kvstore',
-//   NETWORK_BITCOIN: Bitcoin.networks.bitcoin
-// }
