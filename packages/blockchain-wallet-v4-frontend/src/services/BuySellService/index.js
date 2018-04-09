c0ac1796020f88b7ad21eafd006d963200ea376c
plondon <philip@blockchain.com>
2018-04-09 15:34:23 -0400

    Remove api/options/index.js

--- packages/blockchain-wallet-v4-frontend/src/services/BuySellService/index.js
+++ packages/blockchain-wallet-v4-frontend/src/services/BuySellService/index.js
@@ -1,16 +1,13 @@
 import { concat } from 'ramda'
-import { Network } from 'blockchain-wallet-v4/src'
-
-const options = Network.Options().getWalletOptions()
 
 export const partnerCountryWhitelist = () => {
-  const sfox = options.platforms.web.sfox.countries
-  const unocoin = options.platforms.web.unocoin.countries
-  const coinify = options.platforms.web.coinify.countries
+  const sfox = []
+  const unocoin = []
+  const coinify = []
   return concat(concat(sfox, unocoin), coinify)
 }
 
-export const sfoxWhitelist = options.platforms.web.sfox.states
-export const sfoxCountries = options.platforms.web.sfox.countries
-export const unocoinCountries = options.platforms.web.unocoin.countries
-export const coinifyCountries = options.platforms.web.coinify.countries
+export const sfoxWhitelist = []
+export const sfoxCountries = []
+export const unocoinCountries = []
+export const coinifyCountries = []
--- packages/blockchain-wallet-v4/src/network/index.js
+++ packages/blockchain-wallet-v4/src/network/index.js
@@ -2,4 +2,3 @@
 export { default as createApi } from './api'
 export { default as createWalletApi } from './walletApi'
 export { default as Socket } from './Socket'
-export { default as Options } from './api/options'
