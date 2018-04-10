0a998091c0dac6548195e59abc2a381014b18e04
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-04-10 14:23:29 +0100

    refactoring - remove webpack aliases

--- packages/blockchain-wallet-v4-frontend/webpack.config.js
+++ packages/blockchain-wallet-v4-frontend/webpack.config.js
@@ -32,25 +32,23 @@ module.exports = {
     chunkFilename: '[name].[chunkhash:10].js',
     publicPath: '/'
   },
-  resolve: {
-    alias: {
-      'img': PATHS.src + '/assets/img',
-      'locales': PATHS.src + '/assets/locales',
-      'sass': PATHS.src + '/assets/sass',
-      'components': PATHS.src + '/components',
-      'config': PATHS.src + '/config',
-      'data': PATHS.src + '/data',
-      'layouts': PATHS.src + '/layouts',
-      'middleware': PATHS.src + '/middleware',
-      'modals': PATHS.src + '/modals',
-      'providers': PATHS.src + '/providers',
-      'scenes': PATHS.src + '/scenes',
-      'services': PATHS.src + '/services',
-      'store': PATHS.src + '/store',
-      'themes': PATHS.src + '/themes'
-    },
-    symlinks: false
-  },
+  // resolve: {
+  //   alias: {
+  //     'locales': PATHS.src + '/assets/locales',
+  //     'components': PATHS.src + '/components',
+  //     'config': PATHS.src + '/config',
+  //     'data': PATHS.src + '/data',
+  //     'layouts': PATHS.src + '/layouts',
+  //     'middleware': PATHS.src + '/middleware',
+  //     'modals': PATHS.src + '/modals',
+  //     'providers': PATHS.src + '/providers',
+  //     'scenes': PATHS.src + '/scenes',
+  //     'services': PATHS.src + '/services',
+  //     'store': PATHS.src + '/store',
+  //     'test': PATHS.tests
+  //   },
+  //   symlinks: false
+  // },
   module: {
     rules: [
       (isProdBuild ? {
@@ -114,7 +112,7 @@ module.exports = {
       'process.env': { 'NODE_ENV': JSON.stringify(buildEnvString) }
     }),
     ...(!isProdBuild ? [ new Webpack.HotModuleReplacementPlugin() ] : []),
-    ...(runBundleAnalyzer ? [new BundleAnalyzerPlugin({})] : [])
+    ...(runBundleAnalyzer ? [new BundleAnalyzerPlugin({})] : []),
   ],
   optimization: {
     namedModules: true,
