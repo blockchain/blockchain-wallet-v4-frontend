c68e8221d966b0f1c4e5de2b95d12119ee14e327
plondon <philip@blockchain.com>
2018-04-10 16:29:09 -0400

    Tx Feed | Transaction details always links to blockchain.info

--- packages/blockchain-wallet-v4-frontend/src/components/TransactionListItem/Confirmations/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/TransactionListItem/Confirmations/index.js
@@ -24,28 +24,34 @@ const IconWrapper = styled.div`
   & > :last-child { margin-left: 4px; }
 `
 
+const explorers = {
+  BTC: 'https://blockchain.info/tx',
+  ETH: 'https://etherscan.io/tx',
+  BCH: 'https://blockchair.com/bitcoin-cash/transaction'
+}
+
 const Confirmations = (props) => {
   return (
     <Wrapper>
       {props.confirmations >= props.minConfirmations ? (
         <Text size='12px' weight={300} color='received'>
-          <FormattedMessage id='scenes.transactions.bitcoin.content.pages.listitem.confirmation.confirmed' defaultMessage='Transaction confirmed' />
+          <FormattedMessage id='scenes.transactions.content.pages.listitem.confirmation.confirmed' defaultMessage='Transaction confirmed' />
         </Text>
       ) : (
         <Text size='12px' weight={300} color='gray-3'>
-          <FormattedMessage id='scenes.transactions.bitcoin.content.pages.listitem.confirmation.unconfirmed' defaultMessage={`Pending: ${props.confirmations}/${props.minConfirmations} confirmations`} />
+          <FormattedMessage id='scenes.transactions.content.pages.listitem.confirmation.unconfirmed' defaultMessage={`Pending: ${props.confirmations}/${props.minConfirmations} confirmations`} />
         </Text>
       )}
       <IconWrapper>
         {
           props.confirmations < props.minConfirmations && <TransactionTooltip>
             <Tooltip>
-              <FormattedMessage id='scenes.transactions.bitcoin.content.list.listitem.transaction_unconfirmed' defaultMessage='Your transaction will be complete after it has {minConfirmations} confirmations.' values={{minConfirmations: props.minConfirmations}} />
+              <FormattedMessage id='scenes.transactions.content.list.listitem.transaction_unconfirmed' defaultMessage='Your transaction will be complete after it has {minConfirmations} confirmations.' values={{minConfirmations: props.minConfirmations}} />
               <Link href='https://support.blockchain.com/hc/en-us/articles/217116406-Why-hasn-t-my-transaction-confirmed-yet-' target='_blank' size='12px' weight={300} altFont>Learn more.</Link>
             </Tooltip>
           </TransactionTooltip>
         }
-        <Link href={`${settings.ROOT_URL}tx/${props.hash}`} target='_blank'>
+        <Link href={`${explorers[props.coin]}/${props.hash}`} target='_blank'>
           <Icon name='up-arrow-in-circle' color='marketing-primary' cursor='pointer' size='17px' />
         </Link>
       </IconWrapper>
