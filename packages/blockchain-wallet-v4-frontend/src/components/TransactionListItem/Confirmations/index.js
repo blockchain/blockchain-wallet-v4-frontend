a892aa82f0c7908c6b45179fd6a78d87fc6e72fe
plondon <philip@blockchain.com>
2018-04-10 16:17:43 -0400

    Tx Feed | Ether confirmation helper should say '...12 confirmations' rather than 3

--- packages/blockchain-wallet-v4-frontend/src/components/TransactionListItem/Confirmations/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/TransactionListItem/Confirmations/index.js
@@ -40,7 +40,7 @@ const Confirmations = (props) => {
         {
           props.confirmations < props.minConfirmations && <TransactionTooltip>
             <Tooltip>
-              <FormattedMessage id='scenes.transactions.bitcoin.content.list.listitem.transaction_unconfirmed' defaultMessage='Your transaction will be complete after it has 3 confirmations.' />
+              <FormattedMessage id='scenes.transactions.bitcoin.content.list.listitem.transaction_unconfirmed' defaultMessage='Your transaction will be complete after it has {minConfirmations} confirmations.' values={{minConfirmations: props.minConfirmations}} />
               <Link href='https://support.blockchain.com/hc/en-us/articles/217116406-Why-hasn-t-my-transaction-confirmed-yet-' target='_blank' size='12px' weight={300} altFont>Learn more.</Link>
             </Tooltip>
           </TransactionTooltip>
