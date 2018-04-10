e9f24a38c2630c8668b1cef4fa48ff6c1ba81194
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-04-10 16:22:38 +0100

    fix - lint

--- packages/blockchain-wallet-v4-frontend/src/services/LocalesService/index.js
+++ packages/blockchain-wallet-v4-frontend/src/services/LocalesService/index.js
@@ -1,5 +1,5 @@
 import { addLocaleData } from 'react-intl'
-import { map, assoc, flatten, forEach } from 'ramda'
+import { map, flatten } from 'ramda'
 import * as languageService from 'services/LanguageService'
 
 import localesBG from 'assets/locales/bg.json'
@@ -22,20 +22,11 @@ import localesZH from 'assets/locales/zh.json'
 
 const importLocaleData = (language) => require(`react-intl/locale-data/${language}`)
 
-// const importTranslation = (language) => require(`locales/${language}`)
-
-// const appendToMessages = (key, value, messages) => { return assoc(key, value, messages) }
-
 function configureLocales (store) {
   // We add the locale data for each language
   addLocaleData(flatten(map(x => importLocaleData(x.language), languageService.languages)))
 
   // We get all the messages
-  // let messages = {}
-  // forEach(function (x) {
-  //   messages = assoc(x.language, importTranslation(x.language), messages)
-  // }, languageService.languages)
-
   const messages = {
     bg: localesBG,
     da: localesDA,
@@ -50,7 +41,7 @@ function configureLocales (store) {
     ja: localesJA,
     ko: localesKO,
     nl: localesNL,
-    nn: localesNL,
+    nn: localesNN,
     pl: localesPL,
     pt: localesPT,
     zh: localesZH
