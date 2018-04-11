ad898193bdcbfe60a31009f222af277fd0db023a
Pernas <jaume@pernas.cat>
2018-04-11 18:50:42 +0100

    fix merge

--- packages/blockchain-wallet-v4/src/redux/data/bch/sagas.js
+++ packages/blockchain-wallet-v4/src/redux/data/bch/sagas.js
@@ -2,15 +2,17 @@ import { call, select } from 'redux-saga/effects'
 
 import * as wS from '../../wallet/selectors'
 import * as signer from '../../../signer'
+import { futurizeP } from 'futurize'
+import Task from 'data.task'
 
 const taskToPromise = t => new Promise((resolve, reject) => t.fork(reject, resolve))
 
-export const bch = ({ api } = {}) => {
-  // const pushBchTx = futurizeP(Task)(api.pushBchTx)
+export default ({ api } = {}) => {
+  const pushBchTx = futurizeP(Task)(api.pushBchTx)
   const signAndPublish = function * ({ network, selection, password }) {
-    // const wrapper = yield select(wS.getWrapper)
-    // let signAndPublish = signer.bch.sign(network, password, wrapper, selection).chain(pushBchTx)
-    // return yield call(() => taskToPromise(signAndPublish))
+    const wrapper = yield select(wS.getWrapper)
+    let signAndPublish = signer.bch.sign(network, password, wrapper, selection).chain(pushBchTx)
+    return yield call(() => taskToPromise(signAndPublish))
   }
 
   return {
