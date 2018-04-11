a5b567ec133047dfeeb4a69ac0485bc775ccca2c
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-04-11 14:21:37 +0100

    Use cursor in dropdowns

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Bitcoin/OptionItem.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Bitcoin/OptionItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import styled from 'styled-components'
 import { Text } from 'blockchain-info-components'
 
+const ClickableText = styled(Text)`
+  cursor: pointer;
+`
+
 const OptionItem = ({ id, defaultMessage, onClick }) => (
-  <Text size='small' onClick={onClick}>
+  <ClickableText size='small' onClick={onClick}>
     <FormattedMessage id={id} defaultMessage={defaultMessage} />
-  </Text>
+  </ClickableText>
 )
 
 export default OptionItem
