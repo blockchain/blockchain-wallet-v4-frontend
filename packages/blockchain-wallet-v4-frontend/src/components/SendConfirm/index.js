b27cb4f8e085a2e48b28b975e07b341a6d632be3
TheLeoB <leora@blockchain.com>
2018-04-11 11:19:22 -0400

    extra send bitcoin/ether copy

--- packages/blockchain-wallet-v4-frontend/src/components/SendConfirm/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/SendConfirm/index.js
@@ -26,7 +26,7 @@ const Summary = styled.div`
   padding: 10px 0;
   margin: 5px 0;
   margin-bottom: 25px;
-  
+
   & > * { padding: 10px 0; }
 `
 const Footer = styled.div`
@@ -56,8 +56,8 @@ const SendConfirm = props => {
   return (
     <div>
       <div icon='send' onClose={props.closeAll}>
-        {coin === 'BTC' && <FormattedMessage id='modals.sendconfirm.title' defaultMessage='Send Bitcoin' />}
-        {coin === 'ETH' && <FormattedMessage id='modals.sendconfirm.title' defaultMessage='Send Ether' />}
+        {coin === 'BTC' }
+        {coin === 'ETH' }
       </div>
       <div>
         <Row>
