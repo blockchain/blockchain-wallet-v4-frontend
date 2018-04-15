ff09ce668e42fb0f821e36d635bdffb81f9a8d31
plondon <philip@blockchain.com>
2018-04-15 09:35:25 -0400

    Add Informational Banner and inline prop

--- packages/blockchain-info-components/stories/chapters/banners.js
+++ packages/blockchain-info-components/stories/chapters/banners.js
@@ -6,10 +6,11 @@ import Layout from '../components/layout'
 import { Banner } from '../../src'
 
 storiesOf('Banners', module)
-    .addDecorator(story => (<Layout>{story()}</Layout>))
-    .addDecorator((story, context) => withInfo({ text: 'Documentation', inline: true })(story)(context))
-    .add('Alert', () => <Banner type='alert'>This is an alert banner</Banner>)
-    .add('Default', () => <Banner>Hello, I'm a standard banner!</Banner>)
-    .add('Success', () => <Banner type='success'>Hooray, something worked!</Banner>)
-    .add('Warning', () => <Banner type='warning'>Something really bad happened</Banner>)
-    .add('Caution', () => <Banner type='caution'>Be careful, use caution with this action</Banner>)
+  .addDecorator(story => (<Layout>{story()}</Layout>))
+  .addDecorator((story, context) => withInfo({ text: 'Documentation', inline: true })(story)(context))
+  .add('Alert', () => <Banner type='alert'>This is an alert banner</Banner>)
+  .add('Default', () => <Banner>Hello, I'm a standard banner!</Banner>)
+  .add('Success', () => <Banner type='success'>Hooray, something worked!</Banner>)
+  .add('Warning', () => <Banner type='warning'>Something really bad happened</Banner>)
+  .add('Caution', () => <Banner type='caution'>Be careful, use caution with this action</Banner>)
+  .add('Informational', () => <Banner type='informational'>Watch Only</Banner>)
