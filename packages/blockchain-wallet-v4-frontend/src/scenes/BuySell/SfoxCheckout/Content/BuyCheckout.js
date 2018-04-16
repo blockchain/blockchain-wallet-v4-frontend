c3f9d2af8841fa37ec131af446b5a6a91f70de23
Philip Welber <welber@blockchain.com>
2018-04-16 13:45:33 -0400

    fix(SFOX): starts to add logic for incomplete signup

--- packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/SfoxCheckout/Content/BuyCheckout.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/SfoxCheckout/Content/BuyCheckout.js
@@ -27,51 +27,67 @@ const MethodContainer = styled.div`
   background-color: ${props => props.theme['white-blue']};
 `
 
-const BuyCheckout = ({ quoteR, account, onFetchQuote }) => (
-  <ExchangeCheckoutWrapper>
-    <Text style={spacing('ml-10')} size='16px' weight={600}>
-      <FormattedMessage id='buy.output_method.title' defaultMessage='I want to buy' />
-    </Text>
-    <MethodContainer>
-      <Icon name='bitcoin-in-circle-filled' color='bitcoin-orange' size='30px' />
-      <div style={{ ...flex('col'), ...spacing('ml-20') }}>
-        <Text size='14px' weight={300} uppercase>Bitcoin</Text>
-        <Text size='12px' weight={300}>
-          {'@ '}
-          {quoteR
-            .map((quote) => '$' + quote.rate)
-            .getOrElse(
-              <Fragment>
-                <FormattedMessage id='loading' defaultMessage='Loading' />
-                {'...'}
-              </Fragment>
-            )}
+const BuyCheckout = ({ quoteR, account, onFetchQuote, reason, finishAccountSetup }) => {
+  const submitButtonHelper = () => (
+    reason === 'has_remaining_buy_limit'
+      ? <StepTransition next Component={Button} style={spacing('mt-45')} nature='primary' fullwidth disabled={!Remote.Success.is(quoteR)}>
+        <FormattedMessage id='review_order' defaultMessage='Review Order' />
+      </StepTransition>
+      : <div style={{ ...flex('col'), ...spacing('mt-15') }}>
+        <Text size='14px' weight={300}>
+          You need to finish setting up your account before you can buy and sell.
         </Text>
+        <Button style={spacing('mt-15')} nature='primary' onClick={finishAccountSetup}>
+          Continue Where You Left Off
+        </Button>
       </div>
-    </MethodContainer>
-    <Text style={spacing('ml-10')} size='16px' weight={600}>
-      <FormattedMessage id='buy.input_method.title' defaultMessage='I will pay with' />
-    </Text>
-    <MethodContainer>
-      <Icon name='bank-filled' size='30px' />
-      <FundingSource account={account} />
-    </MethodContainer>
-    <Text style={spacing('ml-10')} size='16px' weight={600}>
-      <FormattedMessage id='amount' defaultMessage='Amount' />
-    </Text>
-    <div style={spacing('mt-15')}>
-      <QuoteInput
-        quoteR={quoteR}
-        initialAmount='250.00'
-        debounce={500}
-        spec={quoteInputSpec}
-        onFetchQuote={onFetchQuote}
-      />
-    </div>
-    <StepTransition next Component={Button} style={spacing('mt-45')} nature='primary' fullwidth disabled={!Remote.Success.is(quoteR)}>
-      <FormattedMessage id='review_order' defaultMessage='Review Order' />
-    </StepTransition>
-  </ExchangeCheckoutWrapper>
-)
+  )
+
+  return (
+    <ExchangeCheckoutWrapper>
+      <Text style={spacing('ml-10')} size='16px' weight={600}>
+        <FormattedMessage id='buy.output_method.title' defaultMessage='I want to buy' />
+      </Text>
+      <MethodContainer>
+        <Icon name='bitcoin-in-circle-filled' color='bitcoin-orange' size='30px' />
+        <div style={{ ...flex('col'), ...spacing('ml-20') }}>
+          <Text size='14px' weight={300} uppercase>Bitcoin</Text>
+          <Text size='12px' weight={300}>
+            {'@ '}
+            {quoteR
+              .map((quote) => '$' + quote.rate)
+              .getOrElse(
+                <Fragment>
+                  <FormattedMessage id='loading' defaultMessage='Loading' />
+                  {'...'}
+                </Fragment>
+              )}
+          </Text>
+        </div>
+      </MethodContainer>
+      <Text style={spacing('ml-10')} size='16px' weight={600}>
+        <FormattedMessage id='buy.input_method.title' defaultMessage='I will pay with' />
+      </Text>
+      <MethodContainer>
+        <Icon name='bank-filled' size='30px' />
+        <FundingSource account={account} />
+      </MethodContainer>
+      <Text style={spacing('ml-10')} size='16px' weight={600}>
+        <FormattedMessage id='amount' defaultMessage='Amount' />
+      </Text>
+      <div style={spacing('mt-15')}>
+        <QuoteInput
+          quoteR={quoteR}
+          initialAmount='250.00'
+          debounce={500}
+          spec={quoteInputSpec}
+          onFetchQuote={onFetchQuote}
+          disabled={reason}
+        />
+      </div>
+      { submitButtonHelper() }
+    </ExchangeCheckoutWrapper>
+  )
+}
 
 export default BuyCheckout
