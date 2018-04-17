86f4ee34ca51bb456aac666be3ebe8d83be35f43
schnogz <andrew@blockchain.com>
2018-04-17 10:02:34 -0400

    standardize domain urls with current wallet-options.json

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
-  ROOT_URL: 'https://blockchain.info/',
-  API_DOMAIN: 'https://api.blockchain.info/',
+  ROOT_URL: 'https://blockchain.info',
+  API_DOMAIN: 'https://api.blockchain.info',
   WEB_SOCKET_URL: 'wss://ws.blockchain.info/inv',
-  WALLET_HELPER_DOMAIN: 'https://wallet-helper.blockchain.info/'
+  WALLET_HELPER_DOMAIN: 'https://wallet-helper.blockchain.info'
 }
