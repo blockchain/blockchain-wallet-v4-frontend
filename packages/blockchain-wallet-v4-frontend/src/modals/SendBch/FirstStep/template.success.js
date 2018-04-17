299e3156630eae86ae71463f10e0decfefe1aaa8
schnogz <andrew@blockchain.com>
2018-04-17 16:40:29 -0400

    update settings subnav link names

--- packages/blockchain-wallet-v4-frontend/src/modals/SendBch/FirstStep/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/SendBch/FirstStep/template.success.js
@@ -45,7 +45,7 @@ const validAmount = (value, allValues, props) => parseFloat(value) <= props.effe
 const emptyAmount = (value, allValues, props) => !isEmpty(props.coins) ? undefined : 'Invalid amount. Account is empty.'
 
 const FirstStep = props => {
-  const { invalid, submitting, addressSelectToggled, addressSelectOpened, selection, fee, ...rest } = props
+  const { invalid, submitting, addressSelectToggled, addressSelectOpened, fee, ...rest } = props
   const { handleSubmit, handleClickAddressToggler } = rest
 
   return (
@@ -126,7 +126,6 @@ FirstStep.propTypes = {
   submitting: PropTypes.bool.isRequired,
   addressSelectToggled: PropTypes.bool.isRequired,
   addressSelectOpened: PropTypes.bool.isRequired,
-  // selection: PropTypes.object.isRequired,
   fee: PropTypes.number.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   handleClickAddressToggler: PropTypes.func.isRequired
