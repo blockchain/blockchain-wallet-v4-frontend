8918be029027ce395838f24ad5d58ba1113db910
Philip Welber <welber@blockchain.com>
2018-04-19 16:35:03 -0400

    feat(SFOX): remove unused code from sfox content template

--- packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/SfoxCheckout/Content/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/SfoxCheckout/Content/template.success.js
@@ -3,10 +3,9 @@ import { filter } from 'ramda'
 import styled from 'styled-components'
 import OrderHistory from '../../OrderHistory'
 import { Text } from 'blockchain-info-components'
-import ExchangeCheckout from '../../ExchangeCheckout'
 import { determineStep, determineReason } from 'services/SfoxService'
 import { flex } from 'services/StyleService'
-import { FormattedMessage, FormattedHTMLMessage } from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 import { Remote } from 'blockchain-wallet-v4/src'
 import Stepper, { StepView } from 'components/Utilities/Stepper'
 import BuyCheckout from './BuyCheckout'
@@ -38,42 +37,8 @@ const OrderHistoryContent = styled.div`
 const isPending = (t) => t.state === 'processing'
 const isCompleted = (t) => t.state !== 'processing'
 
-const ContinueButton = props => {
-  const { step, type } = props
-
-  switch (true) {
-    case step === 'verify': return <FormattedMessage id='scenes.buysell.sfoxcheckout.verify.button' defaultMessage='Continue Where You Left Off' />
-    case step === 'upload': return <FormattedMessage id='scenes.buysell.sfoxcheckout.upload.button' defaultMessage='Continue Where You Left Off' />
-    case step === 'funding': return <FormattedMessage id='scenes.buysell.sfoxcheckout.funding.button' defaultMessage='Continue Where You Left Off' />
-    case step === 'verified' && type === 'buy': return <FormattedMessage id='scenes.buysell.sfoxcheckout.verified.buy_bitcoin_button' defaultMessage='Buy Bitcoin' />
-    case step === 'verified' && type === 'sell': return <FormattedMessage id='scenes.buysell.sfoxcheckout.verified.sell_bitcoin_button' defaultMessage='Sell Bitcoin' />
-    default: return null
-  }
-}
-
-const RequiredMsg = props => {
-  const { step } = props
-
-  switch (true) {
-    case step === 'verify': return <FormattedMessage id='scenes.buysell.sfoxcheckout.verify.message' defaultMessage='You need to finish personaling your account before you can buy and sell.' />
-    case step === 'upload': return <FormattedMessage id='scenes.buysell.sfoxcheckout.upload.message' defaultMessage='You need to finish personaling your account before you can buy and sell.' />
-    case step === 'funding': return <FormattedMessage id='scenes.buysell.sfoxcheckout.funding.message' defaultMessage='You need to finish linking your bank account before you can buy and sell.' />
-    default: return null
-  }
-}
-
-const ReasonMsg = props => {
-  const { reason, limit } = props
-  switch (true) {
-    case reason === 'has_remaining_buy_limit': return <FormattedHTMLMessage id='scenes.buysell.sfoxcheckout.buy.remaining_limit' defaultMessage='Your remaining buy limit is <span class="link">{limit} USD.</span>' values={{limit}} />
-    case reason === 'has_remaining_sell_limit': return <FormattedHTMLMessage id='scenes.buysell.sfoxcheckout.sell.remaining_limit' defaultMessage='Your remaining sell limit is <span class="link">{limit} USD.</span>' values={{limit}} />
-    default: return <FormattedHTMLMessage id='placeholder' defaultMessage='&nbsp;' />
-  }
-}
-
 const Success = props => {
   const { changeBuySellTabStatus, fetchQuote, fetchSellQuote, refreshQuote, submitQuote, submitSellQuote, handleTrade, quoteR, sellQuoteR, base, errors, showModal, handleTradeDetailsClick, ...rest } = props
-  const quote = quoteR.getOrElse(null)
 
   const accounts = Remote.of(props.value.accounts).getOrElse([])
   const profile = Remote.of(props.value.profile).getOrElse({ account: { verification_status: {} }, limits: { buy: 0, sell: 0 } })
@@ -82,10 +47,6 @@ const Success = props => {
   const { trades, type, busy } = rest
   const step = determineStep(profile, verificationStatus, accounts)
   const reason = determineReason(type, profile, verificationStatus, accounts)
-  const onSubmit = (e) => {
-    e.preventDefault()
-    step === 'verified' ? handleTrade(quote) : showModal('SfoxExchangeData', { step })
-  }
   const finishAccountSetup = () => showModal('SfoxExchangeData', { step })
 
   const limits = {
@@ -173,26 +134,6 @@ const Success = props => {
         </StepView>
       </Stepper>
     )
-    // return (
-    //   <CheckoutWrapper>
-    //     <ExchangeCheckout
-    //       fiatLimits
-    //       base={base}
-    //       quote={quote}
-    //       errors={errors}
-    //       fiat={'USD'}
-    //       crypto={'BTC'}
-    //       accounts={accounts}
-    //       onSubmit={onSubmit}
-    //       fetchQuote={fetchQuote}
-    //       limits={limits[type]}
-    //       showRequiredMsg={step !== 'verified'}
-    //       requiredMsg={<RequiredMsg step={step} type={type} />}
-    //       continueButton={<ContinueButton step={step} type={type} />}
-    //       reasonMsg={<ReasonMsg reason={reason} limit={profile.limits[type]} />}
-    //     />
-    //   </CheckoutWrapper>
-    // )
   } else if (trades) {
     return (
       <OrderHistoryWrapper>
