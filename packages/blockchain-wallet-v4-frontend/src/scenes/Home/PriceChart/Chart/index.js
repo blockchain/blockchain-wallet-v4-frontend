ed96e962d2b9947864ce678e3822c95ef20131f1
plondon <philip@blockchain.com>
2018-04-19 09:59:29 -0400

    Use saved chart data and config

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/PriceChart/Chart/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/PriceChart/Chart/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actions } from 'data'
+import { isNil } from 'ramda'
 import { getData } from './selectors'
 
 import Error from './template.error'
@@ -10,18 +11,35 @@ import Success from './template.success'
 
 export class ChartContainer extends React.PureComponent {
   componentDidMount () {
-    this.props.actions.initialized('BTC', 'all')
+    const config = JSON.parse(window.localStorage.getItem('ls.chart'))
+    const coin = config ? config.base.toUpperCase() : 'BTC'
+    const time = config ? config.time : '1month'
+    this.props.actions.initialized(coin, time)
   }
 
   render () {
     return this.props.data.cata({
-      Success: (value) => <Success
-        currency={value.currency}
-        coin={value.coin}
-        time={value.time}
-        data={value.data}
-      />,
-      Failure: (message) => <Error>{message}</Error>,
+      Success: (value) => {
+        window.localStorage.setItem('ls.chart-data', JSON.stringify(value))
+        window.localStorage.setItem('ls.chart', JSON.stringify({ base: value.coin, time: value.time }))
+        return (<Success
+          currency={value.currency}
+          coin={value.coin}
+          time={value.time}
+          data={value.data}
+        />)
+      },
+      Failure: (message) => {
+        const value = JSON.parse(window.localStorage.getItem('ls.chart-data'))
+        return isNil(value)
+          ? (<Error>{message}</Error>)
+          : (<Success
+            currency={value.currency}
+            coin={value.coin}
+            time={value.time}
+            data={value.data}
+          />)
+      },
       Loading: () => <Loading />,
       NotAsked: () => <Loading />
     })
