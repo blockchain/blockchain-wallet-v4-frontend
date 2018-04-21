ce82ff5a08d6733b51f824867762dc87a8e1f7cb
plondon <philip@blockchain.com>
2018-04-21 08:25:37 -0400

    Addresses | Search bar is missing

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Btc/Wallets/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Btc/Wallets/template.success.js
@@ -5,7 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap'
 import SwitchableDisplay from 'components/Display/SwitchableDisplay'
 import { SettingDescription, SettingHeader } from 'components/Setting'
 import { Table, TableHeader, TableCell, TableRow, Text, IconButton, Link } from 'blockchain-info-components'
-import { any, filter } from 'ramda'
+import { filter } from 'ramda'
 
 const Wrapper = styled.section`
   box-sizing: border-box;
@@ -16,7 +16,6 @@ const BitcoinWalletsAddressesSettingHeader = SettingHeader.extend`
 
 const Success = ({ wallets, handleClick, onUnarchive, search }) => {
   const isMatch = (wallet) => !search || wallet.label.toLowerCase().indexOf(search) > -1
-  const anyMatch = any(isMatch, wallets)
 
   const walletTableRows = filter(isMatch, wallets).map((wallet) => {
     return (
@@ -56,20 +55,18 @@ const Success = ({ wallets, handleClick, onUnarchive, search }) => {
         <FormattedMessage id='scenes.settings.addresses.bitcoin_wallets_description' defaultMessage='Wallets are a way of organizing your funds. Common ways to organize your funds include dividing them up into categories like spending, savings, or business related expenses. Your wallet automatically manages your bitcoin addresses for you by generating a new one each time you need one to receive a payment. You can click on Manage to the right of a wallet to see all of the individual addresses that have been generated for that specific wallet.' />
       </SettingDescription>
       <Table>
-        {
-          anyMatch && <TableHeader>
-            <TableCell width='40%'>
-              <Text size='13px' weight={500} capitalize>
-                <FormattedMessage id='scenes.settings.addresses.wallet_name' defaultMessage='Wallet Name' />
-              </Text>
-            </TableCell>
-            <TableCell width='40%'>
-              <Text size='13px' weight={500} capitalize>
-                <FormattedMessage id='scenes.settings.addresses.wallet_description' defaultMessage='Balance' />
-              </Text>
-            </TableCell>
-          </TableHeader>
-        }
+        <TableHeader>
+          <TableCell width='40%'>
+            <Text size='13px' weight={500} capitalize>
+              <FormattedMessage id='scenes.settings.addresses.wallet_name' defaultMessage='Wallet Name' />
+            </Text>
+          </TableCell>
+          <TableCell width='40%'>
+            <Text size='13px' weight={500} capitalize>
+              <FormattedMessage id='scenes.settings.addresses.wallet_description' defaultMessage='Balance' />
+            </Text>
+          </TableCell>
+        </TableHeader>
         { walletTableRows }
       </Table>
       <IconButton style={{ marginTop: 10 }} name='up-arrow-in-circle' onClick={handleClick}>
