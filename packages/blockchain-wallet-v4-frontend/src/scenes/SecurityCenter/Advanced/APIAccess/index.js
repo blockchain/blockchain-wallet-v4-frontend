f8fe04b85e11dd361983879bcacb347d45a1795e
TheLeoB <leora@blockchain.com>
2018-04-23 09:08:52 -0400

    linter

--- packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/Advanced/APIAccess/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/Advanced/APIAccess/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Link } from 'blockchain-info-components'
-import { SettingComponent, SettingContainer, SettingDescription, SettingHeader, SettingSummary } from 'components/Setting'
+import { SettingContainer, SettingDescription, SettingHeader, SettingSummary } from 'components/Setting'
 
 const APIAccess = (props) => {
   return (
@@ -15,7 +15,7 @@ const APIAccess = (props) => {
           <Link margin-right='0px' href='https://blockchain.info/api' target='_blank' rel='noopener noreferrer' size='13px' weight={300}>
             <FormattedMessage id='scenes.securitysettings.apiaccess.getstarted' defaultMessage='here' />
           </Link>
-            <FormattedMessage id='scenes.securitysettings.apiaccess.description2' defaultMessage='to get started.' />
+          <FormattedMessage id='scenes.securitysettings.apiaccess.description2' defaultMessage='to get started.' />
         </SettingDescription>
       </SettingSummary>
     </SettingContainer>
