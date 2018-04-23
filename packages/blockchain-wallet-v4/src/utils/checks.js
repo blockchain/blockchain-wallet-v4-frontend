7aaa9668ad09500766525120634e888feab0c57d
Pernas <jaume@pernas.cat>
2018-04-23 13:02:18 +0100

    Fix selecting account 0 on send screens

--- packages/blockchain-wallet-v4/src/utils/checks.js
+++ packages/blockchain-wallet-v4/src/utils/checks.js
@@ -5,7 +5,7 @@ export const isNumber = is(Number)
 
 export const isInteger = (x) => isNumber(x) && x % 1 === 0
 
-export const isPositiveInteger = (x) => isInteger(x) && x > 0
+export const isPositiveInteger = (x) => isInteger(x) && x >= 0
 
 export const isPositiveNumber = (x) => isNumber(x) && x >= 0
 
