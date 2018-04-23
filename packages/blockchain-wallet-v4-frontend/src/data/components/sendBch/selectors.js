9623be4a761b62dc9c1499b7c4e92f82633eba98
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-04-23 18:42:09 +0100

    Linting

--- packages/blockchain-wallet-v4-frontend/src/data/components/sendBch/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/sendBch/selectors.js
@@ -5,4 +5,3 @@ export const getStep = path(['components', 'sendBch', 'step'])
 export const getPayment = path(['components', 'sendBch', 'payment'])
 
 export const getToToggled = path(['components', 'sendBch', 'toToggled'])
-
