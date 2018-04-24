e420c5726c35a2f1558f67e426dddcf2dff68c51
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-04-24 15:41:19 +0100

    Fix btc payment core sagas test

--- packages/blockchain-wallet-v4/src/redux/payment/btc/sagas.spec.js
+++ packages/blockchain-wallet-v4/src/redux/payment/btc/sagas.spec.js
@@ -1,26 +1,26 @@
-// import { call } from 'redux-saga/effects'
-// import createPaymentFactory from './createPayment'
+import { call } from 'redux-saga/effects'
+import createPaymentFactory from './sagas'
 
-// const feeResult = {
-//   'limits': {
-//     'min': 2,
-//     'max': 9
-//   },
-//   'priority': 6,
-//   'regular': 5
-// }
+const feeResult = {
+  'limits': {
+    'min': 2,
+    'max': 9
+  },
+  'priority': 6,
+  'regular': 5
+}
 
-// describe('createPayment', () => {
-//   let api = { getBitcoinFee: () => {} }
-//   let createPayment = createPaymentFactory({ api })
-//   let payment = createPayment()
+describe('createPayment', () => {
+  let api = { getBitcoinFee: () => {} }
+  let { create } = createPaymentFactory({ api })
+  let payment = create()
 
-//   describe('*init', () => {
-//     it('should fetch fee values and set to value.fees', () => {
-//       let gen = payment.init()
-//       expect(gen.next().value).toEqual(call(api.getBitcoinFee))
-//       expect(gen.next(feeResult).value.value().fees).toEqual(feeResult)
-//       expect(gen.next().done).toEqual(true)
-//     })
-//   })
-// })
+  describe('*init', () => {
+    it('should fetch fee values and set to value.fees', () => {
+      let gen = payment.init()
+      expect(gen.next().value).toEqual(call(api.getBitcoinFee))
+      expect(gen.next(feeResult).value.value().fees).toEqual(feeResult)
+      expect(gen.next().done).toEqual(true)
+    })
+  })
+})
