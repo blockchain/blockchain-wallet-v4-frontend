c88765a71e30fc766ef88216808e351298d3e85e
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-04-24 17:05:00 +0100

    Fix build

--- packages/blockchain-wallet-v4-frontend/src/components/Form/SelectBoxBitcoinAddresses/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Form/SelectBoxBitcoinAddresses/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { concat, identity } from 'ramda'
+import { concat } from 'ramda'
 
 import { getData } from './selectors'
 import SelectBox from '../SelectBox'
