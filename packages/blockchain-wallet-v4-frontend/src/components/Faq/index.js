0d6911c03739135460c7a68ad8fe9411867db5cd
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-04-26 13:10:38 +0100

    Repair FAQ search throughout all sections

--- packages/blockchain-wallet-v4-frontend/src/components/Faq/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Faq/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { any, contains, path, toLower } from 'ramda'
 import { selectors } from 'data'
 import FaqContent from './FaqContent'
 
@@ -23,11 +25,25 @@ class FaqContainer extends React.PureComponent {
   render () {
     const { faqContent, handleTrayRightToggle } = this.props
 
-    // Bad WIP
-    faqContent[0].groupQuestions = this.state.filterText ? faqContent[0].groupQuestions.filter((q) =>
-      q.question.toLowerCase().indexOf(this.state.filterText.toLowerCase()) !== -1 ||
-      q.answer.toLowerCase().indexOf(this.state.filterText.toLowerCase()) !== -1
-    ) : faqContent[0].groupQuestions
+    // Search for matching messages in the component subtree starting
+    const containsRecursive = (x) => {
+      const lowerFilterText = toLower(this.state.filterText)
+      if (path(['props', 'defaultMessage'], x)) {
+        return contains(lowerFilterText, toLower(this.context.intl.messages[x.props.id] || x.props.defaultMessage))
+      } else if (path(['props', 'children'], x)) {
+        return any(containsRecursive, path(['props', 'children'], x))
+      } else {
+        return false
+      }
+    }
+
+    faqContent.map(
+      contentPart => {
+        contentPart.groupQuestions = this.state.filterText ? contentPart.groupQuestions.filter((q) =>
+          containsRecursive(q.question) || containsRecursive(q.answer)
+        ) : contentPart.groupQuestions
+      }
+    )
 
     return (
       <Faq filteredContent={faqContent} onFilter={this.onFilter} filterText={this.state.filterText} handleTrayRightToggle={handleTrayRightToggle}/>
@@ -39,5 +55,9 @@ const mapStateToProps = (state) => ({
   countryCode: selectors.core.settings.getCountryCode(state),
   faqContent: FaqContent
 })
+
+FaqContainer.contextTypes = {
+  intl: PropTypes.object.isRequired
+}
 
 export default connect(mapStateToProps, undefined)(FaqContainer)
