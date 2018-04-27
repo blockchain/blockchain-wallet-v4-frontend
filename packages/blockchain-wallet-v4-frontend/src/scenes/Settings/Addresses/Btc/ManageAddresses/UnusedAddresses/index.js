0dfdedc776f396a371fbe7e22b131219f39b0cdc
schnogz <andrew@blockchain.com>
2018-04-27 11:26:51 -0400

    add search capability to unused addresses

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Btc/ManageAddresses/UnusedAddresses/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Btc/ManageAddresses/UnusedAddresses/index.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { length } from 'ramda'
 import PropTypes from 'prop-types'
+import { formValueSelector } from 'redux-form'
 
 import { actions, selectors } from 'data'
 import { Types } from 'blockchain-wallet-v4'
@@ -11,10 +12,8 @@ import UnusedAddressesTemplate from './template'
 
 class UnusedAddressesContainer extends React.PureComponent {
   render () {
-    const { account, labels, receiveIndex, isDefault, coreActions, walletActions, modalsActions, routerActions } = this.props
-
+    const { account, labels, receiveIndex, isDefault, coreActions, walletActions, modalsActions, routerActions, search } = this.props
     const deriveAddress = (i) => Types.HDAccount.getReceiveAddress(account, i, settings.NETWORK_BITCOIN)
-
     const onSetLabel = (i, label) => {
       if (length(labels) >= 15) {
         this.props.alertActions.displayError('You cannot label more than 15 unused addresses in a row; please send some bitcoins to at least one of them.')
@@ -31,7 +30,8 @@ class UnusedAddressesContainer extends React.PureComponent {
       coreActions.setAccountArchived(account.index, true)
       routerActions.push('/settings/addresses')
     }
-    const props = { account, labels, receiveIndex, isDefault, deriveAddress, onSetLabel, onEditLabel, onDeleteLabel, onEditBtcAccountLabel, onShowXPub, onMakeDefault, onSetArchived }
+    const props = { account, labels, receiveIndex, isDefault, deriveAddress, onSetLabel, onEditLabel, onDeleteLabel, onEditBtcAccountLabel, onShowXPub, onMakeDefault, onSetArchived, search }
+
     return <UnusedAddressesTemplate {...props} />
   }
 }
@@ -47,8 +47,9 @@ const mapStateToProps = (state, props) => {
   const nextReceiveIndex = selectors.core.data.bitcoin.getReceiveIndex(account.xpub, state)
   const lastLabeledIndex = labels.reduce((acc, l) => Math.max(acc, l.index), 0)
   const receiveIndex = nextReceiveIndex.map(i => Math.max(i, lastLabeledIndex + 1))
+  const search = formValueSelector('manageAddresses')(state, 'search')
 
-  return { account, labels, receiveIndex, isDefault }
+  return { account, labels, receiveIndex, isDefault, search }
 }
 
 const mapDispatchToProps = (dispatch) => ({
