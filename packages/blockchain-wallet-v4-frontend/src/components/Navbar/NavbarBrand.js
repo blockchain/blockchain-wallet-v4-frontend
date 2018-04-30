fe582ca3b2bcc17675ac03f56d9add4585436517
plondon <philip@blockchain.com>
2018-04-30 12:57:17 +0100

    feat(Mobile): make header responsive

--- packages/blockchain-wallet-v4-frontend/src/components/Navbar/NavbarBrand.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Navbar/NavbarBrand.js
@@ -2,10 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 const BaseBrand = styled.div`
+  display: flex;
   padding-left: 25px;
-  > a {
-    height: 20px;
-    display: block;
+  @media (min-width: 768px) {
+    > span:first-child {
+      display: none;
+    }
   }
 `
 
