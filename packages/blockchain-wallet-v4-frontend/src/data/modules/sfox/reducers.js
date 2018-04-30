4e519b6e56100cc237645384fe6fcf1cf5009878
Philip Welber <welber@blockchain.com>
2018-04-30 17:22:53 +0100

    fix(SFOX): use Remote type for busy/errors on signup step

--- packages/blockchain-wallet-v4-frontend/src/data/modules/sfox/reducers.js
+++ packages/blockchain-wallet-v4-frontend/src/data/modules/sfox/reducers.js
@@ -3,7 +3,7 @@ import { assoc } from 'ramda'
 import { Remote } from 'blockchain-wallet-v4/src'
 
 const INITIAL_STATE = {
-  order: Remote.NotAsked
+  sfoxBusy: Remote.NotAsked
 }
 
 const sfoxSignup = (state = INITIAL_STATE, action) => {
@@ -13,26 +13,20 @@ const sfoxSignup = (state = INITIAL_STATE, action) => {
     case AT.NEXT_STEP: {
       return assoc('signupStep', payload, state)
     }
-    case AT.SFOX_SIGNUP_FAILURE: {
-      return assoc('signupError', payload, state)
-    }
-    case AT.CLEAR_SIGNUP_ERROR: {
-      return assoc('signupError', null, state)
-    }
     case AT.SET_VERIFY_ERROR: {
       return assoc('verifyError', payload, state)
     }
-    case AT.ORDER_NOT_ASKED: {
-      return assoc('order', Remote.NotAsked, state)
+    case AT.SFOX_NOT_ASKED: {
+      return assoc('sfoxBusy', Remote.NotAsked, state)
     }
-    case AT.ORDER_LOADING: {
-      return assoc('order', Remote.Loading, state)
+    case AT.SFOX_LOADING: {
+      return assoc('sfoxBusy', Remote.Loading, state)
     }
-    case AT.ORDER_SUCCESS: {
-      return assoc('order', Remote.Success(payload), state)
+    case AT.SFOX_SUCCESS: {
+      return assoc('sfoxBusy', Remote.Success(payload), state)
     }
-    case AT.ORDER_FAILURE: {
-      return assoc('order', Remote.Failure(payload), state)
+    case AT.SFOX_FAILURE: {
+      return assoc('sfoxBusy', Remote.Failure(payload), state)
     }
     default:
       return state
