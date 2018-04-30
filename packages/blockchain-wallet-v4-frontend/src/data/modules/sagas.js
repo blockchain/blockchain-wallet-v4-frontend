1f3d73c7e22a68c93de3b68449dd9e19187dae9d
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-04-30 14:20:59 +0100

    refactor - removed unused actions/sagas

--- packages/blockchain-wallet-v4-frontend/src/data/modules/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/modules/sagas.js
@@ -6,7 +6,6 @@ import settings from './settings/sagas'
 import securityCenter from './securityCenter/sagas'
 import transferEther from './transferEther/sagas'
 import sfox from './sfox/sagas'
-import shapeshiftHistory from './shapeshiftHistory/sagas'
 
 export default ({ coreSagas }) => function * () {
   yield all([
@@ -15,7 +14,6 @@ export default ({ coreSagas }) => function * () {
     fork(settings({ coreSagas })),
     fork(securityCenter({ coreSagas })),
     fork(transferEther({ coreSagas })),
-    fork(sfox({ coreSagas })),
-    fork(shapeshiftHistory({ coreSagas }))
+    fork(sfox({ coreSagas }))
   ])
 }
