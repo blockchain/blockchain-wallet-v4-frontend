af869280aa058a758639cbe4a13590a8c559a5c9
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-05-02 15:20:50 +0100

    refactor - expose sagas in core

--- packages/blockchain-wallet-v4/src/redux/kvStore/ethereum/sagas.js
+++ packages/blockchain-wallet-v4/src/redux/kvStore/ethereum/sagas.js
@@ -1,20 +1,24 @@
-
-import { call, put, select, takeLatest } from 'redux-saga/effects'
+import { call, put, select } from 'redux-saga/effects'
 import { compose, isNil } from 'ramda'
 import { set } from 'ramda-lens'
 import * as A from './actions'
-import * as AT from './actionTypes'
 import { KVStoreEntry } from '../../../types'
 import { getMetadataXpriv } from '../root/selectors'
 import { derivationMap, ETHEREUM } from '../config'
-import * as eth from '../../../utils/ethereum'
-import { getMnemonic } from '../../wallet/selectors'
 
 const taskToPromise = t => new Promise((resolve, reject) => t.fork(reject, resolve))
 
-export default ({ api }) => {
+export default ({ api } = {}) => {
   const callTask = function * (task) {
     return yield call(compose(taskToPromise, () => task))
+  }
+
+  const fetchEthereum = function * () {
+    const typeId = derivationMap[ETHEREUM]
+    const mxpriv = yield select(getMetadataXpriv)
+    const kv = KVStoreEntry.fromMetadataXpriv(mxpriv, typeId)
+    const newkv = yield callTask(api.fetchKVStore(kv))
+    yield put(A.setEthereum(newkv))
   }
 
   const createEthereum = function * ({ kv, password }) {
@@ -37,7 +41,7 @@ export default ({ api }) => {
         legacy_account: null,
         last_tx: undefined
       }
-      const newkv = set(KVStoreEntry.value, {ethereum}, kv)
+      const newkv = set(KVStoreEntry.value, { ethereum }, kv)
       yield put(A.createMetadataEthereum(newkv))
     } catch (e) {
       throw new Error('[NOT IMPLEMENTED] MISSING_SECOND_PASSWORD in core.createEthereum saga')
@@ -65,7 +69,9 @@ export default ({ api }) => {
     }
   }
 
-  return function * () {
-    yield takeLatest(AT.FETCH_METADATA_ETHEREUM, fetchMetadataEthereum)
+  return {
+    createEthereum,
+    fetchEthereum,
+    fetchMetadataEthereum
   }
 }
