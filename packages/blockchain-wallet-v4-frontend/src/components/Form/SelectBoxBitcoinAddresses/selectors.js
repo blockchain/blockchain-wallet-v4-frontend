f6ab25897a91b7f048eedb65115ede3f27b16b92
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-05-02 16:44:52 +0100

    refactor(selectboxbitcoin template)

--- packages/blockchain-wallet-v4-frontend/src/components/Form/SelectBoxBitcoinAddresses/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Form/SelectBoxBitcoinAddresses/selectors.js
@@ -1,22 +1,9 @@
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
 import { assoc, assocPath, compose, concat, lift, map, path, prop, sequence } from 'ramda'
-import { Banner, Text } from 'blockchain-info-components'
-import { Exchange, Remote } from 'blockchain-wallet-v4/src'
+import { Remote } from 'blockchain-wallet-v4/src'
 import { selectors } from 'data'
 
 export const getData = (state, coin) => {
   const toDropdown = map(x => ({ text: x.label, value: x }))
-
-  const formatAddressesData = (addressesData) => addressesData.map(a => a.data.map(d => {
-    d.text = <Text weight={300} size='12px'>
-      {d.text} ({Exchange.displayCoinToCoin({ value: d.value.balance, coin: d.value.coin, baseToStandard: true })})
-      {path(['value', 'watchOnly'], d) &&
-        <Banner type='informational' inline>
-          <FormattedMessage id='components.selectboxbitcoin.watchonly' defaultMessage='Watch Only'/>
-        </Banner>}
-    </Text>
-  }))
 
   const formatAddress = (addressData) => {
     const formattedAddress = {}
@@ -51,7 +38,7 @@ export const getData = (state, coin) => {
   }
 
   const addressesData = getAddressesData(coin)
-  formatAddressesData(addressesData)
+  // formatAddressesData(addressesData)
 
   return addressesData
 }
