088e0723cfb90689e1777cb97ff7ce0d3f8dbf6a
schnogz <andrew@blockchain.com>
2018-05-02 15:23:16 +0100

    saga tests mocks start

--- packages/blockchain-wallet-v4-frontend/src/data/components/priceChart/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/priceChart/sagas.js
@@ -1,43 +1,62 @@
-import { select, put } from 'redux-saga/effects'
+import * as effects from 'redux-saga/effects'
 import * as S from './selectors'
 import * as actions from '../../actions'
 import { calculateStart, calculateScale } from 'services/ChartService'
 
+/**
+ * Price Chart Sagas
+ * @return {Object} price chart sagas
+ */
 export default ({ coreSagas }) => {
-  // initialize priceChart component
+  /**
+   * @desc initialize priceChart component and fetches price data based on coin and time
+   * @property {action} foo this is description.
+   */
   const initialized = function * (action) {
     try {
       const { coin, time } = action.payload
       const currency = 'USD'
       const start = calculateStart(coin, time)
       const scale = calculateScale(coin, time)
-      yield put(actions.core.data.misc.fetchPriceIndexSeries(coin, currency, start, scale))
+      yield effects.put(actions.core.data.misc.fetchPriceIndexSeries(coin, currency, start, scale))
     } catch (e) {
+      // TODO: create error wrapper!
+      console.log('Error in initialized saga')
       // yield put(actions.alerts.displayError('Price index series chart could not be initialized.'))
     }
   }
 
+  /**
+   * @desc click handler for priceChart component that fetches price data for new coin
+   * @property {action} foo this is description.
+   */
   const coinClicked = function * (action) {
     try {
       const { coin } = action.payload
       const currency = 'USD'
-      const time = yield select(S.getTime)
-      const start = calculateStart(coin, time)
-      const scale = calculateScale(coin, time)
-      yield put(actions.core.data.misc.fetchPriceIndexSeries(coin, currency, start, scale))
+      yield effects.select(S.getTime)
+      const start = calculateStart(coin, 'tt')
+      const scale = calculateScale(coin, '')
+      yield effects.put(actions.core.data.misc.fetchPriceIndexSeries(coin, currency, start, scale))
     } catch (e) {
+      // TODO: create error wrapper!
+      console.log('Error in coinClicked saga')
       // yield put(actions.alerts.displayError('Price index series chart could not be initialized.'))
     }
   }
 
+  /**
+   * @desc click handler for priceChart component that fetches price data for when time is changed
+   * @property {action} foo this is description.
+   */
   const timeClicked = function * (action) {
     try {
       const { time } = action.payload
       const currency = 'USD'
-      const coin = yield select(S.getCoin)
+      const coin = yield effects.select(S.getCoin)
       const start = calculateStart(coin, time)
       const scale = calculateScale(coin, time)
-      yield put(actions.core.data.misc.fetchPriceIndexSeries(coin, currency, start, scale))
+      yield effects.put(actions.core.data.misc.fetchPriceIndexSeries(coin, currency, start, scale))
     } catch (e) {
       // yield put(actions.alerts.displayError('Price index series chart could not be initialized.'))
     }
--- packages/blockchain-wallet-v4-frontend/src/data/components/priceChart/sagas.spec.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/priceChart/sagas.spec.js
@@ -1,20 +1,107 @@
 import sagas from './sagas'
 import * as actions from '../../actions'
 import * as chartService from 'services/ChartService'
+// import { testSaga } from 'redux-saga-test-plan'
+// import * as SE from './selectors'
+
+jest.mock('./selectors')
 
 describe('priceChart sagas', () => {
-  let calcStartSpy = jest.spyOn(chartService, 'calculateStart').mockReturnValueOnce('test')
-  let calcScaleSpy = jest.spyOn(chartService, 'calculateScale').mockReturnValueOnce('test')
-  let actionSpy = jest.spyOn(actions.core.data.misc, 'fetchPriceIndexSeries').mockImplementation(() => jest.fn())
+  let calcScaleSpy
+  let calcStartSpy
+  let fetchPriceActionSpy
+  let errorSpy
 
-  it('should initialize correctly', () => {
-    const saga = sagas({ api: {}, coreSagas: {} }).initialized({ type: 'whatever', payload: { coin: 'BTC', time: 'all' } })
-    saga.next()
-    expect(calcStartSpy).toHaveBeenCalledTimes(1)
-    expect(calcStartSpy).toHaveBeenCalledWith('BTC', 'all')
-    expect(calcScaleSpy).toHaveBeenCalledTimes(1)
-    expect(calcScaleSpy).toHaveBeenCalledWith('BTC', 'all')
-    expect(actionSpy).toHaveBeenCalledTimes(1)
-    expect(actionSpy).toHaveBeenCalledWith('BTC', 'USD', 'test', 'test')
+  beforeEach(() => {
+    calcStartSpy = jest.spyOn(chartService, 'calculateStart').mockReturnValueOnce('test')
+    calcScaleSpy = jest.spyOn(chartService, 'calculateScale').mockReturnValueOnce('test')
+    errorSpy = jest.spyOn(window.console, 'log').mockImplementation(() => jest.fn())
+    fetchPriceActionSpy = jest.spyOn(actions.core.data.misc, 'fetchPriceIndexSeries').mockImplementation(() => { return 't' })
   })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  describe('initialized saga', () => {
+    it('should run correct routine', () => {
+      // arrange
+      const saga = sagas({}).initialized({type: '', payload: {coin: 'BTC', time: 'all'}})
+
+      // act
+      saga.next()
+
+      // assert
+      expect(calcStartSpy).toHaveBeenCalledTimes(1)
+      expect(calcStartSpy).toHaveBeenCalledWith('BTC', 'all')
+      expect(calcScaleSpy).toHaveBeenCalledTimes(1)
+      expect(calcScaleSpy).toHaveBeenCalledWith('BTC', 'all')
+      expect(fetchPriceActionSpy).toHaveBeenCalledTimes(1)
+      expect(fetchPriceActionSpy).toHaveBeenCalledWith('BTC', 'USD', 'test', 'test')
+    })
+
+    it('should handle errors', () => {
+      // arrange
+      fetchPriceActionSpy = jest.spyOn(actions.core.data.misc, 'fetchPriceIndexSeries').mockImplementation(() => { throw new Error('') })
+      const saga = sagas({}).initialized({type: '', payload: {coin: 'BTC', time: 'all'}})
+
+      // act
+      saga.next()
+
+      // assert
+      expect(fetchPriceActionSpy).toHaveBeenCalledTimes(1)
+      expect(errorSpy).toHaveBeenCalledWith('Error in initialized saga')
+    })
+  })
+
+  // describe('coinClicked saga', () => {
+  //   it('should run correct routine', () => {
+  //     // arrange
+  //     const saga = sagas({}).coinClicked({type: '', payload: {coin: 'BTC'}})
+  //     calcStartSpy = jest.spyOn(chartService, 'calculateStart').mockReturnValueOnce('test')
+  //     calcScaleSpy = jest.spyOn(chartService, 'calculateScale').mockReturnValueOnce('test')
+  //
+  //     saga.next()
+  //     saga.next()
+  //
+  //     // act/assert
+  //     expect(calcStartSpy).toHaveBeenCalledTimes(1)
+  //     expect(calcStartSpy).toHaveBeenCalledWith('BTC', 'MOCK_TIME')
+  //   })
+  //
+  //   it('should run correct routine', () => {
+  //    // var t = effects
+  //     // arrange
+  //     jest.spyOn(effects, 'select').mockImplementation(() => {
+  //       return 'test'
+  //     })
+  //     const saga = sagas({}).coinClicked({type: '', payload: {coin: 'BTC'}})
+  //
+  //     // act/assert
+  //     let next = saga.next()
+  //     expect(next.value).toEqual('test')
+  //
+  //     // act/assert
+  //     saga.next()
+  //     expect(calcStartSpy).toHaveBeenCalledTimes(1)
+  //     expect(calcStartSpy).toHaveBeenCalledWith('BTC', 'test')
+  //     expect(calcScaleSpy).toHaveBeenCalledTimes(1)
+  //     expect(calcScaleSpy).toHaveBeenCalledWith('BTC', 'all')
+  //     expect(fetchPriceActionSpy).toHaveBeenCalledTimes(1)
+  //     expect(fetchPriceActionSpy).toHaveBeenCalledWith('BTC', 'USD', 'test', 'test')
+  //   })
+  //
+  //   it('should handle errors', () => {
+  //     // arrange
+  //     fetchPriceActionSpy = jest.spyOn(actions.core.data.misc, 'fetchPriceIndexSeries').mockImplementation(() => { throw new Error('') })
+  //     const saga = sagas({api: {}, coreSagas: {}}).initialized({type: '', payload: {coin: 'BTC'}})
+  //
+  //     // act
+  //     saga.next()
+  //
+  //     // assert
+  //     expect(fetchPriceActionSpy).toHaveBeenCalledTimes(1)
+  //     expect(errorSpy).toHaveBeenCalledWith('Error in Initialized Price Chart Saga')
+  //   })
+  // })
 })
