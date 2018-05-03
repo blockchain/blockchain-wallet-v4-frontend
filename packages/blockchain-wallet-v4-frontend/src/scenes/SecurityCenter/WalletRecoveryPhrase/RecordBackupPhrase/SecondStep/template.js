d72c1f79135adc89ee3e49336656d3bc91f2e187
TheLeoB <leora@blockchain.com>
2018-05-03 17:38:45 +0100

    fix(Style): backup phrase

--- packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/WalletRecoveryPhrase/RecordBackupPhrase/SecondStep/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/WalletRecoveryPhrase/RecordBackupPhrase/SecondStep/template.js
@@ -9,7 +9,7 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  @media @media(min-width: 992px) {
+  @media (min-width: 992px) {
     width: 118%;
   }
 `
