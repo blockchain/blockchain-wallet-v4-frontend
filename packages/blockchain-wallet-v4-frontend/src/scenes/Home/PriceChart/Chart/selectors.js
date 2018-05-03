b4f4e63efd3477d32f8c86132a4fa62098846348
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-05-03 10:37:50 +0100

    fix(price chart preferences selectors)

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/PriceChart/Chart/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/PriceChart/Chart/selectors.js
@@ -1,9 +1,11 @@
-import { lift, map } from 'ramda'
+import { lift, map, prop } from 'ramda'
 import { selectors } from 'data'
 
 export const getData = (state) => {
   const currencyR = selectors.core.settings.getCurrency(state)
-  const { cacheCoin, cacheTime } = selectors.preferences.getPriceChart(state)
+  const priceChartPref = selectors.preferences.getPriceChart(state)
+  const cacheCoin = prop('coin', priceChartPref)
+  const cacheTime = prop('time', priceChartPref)
   const coin = selectors.components.priceChart.getCoin(state)
   const time = selectors.components.priceChart.getTime(state)
   const priceIndexSeriesDataR = selectors.core.data.misc.getPriceIndexSeries(state)
