a278afe94a0086720cf4e9d37c99a0f59ef14d0e
Salome <salomejumala@gmail.com>
2018-05-03 16:30:38 +0400

    add support for notifications when payment is received

--- packages/blockchain-wallet-v4-frontend/src/data/alerts/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/alerts/sagas.js
@@ -1,19 +1,24 @@
 import { delay } from 'redux-saga'
 import { takeEvery, call, put } from 'redux-saga/effects'
 
-import * as AT from './actionTypes'
-import * as actions from '../actions.js'
+import * as AT from '../actionTypes'
+import * as actions from './actions.js'
 
 const DISMISS_AFTER = 7000
 
 const handleTimer = function * (action) {
   const { id } = action.payload
   yield call(delay, DISMISS_AFTER)
-  yield put(actions.alerts.dismissAlert(id))
+  yield put(actions.dismissAlert(id))
+}
+
+const paymentReceived = function * (action) {
+  yield put(actions.displaySuccess(action.message))
 }
 
 function * sagas () {
-  yield takeEvery(AT.ALERTS_SHOW, handleTimer)
+  yield takeEvery(AT.alerts.ALERTS_SHOW, handleTimer)
+  yield takeEvery(AT.core.webSocket.bitcoin.PAYMENT_RECEIVED, paymentReceived)
 }
 
 export default sagas
--- packages/blockchain-wallet-v4/src/redux/webSocket/bch/middleware.js
+++ packages/blockchain-wallet-v4/src/redux/webSocket/bch/middleware.js
@@ -5,13 +5,8 @@ import * as T from './actionTypes'
 import * as walletTypes from '../../wallet/actionTypes'
 
 const socket = (socket) => (store) => {
-  const send = socket.send.bind(socket)
   return (next) => (action) => {
     const { type } = action
-
-    let whitelist = {
-      [walletTypes.CREATE_LEGACY_ADDRESS]: compose(send, Socket.addrSubMessage, prop('address'))
-    }
 
     if (type === T.START_SOCKET) {
       socket.connect(
@@ -22,8 +17,6 @@ const socket = (socket) => (store) => {
     }
 
     if (type === T.STOP_SOCKET) { socket.close() }
-
-    if (whitelist[type]) whitelist[type](action)
 
     return next(action)
   }
