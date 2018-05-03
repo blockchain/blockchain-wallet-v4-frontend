802c74496a3293c680f0619737036e38684a997a
schnogz <andrew@blockchain.com>
2018-05-03 10:25:45 +0100

    clean up config files and add unit tests

--- packages/blockchain-wallet-v4-frontend/src/config/index.js
+++ packages/blockchain-wallet-v4-frontend/src/config/index.js
@@ -1,4 +1,13 @@
-let config = (process.env.NODE_ENV === 'production')
-  ? require('./config.prod')
-  : require('./config.dev')
-export default config.default
+import Bitcoin from 'bitcoinjs-lib'
+import BitcoinCash from 'bitcoinforksjs-lib'
+
+export default {
+  WALLET_DATA_PATH: 'wallet.data',
+  WALLET_PAYLOAD_PATH: 'wallet.payload',
+  WALLET_SETTINGS_PATH: 'wallet.settings',
+  WALLET_OPTIONS_PATH: 'wallet.options',
+  WALLET_KVSTORE_PATH: 'wallet.kvstore',
+  NETWORK: Bitcoin.networks.bitcoin,
+  NETWORK_ETHEREUM: 1,
+  NETWORK_BCH: BitcoinCash.networks.bitcoin
+}
