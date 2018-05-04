989de456ab427dc69bfbfb6319eaa87b2be60cbd
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-05-04 14:24:38 +0100

    refactor - missing file

--- packages/blockchain-wallet-v4-frontend/src/data/components/btcTransactions/actions.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/btcTransactions/actions.js
@@ -2,6 +2,4 @@ import * as AT from './actionTypes'
 
 export const initialized = () => ({ type: AT.BTC_TRANSACTIONS_INITIALIZED })
 
-export const endlessScrollTriggered = () => ({ type: AT.BTC_TRANSACTIONS_ENDLESS_SCROLL_TRIGGERED })
-
 export const reportClicked = () => ({ type: AT.BTC_TRANSACTIONS_REPORT_CLICKED })
