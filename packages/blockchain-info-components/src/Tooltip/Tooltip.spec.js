43f2f76f331db8ebf248c863ce559a34afe86e37
plondon <philip@blockchain.com>
2018-05-04 14:53:27 +0100

    feat(Tooltips): add onclickoutside

--- packages/blockchain-info-components/src/Tooltip/Tooltip.spec.js
+++ packages/blockchain-info-components/src/Tooltip/Tooltip.spec.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
-
-import Tooltip from './Tooltip'
+import Tooltip from './template.js'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
 
 describe('Tooltip component', () => {
   it('renders correctly', () => {
-    const component = shallow(<Tooltip><span>Default</span></Tooltip>)
-    const tree = toJson(component)
+    const component = renderer.create(<Tooltip colors={{'backgroundColor': '#12345'}}><span>Default</span></Tooltip>)
+    const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
-  })
-
-  it('default state is correct', () => {
-    const component = shallow(<Tooltip />)
-    expect(component.state().displayed).toBeFalsy()
-  })
-
-  it('click handler sets state properly', () => {
-    const component = shallow(<Tooltip />)
-    component.instance().handleClick()
-    expect(component.state().displayed).toBeTruthy()
   })
 })
