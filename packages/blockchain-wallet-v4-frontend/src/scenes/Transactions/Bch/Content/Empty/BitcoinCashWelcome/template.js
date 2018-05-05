6eacf1ae8ab899e1a0d55038f9936894d92e9403
TheLeoB <leora@blockchain.com>
2018-05-05 19:19:48 +0100

    style(Welcome): tx feed welcome styling

--- packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bch/Content/Empty/BitcoinCashWelcome/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bch/Content/Empty/BitcoinCashWelcome/template.js
@@ -27,7 +27,6 @@ const Container = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   width: 100%;
-  margin: 20px 30px 0 30px;
 `
 const Row = styled.div`
   display: flex;
@@ -42,7 +41,7 @@ const Row = styled.div`
 `
 const Cell = styled.div`
   width: 100%;
-  padding: 15px;
+  padding: 15px 0 15px 15px;
   box-sizing: border-box;
   @media(min-width: 768px) { ${props => props.small ? '30%' : '35%'}; }
 `
@@ -59,53 +58,56 @@ const BitcoinCashWelcome = props => {
       <BitcoinCashImage name='half-bitcoincash' />
       <Container>
         <Row>
+          <Cell small>
+            <Text size='24px' weight={300} color='brand-primary' uppercase>
+              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.welcome' defaultMessage='Welcome to' />
+            </Text>
+            <Text size='24px' weight={300} color='brand-primary' uppercase>
+              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.welcome2' defaultMessage='Bitcoin Cash' />
+            </Text>
+            <Link href='https://support.blockchain.com/hc/en-us/categories/115001262946-Bitcoin-Cash' size='16px' target='_blank' weight={300} uppercase>
+              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.learnmore' defaultMessage='Learn More' />
+              <Icon name='right-arrow' color='brand-secondary' />
+            </Link>
+          </Cell>
+          <Cell>
+            <Text weight={300} color='brand-primary' uppercase>
+              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.what' defaultMessage='What is Bitcoin Cash?' />
+            </Text>
+            <Separator />
+            <TextGroup inline>
+              <Text size='12px' weight={300}>
+                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.currency' defaultMessage='Bitcoin Cash is a form of peer-to-peer electronic cash that was created after a fork of the Bitcoin block chain in August 2017.' />
+              </Text>
+              <Text size='12px' weight={300}>
+                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.top' defaultMessage='Bitcoin Cash has since grown to be one of the top cryptocurrencies, along with bitcoin and ether.' />
+              </Text>
+            </TextGroup>
+          </Cell>
+          <Cell>
+            <Text weight={300} color='brand-primary' uppercase>
+              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.why' defaultMessage='How do I get Bitcoin Cash?' />
+            </Text>
+            <Separator />
+            <TextGroup inline>
+              <Text size='12px' weight={300}>
+                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.fees' defaultMessage='If you had bitcoin in your Blockchain wallet before the fork in August, you already have Bitcoin Cash.' />
+              </Text>
+              <Text size='12px' weight={300}>
+                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.fees' defaultMessage='If you’re new to Blockchain, you can get started with Bitcoin Cash by exchanging bitcoin or ether.' />
+              </Text>
+            </TextGroup>
+          </Cell>
+        </Row>
+        <Row>
           <LinkContainer to='/exchange'>
             <Button nature='primary' fullwidth uppercase>
               <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.getstarted' defaultMessage='Get started with Bitcoin Cash' />
             </Button>
           </LinkContainer>
         </Row>
-        <Row>
-          <Cell small>
-            <Text size='28px' weight={300} color='brand-primary' uppercase>
-              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.welcome' defaultMessage='Welcome to Bitcoin Cash!' />
-            </Text>
-            <Link href='https://support.blockchain.com/hc/en-us/categories/115001262946-Bitcoin-Cash' target='_blank' weight={300} uppercase>
-              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.learnmore' defaultMessage='Learn More' />
-              <Icon name='right-arrow' color='brand-secondary' />
-            </Link>
-          </Cell>
-          <Cell>
-            <Text size='18px' weight={300} color='brand-primary' uppercase>
-              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.what' defaultMessage='What is Bitcoin Cash?' />
-            </Text>
-            <Separator />
-            <TextGroup inline>
-              <Text weight={300}>
-                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.currency' defaultMessage='Bitcoin Cash is a form of peer-to-peer electronic cash that was created after a fork of the Bitcoin block chain in August 2017.' />
-              </Text>
-              <Text weight={300}>
-                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.top' defaultMessage='Bitcoin Cash has since grown to be one of the top cryptocurrencies, along with bitcoin and ether.' />
-              </Text>
-            </TextGroup>
-          </Cell>
-          <Cell>
-            <Text size='18px' weight={300} color='brand-primary' uppercase>
-              <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.why' defaultMessage='How do I get Bitcoin Cash?' />
-            </Text>
-            <Separator />
-            <TextGroup inline>
-              <Text weight={300}>
-                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.fees' defaultMessage='If you had bitcoin in your Blockchain wallet before the fork in August, you already have Bitcoin Cash.' />
-              </Text>
-              <Text weight={300}>
-                <FormattedMessage id='scenes.transaction.bitcoincash.content.empty.bitcoincashwelcome.fees' defaultMessage='If you’re new to Blockchain, you can get started with Bitcoin Cash by exchanging bitcoin or ether.' />
-              </Text>
-            </TextGroup>
-          </Cell>
-        </Row>
       </Container>
-      <CloseArrow name='close' size='14px' cursor onClick={handleClick} />
+      <CloseArrow name='close' size='12px' cursor onClick={handleClick} />
     </Wrapper>
   )
 }
