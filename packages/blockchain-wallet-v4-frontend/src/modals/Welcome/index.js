66681024c21d88318be21254bd3dcbab3ab83baa
plondon <philip@blockchain.com>
2018-05-07 10:37:17 -0400

    fix(Welcome): handle continue

--- packages/blockchain-wallet-v4-frontend/src/modals/Welcome/index.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/Welcome/index.js
@@ -1,19 +1,35 @@
 import React from 'react'
-import { compose } from 'redux'
-
+import { connect } from 'react-redux'
+import { bindActionCreators, compose } from 'redux'
+import { actions } from 'data'
 import modalEnhancer from 'providers/ModalEnhancer'
 import Welcome from './template.js'
 
 class WelcomeContainer extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.handleContinue = this.handleContinue.bind(this)
+  }
+
+  handleContinue () {
+    this.props.modalActions.closeModal()
+    this.props.modalActions.showModal('RequestBitcoin')
+  }
+
   render () {
     return (
-      <Welcome {...this.props} />
+      <Welcome {...this.props} handleContinue={this.handleContinue} />
     )
   }
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  modalActions: bindActionCreators(actions.modals, dispatch)
+})
+
 const enhance = compose(
-  modalEnhancer('Welcome')
+  modalEnhancer('Welcome'),
+  connect(undefined, mapDispatchToProps)
 )
 
 export default enhance(WelcomeContainer)
