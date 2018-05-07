4012b8145c3c5f8c348832415a26dfa81ca32d70
TheLeoB <leora@blockchain.com>
2018-05-07 06:40:43 -0400

    fix(Copy): remove html text from default messages

--- packages/blockchain-wallet-v4-frontend/src/modals/ShowXPub/template.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/ShowXPub/template.js
@@ -23,7 +23,7 @@ const FirstStep = () => (
       <FormattedMessage id='modals.xpub.warning' defaultMessage='Warning' />
     </Text>
     <Text size='14px' style={{ marginTop: 10 }} weight={300}>
-      <FormattedMessage id='modals.xpub.warning_message' defaultMessage={'Don\'t share your Extended Public Key (xPub) with an untrusted source. Anyone with access to this can keep track of your payments and may be able to disrupt access to your wallet.'} />
+      <FormattedMessage id='modals.xpub.warning_message' defaultMessage="Don't share your Extended Public Key (xPub) with an untrusted source. Anyone with access to this can keep track of your payments and may be able to disrupt access to your wallet." />
     </Text>
   </div>
 )
