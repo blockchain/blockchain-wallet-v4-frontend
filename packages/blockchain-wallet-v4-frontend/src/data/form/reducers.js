f4e39e23b9752bbe55fb53703085f50fc9486cf2
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-05-08 17:45:18 +0100

    fix - bug when swapping accounts on exchange page multiple time

--- packages/blockchain-wallet-v4-frontend/src/data/form/reducers.js
+++ packages/blockchain-wallet-v4-frontend/src/data/form/reducers.js
@@ -1,1 +1,22 @@
-export { reducer as formReducer } from 'redux-form'
+import * as AT from './actionTypes'
+import { reducer as formReducer } from 'redux-form'
+import { assocPath } from 'ramda'
+
+const reducerReducers = (...reducers) => (prevState, value, ...args) =>
+  reducers.reduce(
+    (newState, reducer) => reducer(newState, value, ...args),
+    prevState
+  )
+
+const extendedReducer = (state, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case AT.CHANGE2: {
+      const { form, field, value } = payload
+      return assocPath([form, 'values', field], value, state)
+    }
+    default: return state
+  }
+}
+
+export default reducerReducers(formReducer, extendedReducer)
