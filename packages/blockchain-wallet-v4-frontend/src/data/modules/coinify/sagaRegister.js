fa1813a72da0b848ec53384f961c4a2b3a747f5d
Philip Welber <welber@blockchain.com>
2018-05-09 11:53:12 -0400

    feat(BuySell): setMin; KYC limit notice; use logger

--- packages/blockchain-wallet-v4-frontend/src/data/modules/coinify/sagaRegister.js
+++ packages/blockchain-wallet-v4-frontend/src/data/modules/coinify/sagaRegister.js
@@ -14,6 +14,7 @@ export default ({ coreSagas }) => {
     yield takeLatest(actionTypes.CHANGE, coinifySagas.handleChange)
     yield takeLatest(AT.COINIFY_INITIALIZED, coinifySagas.initialized)
     yield takeLatest(AT.COINIFY_SET_CHECKOUT_MAX, coinifySagas.setCheckoutMax)
+    yield takeLatest(AT.COINIFY_SET_CHECKOUT_MIN, coinifySagas.setCheckoutMin)
     yield takeLatest(AT.COINIFY_FROM_ISX, coinifySagas.fromISX)
     yield takeLatest(AT.COINIFY_TRIGGER_KYC, coinifySagas.triggerKYC)
     yield takeLatest(AT.OPEN_KYC, coinifySagas.openKYC)
