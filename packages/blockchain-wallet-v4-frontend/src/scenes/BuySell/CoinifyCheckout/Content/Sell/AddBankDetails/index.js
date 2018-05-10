f9705a463a503537b91312b74e3ec17544046585
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-05-10 17:44:31 +0100

    feature(get payment method when adding a bank account)

--- packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/CoinifyCheckout/Content/Sell/AddBankDetails/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/CoinifyCheckout/Content/Sell/AddBankDetails/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { actions } from 'data'
 import AddBankDetails from './template.js'
 
 class AddBankDetailsContainer extends React.PureComponent {
   constructor (props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  componentDidMount () {
+    const { quoteR } = this.props
+    quoteR.map(quote => this.props.coinifyDataActions.getPaymentMediums(quote))
   }
 
   onSubmit (e) {
@@ -22,6 +30,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
+  coinifyDataActions: bindActionCreators(actions.core.data.coinify, dispatch)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddBankDetailsContainer)
