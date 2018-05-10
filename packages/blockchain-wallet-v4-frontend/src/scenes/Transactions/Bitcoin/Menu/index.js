045318262925a7197bc46dc5a224f37d4ff75d7a
Salome <salomejumala@gmail.com>
2018-05-10 16:24:47 +0400

    fix linter errors

--- packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bitcoin/Menu/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Transactions/Bitcoin/Menu/index.js
@@ -11,14 +11,14 @@ class MenuContainer extends React.PureComponent {
     super(props)
     this.handleClickReporting = this.handleClickReporting.bind(this)
   }
-  
+
   handleClickReporting () {
     this.props.modalActions.showModal('TransactionReport', {coin: 'BTC'})
   }
-  
+
   render () {
-    const { data} = this.props
-    
+    const { data } = this.props
+
     return data.cata({
       Success: (value) => <Menu accounts={value.data} handleClickReporting={this.handleClickReporting} />,
       Failure: (message) => <div>{message}</div>,
