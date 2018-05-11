a1ff195280e4c6e2edc1be66d42227f7d35f8fbd
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-05-11 11:03:54 +0100

    feat(improve stepper to support onClick on transition)

--- packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/CoinifyCheckout/Content/Sell/AddBankDetails/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/CoinifyCheckout/Content/Sell/AddBankDetails/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 
-import { actions } from 'data'
 import AddBankDetails from './template.js'
 
 class AddBankDetailsContainer extends React.PureComponent {
   constructor (props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
-  }
-
-  componentDidMount () {
-    const { quoteR } = this.props
-    quoteR.map(quote => this.props.coinifyDataActions.getPaymentMediums(quote))
   }
 
   onSubmit (e) {
@@ -30,7 +23,6 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  coinifyDataActions: bindActionCreators(actions.core.data.coinify, dispatch)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddBankDetailsContainer)
