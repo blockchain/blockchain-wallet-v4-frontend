832c46839f2877bb1af6ebea6fb41f77b061e1c6
Philip Welber <welber@blockchain.com>
2018-05-11 09:10:09 -0400

    feat(BuySell): cancel button on ISX working from modal

--- packages/blockchain-wallet-v4-frontend/src/data/modules/coinify/actions.js
+++ packages/blockchain-wallet-v4-frontend/src/data/modules/coinify/actions.js
@@ -39,3 +39,5 @@ export const fromISX = (state) => ({ type: AT.COINIFY_FROM_ISX, payload: state }
 export const triggerKYC = () => ({ type: AT.COINIFY_TRIGGER_KYC })
 
 export const openKYC = (kyc) => ({ type: AT.OPEN_KYC, payload: kyc })
+
+export const cancelISX = () => ({ type: AT.CANCEL_ISX })
