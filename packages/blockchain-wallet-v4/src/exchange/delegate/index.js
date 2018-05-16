e7444d45eecf7dd25ab91700a7ef0bcedc54fffc
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-05-17 00:47:21 +0100

    style(resolve build issues)

--- packages/blockchain-wallet-v4/src/exchange/delegate/index.js
+++ packages/blockchain-wallet-v4/src/exchange/delegate/index.js
@@ -86,7 +86,7 @@ export class ExchangeDelegate {
   }
 
   reserveReceiveAddress () {
-    // const receiveAddress = bitcoin.getNextAvailableReceiveAddress(Bitcoin.networks.bitcoin.NETWORK_BITCOIN, 0, this.state)
+    const receiveAddress = bitcoin.getNextAvailableReceiveAddress(Bitcoin.networks.bitcoin.NETWORK_BITCOIN, 0, this.state)
     return {
       // receiveAddress: '2N7FwMpgyXQA85SaVXumm3UZowq2VKChehP', // receiveAddress.data for now testnet address used on staging
       receiveAddress: receiveAddress.data,
