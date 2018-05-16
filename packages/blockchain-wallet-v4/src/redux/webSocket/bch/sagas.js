d5cc545e0a3288fe5e758dbec7531bf3ac0e70c9
TheLeoB <leora@blockchain.com>
2018-05-16 12:10:45 -0600

    fix(Alerts): send alert copy

--- packages/blockchain-wallet-v4/src/redux/webSocket/bch/sagas.js
+++ packages/blockchain-wallet-v4/src/redux/webSocket/bch/sagas.js
@@ -19,7 +19,7 @@ export default ({ api, bchSocket }) => {
 
     switch (message.op) {
       case 'utx':
-        yield put(A.webSocket.bitcoin.paymentReceived('You\'ve just received a Bitcoin Cash payment.'))
+        yield put(A.webSocket.bitcoin.paymentReceived('You\'ve just received a bitcoin cash payment.'))
         const walletContext = yield select(walletSelectors.getWalletContext)
         yield put(bchActions.fetchData(walletContext))
         yield put(bchActions.fetchTransactions('', true))
