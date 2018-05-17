85e83b019d04d551f08a26134f7960d861009bea
schnogz <andrew@blockchain.com>
2018-05-16 19:13:06 -0600

    show/hide advanced settings buttons

--- packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/Advanced/PasswordHint/Settings/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/Advanced/PasswordHint/Settings/template.js
@@ -2,31 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 import { Field, reduxForm } from 'redux-form'
+import styled from 'styled-components'
 
 import { Button, ButtonGroup } from 'blockchain-info-components'
 import { TextBox } from 'components/Form'
 import { SettingForm, SettingWrapper } from 'components/Setting'
 import Hint from './Hint'
 
+const ButtonWrapper = styled(ButtonGroup)`
+  margin-top: 5px;
+  & > :first-child {
+    margin-right: 5px;
+  }
+`
+
 const Settings = (props) => {
   const { updateToggled, handleToggle, handleClick, submitting, invalid, handleCancel } = props
   return (
     <SettingWrapper>
       <Hint />
-      <Button nature='primary' onClick={handleToggle}>
-        <FormattedMessage id='scenes.securitysettings.basicsecurity.passwordhint.settings.change' defaultMessage='Change' />
-      </Button>
+      { !updateToggled &&
+        <Button nature='primary' onClick={handleToggle}>
+          <FormattedMessage id='scenes.securitysettings.basicsecurity.passwordhint.settings.change' defaultMessage='Change'/>
+        </Button>
+      }
       { updateToggled &&
         <SettingForm>
           <Field name='passwordHint' component={TextBox} />
-          <ButtonGroup>
+          <ButtonWrapper>
             <Button nature='empty' capitalize onClick={handleCancel}>
               <FormattedMessage id='scenes.securitysettings.basicsecurity.passwordhint.settings.cancel' defaultMessage='Cancel' />
             </Button>
             <Button nature='primary' capitalize disabled={submitting || invalid} onClick={handleClick}>
               <FormattedMessage id='scenes.securitysettings.basicsecurity.passwordhint.settings.save' defaultMessage='Change' />
             </Button>
-          </ButtonGroup>
+          </ButtonWrapper>
         </SettingForm>
       }
     </SettingWrapper>
