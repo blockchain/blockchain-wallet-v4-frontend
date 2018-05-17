c4cac948ee1922f236826a2005babba356c895cd
plondon <philip@blockchain.com>
2018-05-17 17:43:24 -0400

    fix(Eth Tx): endless scroll fix and latestBlock

--- packages/blockchain-wallet-v4/src/redux/data/ethereum/reducers.js
+++ packages/blockchain-wallet-v4/src/redux/data/ethereum/reducers.js
@@ -49,7 +49,7 @@ export default (state = INITIAL_STATE, action) => {
       return assoc('fee', Remote.Failure(payload), state)
     }
     case AT.FETCH_ETHEREUM_LATEST_BLOCK_LOADING: {
-      return assoc('latest_block', Remote.Loading, state)
+      return state
     }
     case AT.FETCH_ETHEREUM_LATEST_BLOCK_SUCCESS: {
       return assoc('latest_block', Remote.Success(payload), state)
@@ -75,32 +75,6 @@ export default (state = INITIAL_STATE, action) => {
     case AT.FETCH_ETHEREUM_TRANSACTIONS_FAILURE: {
       return over(lensProp('transactions'), compose(append(Remote.Failure(payload)), dropLast(1)), state)
     }
-    // case AT.FETCH_ETHEREUM_TRANSACTIONS: {
-    //   const { address } = payload
-    //   const path = ['transactions', address]
-    //   const lens = lensPath(path)
-    //   const data = view(lens, state)
-    //   return isNil(data) ? assocPath(path, [], state) : state
-    // }
-    // case AT.FETCH_ETHEREUM_TRANSACTIONS_LOADING: {
-    //   const { address } = payload
-    //   const lens = lensPath(['transactions', address])
-    //   return over(lens, append(Remote.Loading), state)
-    // }
-    // case AT.FETCH_ETHEREUM_TRANSACTIONS_SUCCESS: {
-    //   const { address, transactions } = payload
-    //   const lens = lensPath(['transactions', address])
-    //   const data = map(x => Remote.Success(x), transactions)
-    //   const newState = over(lens, compose(concat(data), dropLast(1)), state)
-    //   console.log('newState', newState)
-    //   return newState
-    // }
-    // case AT.FETCH_ETHEREUM_TRANSACTIONS_FAILURE: {
-    //   const { address, error } = payload
-    //   const lens = lensPath(['transactions', address])
-    //   return over(lens, compose(concat(Remote.Failure(error)), dropLast(1)), state)
-    // }
-
     default:
       return state
   }
