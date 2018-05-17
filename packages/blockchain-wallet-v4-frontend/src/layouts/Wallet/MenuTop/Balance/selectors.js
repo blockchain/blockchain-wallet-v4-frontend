c20b4b834b9aa67a8bfd893b0bfc3e113e3d8445
plondon <philip@blockchain.com>
2018-05-16 23:13:31 -0400

    Renaming

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/selectors.js
@@ -3,10 +3,10 @@ import { selectors } from 'data'
 import { join, lift } from 'ramda'
 
 export const getData = (state) => {
-  const bitcoinContext = Remote.of(join('|', selectors.core.wallet.getWalletContext(state)))
-  const etherContext = selectors.core.kvStore.ethereum.getContext(state)
+  const ethContext = selectors.core.kvStore.ethereum.getContext(state)
+  const btcContext = Remote.of(join('|', selectors.core.wallet.getWalletContext(state)))
   const bchContext = Remote.of(join('|', selectors.core.wallet.getWalletContext(state)))
   const path = state.router.location.pathname
-  const transform = lift((bitcoinContext, etherContext, bchContext) => ({bitcoinContext, etherContext, bchContext, path}))
-  return transform(bitcoinContext, etherContext, bchContext)
+  const transform = lift((btcContext, ethContext, bchContext) => ({btcContext, ethContext, bchContext, path}))
+  return transform(btcContext, ethContext, bchContext)
 }
