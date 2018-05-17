f42f3994395a5fc08307e80bd708c8ea777dee7f
plondon <philip@blockchain.com>
2018-05-17 01:13:59 -0400

    fix(Addresses): separate bch context from btc

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/selectors.js
@@ -3,10 +3,11 @@ import { selectors } from 'data'
 import { join, lift } from 'ramda'
 
 export const getData = (state) => {
+  const btcContext = selectors.core.wallet.getWalletContext(state)
   const ethContext = selectors.core.kvStore.ethereum.getContext(state)
-  const btcContext = Remote.of(join('|', selectors.core.wallet.getWalletContext(state)))
-  const bchContext = Remote.of(join('|', selectors.core.wallet.getWalletContext(state)))
+  const bchContext = Remote.of(selectors.core.kvStore.bch.getContext(state, btcContext))
+  const btcContextR = Remote.of(join('|', btcContext))
   const path = state.router.location.pathname
-  const transform = lift((btcContext, ethContext, bchContext) => ({btcContext, ethContext, bchContext, path}))
-  return transform(btcContext, ethContext, bchContext)
+  const transform = lift((btcContextR, ethContext, bchContext) => ({btcContext, ethContext, bchContext, path}))
+  return transform(btcContextR, ethContext, bchContext)
 }
