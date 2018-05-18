644865d0b0a982f127c004587437028a80b29a34
plondon <philip@blockchain.com>
2018-05-18 00:14:58 -0400

    fix(Themes): prefer props.theme over Color()

--- packages/blockchain-wallet-v4-frontend/src/components/Setting/SettingHeader.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Setting/SettingHeader.js
@@ -9,7 +9,7 @@ const SettingHeader = styled.div`
   font-family: 'Montserrat', sans-serif;
   font-weight: 500;
   font-size: 16px;
-  color: ${Color('gray-5')};
+  color: ${props => props.theme['gray-5']};
 
   & > * { margin-right: 10px; }
 `
