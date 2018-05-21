ab403df08ba3861eb53d9c0f89c3267966783376
schnogz <andrew@blockchain.com>
2018-05-21 16:10:42 -0400

    fix lint issues

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/BuySellStepper/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/BuySellStepper/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import BuySellStepper from './template.js'
 
 class BuySellStepperContainer extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.goToBuySell = this.goToBuySell.bind(this)
+  }
 
   goToBuySell = () => {
-    window.alert('hi')
+    this.props.history.push('/buy-sell')
   }
+
   render () {
     return (
       <BuySellStepper currentStep={2} goToBuySell={this.goToBuySell} />
@@ -14,4 +21,9 @@ class BuySellStepperContainer extends React.PureComponent {
   }
 }
 
-export default BuySellStepperContainer
+const mapStateToProps = (state) => ({
+  // data: getData(state),
+  // canBuy: selectors.exchange.getCanTrade(state, 'Buy')
+})
+
+export default withRouter(connect(mapStateToProps)(BuySellStepperContainer))
