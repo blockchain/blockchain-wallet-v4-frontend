cb16f79426c5650f8a9a1ffdcb5920205337a3df
plondon <philip@blockchain.com>
2018-05-21 11:26:44 -0400

    Naming remotes

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/selectors.js
@@ -4,10 +4,10 @@ import { join, lift } from 'ramda'
 
 export const getData = (state) => {
   const btcContext = selectors.core.wallet.getWalletContext(state)
-  const ethContext = selectors.core.kvStore.ethereum.getContext(state)
-  const bchContext = Remote.of(selectors.core.kvStore.bch.getContext(state, btcContext))
+  const ethContextR = selectors.core.kvStore.ethereum.getContext(state)
+  const bchContextR = Remote.of(selectors.core.kvStore.bch.getContext(state, btcContext))
   const btcContextR = Remote.of(join('|', btcContext))
   const path = state.router.location.pathname
-  const transform = lift((btcContextR, ethContext, bchContext) => ({btcContext, ethContext, bchContext, path}))
-  return transform(btcContextR, ethContext, bchContext)
+  const transform = lift((btcContext, ethContext, bchContext) => ({btcContext, ethContext, bchContext, path}))
+  return transform(btcContextR, ethContextR, bchContextR)
 }
