2697d808113a864aa67ddf97e11f571093923951
plondon <philip@blockchain.com>
2018-05-22 16:45:00 -0400

    fix(Request BCH): should open to default BCH account, not BTC

--- packages/blockchain-wallet-v4-frontend/src/modals/RequestBch/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/RequestBch/selectors.js
@@ -1,16 +1,17 @@
 import { formValueSelector } from 'redux-form'
-import { equals, head, filter, map, prop } from 'ramda'
+import { lift, head, map } from 'ramda'
 import settings from 'config'
 import { selectors } from 'data'
 import { Remote, utils } from 'blockchain-wallet-v4/src'
 
 // extractAddress :: (Int -> Remote(String)) -> Int -> Remote(String)
-const extractAddress = (selectorFunction, value) =>
-  value
+const extractAddress = (selectorFunction, value) => {
+  return value
     ? value.address
       ? Remote.of(value.address)
       : selectorFunction(value.index)
     : Remote.Loading
+}
 
 export const getData = state => {
   // TODO: USE BCH network
@@ -24,7 +25,10 @@ export const getData = state => {
 export const getInitialValues = state => {
   const toDropdown = map(x => ({ text: x.label, value: x }))
   const balancesR = selectors.core.common.bch.getAccountsBalances(state).map(toDropdown)
-  const xpub = selectors.core.wallet.getDefaultAccountXpub(state)
-  const defaultElementR = balancesR.map(x => prop('value', head(filter(y => equals(y.value.xpub, xpub), x))))
-  return defaultElementR.map(to => ({to, coin: 'BCH'}))
+  const defaultIndexR = selectors.core.kvStore.bch.getDefaultAccountIndex(state)
+  const transform = (defaultIndex, balances) => {
+    const defaultElement = head(balances.filter(x => x.value.index === defaultIndex))
+    return ({to: defaultElement.value, coin: 'BCH'})
+  }
+  return lift(transform)(defaultIndexR)(balancesR)
 }
