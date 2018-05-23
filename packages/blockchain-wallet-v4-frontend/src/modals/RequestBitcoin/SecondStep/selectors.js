383a3b37fe3df4f28f761c96a04d72e70b8aa49b
plondon <philip@blockchain.com>
2018-05-23 09:25:57 -0400

    style(Payment Requests): fix pr amount, give pr desc params

--- packages/blockchain-wallet-v4-frontend/src/modals/RequestBitcoin/SecondStep/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/RequestBitcoin/SecondStep/selectors.js
@@ -6,6 +6,6 @@ export const getData = (state, ownProps) => {
   const amount = formValueSelector('requestBitcoin')(state, 'amount')
   const message = formValueSelector('requestBitcoin')(state, 'message')
   const satoshis = Exchange.convertBitcoinToBitcoin({ value: amount.coin, fromUnit: 'BTC', toUnit: 'SAT' }).value
-  const link = `https://blockchain.info/payment_request?address=${receiveAddress}&amount=${amount}&message=${message}`
+  const link = `https://blockchain.info/payment_request?address=${receiveAddress}&amount=${amount.coin}&message=${message}`
   return Remote.of({ satoshis, link, amount, message, receiveAddress })
 }
