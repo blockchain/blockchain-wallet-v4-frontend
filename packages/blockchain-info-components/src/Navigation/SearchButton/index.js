8caced61ba4ad3dcef871c4d49beb9fc23710665
plondon <philip@blockchain.com>
2018-05-29 14:10:41 -0400

    feat(Components): add nav/footer

---
+++ packages/blockchain-info-components/src/Navigation/SearchButton/index.js
@@ -0,0 +1,44 @@
+
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  display: none;
+  @media screen and (max-width: 1024px) {
+    background: none;
+    border: none;
+    display: block;
+    float: right;
+    height: 20px;
+    margin-right: 25px;
+    position: relative;
+    width: 20px;
+
+    &:after {
+      color: #fff;
+      content: '\e92d';
+      font-family: 'icomoon';
+      font-size: 17px;
+      font-style: normal;
+      font-variant: normal;
+      font-weight: normal;
+      left: 2px;
+      position: absolute;
+      speak: none;
+      text-transform: none;
+      top: 0px;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+  }
+`
+
+class SearchButton extends Component {
+  render() {
+    return (
+      <Button />
+    )
+  }
+}
+
+export default SearchButton
