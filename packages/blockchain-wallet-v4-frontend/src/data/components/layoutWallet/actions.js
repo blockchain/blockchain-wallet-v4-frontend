bcb28b686e35c76c2f3f5057b1e404d84ecb9ff9
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-05-31 18:46:48 +0100

    refactoring - walletLayout to improve performances refactoring - tray right, menuLeft

---
+++ packages/blockchain-wallet-v4-frontend/src/data/components/layoutWallet/actions.js
@@ -0,0 +1,13 @@
+import * as AT from './actionTypes'
+
+export const layoutWalletFaqClicked = () => ({ type: AT.LAYOUT_WALLET_HEADER_FAQ_CLICKED })
+
+export const layoutWalletWhatsnewClicked = () => ({ type: AT.LAYOUT_WALLET_HEADER_WHATSNEW_CLICKED })
+
+export const layoutWalletTrayCloseClicked = () => ({ type: AT.LAYOUT_WALLET_TRAY_CLOSE_CLICKED })
+
+export const layoutWalletMenuToggleClicked = () => ({ type: AT.LAYOUT_WALLET_MENU_TOGGLE_CLICKED })
+
+export const layoutWalletMenuCloseClicked = () => ({ type: AT.LAYOUT_WALLET_MENU_CLOSE_CLICKED })
+
+export const layoutWalletMenuSettingsClicked = () => ({ type: AT.LAYOUT_WALLET_MENU_SETTINGS_CLICKED })
