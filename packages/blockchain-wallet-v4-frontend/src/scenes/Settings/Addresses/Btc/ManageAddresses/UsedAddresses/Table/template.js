5c00c139df9b8662f63a041749378ba3774bc519
schnogz <andrew@blockchain.com>
2018-05-30 20:11:45 -0400

    ensure search box filters box used and unused address tables

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Btc/ManageAddresses/UsedAddresses/Table/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Addresses/Btc/ManageAddresses/UsedAddresses/Table/template.js
@@ -27,35 +27,43 @@ const UsedTable = ({ children }) => (
   </Table>
 )
 
-const UsedTableEntry = ({ address }) => {
-  return (
-    <TableRow>
-      <TableCell width='40%'>
-        <Link href={`https://blockchain.info/address/${address.address}`} size='small' weight={300} target='_blank'>
-          {address.address}
-        </Link>
-      </TableCell>
-      <TableCell width='40%'>
-        <Text size='13px'>
-          {address.label}
-        </Text>
-      </TableCell>
-      <TableCell style={{display: 'flex', justifyContent: 'flex-end'}} width='20%'>
-        <Text size='13px'>
-          <CoinDisplay coin={'BTC'} size='13px' weight={300}>{address.final_balance}</CoinDisplay>
-        </Text>
-      </TableCell>
-    </TableRow>
-  )
+const UsedTableEntry = ({ address, search }) => {
+  const isMatch = () => {
+    return !search ||
+      address.address.toLowerCase().indexOf(search.toLowerCase()) > -1 ||
+      (address.label && address.label.toLowerCase().indexOf(search.toLowerCase()) > -1)
+  }
+
+  return !isMatch()
+    ? null
+    : (
+      <TableRow>
+        <TableCell width='40%'>
+          <Link href={`https://blockchain.info/address/${address.address}`} size='small' weight={300} target='_blank'>
+            {address.address}
+          </Link>
+        </TableCell>
+        <TableCell width='40%'>
+          <Text size='13px'>
+            {address.label}
+          </Text>
+        </TableCell>
+        <TableCell style={{display: 'flex', justifyContent: 'flex-end'}} width='20%'>
+          <Text size='13px'>
+            <CoinDisplay coin={'BTC'} size='13px' weight={300}>{address.final_balance}</CoinDisplay>
+          </Text>
+        </TableCell>
+      </TableRow>
+    )
 }
 
-const UsedAddressesTable = ({ usedAddresses }) => (
+const UsedAddressesTable = ({ usedAddresses, search }) => (
   <React.Fragment>
     <Text weight={200} size='small' style={{ marginTop: 10, marginBottom: 15 }}>
       <FormattedMessage id='scenes.settings.manage_addresses.used_addresses_message' defaultMessage='Previously used addresses are helpful for debugging purposes and viewing associated balances. For privacy reasons, we do not recommend re-using these addresses. Change addresses are not included here.'/>
     </Text>
     { usedAddresses.length
-      ? <UsedTable>{usedAddresses.map((address, i) => (<UsedTableEntry key={i} address={address} />))}</UsedTable>
+      ? <UsedTable>{usedAddresses.map((address, i) => (<UsedTableEntry key={i} address={address} search={search} />))}</UsedTable>
       : <Text weight={300} style={{ marginTop: 10, textAlign: 'center' }}><FormattedMessage id='scenes.settings.manage_addresses.no_used_addresses_message' defaultMessage='This wallet has no previously used addresses.'/></Text>
     }
   </React.Fragment>
