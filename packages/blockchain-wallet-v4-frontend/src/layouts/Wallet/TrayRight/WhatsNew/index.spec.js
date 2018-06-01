00ea035bfbccc1416e06e228966d9d678d8d041d
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-06-01 16:45:23 +0100

    fix - test

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/TrayRight/WhatsNew/index.spec.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/TrayRight/WhatsNew/index.spec.js
@@ -4,17 +4,10 @@ import toJson from 'enzyme-to-json'
 import WhatsNew from './index'
 jest.mock('blockchain-info-components', () => ({ Text: 'text', TextGroup: 'text-group', Color: (val) => val, ModalHeader: 'modal-header', ModalBody: 'modal-body' }))
 
-const mockTrayToggle = jest.fn()
-
 describe('Whats New', () => {
   it('renders correctly', () => {
-    const component = shallow(<WhatsNew handleTrayRightToggle={mockTrayToggle} />)
+    const component = shallow(<WhatsNew />)
     const tree = toJson(component)
     expect(tree).toMatchSnapshot()
-  })
-  it('handles Link clicks', () => {
-    const component = shallow(<WhatsNew handleTrayRightToggle={mockTrayToggle} />)
-    component.find('template__WhatsNewLink').first().simulate('click')
-    expect(mockTrayToggle.mock.calls.length).toEqual(1)
   })
 })
