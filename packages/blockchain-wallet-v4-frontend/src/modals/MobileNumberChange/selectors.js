c72f001095b78648b682ad1de00600c9f2346f5e
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-06-05 15:48:53 +0100

    fix(test and reselect)

--- packages/blockchain-wallet-v4-frontend/src/modals/MobileNumberChange/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/MobileNumberChange/selectors.js
@@ -1,17 +1,20 @@
 import PhoneNumber from 'awesome-phonenumber'
 import { lift } from 'ramda'
-
 import { formValueSelector } from 'redux-form'
+
 import { selectors } from 'data'
+import { createDeepEqualSelector } from 'services/ReselectHelper'
 
 const getCountryCode = (defaultCode, currentNumber) =>
   currentNumber ? PhoneNumber(currentNumber).getRegionCode() : defaultCode
 
-export const getData = (state) => {
-  const currentNumber = selectors.core.settings.getSmsNumber(state)
-  const defaultCode = selectors.core.settings.getCountryCode(state)
-  return {
+export const getData = (state) => createDeepEqualSelector(
+  [
+    selectors.core.settings.getSmsNumber,
+    selectors.core.settings.getCountryCode
+  ],
+  (currentNumber, defaultCode) => ({
     mobileNumber: formValueSelector('mobileNumberChange')(state, 'mobileNumber'),
     countryCode: lift(getCountryCode)(defaultCode, currentNumber)
-  }
-}
+  })
+)(state)
