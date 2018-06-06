3a21f30cf177430f0ead1ab0f2f31483c15ce31a
plondon <philip@blockchain.com>
2018-06-06 17:14:50 -0400

    fix(Translations): update translations based on preferences

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Preferences/WalletLanguage/Settings/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/Preferences/WalletLanguage/Settings/template.js
@@ -4,10 +4,12 @@ import { Field, reduxForm } from 'redux-form'
 import { SelectBoxLanguages } from 'components/Form'
 import { SettingSelectBoxWrapper } from 'components/Setting'
 
-const Settings = (props) => (
-  <SettingSelectBoxWrapper>
-    <Field name='language' component={SelectBoxLanguages} />
-  </SettingSelectBoxWrapper>
-)
+const Settings = (props) => {
+  return (
+    <SettingSelectBoxWrapper>
+      <Field name='language' component={SelectBoxLanguages} />
+    </SettingSelectBoxWrapper>
+  )
+}
 
 export default reduxForm({ form: 'settingLanguage' })(Settings)
