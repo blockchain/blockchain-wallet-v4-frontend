6ccf1c6e689362d3c9e92b31a49cbbc274ddc9e8
plondon <philip@blockchain.com>
2018-06-08 18:03:13 -0400

    feat(Data): btc/bch/eth tx list data failure

--- packages/blockchain-wallet-v4-frontend/src/data/components/activityList/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/activityList/sagas.js
@@ -1,5 +1,4 @@
 import { select, put } from 'redux-saga/effects'
-import { isEmpty } from 'ramda'
 import { Remote } from 'blockchain-wallet-v4/src'
 import { actions, selectors } from 'data'
 
@@ -11,9 +10,9 @@ export default ({ coreSagas }) => {
       const bchTransactions = yield select(selectors.core.data.bch.getTransactions)
       const ethTransactions = yield select(selectors.core.data.ethereum.getTransactions)
       if (!Remote.Success.is(logsR)) yield put(actions.core.data.misc.fetchLogs())
-      if (isEmpty(btcTransactions)) yield put(actions.core.data.bitcoin.fetchData('', true))
-      if (isEmpty(bchTransactions)) yield put(actions.core.data.bch.fetchData('', true))
-      if (isEmpty(ethTransactions)) yield put(actions.core.data.ethereum.fetchData())
+      if (Remote.NotAsked.is(btcTransactions)) yield put(actions.core.data.bitcoin.fetchData('', true))
+      if (Remote.NotAsked.is(bchTransactions)) yield put(actions.core.data.bch.fetchData('', true))
+      if (Remote.NotAsked.is(ethTransactions)) yield put(actions.core.data.ethereum.fetchData())
     } catch (e) {
       yield put(actions.logs.logErrorMessage('components/activityList/sagas', 'initialized', e))
     }
