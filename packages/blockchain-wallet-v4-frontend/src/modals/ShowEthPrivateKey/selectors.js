7bec07e449db16c9bac38ac17b767da70e0bf1e5
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-06-11 16:02:39 +0100

    fix(show 0 balance for eth legacy)

--- packages/blockchain-wallet-v4-frontend/src/modals/ShowEthPrivateKey/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/ShowEthPrivateKey/selectors.js
@@ -3,7 +3,7 @@ import { selectors } from 'data'
 export const getData = (state, props) => {
   const ethKvStoreSelectors = selectors.core.kvStore.ethereum
   return {
-    balance: props.archived ? 0 : selectors.core.data.ethereum.getBalance(state).getOrElse(0),
+    balance: props.isLegacy ? 0 : selectors.core.data.ethereum.getBalance(state).getOrElse(0),
     addr: (props.isLegacy
       ? ethKvStoreSelectors.getLegacyAccountAddress(state)
       : ethKvStoreSelectors.getContext(state)).getOrElse(''),
