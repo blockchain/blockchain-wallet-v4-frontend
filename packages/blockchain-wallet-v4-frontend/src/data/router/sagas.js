04ee0bbbe690840e08be06841fb0306823fc42bf
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-06-11 10:59:18 +0100

    feat(close all modals on router location change)

---
+++ packages/blockchain-wallet-v4-frontend/src/data/router/sagas.js
@@ -0,0 +1,18 @@
+import { put } from 'redux-saga/effects'
+import { actions } from 'data'
+
+export default () => {
+  const logLocation = 'modules/coinify/sagas'
+
+  const changeLocation = function * () {
+    try {
+      yield put(actions.modals.closeAllModals())
+    } catch (e) {
+      yield put(actions.logs.logErrorMessage(logLocation, 'router change location', e))
+    }
+  }
+
+  return {
+    changeLocation
+  }
+}
