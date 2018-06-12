ef8a90fc4b5357e1e80f2491ab7ae02f5cba1161
TheLeoB <leora@blockchain.com>
2018-06-12 15:02:23 -0400

    fix(FAQ): use nbsp instead of space

--- packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Shapeshift/ThirdStep/template.success.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Shapeshift/ThirdStep/template.success.js
@@ -103,7 +103,8 @@ const Success = props => {
           {status === 'complete' &&
             <Notice>
               <Text size='13px' weight={300}>
-                <FormattedMessage id='modals.exchange.shapeshift.explain3' defaultMessage='Your exchange is complete. ' />
+                <FormattedMessage id='modals.exchange.shapeshift.explain3' defaultMessage='Your exchange is complete.' />
+                <span>&nbsp;</span>
                 <FormattedMessage id='modals.exchange.shapeshift.explain4' defaultMessage='It may take a few minutes for the funds to show in your balance.' />
               </Text>
             </Notice>
