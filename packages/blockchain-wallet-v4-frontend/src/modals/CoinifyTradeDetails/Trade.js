4817690d002000c77ab21386dceecb9bf1b5b009
Philip Welber <welber@blockchain.com>
2018-06-13 10:26:06 -0400

    fix(Coinify): trade details modal header copy

--- packages/blockchain-wallet-v4-frontend/src/modals/CoinifyTradeDetails/Trade.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/CoinifyTradeDetails/Trade.js
@@ -34,7 +34,7 @@ const Trade = ({ trade, close, status }) => {
     <Fragment>
       <ModalHeader onClose={close}>
         <Text color={headerStatus.color}>
-          { trade.isBuy ? `Buy Order` : 'Sell Order' } {headerStatus.text}
+          { trade.isBuy ? `Buy Trade` : 'Sell Trade' } {headerStatus.text}
         </Text>
       </ModalHeader>
       <ModalBody>
