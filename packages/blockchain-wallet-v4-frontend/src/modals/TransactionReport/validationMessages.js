29c230b90b7acdf3f2064225438bbb55047586f6
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-06-14 15:16:48 +0100

    feat - validation rule on transaction export modal, in case users are manually changing the date instead of using the calendar fix - added locale/culture on DateInput calendar

---
+++ packages/blockchain-wallet-v4-frontend/src/modals/TransactionReport/validationMessages.js
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+export const InvalidDateMessage = () => (
+  <FormattedMessage id='modals.transactionreport.invalid' defaultMessage='Invalid date' />
+)
