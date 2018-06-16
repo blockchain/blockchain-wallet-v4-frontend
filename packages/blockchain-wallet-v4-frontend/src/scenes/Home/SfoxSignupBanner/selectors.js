7e6327d6fdec49fa89a4bb1e58405d560b7e7a07
schnogz <andrew@blockchain.com>
2018-06-16 12:15:33 -0400

    ensure new wallets with no sfox acct token render the stepper

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/SfoxSignupBanner/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/SfoxSignupBanner/selectors.js
@@ -1,16 +1,22 @@
 import { lift } from 'ramda'
 
+import { Remote } from 'blockchain-wallet-v4/src'
 import { createDeepEqualSelector } from 'services/ReselectHelper'
 import { selectors } from 'data'
 
-export const getData = createDeepEqualSelector(
+export const getData = (state, ownProps) => createDeepEqualSelector(
   [
     selectors.core.data.sfox.getProfile,
     selectors.core.data.sfox.getAccounts,
     selectors.core.data.sfox.getVerificationStatus
   ], (sfoxProfile, sfoxAccounts, verificationStatus) => {
+    if (!ownProps.sfoxKvData.account_token) {
+      // no account token, dont fetch sfox data and render first step
+      return Remote.of({})
+    }
+
     return lift((sfoxProfile, sfoxAccounts, verificationStatus) => ({
       sfoxProfile, sfoxAccounts, verificationStatus
     }))(sfoxProfile, sfoxAccounts, verificationStatus)
   }
-)
+)(state, ownProps)
