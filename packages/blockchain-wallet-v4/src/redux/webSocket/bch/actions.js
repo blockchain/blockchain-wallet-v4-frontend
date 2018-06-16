e14355406b1c55889de61793e7423493d1d15069
TheLeoB <leora@blockchain.com>
2018-06-16 17:48:18 -0400

    receive alerts

--- packages/blockchain-wallet-v4/src/redux/webSocket/bch/actions.js
+++ packages/blockchain-wallet-v4/src/redux/webSocket/bch/actions.js
@@ -6,5 +6,3 @@ export const stopSocket = () => ({ type: T.STOP_SOCKET })
 export const openSocket = () => ({ type: T.OPEN_SOCKET })
 export const messageSocket = (payload) => ({ type: T.MESSAGE_SOCKET, payload })
 export const closeSocket = () => ({ type: T.CLOSE_SOCKET })
-
-export const bchPaymentReceived = (message) => ({type: T.BCH_PAYMENT_RECEIVED, message})
