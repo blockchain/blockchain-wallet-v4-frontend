51a4f16a06459f8de77a6d213788df5ea6fb2d38
TheLeoB <leora@blockchain.com>
2018-06-16 16:50:53 -0400

    fix(Did You Know): Copy

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/DidYouKnow/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/DidYouKnow/template.js
@@ -54,7 +54,7 @@ const PaddedIcon = styled(Icon)`
 const selectTitle = (number) => {
   switch (number) {
     case 1: return <FormattedMessage id='scenes.home.didyouknow.title1' defaultMessage='We don’t store your password' />
-    case 2: return <FormattedMessage id='scenes.home.didyouknow.title2' defaultMessage='How It Works' />
+    case 2: return <FormattedMessage id='scenes.home.didyouknow.title2' defaultMessage='How it works' />
     case 3: return <FormattedMessage id='scenes.home.didyouknow.title3' defaultMessage='Why does my balance change?' />
     case 4: return <FormattedMessage id='scenes.home.didyouknow.title4' defaultMessage='Our wallet is open source' />
     case 5: return <FormattedMessage id='scenes.home.didyouknow.title5' defaultMessage='Set your own fees' />
@@ -62,12 +62,12 @@ const selectTitle = (number) => {
     case 7: return <FormattedMessage id='scenes.home.didyouknow.title7' defaultMessage='Transaction fees' />
     case 8: return <FormattedMessage id='scenes.home.didyouknow.title8' defaultMessage='Use Your Blockchain wallet on mobile' />
     case 9: return <FormattedMessage id='scenes.home.didyouknow.title9' defaultMessage='Top security tips' />
-    case 10: return <FormattedMessage id='scenes.home.didyouknow.title10' defaultMessage='Your wallet ID cannot be used to send/receive funds' />
-    case 11: return <FormattedMessage id='scenes.home.didyouknow.title11' defaultMessage='Report Phishing Sites' />
-    case 12: return <FormattedMessage id='scenes.home.didyouknow.title12' defaultMessage='Identifying SSL Protection' />
-    case 13: return <FormattedMessage id='scenes.home.didyouknow.title13' defaultMessage="We Can' t Suspend or Lock Your Wallet" />
+    case 10: return <FormattedMessage id='scenes.home.didyouknow.title10' defaultMessage='Your wallet ID is not an address' />
+    case 11: return <FormattedMessage id='scenes.home.didyouknow.title11' defaultMessage='Report phishing sites' />
+    case 12: return <FormattedMessage id='scenes.home.didyouknow.title12' defaultMessage='Identifying SSL protection' />
+    case 13: return <FormattedMessage id='scenes.home.didyouknow.title13' defaultMessage='We can’t suspend or lock your wallet' />
     case 14: return <FormattedMessage id='scenes.home.didyouknow.title14' defaultMessage='What are phishing scams?' />
-    case 15: return <FormattedMessage id='scenes.home.didyouknow.title15' defaultMessage="Scam Warning: Bitcoins Can't Be Doubled" />
+    case 15: return <FormattedMessage id='scenes.home.didyouknow.title15' defaultMessage='Scam warning: Bitcoin cannot be doubled' />
     case 16: return <FormattedMessage id='scenes.home.didyouknow.title16' defaultMessage='Investment Scams' />
     case 17: return <FormattedMessage id='scenes.home.didyouknow.title17' defaultMessage='How can I spot an investment scam?' />
     case 18: return <FormattedMessage id='scenes.home.didyouknow.title18' defaultMessage='What are private keys?' />
@@ -95,9 +95,9 @@ const selectContent = (number) => {
       <Link href='https://github.com/blockchain/blockchain-wallet-v4-frontend/' target='_blank' size='14px' weight={200}>
         <FormattedMessage id='scenes.faq.group.walletfunctionality.content4.link' defaultMessage='Github repo' />
       </Link>
-      <FormattedMessage id='scenes.home.didyouknow.content4.one' defaultMessage='to contribute to the development of our wallet.' />
+      <FormattedMessage id='scenes.home.didyouknow.content4.two' defaultMessage='to contribute to the development of our wallet.' />
     </DidYouKnowWrapper>)
-    case 5: return <FormattedMessage id='scenes.home.didyouknow.content5' defaultMessage="Want to set your own bitcoin fees? You can! Just click on 'Customize Fee' in the send screen." />
+    case 5: return <FormattedMessage id='scenes.home.didyouknow.content5' defaultMessage='Want to set your own bitcoin fees? You can! Just click on ‘Customize Fee’ in the send screen.' />
     case 6: return <FormattedMessage id='scenes.home.didyouknow.content6' defaultMessage='We adjust your fees dynamically depending on network conditions and the size of your transaction to ensure your transaction gets confirmed quickly.' />
     case 7: return <FormattedMessage id='scenes.home.didyouknow.content7' defaultMessage='Transaction fees are needed for sending any funds, and are collected by the network of miners of each currency. To assure your transaction is confirmed, we automatically include an appropriate fee based on network standards.' />
     case 8: return (<DidYouKnowWrapper>
@@ -111,42 +111,47 @@ const selectContent = (number) => {
       </Link>
       <FormattedMessage id='scenes.home.didyouknow.content8.three' defaultMessage='device to use your wallet on the go.' />
     </DidYouKnowWrapper>)
-    case 9: return <FormattedMessage id='scenes.home.didyouknow.content9' defaultMessage="Keep your wallet safe and secure by visiting the Security Center to record your backup phrase and enabling two-factor authentication. We also recommend using a unique, random password that's at least 16 characters or more." />
-    case 10: return <FormattedMessage id='scenes.home.didyouknow.content10' defaultMessage="It's like a username, and you only use it to log in to your wallet. To send /receive bitcoin, ether, or bitcoin cash you need an address.To find your address, click Request, and select a digital asset in the currency dropdown." />
+    case 9: return <FormattedMessage id='scenes.home.didyouknow.content9' defaultMessage='Keep your wallet safe and secure by visiting the Security Center to record your backup phrase and enabling 2-step Verification. We also recommend using a unique, random password that’s at least 16 characters or more!' />
+    case 10: return <FormattedMessage id='scenes.home.didyouknow.content10' defaultMessage='It‘s like a username, and you only use it to log in to your wallet. To send/receive any digital currencies you need an address. To find your address, click Request, and select a digital asset in the currency dropdown.' />
     case 11: return (<DidYouKnowWrapper>
       <FormattedMessage id='scenes.home.didyouknow.content11' defaultMessage='Did you know you can help us take down phishing sites? Report them directly to Google using' />
+      <span>&nbsp;</span>
       <Link href='https://support.google.com/adwords/troubleshooter/4578507' target='_blank' size='14px' weight={200}>
-        <FormattedMessage id='scenes.home.didyouknow.content11.link' defaultMessage=' this form' />
+        <FormattedMessage id='scenes.home.didyouknow.content11.link' defaultMessage='this form' />
       </Link>
-      <FormattedMessage id='scenes.home.didyouknow.content11.two' defaultMessage=". Bookmark it so it's always on hand!" />
+      <FormattedMessage id='scenes.home.didyouknow.content11.two' defaultMessage='. Bookmark it so it’s always on hand!' />
     </DidYouKnowWrapper>)
-    case 12: return <FormattedMessage id='scenes.home.didyouknow.content12' defaultMessage="Did you know you can identify whether a site protects your sensitive information with SSL? Simply look for a green lock in the left of your browser's URL bar." />
+    case 12: return <FormattedMessage id='scenes.home.didyouknow.content12' defaultMessage='Did you know you can identify whether a site protects your sensitive information with SSL? Simply look for a green lock in the left of your browser’s URL bar.' />
     case 13: return (<DidYouKnowWrapper>
-      <FormattedMessage id='scenes.home.didyouknow.content13.one' defaultMessage="Beware of emails that say your Blockchain Wallet is suspended or locked. Because we're noncustodial, you're the only one who can manage your funds, keep them secure, and access your wallet. Learn more about this topic" />
+      <FormattedMessage id='scenes.home.didyouknow.content13.one' defaultMessage='Beware of emails that say your Blockchain Wallet is suspended or locked. Because we’re non-custodial, you’re the only one who can manage your funds, keep them secure, and access your wallet. Learn more about this topic' />
+      <span>&nbsp;</span>
       <Link href='https://blockchain.info/wallet/how-it-works' target='_blank' size='14px' weight={200}>
-        <FormattedMessage id='scenes.home.didyouknow.content12.link' defaultMessage=' here' />
+        <FormattedMessage id='scenes.home.didyouknow.content12.link' defaultMessage='here' />
       </Link>
       <FormattedMessage id='scenes.home.didyouknow.content13.two' defaultMessage='.' />
     </DidYouKnowWrapper>)
     case 14: return (<DidYouKnowWrapper>
       <FormattedMessage id='scenes.home.didyouknow.content14.one' defaultMessage='Phishing scams exist to try and deceive users into providing personal or financial information. Outsmart them by learning about the' />
+      <span>&nbsp;</span>
       <Link href='https://blog.blockchain.com/2017/11/01/scams-social-media-recent-trends-outsmart/' target='_blank' size='14px' weight={200}>
-        <FormattedMessage id='scenes.home.didyouknow.content14.link' defaultMessage=' most common tricks ' />
+        <FormattedMessage id='scenes.home.didyouknow.content14.link' defaultMessage='most common tricks' />
       </Link>
+      <span>&nbsp;</span>
       <FormattedMessage id='scenes.home.didyouknow.content14.two' defaultMessage='used.' />
     </DidYouKnowWrapper>)
-    case 15: return <FormattedMessage id='scenes.home.didyouknow.content15' defaultMessage="Like gold, bitcoins can never be doubled or multiplied. This is because the total number of coins and how they can be generated are already built-in to the protocol code. If a company claims they've discovered a magic doubling formula, consider that a sure-fire sign they're a scam." />
+    case 15: return <FormattedMessage id='scenes.home.didyouknow.content15' defaultMessage='Like gold, bitcoins can never be doubled or multiplied. This is because the total number of coins and how they can be generated are already built-in to the protocol code. If a company claims they’ve discovered a magic doubling formula, consider that a sure-fire sign they’re a scam.' />
     case 16: return <FormattedMessage id='scenes.home.didyouknow.content16' defaultMessage='Blockchain does not offer bitcoin investment opportunities, but scams exist that desperately want you to believe otherwise. If one of these scams sneak their way into your inbox, be sure to do us a solid and grab a screenshot to share with our support team before you delete it.' />
-    case 17: return <FormattedMessage id='scenes.home.didyouknow.content17' defaultMessage="Legitimate investing platforms do exist, but their scheming counterparts present some serious red flags: they'll contact you through an unsolicited email and they will guarantee profits. Before investing with any platform, always do your research about the company's reputation, location, and contact details." />
+    case 17: return <FormattedMessage id='scenes.home.didyouknow.content17' defaultMessage='Legitimate investing platforms do exist, but their scheming counterparts present some serious red flags: they’ll contact you through an unsolicited email, and they’ll guarantee profits. Before investing with any platform, always do your research about the company’s reputation, location, and contact details.' />
     case 18: return <FormattedMessage id='scenes.home.didyouknow.content18' defaultMessage='Every address has an accompanying private key, which is what indicates ownership or control over funds at that particular address. In other words, whoever has the private key can spend those funds.' />
     case 19: return (<DidYouKnowWrapper>
-      <FormattedMessage id='scenes.home.didyouknow.content19.one' defaultMessage='Your Blockchain wallet backup phrase serves as the copy of your private keys. You can find this phrase in your' />
+      <FormattedMessage id='scenes.home.didyouknow.content19.one' defaultMessage='Your Blockchain wallet backup phrase serves as the copy of your private keys, you can find this phrase in your' />
+      <span>&nbsp;</span>
       <NavLink to='/security-center' style={{textDecoration: 'none', color: Color('brand-secondary')}} >
-        <FormattedMessage id='scenes.home.didyouknow.content19.navlink' defaultMessage=' Security Center' />
+        <FormattedMessage id='scenes.home.didyouknow.content19.navlink' defaultMessage='Security Center' />
       </NavLink>
       <FormattedMessage id='scenes.home.didyouknow.content19.two' defaultMessage='. It makes backing up a one- time process for all funds except Imported Addresses.' />
     </DidYouKnowWrapper>)
-    case 20: return <FormattedMessage id='scenes.home.didyouknow.content20' defaultMessage='Your passwords are considered secure as long as they are 16 characters minimum, unique, and include a diverse range of letters, numbers, and symbols. ' />
+    case 20: return <FormattedMessage id='scenes.home.didyouknow.content20' defaultMessage='Your passwords are considered secure as long as they are 16 characters minimum, unique, and include a diverse range of letters, numbers, and symbols.' />
     case 21: return <FormattedMessage id='scenes.home.didyouknow.content22' defaultMessage='Our wallet uses a dynamic fee structure. This means that when you send bitcoin, fees adjust based on network conditions & your transaction size.' />
     default: return <FormattedMessage id='scenes.home.didyouknow.content23' defaultMessage='You only have to write down your Wallet Backup Phrase *once* to make sure all funds (except Imported Addresses) are backed up. To make sure your imported addresses are backed up too, transfer your funds to your wallet by using Send.' />
   }
