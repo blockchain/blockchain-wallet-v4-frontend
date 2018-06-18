5d1f6b9f848b18f36d0431441dd641ed47436eae
schnogz <andrew@blockchain.com>
2018-06-18 15:39:22 -0400

    verify words on backup phrase form onSubmit

--- packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/WalletRecoveryPhrase/RecordBackupPhrase/ThirdStep/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/WalletRecoveryPhrase/RecordBackupPhrase/ThirdStep/template.js
@@ -3,34 +3,34 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
 import { reduxForm, Field } from 'redux-form'
-import { toLower, equals } from 'ramda'
 
-import { Button, Link, Icon, Text } from 'blockchain-info-components'
+import { Button, Link, Text } from 'blockchain-info-components'
 import { Form, TextBox } from 'components/Form'
-import { SuccessOverlay } from 'components/Security'
 import { required } from 'services/FormHelper'
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (min-width: 992px) {
+    width: 118%;
+  }
+`
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-  width: 75%;
-  margin: 0 auto;
+  width: 100%;
+  padding: 25px 0;
 `
 const Buttons = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 20px;
-  button {
-    margin-bottom: 20px;
-  }
-`
-const VerificationContainer = styled.div`
-  width: 118%;
-  opacity: ${props => props.authType ? 0.3 : 1};
+  margin: 15px 0;
 `
 
 const WordContainer = styled.div`
@@ -39,26 +39,8 @@ const WordContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   height: 60px;
-  width: 100px;
-  div:first-of-type {
-    margin-bottom: 30px;
-  }
+  width: 125px;
 `
-const WordError = styled.label`
-  position: absolute;
-  top: ${props => props.errorBottom ? '35px' : '60px'};
-  right: 0;
-  display: block;
-  height: 15px;
-  font-size: 12px;
-  font-weight: 300;
-  color: ${props => props.theme['error']};
-`
-
-const validateWord = index => (value, allValues, props) => {
-  console.info(value, allValues, props, index)
-  return equals(toLower(value), toLower(props.recoveryPhrase[index])) ? undefined : <WordError errorBottom={props.errorBottom}>Invalid</WordError>
-}
 
 const languageHelper = (num) => {
   switch (num) {
@@ -70,25 +52,27 @@ const languageHelper = (num) => {
 }
 
 const ThirdStep = (props) => {
-  const { previousStep, showSuccess, recoveryPhrase, submitting, invalid, ...rest } = props
-  const { indexes, handleSubmit, isMnemonicVerified } = rest
+  const { previousStep, submitting, invalid, hasError, ...rest } = props
+  const { indexes, handleSubmit } = rest
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <SuccessOverlay success={showSuccess && isMnemonicVerified}>
-        <Icon name='checkmark-in-circle' size='150px' color='success' />
-        <Text size='14px' weight={300} color='success'>
-          <FormattedMessage id='scenes.securitycenter.walletrecoveryphrase.thirdstep.verified' defaultMessage="Congrats! You've successfully verified your Backup Phrase!" />
-        </Text>
-      </SuccessOverlay>
-      <VerificationContainer authType={showSuccess && isMnemonicVerified}>
+    <Wrapper>
+      { hasError
+        ? (
+          <Link size='14px' weight={300} onClick={previousStep} style={{ margin: '10px 0' }}>
+            <FormattedMessage id='scenes.securitycenter.walletrecoveryphrase.thirdstep.reviewPhrase' defaultMessage='Review your backup phrase' />
+          </Link>
+        )
+        : null
+      }
+      <Form onSubmit={handleSubmit}>
         <Container>
           {indexes.map(index =>
-            <WordContainer>
-              <Text size='13px' weight={300}>
+            <WordContainer key={index}>
+              <Text size='14px' weight={300} style={{ marginBottom: '4px' }}>
                 {`${languageHelper(index)} word`}
               </Text>
-              <Field name={`word${index}`} component={TextBox} validate={[required]} center />
+              <Field name={`w${index}`} component={TextBox} validate={[required]} errorBottom />
             </WordContainer>
           )}
         </Container>
@@ -96,16 +80,9 @@ const ThirdStep = (props) => {
           <Button type='submit' nature='primary' disabled={submitting || invalid}>
             <FormattedMessage id='scenes.securitycenter.walletrecoveryphrase.thirdstep.confirm' defaultMessage='Confirm' />
           </Button>
-          {/*{*/}
-            {/*invalid*/}
-              {/*? <Link size='12px' weight={300} onClick={previousStep}>*/}
-                {/*<FormattedMessage id='scenes.securitycenter.walletrecoveryphrase.thirdstep.doublecheck' defaultMessage='Double check your backup phrase' />*/}
-              {/*</Link>*/}
-              {/*: null*/}
-          {/*}*/}
         </Buttons>
-      </VerificationContainer>
-    </Form>
+      </Form>
+    </Wrapper>
   )
 }
 
