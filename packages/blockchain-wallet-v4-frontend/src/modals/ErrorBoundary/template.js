56766b581260ca8482075fa60ebe4300c5a8a426
schnogz <andrew@blockchain.com>
2018-06-18 10:27:21 -0400

    update copy for warning screen

--- packages/blockchain-wallet-v4-frontend/src/modals/ErrorBoundary/template.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/ErrorBoundary/template.js
@@ -35,14 +35,14 @@ const ErrorBoundary = (props) => {
           <TitleGroup inline>
             <Icon name='alert-filled' size='28px' color='brand-primary' />
             <Text weight={300} size={'22px'} color='brand-primary' style={{ paddingLeft: '8px' }}>
-              <FormattedMessage id='layout.errorboundary.title' defaultMessage='Something went terribly wrong!' />
+              <FormattedMessage id='modal.errorboundary.title' defaultMessage="Oops! Something's not right here" />
             </Text>
           </TitleGroup>
         </ModalHeader>
         <ModalBody>
           <TextGroup>
             <Text weight={300} size={'14px'} color='gray-5'>
-              <FormattedMessage id='layout.errorboundary.subtitle1' defaultMessage="We're sorry, but something very unexpected has gone wrong. Please try again or contact support if the issue persists." />
+              <FormattedMessage id='modal.errorboundary.message' defaultMessage="We're sorry, but it seems like something is not quite right. Please try again or contact support if the problem persists." />
             </Text>
           </TextGroup>
           <Text weight={300} size={'14px'}>
@@ -56,7 +56,7 @@ const ErrorBoundary = (props) => {
         </ModalBody>
         <ModalFooter align='right'>
           <Button type='submit' nature='primary' onClick={() => { }}>
-            <FormattedMessage id='layout.errorboundary.continue' defaultMessage='Continue' />
+            <FormattedMessage id='modal.errorboundary.continue' defaultMessage='Continue' />
           </Button>
         </ModalFooter>
       </Form>
