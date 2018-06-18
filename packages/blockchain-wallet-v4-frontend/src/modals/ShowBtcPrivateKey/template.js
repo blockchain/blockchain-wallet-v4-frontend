07de509e6294e9891efa2671fcecd3be271d4c13
TheLeoB <leora@blockchain.com>
2018-06-18 17:23:57 -0400

    fix(Private Key): wrap BTC key

--- packages/blockchain-wallet-v4-frontend/src/modals/ShowBtcPrivateKey/template.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/ShowBtcPrivateKey/template.js
@@ -16,9 +16,14 @@ const DetailRow = styled.div`
   display: flex;
   align-items: center;
   flex-direction: row;
+  flex-wrap: wrap;
 `
 const DetailRowText = styled(Text)`
   white-space: nowrap;
+`
+const KeyText = styled(Text)`
+  min-width: 0;
+  word-wrap: break-word;
 `
 
 const FirstStep = () => (
@@ -61,7 +66,7 @@ const SecondStep = ({ addr, balance, priv, format, formats, onChangeFormat }) =>
         {':'}&nbsp;
         {utils.bitcoin.formatPrivateKeyString(priv, format, addr).fold(
           error => (<Text size='14px' weight={300} color='error'>{error.message}</Text>),
-          keyString => (<Text size='14px' weight={300}>{keyString}</Text>)
+          keyString => (<KeyText size='14px' weight={300}>{keyString}</KeyText>)
         )}
       </DetailRow>
       <DetailRow>
