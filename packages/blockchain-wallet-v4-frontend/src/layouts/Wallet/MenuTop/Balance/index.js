caf97015db770db6025190d859dd25f33f3578fc
Guillaume Marquilly <guillaume.marquilly@gmail.com>
2018-06-19 16:21:16 +0100

    fix - forgotten files

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/index.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 
-import { actions, selectors } from 'data'
+import { selectors } from 'data'
 import Template from './template'
 
 class Balance extends React.PureComponent {
-  componentDidMount () {
-    this.props.actions.initialized()
-  }
   render () {
     return <Template path={this.props.path} />
   }
@@ -18,8 +14,4 @@ const mapStateToProps = state => ({
   path: selectors.router.getPathname(state)
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(actions.components.balance, dispatch)
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Balance)
+export default connect(mapStateToProps)(Balance)
