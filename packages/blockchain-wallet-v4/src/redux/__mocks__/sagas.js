86ce17ac0c60c8af4e78d653698922155c89165a
tony-blockchain <tony@blockchain.com>
2018-06-21 13:04:00 +0300

    Test - added 2fa reset and sms login resend test flows

--- packages/blockchain-wallet-v4/src/redux/__mocks__/sagas.js
+++ packages/blockchain-wallet-v4/src/redux/__mocks__/sagas.js
@@ -1,8 +1,13 @@
+
+// TODO: remake mock to be based on actual sagas:
+// If they cease to exist mocks must also break
 export default () => ({
   wallet: {
     createWalletSaga: jest.fn(),
     fetchWalletSaga: jest.fn(),
     remindWalletGuidSaga: jest.fn(),
+    resendSmsLoginCode: jest.fn(),
+    resetWallet2fa: jest.fn(),
     restoreWalletSaga: jest.fn()
   },
   kvStore: {
