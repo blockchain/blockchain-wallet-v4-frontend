44e8ffcabf509282c5313b537d2d7eb86e23cba8
schnogz <andrew@blockchain.com>
2018-06-21 15:02:47 -0400

    ditch using a banner, creating custom service messaging component

--- packages/blockchain-wallet-v4-frontend/src/components/ServiceAnnouncement/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/ServiceAnnouncement/index.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+
 import { actions, selectors } from 'data'
 import Announcement from './template.js'
 
 class ServiceAnnouncement extends React.PureComponent {
   constructor (props) {
     super(props)
+    this.state = { collapsed: false, visible: true }
     this.handleDismiss = this.handleDismiss.bind(this)
+    this.toggleCollapse = this.toggleCollapse.bind(this)
   }
 
+  componentWillMount () {}
+
+  componentWillUnmount () {}
+
   handleDismiss (id) {
+    this.setState({ visible: false })
     // TODO: write to local storage that user dismissed
     this.props.alertActions.dismissAlert(id)
   }
 
+  toggleCollapse () {
+    this.setState({ collapsed: !this.state.collapsed })
+  }
+
   render () {
     const { announcements, type } = this.props
-    return <Announcement announcement={announcements.data[type]} handleDismiss={this.handleDismiss} />
+
+    return this.state.visible && <Announcement announcement={announcements.data[type]} collapsed={this.state.collapsed} handleDismiss={this.handleDismiss} toggleCollapse={this.toggleCollapse} />
   }
 }
 
@@ -28,5 +42,9 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   alertActions: bindActionCreators(actions.alerts, dispatch)
 })
+
+ServiceAnnouncement.propTypes = {
+  type: PropTypes.oneOf(['global', 'wallet']).isRequired
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(ServiceAnnouncement)
