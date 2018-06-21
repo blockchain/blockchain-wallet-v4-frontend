1e5220e916ad5423c6646b6f222081f050c9b606
schnogz <andrew@blockchain.com>
2018-06-21 17:25:01 -0400

    pull dismiss/toggle from wallet options and style

--- packages/blockchain-wallet-v4-frontend/src/components/ServiceAnnouncement/template.js
+++ packages/blockchain-wallet-v4-frontend/src/components/ServiceAnnouncement/template.js
@@ -7,32 +7,36 @@ import { Link, Icon, Text, TextGroup } from 'blockchain-info-components'
 const Container = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: flex-start;
+  align-items: center;
   background: ${props => props.theme[props.color]};
-  border: 1px solid ${props => props.theme[props.color]};
+  border: 2px solid ${props => props.theme[props.color]};
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   margin: 0 auto;
   overflow: hidden;
-  height: ${props => props.collapsed ? '26px' : ''};
+  height: ${props => props.collapsed ? '35px' : ''};
   width: 100%;
-  padding: 5px 10px;
-  & > span:first-child { margin-right: ${props => props.inline ? '3px' : '5px'} };
+`
+const ActionLink = styled(Link)`
+  margin-left: 0;
+  margin-top: 0;
+  color: blue;
+  text-decoration: underline;
 `
 const ActionIcon = styled(Icon)`
-  margin-top: 5px;
   &:hover {
     cursor: pointer;
   }
 `
+const IconContainer = styled.div`
+  margin: 0 25px;
+`
 
 const selectStyle = type => {
   switch (type) {
-    case 'success': return { color: 'success', uppercase: false, icon: 'checkmark-in-circle' }
-    case 'warning': return { color: 'error', uppercase: true, icon: 'alert' }
-    case 'alert': return { color: 'brand-secondary', uppercase: false, icon: 'bell' }
-    case 'caution': return { color: 'brand-yellow', uppercase: false, icon: 'alert' }
+    case 'danger': return { color: 'brand-yellow', uppercase: true, icon: 'alert' }
     case 'info': return { color: 'brand-tertiary', uppercase: false, icon: null }
+    case 'warning': return { color: 'error', uppercase: false, icon: 'alert' }
     default: return { color: 'brand-tertiary', uppercase: false, icon: null }
   }
 }
@@ -41,28 +45,29 @@ const Announcement = props => {
   const { announcement, collapsed, handleDismiss, toggleCollapse } = props
   const style = selectStyle(announcement.type)
   const { color, uppercase, icon } = style
-  const dismissible = true
-  const collapsible = true
 
   return (
     <Container color={color} collapsed={collapsed}>
-      <Icon name='bell' size='14px' weight={600} />
-      { icon && <Icon name={icon} size='18px' weight={600} color={color} /> }
-      <div>
-        <Text size='20px' uppercase={uppercase}>{announcement.header.en}</Text>
+      <IconContainer>
+        { icon && <Icon name={icon} size='24px' weight={600} color={color} /> }
+      </IconContainer>
+      <div style={{width: '100%'}}>
+        <Text weight={300} size='20px' uppercase={uppercase} style={{margin: '6px 0'}}>{announcement.header.en}</Text>
         <TextGroup style={{ display: collapsed ? 'none' : '' }}>
           {
             announcement.sections.map(section => {
-              return <Text size='13px'>{section.body.en}</Text>
+              return <Text size='13px' style={{marginBottom: '2px'}}>{section.body.en}</Text>
             })
           }
-          <Link href={announcement.action.link} target='_blank' style={{marginLeft: 0, color: 'blue'}}>
+          <ActionLink href={announcement.action.link} target='_blank'>
             <Text color='brand-primary' size='14px'>{announcement.action.title.en}</Text>
-          </Link>
+          </ActionLink>
         </TextGroup>
       </div>
-      { collapsible && <ActionIcon name={collapsed ? 'down-arrow' : 'up-arrow'} size='14px' weight={600} onClick={toggleCollapse} /> }
-      { dismissible && <ActionIcon name='close' size='14px' weight={600} onClick={handleDismiss} /> }
+      <IconContainer>
+        { announcement.hideType === 'collapse' && <ActionIcon name={collapsed ? 'down-arrow' : 'up-arrow'} size='18px' weight={600} onClick={toggleCollapse} /> }
+        { announcement.hideType === 'dismiss' && <ActionIcon name='close' size='18px' weight={600} onClick={handleDismiss} /> }
+      </IconContainer>
     </Container>
   )
 }
