2fda8b34becf00d736b71dadf9fdcc4717e2de67
TheLeoB <leora@blockchain.com>
2018-06-22 11:20:23 -0400

    feat(Alerts): finish receive currency success alerts

--- packages/blockchain-wallet-v4-frontend/src/data/alerts/sagaRegister.js
+++ packages/blockchain-wallet-v4-frontend/src/data/alerts/sagaRegister.js
@@ -4,7 +4,4 @@ import * as sagas from './sagas'
 
 export default function * () {
   yield takeEvery(AT.alerts.ALERTS_SHOW, sagas.handleTimer)
-  yield takeEvery(AT.core.webSocket.bitcoin.BTC_PAYMENT_RECEIVED, sagas.bitcoinPaymentReceived)
-  yield takeEvery(AT.core.webSocket.bch.BCH_PAYMENT_RECEIVED, sagas.bchPaymentReceived)
-  yield takeEvery(AT.core.webSocket.ethereum.ETH_PAYMENT_RECEIVED, sagas.ethereumPaymentReceived)
 }
--- packages/blockchain-wallet-v4-frontend/src/data/alerts/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/alerts/sagas.js
@@ -1,7 +1,6 @@
 import { delay } from 'redux-saga'
 import { call, put } from 'redux-saga/effects'
 import * as actions from './actions.js'
-import * as C from 'services/AlertService'
 
 const DISMISS_AFTER = 7000
 
@@ -9,14 +8,4 @@ export const handleTimer = function * (action) {
   const { id } = action.payload
   yield call(delay, DISMISS_AFTER)
   yield put(actions.dismissAlert(id))
-}
-
-export const bitcoinPaymentReceived = function * (action) {
-  yield put(actions.displaySuccess(C.RECEIVE_BTC_SUCCESS))
-}
-export const bchPaymentReceived = function * (action) {
-  yield put(actions.displaySuccess(C.RECEIVE_BCH_SUCCESS))
-}
-export const ethereumPaymentReceived = function * (action) {
-  yield put(actions.displaySuccess(C.RECEIVE_ETH_SUCCESS))
 }
