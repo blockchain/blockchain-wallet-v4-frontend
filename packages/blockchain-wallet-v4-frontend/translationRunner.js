bbc6e429ea78fcbce7d52d6f8a47adc9b667307f
plondon <philip@blockchain.com>
2018-06-22 21:44:29 -0400

    fix(Translations): make files compatible with one hour

--- packages/blockchain-wallet-v4-frontend/translationRunner.js
+++ packages/blockchain-wallet-v4-frontend/translationRunner.js
@@ -1,6 +1,23 @@
 import manageTranslations from 'react-intl-translations-manager'
+import bcManageTranslations from 'bc-react-intl-translations-manager'
+
+// TODO: fork and publish version of react-intl-translations-manager
+
+// react-intl-translations-manager sets the defaultMessage as the value
+// for translations that are not yet set. one hour needs only the en.json
+// and the translation file with the same keys but the values empty or null
 
 manageTranslations({
+  messagesDirectory: './build/extractedMessages',
+  translationsDirectory: './src/assets/locales',
+  whitelistsDirectory: './src/assets/locales/whitelists',
+  singleMessagesFile: true,
+  languages: [
+    'en'
+  ]
+})
+
+bcManageTranslations({
   messagesDirectory: './build/extractedMessages',
   translationsDirectory: './src/assets/locales',
   whitelistsDirectory: './src/assets/locales/whitelists',
@@ -10,7 +27,6 @@ manageTranslations({
     'da',
     'de',
     'el',
-    'en',
     'es',
     'fr',
     'hi',
