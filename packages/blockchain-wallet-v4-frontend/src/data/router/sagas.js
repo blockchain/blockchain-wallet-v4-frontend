ed1e6dbe3413b4fb14dd18966725568a5c82e8fd
Salome <salomejumala@gmail.com>
2018-06-24 00:48:01 +0400

    log out when navigating back to login page

--- packages/blockchain-wallet-v4-frontend/src/data/router/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/router/sagas.js
@@ -4,7 +4,12 @@ import { actions } from 'data'
 export default () => {
   const logLocation = 'modules/router/sagas'
 
-  const changeLocation = function * () {
+  const changeLocation = function * ({payload}) {
+    
+    const {location, action} = payload
+    if (action === 'POP' && location.pathname === '/login') {
+      yield put(actions.auth.logout())
+    }
     try {
       yield put(actions.modals.closeAllModals())
     } catch (e) {
