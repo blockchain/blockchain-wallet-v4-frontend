0ec330728439a6d11a63097ad3796f81ba015ed2
schnogz <andrew@blockchain.com>
2018-06-25 10:01:16 -0400

    add announcements state to cache

--- packages/blockchain-wallet-v4-frontend/src/data/cache/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/data/cache/selectors.js
@@ -1,3 +1,4 @@
 import { path } from 'ramda'
 
+export const getAnnouncemnt = (id, state) => path(['cache', 'announcements', id], state)
 export const getLastGuid = (state) => path(['cache', 'lastGuid'], state)
