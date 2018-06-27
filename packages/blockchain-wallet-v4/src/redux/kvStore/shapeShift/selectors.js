f0267f797d1b936de70fc9e5c328a98820a1a58e
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2018-06-27 18:25:12 +0100

    refactor(improve use of ramda)

--- packages/blockchain-wallet-v4/src/redux/kvStore/shapeShift/selectors.js
+++ packages/blockchain-wallet-v4/src/redux/kvStore/shapeShift/selectors.js
@@ -1,9 +1,9 @@
-import { compose, curry, filter, head, path, reverse } from 'ramda'
+import { compose, curry, equals, filter, head, path, reverse } from 'ramda'
 import { SHAPESHIFT } from '../config'
 import { kvStorePath } from '../../paths'
 
 export const getMetadata = path([kvStorePath, SHAPESHIFT])
 export const getUsState = state => getMetadata(state).map(path(['value', 'USAState']))
-export const getTrades = state => getMetadata(state).map(path(['value', 'trades'])).map(trades => reverse(trades))
+export const getTrades = state => getMetadata(state).map(compose(reverse, path(['value', 'trades'])))
 export const getTrade = curry((address, state) => getTrades(state)
-  .map(compose(head, filter(x => path(['quote', 'deposit'], x) === address))))
+  .map(compose(head, filter(x => equals(path(['quote', 'deposit'], x), address)))))
