a507ae7e9465cf7fc50d79fe0e6001e3b8586c8e
tony-blockchain <tony@blockchain.com>
2018-06-29 04:00:34 +0300

    fix - added node memoizing, future address derivation and upgraded sync with active addresses

--- packages/blockchain-wallet-v4/src/redux/walletSync/middleware.js
+++ packages/blockchain-wallet-v4/src/redux/walletSync/middleware.js
@@ -1,35 +1,79 @@
 import { futurizeP } from 'futurize'
 import Task from 'data.task'
-import { compose, assoc, join, pipe, curry, map, range, identity, converge, add, keysIn } from 'ramda'
+import { compose, assoc, join, pipe, curry, map, range,
+  identity, converge, add, keysIn } from 'ramda'
 import { networks } from 'bitcoinjs-lib'
 
 import * as A from '../actions'
 import * as T from '../actionTypes'
-import { Wrapper, HDAccount, Wallet } from '../../types'
+import { Wrapper, Wallet, HDAccount } from '../../types'
 import * as selectors from '../selectors'
 
-const getHDAccountAddresses = curry((state, account) => {
+/**
+ * Number of unused addresses for each HD Account
+ * to sync with platform
+ */
+export const unusedAddressesCount = 10
+
+/**
+ * Launches derivation of future addresses for target HDAccount
+ * @curried
+ * @param {any} state redux state
+ * @param {HDAccount.HDAccount} account
+ * @returns {Promise<String>[]} array of promises, each of them resolves into address
+ */
+export const getHDAccountAddressPromises = curry((state, account) => {
   const xpub = selectors.wallet.getAccountXpub(account.index, state)
   return selectors.data.bitcoin.getReceiveIndex(xpub, state)
     .map(pipe(
-      converge(range, [identity, add(20)]),
-      map((index) => HDAccount.getReceiveAddress(
-        account,
-        index,
-        networks.bitcoin.NETWORK_BITCOIN
+      /**
+       * Get the range of address indexes
+       * (receiveIndex, receiveIndex + unusedAddressesCount)
+       */
+      converge(range, [identity, add(unusedAddressesCount)]),
+
+      /**
+       * Get each address within separate event queue entry
+       * in order to unblock the UI during heavy scripting
+       * setTimeout runs infrequently and is less blocking
+       * requestAnimation frame blocks UI heavier
+       */
+      map((index) => new Promise((resolve) => setTimeout(() =>
+        resolve(
+          HDAccount.getReceiveAddress(
+            account,
+            index,
+            networks.bitcoin.NETWORK_BITCOIN
+          )
+        ), 0)
       ))
     ))
     .getOrElse([])
 })
 
-const getWalletAddresses = state => {
+/**
+ * Derives wallet's unused addresses
+ * @param {any} state redux state
+ * @returns {Promise<String[]>} unused wallet addresses
+ */
+export const getWalletAddresses = async state => {
   const activeAddresses = keysIn(selectors.wallet.getActiveAddresses(state))
   const hdAccounts = compose(Wallet.selectHDAccounts, selectors.wallet.getWallet)(state)
-  const hdAddresses = hdAccounts.flatMap(getHDAccountAddresses(state)).toJS()
+  const hdAddresses = await Promise.all(
+    hdAccounts
+      .flatMap(getHDAccountAddressPromises(state))
+      .toJS()
+  )
 
   return activeAddresses.concat(hdAddresses)
 }
 
+/**
+ * Wallet sync middleware
+ * Calls sync on special conditions
+ *
+ * TODO: refactor to sagas, VERY painful to test/write mocks
+ */
 const walletSync = ({ isAuthenticated, api } = {}) => (store) => (next) => (action) => {
   const prevState = store.getState()
   const prevWallet = selectors.wallet.getWrapper(prevState)
@@ -50,9 +94,13 @@ const walletSync = ({ isAuthenticated, api } = {}) => (store) => (next) => (acti
     return encrypted
   }
 
-  const sync = (apiCall) =>
+  const sync = async (apiCall) =>
     Wrapper.toEncJSON(nextWallet)
-      .map(assoc('active', join('|', getWalletAddresses(state))))
+      /**
+       * To get notifications working you have to add list of unused addresses
+       * For each of the wallet's accounts
+       */
+      .map(assoc('active', join('|', await getWalletAddresses(state))))
       .map(handleChecksum)
       .chain(promiseToTask(apiCall))
       .fork(
