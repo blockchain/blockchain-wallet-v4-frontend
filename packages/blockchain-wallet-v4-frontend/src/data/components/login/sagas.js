09aaacf7b253895e8ff4519938a9f02a1eae73c2
plondon <philip@blockchain.com>
2018-07-10 09:42:04 +0100

    feat(Migration): attach lastGuid and session to state from transfer_stored_values

--- packages/blockchain-wallet-v4-frontend/src/data/components/login/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/login/sagas.js
@@ -1,30 +1,16 @@
-import { put, select } from 'redux-saga/effects'
+import { call, put, select, take } from 'redux-saga/effects'
+import { eventChannel, END } from 'redux-saga'
 import { actions, selectors } from 'data'
+import { assoc } from 'ramda'
+
+// TODO:: DEPRECATE
+// We should not need this in the future. It is used to handle transferring cookies from .info to .com domain
 
 export default () => {
-  // TODO:: DEPRECATE
-  // We should not need this but it is used to handle transferring cookies from .info to .com domain
-  // Consider removing this code in the future
   const initialized = function * () {
     try {
       const alreadyTransferredCookiesKey = 'did_already_transfer_cookies_from_dot_info'
       const shouldTransfer = window.localStorage.getItem(alreadyTransferredCookiesKey) == null
-
-      const createFrame = (src) => {
-        const frame = document.createElement('iframe')
-        frame.src = src
-        frame.style.display = 'none'
-        document.body.appendChild(frame)
-        return frame
-      }
-
-      const tryJsonParse = (value) => {
-        try {
-          return JSON.parse(value)
-        } catch (e) {
-          return value
-        }
-      }
 
       if (shouldTransfer) {
         const domainsR = yield select(selectors.core.walletOptions.getDomains)
@@ -32,23 +18,56 @@ export default () => {
         const rootURL = domains.root
         const frame = createFrame(`${rootURL}/Resources/transfer_stored_values.html`)
 
-        window.addEventListener('message', (event) => {
-          if (event.data.type === 'cookie') {
-            const cookie = event.data.payload
-            window.localStorage.setItem(cookie.id, tryJsonParse(cookie.data))
-          }
+        const windowChannel = function () {
+          return eventChannel((emitter) => {
+            window.addEventListener('message', function (event) {
+              if (event.data.type === 'cookie') {
+                const cookie = event.data.payload
+                window.localStorage.setItem(cookie.id, tryJsonParse(cookie.data))
+              }
 
-          if (event.data.type === 'cookies-sent') {
-            frame.remove()
-            window.localStorage.setItem(alreadyTransferredCookiesKey, true)
-            const lastGuid = window.localStorage.getItem('ls.guid')
-            console.log(lastGuid)
-            // yield put(actions.cache.guidEntered(lastGuid))
+              if (event.data.type === 'cookies-sent') {
+                frame.remove()
+                const lastGuid = window.localStorage.getItem('ls.guid')
+                const session = window.localStorage.getItem('ls.session')
+                window.localStorage.setItem(alreadyTransferredCookiesKey, true)
+                emitter({ lastGuid, session })
+                emitter(END)
+              }
+            })
+
+            return () => {}
+          })
+        }
+
+        const chan = yield call(windowChannel)
+
+        try {
+          while (true) {
+            let { session, lastGuid } = yield take(chan)
+            yield put(actions.cache.guidEntered(lastGuid))
+            yield put(actions.session.saveSession(assoc(lastGuid, session, {})))
           }
-        })
+        } finally {}
       }
     } catch (e) {
       yield put(actions.logs.logErrorMessage('components/login/sagas', 'initialized', e))
+    }
+  }
+
+  const createFrame = (src) => {
+    const frame = document.createElement('iframe')
+    frame.src = src
+    frame.style.display = 'none'
+    document.body.appendChild(frame)
+    return frame
+  }
+
+  const tryJsonParse = (value) => {
+    try {
+      return JSON.parse(value)
+    } catch (e) {
+      return value
     }
   }
 
