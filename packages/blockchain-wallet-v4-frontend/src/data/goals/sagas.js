8b6301ed98052c9e32eef3679eff31f4044181c0
Jack Stettner <jack@stet.com>
2018-07-12 10:54:28 -0400

    more description consistency and fix

--- packages/blockchain-wallet-v4-frontend/src/data/goals/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/goals/sagas.js
@@ -7,12 +7,12 @@ import { Exchange } from 'blockchain-wallet-v4/src'
 export default ({ coreSagas }) => {
   const sendBtcGoalSaga = function * (goal) {
     const { id, data } = goal
-    const { amount, address, message } = data
+    const { amount, address, description } = data
     const currency = yield select(selectors.core.settings.getCurrency)
     const btcRates = yield select(selectors.core.data.bitcoin.getRates)
     const fiat = Exchange.convertBitcoinToFiat({ value: amount, fromUnit: 'BTC', toCurrency: currency.data, rates: btcRates.data }).value
     // Goal work
-    yield put(actions.modals.showModal('SendBitcoin', { to: address, message, amount: {coin: amount, fiat} }))
+    yield put(actions.modals.showModal('SendBitcoin', { to: address, description, amount: {coin: amount, fiat} }))
     // Goal removed from state
     yield put(actions.goals.deleteGoal(id))
   }
