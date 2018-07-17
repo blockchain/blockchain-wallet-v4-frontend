ff8839545f9eb247f20e9df0551204bd5546631c
plondon <philip@blockchain.com>
2018-07-17 19:55:26 -0400

    feat(Jumio): add 'next' to jumio flow, if user has account close, if not go to funding

--- packages/blockchain-wallet-v4-frontend/src/modals/SfoxExchangeData/Jumio/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/SfoxExchangeData/Jumio/selectors.js
@@ -3,14 +3,19 @@ import { lift, path } from 'ramda'
 import { createDeepEqualSelector } from 'services/ReselectHelper'
 
 export const getData = createDeepEqualSelector(
-  [selectors.core.walletOptions.getOptions, path(['sfoxSignup', 'jumioToken'])],
-  (optionsR, tokenR) => {
-    const transform = (options, token) => {
+  [
+    selectors.core.data.sfox.getAccounts,
+    selectors.core.walletOptions.getOptions,
+    path(['sfoxSignup', 'jumioToken'])
+  ],
+  (accountsR, optionsR, tokenR) => {
+    const transform = (accounts, options, token) => {
       return {
+        accounts,
         options,
         token
       }
     }
-    return lift(transform)(optionsR, tokenR)
+    return lift(transform)(accountsR, optionsR, tokenR)
   }
 )
