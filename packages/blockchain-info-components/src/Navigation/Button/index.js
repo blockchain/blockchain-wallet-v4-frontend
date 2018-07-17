81b7cb6fb8afac81e3d391a771355ec4029c783a
Terence Buchannan <terence@blockchain.com>
2018-07-17 15:55:44 -0400

    MobileNav: Correct Colors reference

--- packages/blockchain-info-components/src/Navigation/Button/index.js
+++ packages/blockchain-info-components/src/Navigation/Button/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import * as Color from "../../Colors"
+import * as Color from "../Colors"
 import { trackButtonEvent } from "../Events"
 import Link from "../Link"
 
