0581a2b032971a5f8204160ed4b760403d89492d
schnogz <andrew@blockchain.com>
2018-07-18 18:08:08 -0400

    add route and basic page for Lockbox

---
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Lockbox/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Lockbox from './template.js'
+
+class LockboxContainer extends React.PureComponent {
+  render () {
+    return <Lockbox />
+  }
+}
+
+export default LockboxContainer
