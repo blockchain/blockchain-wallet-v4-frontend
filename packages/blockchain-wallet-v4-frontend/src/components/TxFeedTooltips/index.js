ee745936e65185f6d77c010611a783da2be41325
Jack Stettner <jack@stet.com>
2018-07-19 16:49:35 -0400

    renamed file, fixed styling, exports, and test

--- packages/blockchain-wallet-v4-frontend/src/components/TxFeedTooltips/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/TxFeedTooltips/index.js
@@ -1,49 +1,28 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
-import ReactTooltip from 'react-tooltip'
-import { Link } from 'blockchain-info-components'
-import styled from 'styled-components'
-
-const StyledTip = styled(ReactTooltip)`
-  pointer-events: auto !important;
-  &:hover {
-    visibility: visible !important;
-    opacity: 1 !important;
-  }
-`
+import { Tooltip, Link } from 'blockchain-info-components'
 
 class TxFeedTooltips extends React.PureComponent {
-  render () {
+  render() {
     return (
       <div>
-        <StyledTip
-          id='addr'
-          multiline
-          effect='solid'
-          delayHide={200}
-          offset={{ bottom: 8 }}
-        />
-        <StyledTip
-          id='confirmations'
-          effect='solid'
-          delayHide={200}
-          offset={{ bottom: 8 }}
-        >
+        <Tooltip id="addr" multiline />
+        <Tooltip id="confirmations">
           <FormattedMessage
-            id='scenes.transactions.content.list.listitem.transactionunconfirmed'
-            defaultMessage='Your transaction will be complete after it has {minConfirmations} confirmations.'
+            id="scenes.transactions.content.list.listitem.transactionunconfirmed"
+            defaultMessage="Your transaction will be complete after it has {minConfirmations} confirmations."
             values={{ minConfirmations: 3 }}
           />
           <Link
-            href='https://support.blockchain.com/hc/en-us/articles/217116406-Why-hasn-t-my-transaction-confirmed-yet-'
-            target='_blank'
-            size='11px'
+            href="https://support.blockchain.com/hc/en-us/articles/217116406-Why-hasn-t-my-transaction-confirmed-yet-"
+            target="_blank"
+            size="11px"
             weight={300}
             altFont
           >
             Learn more.
           </Link>
-        </StyledTip>
+        </Tooltip>
       </div>
     )
   }
