fa9d51e8b60674adf4179d4194ceb40cd58cfbf1
Jack Stettner <jack@stet.com>
2018-07-25 09:58:36 -0400

    fixes

--- packages/blockchain-wallet-v4-frontend/src/components/Tooltips/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Tooltips/index.js
@@ -7,22 +7,6 @@ class Tooltips extends React.PureComponent {
     return (
       <div>
         <Tooltip id='addr' multiline offset={{ bottom: 8 }} />
-        <Tooltip id='confirmations' offset={{ bottom: 8 }}>
-          <FormattedMessage
-            id='scenes.transactions.content.list.listitem.transactionunconfirmed'
-            defaultMessage='Your transaction will be complete after it has {minConfirmations} confirmations.'
-            values={{ minConfirmations: 3 }}
-          />
-          <Link
-            href='https://support.blockchain.com/hc/en-us/articles/217116406-Why-hasn-t-my-transaction-confirmed-yet-'
-            target='_blank'
-            size='11px'
-            weight={300}
-            altFont
-          >
-            Learn more.
-          </Link>
-        </Tooltip>
         <Tooltip id='isx.expiredtooltip'>
           <FormattedMessage
             id='scenes.buysell.coinify.isx.expiredtooltip'
