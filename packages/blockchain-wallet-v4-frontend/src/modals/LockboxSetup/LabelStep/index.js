abb150a0fc2a1d435f550597c5a680caac29e259
schnogz <andrew@blockchain.com>
2018-08-01 15:41:10 -0400

    add redux form and basic styles to name device step

--- packages/blockchain-wallet-v4-frontend/src/modals/LockboxSetup/LabelStep/index.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/LockboxSetup/LabelStep/index.js
@@ -3,19 +3,33 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actions } from 'data'
 
-import Template from './template'
+import NameDeviceStep from './template'
+import { formValueSelector } from 'redux-form'
 
-class LabelStep extends React.PureComponent {
+class NameDeviceStepContainer extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit () {
+    this.props.lockboxActions.addDevice(this.props.deviceName)
+  }
+
   render () {
-    return <Template {...this.props} />
+    return <NameDeviceStep onSubmit={this.onSubmit} />
   }
 }
+
+const mapStateToProps = state => ({
+  deviceName: formValueSelector('lockboxNameDevice')(state, 'deviceName')
+})
 
 const mapDispatchToProps = dispatch => ({
   lockboxActions: bindActionCreators(actions.components.lockbox, dispatch)
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
-)(LabelStep)
+)(NameDeviceStepContainer)
--- packages/blockchain-wallet-v4-frontend/src/modals/LockboxSetup/LabelStep/template.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/LockboxSetup/LabelStep/template.js
@@ -1,13 +1,75 @@
 import React from 'react'
 
-const LabelStep = props => {
+import styled from 'styled-components'
+import { required } from 'services/FormHelper'
+import { Button, Text } from 'blockchain-info-components'
+import { FormattedMessage } from 'react-intl'
+import { Field, reduxForm } from 'redux-form'
+import { Form, FormGroup, FormItem, TextBox } from 'components/Form'
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 10px 0;
+
+  & > * {
+    width: 150px;
+  }
+  & > :last-child {
+    width: 100%;
+  }
+  &:first-child {
+    padding-top: 0px;
+  }
+`
+const Label = styled(Text)`
+  display: block;
+  margin-bottom: 5px;
+`
+
+const NameDeviceStep = props => {
+  const { handleSubmit, invalid } = props
+
   return (
-    <React.Fragment>
-      <div onClick={() => props.lockboxActions.addDevice()}>
-        Device is plugged in but has not been added before. Add label now.
-      </div>
-    </React.Fragment>
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Text size='14px' weight={300}>
+          <FormattedMessage
+            id='modals.lockboxsetup.labelstep.plugindevice'
+            defaultMessage='Connection to device established. Please name your Lockbox.'
+          />
+        </Text>
+      </Row>
+      <Row>
+        <FormItem>
+          <Label for='deviceName' weight={400}>
+            <FormattedMessage
+              id='modals.lockboxsetup.labelstep.devicename'
+              defaultMessage='Device Name'
+            />
+          </Label>
+          <Field
+            name='deviceName'
+            defaultValue={'My Lockbox 1'}
+            validate={[required]}
+            component={TextBox}
+          />
+        </FormItem>
+      </Row>
+      <Row>
+        <FormGroup>
+          <Button type='submit' nature='primary' uppercase disabled={invalid}>
+            <FormattedMessage
+              id='modals.lockboxsetup.labelstep.continue'
+              defaultMessage='Continue'
+            />
+          </Button>
+        </FormGroup>
+      </Row>
+    </Form>
   )
 }
 
-export default LabelStep
+export default reduxForm({ form: 'lockboxNameDevice' })(NameDeviceStep)
