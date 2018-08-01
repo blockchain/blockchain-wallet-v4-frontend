d4bf494535463b6eba208b26a9ce70847c6a1242
schnogz <andrew@blockchain.com>
2018-08-01 15:52:27 -0400

    style connect step

--- packages/blockchain-wallet-v4-frontend/src/modals/LockboxSetup/ConnectStep/template.failure.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/LockboxSetup/ConnectStep/template.failure.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import { Text } from 'blockchain-info-components'
 
 const Failure = props => {
   return (
     <React.Fragment>
-      <div>
-        Error connecting to carbon please make sure your device is connected
-      </div>
+      <Text size='16px' weight={300}>
+        <FormattedMessage
+          id='modals.lockboxsetup.connectstep.error'
+          defaultMessage='Unable to connect to your device. Please make sure your device is connected and the bitcoin app is open!'
+        />
+      </Text>
     </React.Fragment>
   )
 }
