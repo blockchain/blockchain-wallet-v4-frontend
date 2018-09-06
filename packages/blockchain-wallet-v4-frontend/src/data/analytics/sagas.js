00e2129b6d7351c7ab91541abd70c1f03a8d1c6f
Philip Welber <welber@blockchain.com>
2018-09-06 13:29:07 -0400

    fix(Analytics): add check for Success on remotes

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/sagas.js
@@ -1,6 +1,8 @@
-import { call, select, put, fork, join } from 'redux-saga/effects'
+import { call, select, put, fork, join, take } from 'redux-saga/effects'
 import * as selectors from '../selectors.js'
 import * as actions from '../actions'
+import * as actionTypes from '../actionTypes'
+import { Remote } from 'blockchain-wallet-v4/src'
 
 export const logLocation = 'analytics/sagas'
 
@@ -8,22 +10,39 @@ export default ({ api, coreSagas }) => {
   const getEthBalance = function*() {
     try {
       const ethBalanceR = yield select(selectors.core.data.ethereum.getBalance)
+      if (!Remote.Success.is(ethBalanceR)) {
+        yield take(actionTypes.core.data.ethereum.FETCH_ETHEREUM_DATA_SUCCESS)
+        const ethBalanceR = yield select(selectors.core.data.ethereum.getBalance)
+        return ethBalanceR.getOrElse(0)
+      }
       return ethBalanceR.getOrElse(0)
     } catch (e) {
       yield put(actions.logs.logErrorMessage(logLocation, 'getEthbalance', e))
     }
   }
+
   const getBtcBalance = function* () {
     try {
       const btcBalanceR = yield select(selectors.core.data.bitcoin.getBalance)
+      if (!Remote.Success.is(btcBalanceR)) {
+        yield take(actionTypes.core.data.bitcoin.FETCH_BITCOIN_DATA_SUCCESS)
+        const btcBalanceR = yield select(selectors.core.data.bitcoin.getBalance)
+        return btcBalanceR.getOrElse(0)
+      }
       return btcBalanceR.getOrElse(0)
     } catch (e) {
       yield put(actions.logs.logErrorMessage(logLocation, 'getBtcBalance', e))
     }
   }
+
   const getBchBalance = function* () {
     try {
       const bchBalanceR = yield select(selectors.core.data.bch.getBalance)
+      if (!Remote.Success.is(bchBalanceR)) {
+        yield take(actionTypes.core.data.bch.FETCH_BCH_DATA_SUCCESS)
+        const bchBalanceR = yield select(selectors.core.data.bch.getBalance)
+        return bchBalanceR.getOrElse(0)
+      }
       return bchBalanceR.getOrElse(0)
     } catch (e) {
       yield put(actions.logs.logErrorMessage(logLocation, 'getBchBalance', e))
