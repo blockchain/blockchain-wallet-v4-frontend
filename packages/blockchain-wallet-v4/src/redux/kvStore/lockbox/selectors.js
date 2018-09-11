2dac7922407469c6c8cd56f8a998a049a019fe22
Salome Vakhtangadze <salomejumala@gmail.com>
2018-09-11 11:00:07 +0400

    export xpub

--- packages/blockchain-wallet-v4/src/redux/kvStore/lockbox/selectors.js
+++ packages/blockchain-wallet-v4/src/redux/kvStore/lockbox/selectors.js
@@ -53,6 +53,11 @@ export const getDeviceFromBtcXpubs = (state, xpubs) => {
     .map(head)
 }
 
+export const getLockboxBtcDefaultAccount = (state, deviceId) =>
+  getDevice(state, deviceId)
+    .map(path(['btc', 'accounts']))
+    .map(head)
+
 // BCH
 export const getLockboxBch = state => getDevices(state).map(map(path(['bch'])))
 
