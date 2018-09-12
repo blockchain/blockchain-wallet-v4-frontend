2631a7d92f51eecaea45f32d2e05e883be356133
Philip Welber <welber@blockchain.com>
2018-09-12 11:40:27 -0400

    fix submit button enabled/disabled validation test

--- packages/blockchain-wallet-v4-frontend/src/modals/IdentityVerification/IdentityVerification.spec.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/IdentityVerification/IdentityVerification.spec.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { TestBed, getDispatchSpyReducer, createTestStore } from 'utils/testbed'
+import { flushPromises } from 'utils/test.utils'
 import { mount } from 'enzyme'
 import { combineReducers } from 'redux'
 import { actions, model } from 'data'
 
 import { coreReducers, paths, coreSagasFactory, Remote } from 'blockchain-wallet-v4/src'
 import identityVerificationReducer from 'data/components/identityVerification/reducers'
+import { getPossibleAddresses, getSupportedCountries } from 'data/components/identityVerification/selectors'
 import modalsReducer from 'data/modals/reducers'
 import profileReducer from 'data/modules/profile/reducers'
 import identityVerificationSaga from 'data/components/identityVerification/sagaRegister'
 import securityCenterSagas from 'data/modules/securityCenter/sagaRegister'
 import settingsSagas from 'data/modules/settings/sagaRegister'
+import profileSagas from 'data/modules/profile/sagaRegister'
 import * as actionTypes from 'data/actionTypes'
 import IdentityVerification from './index'
 import Tray from 'components/Tray'
 import { ModalHeader } from 'blockchain-info-components'
-import { last } from 'ramda'
+import { last, contains } from 'ramda'
+import {
+  getUserId,
+  getLifetimeToken
+} from 'blockchain-wallet-v4/src/redux/kvStore/userCredentials/selectors'
+import { getEmail, getSmsVerified, getCountryCode } from 'blockchain-wallet-v4/src/redux/settings/selectors'
+import { getGuid } from 'blockchain-wallet-v4/src/redux/wallet/selectors'
 
 const { MODAL_NAME } = model.components.identityVerification
 
@@ -24,17 +33,42 @@ const { dispatchSpy, spyReducer } = getDispatchSpyReducer()
 jest.useFakeTimers()
 
 jest.mock('blockchain-wallet-v4/src/redux/sagas')
+jest.mock('blockchain-wallet-v4/src/redux/settings/selectors')
+jest.mock('blockchain-wallet-v4/src/redux/kvStore/userCredentials/selectors')
+jest.mock('blockchain-wallet-v4/src/redux/wallet/selectors')
+jest.mock('data/components/identityVerification/selectors')
+
+const POSSIBLE_ADDRESSES = [
+  { line1: 'Square Louvois', line2: '', postCode: '75002', city: '', state: 'Île-de-France' },
+  { line1: 'Rue Montmartre, 108', line2: '', postCode: '75002', city: '', state: 'Île-de-France' }
+]
+const SUPPORTED_COUNTRIES = [{ code: 'FR', name: 'France' }]
+
 const coreSagas = coreSagasFactory({ api: {} })
 const api = {
   obtainSessionToken: jest.fn(),
   deauthorizeBrowser: jest.fn(),
-  getSupportedCountries: () => Remote.of([{ name: 'France' }, { name: 'Spain' }])
+  getSupportedCountries: () => Remote.of([{ name: 'France' }, { name: 'Spain' }]),
+  fetchKycAddresses: () => Remote.of(POSSIBLE_ADDRESSES)
 }
+
+getUserId.mockImplementation(() => Remote.of(123))
+getLifetimeToken.mockImplementation(() => Remote.of(456))
+getSmsVerified.mockImplementation(() => Remote.of(0))
+getEmail.mockImplementation(() => Remote.of('email@email.com'))
+getGuid.mockImplementation(() => Remote.of('123-abc-456-def'))
+getCountryCode.mockImplementation(() => Remote.of('FR'))
+getPossibleAddresses.mockImplementation(() => POSSIBLE_ADDRESSES)
+getSupportedCountries.mockImplementation(() => Remote.Success(SUPPORTED_COUNTRIES))
+
+profileSagas.createUser = jest.fn()
+// mock entire createUser saga
+// profilesagas.createuser = jest.fn()
 
 const stubMail = 'mail@mail.com'
 const stubMobile = '+1 123 1234567'
 
-describe('IdentityVerification Modal', () => {
+fdescribe('IdentityVerification Modal', () => {
   beforeEach(() => {
     dispatchSpy.mockClear()
   })
@@ -50,7 +84,8 @@ describe('IdentityVerification Modal', () => {
   const sagas = [
     identityVerificationSaga({ coreSagas, api }),
     securityCenterSagas({ coreSagas }),
-    settingsSagas({ coreSagas })
+    settingsSagas({ coreSagas }),
+    profileSagas({ coreSagas, api })
   ]
   let store
   let wrapper
@@ -96,7 +131,7 @@ describe('IdentityVerification Modal', () => {
         store.dispatch(actions.core.settings.setEmail(stubMail))
         store.dispatch(actions.core.settings.setEmailVerified())
         store.dispatch(actions.core.settings.setMobile(stubMobile))
-        store.dispatch(actions.core.settings.setMobileVerified())        
+        store.dispatch(actions.core.settings.setMobileVerified())     
         wrapper.update()
       })
 
@@ -110,7 +145,7 @@ describe('IdentityVerification Modal', () => {
         )
       })
 
-      it('should enable continue if all fields are filled', () => {
+      it('should enable continue if all fields are filled', async () => {
         wrapper
           .find('Field[name="dob"]')
           .find('input[name="date"]')
@@ -148,9 +183,35 @@ describe('IdentityVerification Modal', () => {
           .simulate('change', {
             target: { value: '75002' }
           })
-
-        jest.runAllTimers()
+        wrapper.unmount().mount()
+        wrapper
+          .find('Field[name="address"]')
+          .find('SelectBox')
+          .prop('input')
+          .onChange(POSSIBLE_ADDRESSES[0])
+        wrapper.unmount().mount()
+        wrapper
+          .find('Field[name="line1"]')
+          .find('input[name="line1"]')
+          .simulate('change', {
+            target: { value: POSSIBLE_ADDRESSES[0]['line1'] }
+          })
+        wrapper
+          .find('Field[name="city"]')
+          .find('input[name="city"]')
+          .simulate('change', {
+            target: { value: 'Paris' }
+          })
+        wrapper
+          .find('Field[name="state"]')
+          .find('input[name="state"]')
+          .simulate('change', {
+            target: { value: POSSIBLE_ADDRESSES[0]['state'] }
+          })
+        await jest.runAllTimers()
+        flushPromises()
         wrapper.update()
+
         expect(wrapper.find('Button[type="submit"]').prop('disabled')).toBe(
           false
         )
