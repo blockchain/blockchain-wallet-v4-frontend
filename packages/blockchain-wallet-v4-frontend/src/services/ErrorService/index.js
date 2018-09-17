cd764ebee2882ae5f6fc10262eb25a1e5faf0584
Philip Welber <welber@blockchain.com>
2018-09-17 14:42:39 -0400

    change btc addr length

--- packages/blockchain-wallet-v4-frontend/src/services/ErrorService/index.js
+++ packages/blockchain-wallet-v4-frontend/src/services/ErrorService/index.js
@@ -2,7 +2,7 @@ import { match, isEmpty, head } from 'ramda'
 import { utils } from 'blockchain-wallet-v4/src'
 
 const checkForVulnerableAddressError = (message) => {
-  const re = /A security issue effects address [13][a-km-zA-HJ-NP-Z0-9]{26,33}. Please Archive It and Contact support@blockchain.zendesk.com/
+  const re = /A security issue effects address [13][a-km-zA-HJ-NP-Z0-9]{25,34}. Please Archive It and Contact support@blockchain.zendesk.com/
   const testForErrorMatch = match(re, message)
   const addressMatch = !isEmpty(testForErrorMatch) && match(/[13][a-km-zA-HJ-NP-Z0-9]{26,33}/, message)
   const matchResult = !isEmpty(addressMatch) && head(addressMatch)
