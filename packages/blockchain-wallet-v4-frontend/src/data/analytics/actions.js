dc2dc3caae0ba30fa26d73a812d259682e8fd083
tony-blockchain <tony@blockchain.com>
2018-09-17 13:20:22 +0300

    fix(analytics): refactored actions to avoid devtools crash

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/actions.js
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/actions.js
@@ -3,11 +3,11 @@ import * as AT from './actionTypes'
 export const reportBalanceStats = () => ({
   type: AT.REPORT_BALANCE_STATS
 })
-export const logLeftNavClick = event => ({
+export const logLeftNavClick = text => ({
   type: AT.LOG_LEFT_NAV_CLICK,
-  event
+  payload: { text }
 })
 export const logClick = name => ({
   type: AT.LOG_CLICK,
-  name
+  payload: { name }
 })
