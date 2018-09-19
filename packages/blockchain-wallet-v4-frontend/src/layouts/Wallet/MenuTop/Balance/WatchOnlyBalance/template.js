36cc23f13f263b78c9acbf4f1dc1df1fa608b0a9
plondon <philip@blockchain.com>
2018-09-19 15:26:48 -0400

    fix(Balance): balance dropdown toggle handler

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/WatchOnlyBalance/template.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuTop/Balance/WatchOnlyBalance/template.js
@@ -13,8 +13,8 @@ import {
 
 const Template = props => (
   <Wrapper>
-    <Header>
-      <HeaderText size='14px' onClick={props.handleToggle}>
+    <Header onClick={props.handleToggle}>
+      <HeaderText size='14px'>
         <Icon name='forbidden' size='12px' style={{ marginRight: '10px' }} />
         <FormattedMessage
           id='layouts.wallet.menutop.balance.walletbalance.nonspendable'
