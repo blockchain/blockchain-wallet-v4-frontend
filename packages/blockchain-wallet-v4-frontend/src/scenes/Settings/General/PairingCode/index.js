1e371b9b670018bde97b0daa125fa2a8f312034f
schnogz <andrew@blockchain.com>
2018-09-19 10:43:09 -0400

    refactor pairingCode component

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/PairingCode/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/PairingCode/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-import { Badge, Text } from 'blockchain-info-components'
+import { actions } from 'data'
+import { Badge, Button, Text } from 'blockchain-info-components'
 import {
   SettingComponent,
   SettingContainer,
@@ -10,7 +13,6 @@ import {
   SettingHeader,
   SettingSummary
 } from 'components/Setting'
-import Settings from './Settings'
 
 const BadgesContainer = styled.div`
   display: block;
@@ -21,44 +23,67 @@ const BadgesContainer = styled.div`
   }
 `
 
-const PairingCode = () => {
-  return (
-    <SettingContainer>
-      <SettingSummary>
-        <SettingHeader>
-          <FormattedMessage
-            id='scenes.settings.general.pairingcode.title'
-            defaultMessage='Mobile App Pairing Code'
-          />
-        </SettingHeader>
-        <SettingDescription>
-          <FormattedMessage
-            id='scenes.settings.general.pairingcode.description'
-            defaultMessage="Scan the code (click on 'Show Pairing Code') with your Blockchain Wallet (iOS or Android) for a seamless connection to your wallet."
-            altFont
-            light
-          />
-          <FormattedMessage
-            id='scenes.settings.general.pairingcode.description2'
-            defaultMessage='Download our mobile applications below.'
-          />
-          <Text size='14px' weight={300} color='error'>
+class PairingCode extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.onShowCode = this.onShowCode.bind(this)
+  }
+
+  onShowCode () {
+    this.props.actions.showModal('PairingCode')
+  }
+
+  render () {
+    return (
+      <SettingContainer>
+        <SettingSummary>
+          <SettingHeader>
             <FormattedMessage
-              id='scenes.settings.general.pairingcode.warning'
-              defaultMessage='Do not share your Pairing Code with others.'
+              id='scenes.settings.general.pairingcode.title'
+              defaultMessage='Mobile App Pairing Code'
             />
-          </Text>
-          <BadgesContainer>
-            <Badge type='applestore' />
-            <Badge type='googleplay' />
-          </BadgesContainer>
-        </SettingDescription>
-      </SettingSummary>
-      <SettingComponent>
-        <Settings />
-      </SettingComponent>
-    </SettingContainer>
-  )
+          </SettingHeader>
+          <SettingDescription>
+            <FormattedMessage
+              id='scenes.settings.general.pairingcode.description'
+              defaultMessage="Scan the code (click on 'Show Pairing Code') with your Blockchain Wallet (iOS or Android) for a seamless connection to your wallet."
+              altFont
+              light
+            />
+            <FormattedMessage
+              id='scenes.settings.general.pairingcode.description2'
+              defaultMessage='Download our mobile applications below.'
+            />
+            <Text size='14px' weight={300} color='error'>
+              <FormattedMessage
+                id='scenes.settings.general.pairingcode.warning'
+                defaultMessage='Do not share your Pairing Code with others.'
+              />
+            </Text>
+            <BadgesContainer>
+              <Badge type='applestore' />
+              <Badge type='googleplay' />
+            </BadgesContainer>
+          </SettingDescription>
+        </SettingSummary>
+        <SettingComponent>
+          <Button nature='primary' onClick={this.onShowCode}>
+            <FormattedMessage
+              id='scenes.settings.general.pairingcode.settings.show'
+              defaultMessage='Show Pairing Code'
+            />
+          </Button>
+        </SettingComponent>
+      </SettingContainer>
+    )
+  }
 }
 
-export default PairingCode
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(actions.modals, dispatch)
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(PairingCode)
