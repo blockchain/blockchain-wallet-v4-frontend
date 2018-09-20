56a7da90ee5b197d4cc049f576360964d7433ada
schnogz <andrew@blockchain.com>
2018-09-20 13:51:10 -0400

    finish modal stepper

--- packages/blockchain-wallet-v4-frontend/src/modals/LockboxFirmware/index.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/LockboxFirmware/index.js
@@ -6,8 +6,8 @@ import { connect } from 'react-redux'
 import { actions, selectors } from 'data'
 import modalEnhancer from 'providers/ModalEnhancer'
 import LockboxFirmware from './template'
-// import CheckForUpdatesStep from './CheckForUpdatesStep'
-// import UpgradeFirmwareStep from './UpgradeFirmwareStep'
+import CheckForUpdatesStep from './CheckForUpdatesStep'
+import UpgradeFirmwareStep from './UpgradeFirmwareStep'
 
 class LockboxFirmwareContainer extends React.PureComponent {
   componentWillUnmount () {
@@ -15,15 +15,39 @@ class LockboxFirmwareContainer extends React.PureComponent {
   }
 
   render () {
-    const { step, deviceIndex, position, total, closeAll } = this.props
+    const { currentStep, deviceIndex, position, total, closeAll } = this.props
+    let step
+    switch (currentStep) {
+      case 'check-for-updates':
+        step = 1
+        break
+      case 'verify-identifier':
+        step = 2
+        break
+      case 'install-mcu':
+        step = 3
+        break
+      case 'install-firmware':
+        step = 4
+        break
+      default:
+        step = 0
+        break
+    }
 
     return (
-      <LockboxFirmware position={position} total={total} closeAll={closeAll} step={step}>
-        {(!step || step === 'connect-device') && <div />}
-        {step === 'check-for-updates' && <div />}
-        {step === 'verify-identifier' && <div />}
-        {step === 'install-mcu' && <div />}
-        {step === 'install-firmware' && <div />}
+      <LockboxFirmware
+        position={position}
+        total={total}
+        closeAll={closeAll}
+        step={step}
+        totalSteps={5}
+      >
+        {step === 0 && <CheckForUpdatesStep deviceIndex={deviceIndex} />}
+        {step === 1 && <UpgradeFirmwareStep />}
+        {step === 2 && <div>Step 3</div>}
+        {step === 3 && <div>Step 4</div>}
+        {step === 4 && <div>Step 5</div>}
       </LockboxFirmware>
     )
   }
--- packages/blockchain-wallet-v4-frontend/src/modals/LockboxFirmware/template.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/LockboxFirmware/template.js
@@ -1,29 +1,97 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Modal, ModalBody } from 'blockchain-info-components'
+import { FormattedMessage } from 'react-intl'
+
+import { Modal, ModalBody, Text } from 'blockchain-info-components'
 
 const StepHeader = styled.div`
   background-color: ${props => props.theme['brand-secondary']};
   border-radius: 4px 4px 0 0;
-  height: 75px;
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 0 5px 10px 5px;
 `
 const ContentWrapper = styled(ModalBody)`
   padding: 20px;
   height: 300px;
 `
+const Step = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`
+const LineWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  margin-top: 16px;
+`
 
-const LockboxFirmware = props => (
-  <Modal size='large' position={props.position} total={props.total}>
-    <StepHeader>
-      Step 1
-    </StepHeader>
-    <ContentWrapper>
-      <div>Content</div>
-      {props.children}
-    </ContentWrapper>
-  </Modal>
-)
+const Line = styled.div`
+  width: 117px;
+  border-top: 2px solid
+    ${props =>
+      props.step > props.i
+        ? 'rgba(255, 255, 255, 1)'
+        : 'rgba(255, 255, 255, 0.3)'};
+  position: absolute;
+  top: 6px;
+  left: -26px;
+`
+const Circle = styled.div`
+  width: 14px;
+  height: 14px;
+  line-height: 14px;
+  border-radius: 15px;
+  background-color: ${props =>
+    props.step >= props.i
+      ? 'rgba(255, 255, 255, 1)'
+      : 'rgba(255, 255, 255, 0.3)'};
+  margin-top: 15px;
+  z-index: 10;
+
+  & > span:first-child {
+    padding-left: 8px;
+  }
+`
+
+const LockboxFirmware = props => {
+  const { children, position, total, step, totalSteps } = props
+
+  return (
+    <Modal size='large' position={position} total={total}>
+      <StepHeader>
+        {[...Array(totalSteps)].map((r, i) => {
+          return (
+            <React.Fragment key={i}>
+              <Step>
+                <Text size='12px' weight={400} color='white'>
+                  <FormattedMessage
+                    id='modals.lockboxfirmware.header.step'
+                    defaultMessage='Step {number}'
+                    values={{ number: i + 1 }}
+                  />
+                </Text>
+                <Circle step={step} i={i} />
+              </Step>
+              {i !== totalSteps - 1 && (
+                <LineWrapper>
+                  <Line step={step} i={i} />
+                </LineWrapper>
+              )}
+            </React.Fragment>
+          )
+        })}
+      </StepHeader>
+      <ContentWrapper>{children}</ContentWrapper>
+    </Modal>
+  )
+}
 
 LockboxFirmware.propTypes = {
   position: PropTypes.number.isRequired,
