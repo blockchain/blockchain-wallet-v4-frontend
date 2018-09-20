cd86af532fcd15e8440f63cd346b8eb04a3268e9
Philip Welber <welber@blockchain.com>
2018-09-20 11:41:04 -0400

    handle vulnerable address errors

--- packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.js
@@ -7,12 +7,15 @@ import * as actions from '../actions.js'
 import * as actionTypes from '../actionTypes.js'
 import * as selectors from '../selectors.js'
 import * as C from 'services/AlertService'
+import * as CC from 'services/ConfirmService'
 import {
   askSecondPasswordEnhancer,
+  confirm,
   promptForSecondPassword,
   forceSyncWallet
 } from 'services/SagaService'
-import { Types } from 'blockchain-wallet-v4/src'
+import { Types, Remote } from 'blockchain-wallet-v4/src'
+import { checkForVulnerableAddressError } from 'services/ErrorCheckService'
 
 export const logLocation = 'auth/sagas'
 export const defaultLoginErrorMessage = 'Error logging into your wallet'
@@ -131,6 +134,7 @@ export default ({ api, coreSagas }) => {
       yield fork(transferEthSaga)
       yield fork(welcomeSaga, firstLogin)
       yield fork(reportStats, mobileLogin)
+      yield fork(checkDataErrors)
       yield fork(logoutRoutine, yield call(setLogoutEventListener))
       if (!firstLogin) {
         yield put(actions.alerts.displaySuccess(C.LOGIN_SUCCESS))
@@ -153,6 +157,25 @@ export default ({ api, coreSagas }) => {
         api.incrementSecPasswordStats,
         Types.Wallet.isDoubleEncrypted(wallet)
       )
+    }
+  }
+
+  const checkDataErrors = function*() {
+    const btcDataR = yield select(selectors.core.data.bitcoin.getInfo)
+    if (Remote.Failure.is(btcDataR)) {
+      const error = prop('error', btcDataR)
+      const vulnerableAddress = checkForVulnerableAddressError(error)
+      yield put(actions.modals.closeAllModals())
+      if (vulnerableAddress) {
+        const confirmed = yield call(confirm, {
+          title: CC.ARCHIVE_VULNERABLE_ADDRESS_TITLE,
+          message: CC.ARCHIVE_VULNERABLE_ADDRESS_MSG,
+          confirm: CC.ARCHIVE_VULNERABLE_ADDRESS_CONFIRM,
+          cancel: CC.ARCHIVE_VULNERABLE_ADDRESS_CANCEL,
+          messageValues: { vulnerableAddress }
+        })
+        if (confirmed) yield put(actions.core.wallet.setAddressArchived(vulnerableAddress, true))
+      }
     }
   }
 
@@ -465,6 +488,7 @@ export default ({ api, coreSagas }) => {
   }
 
   return {
+    checkDataErrors,
     deauthorizeBrowser,
     login,
     logout,
--- packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.spec.js
+++ packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.spec.js
@@ -2,7 +2,7 @@ import { select } from 'redux-saga/effects'
 import { expectSaga, testSaga } from 'redux-saga-test-plan'
 import { fork, call } from 'redux-saga-test-plan/matchers'
 
-import { askSecondPasswordEnhancer } from 'services/SagaService'
+import { askSecondPasswordEnhancer, confirm } from 'services/SagaService'
 import { coreSagasFactory, Remote } from 'blockchain-wallet-v4/src'
 import * as selectors from '../selectors.js'
 import * as actions from '../actions.js'
@@ -24,6 +24,8 @@ const api = {
   obtainSessionToken: jest.fn(),
   deauthorizeBrowser: jest.fn()
 }
+
+const VULNERABLE_ADDRESS = '1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH'
 
 describe('authSagas', () => {
   // Mocking Math.random() to have identical popup ids for action testing
@@ -334,6 +336,7 @@ describe('authSagas', () => {
 
   describe('login routine', () => {
     const {
+      checkDataErrors,
       loginRoutineSaga,
       logoutRoutine,
       reportStats,
@@ -471,6 +474,10 @@ describe('authSagas', () => {
 
     it('should launch reportStats saga', () => {
       saga.next().fork(reportStats, mobileLogin)
+    })
+
+    it('should check for data errors', () => {
+      saga.next().fork(checkDataErrors)
     })
 
     it('should start listening for logout event', () => {
@@ -1141,6 +1148,33 @@ describe('authSagas', () => {
       })
 
       pageReloadTest()
+    })
+  })
+
+  describe('checkDataErrors', () => {
+    const { checkDataErrors } = authSagas({ api, coreSagas })
+    const saga = testSaga(checkDataErrors)
+    it('should select btc getInfo', () => {
+      saga.next().select(selectors.core.data.bitcoin.getInfo)
+    })
+    it('should call closeAllModals', () => {
+      let failedData = Remote.Failure(`A security issue effects address ${VULNERABLE_ADDRESS}. Please Archive It and Contact support@blockchain.zendesk.com`)
+      saga.next(failedData).put(actions.modals.closeAllModals())
+    })
+    it('should call confirm service', () => {
+      saga.next().call(confirm, {
+        title: 'archive_vulnerable_address_title',
+        image: null,
+        message: 'archive_vulnerable_address_msg',
+        confirm: 'archive_vulnerable_address_confirm',
+        cancel: undefined,
+        messageValues: { vulnerableAddress: VULNERABLE_ADDRESS }
+      })
+    })
+    it('should archive the address', () => {
+      saga
+        .next(true)
+        .put(actions.core.wallet.setAddressArchived(VULNERABLE_ADDRESS, true))
     })
   })
 })
