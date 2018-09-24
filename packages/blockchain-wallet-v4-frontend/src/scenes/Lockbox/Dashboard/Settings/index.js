20f0163668231991846285afeca5a9a749355908
schnogz <andrew@blockchain.com>
2018-09-24 11:29:27 -0400

    add browser warn banner to settings page

--- packages/blockchain-wallet-v4-frontend/src/scenes/Lockbox/Dashboard/Settings/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Lockbox/Dashboard/Settings/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FormattedMessage } from 'react-intl'
 
+import { Banner, Text } from 'blockchain-info-components'
 import RenameDevice from './RenameDevice'
 import RemoveDevice from './RemoveDevice'
 import ExportXPub from './ExportXPub'
@@ -13,11 +15,25 @@ const SettingsContainer = styled.div`
   padding: 0 15px;
 `
 
+const BrowserWarning = styled(Banner)`
+  margin-top: 18px;
+`
+
 export default class LockboxSettings extends React.PureComponent {
   render () {
-    const { deviceIndex } = this.props
+    const { browserWarn, deviceIndex } = this.props
     return (
       <SettingsContainer>
+        {browserWarn && (
+          <BrowserWarning type='warning'>
+            <Text color='warning' size='14px'>
+              <FormattedMessage
+                id='scenes.lockbox.settings.browserwarn'
+                defaultMessage='Installing applications and updating firmware can only be done while using the Chrome browser.'
+              />
+            </Text>
+          </BrowserWarning>
+        )}
         <RenameDevice deviceIndex={deviceIndex} />
         <UpdateDevice deviceIndex={deviceIndex} />
         <InstallApps deviceIndex={deviceIndex} />
