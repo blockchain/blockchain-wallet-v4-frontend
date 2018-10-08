6668fcaecbb00da9529f16e2cbd372657e4ca96f
tony-blockchain <tony@blockchain.com>
2018-10-09 01:58:55 +0300

    feat(Homebrew): country check

--- packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.js
@@ -95,6 +95,20 @@ export default ({ api, coreSagas }) => {
     }
   }
 
+  const authNabu = function*() {
+    yield put(actions.components.identityVerification.fetchSupportedCountries())
+    yield take(
+      action =>
+        action.type ===
+          actionTypes.components.identityVerification.SET_SUPPORTED_COUNTRIES &&
+        !Remote.Loading.is(action.payload.countries)
+    )
+    const userFlowSupported = (yield select(
+      selectors.modules.profile.userFlowSupported
+    )).getOrElse(false)
+    if (userFlowSupported) yield put(actions.modules.profile.signIn())
+  }
+
   const loginRoutineSaga = function*(mobileLogin, firstLogin) {
     try {
       // If needed, the user should upgrade its wallet before being able to open the wallet
@@ -116,10 +130,7 @@ export default ({ api, coreSagas }) => {
       yield call(coreSagas.kvStore.lockbox.fetchMetadataLockbox)
       yield put(actions.router.push('/home'))
       yield call(coreSagas.settings.fetchSettings)
-      const userFlowSupported = (yield select(
-        selectors.modules.profile.userFlowSupported
-      )).getOrElse(false)
-      if (userFlowSupported) yield put(actions.modules.profile.signIn())
+      yield call(authNabu)
       yield call(upgradeAddressLabelsSaga)
       yield put(actions.auth.loginSuccess())
       yield put(actions.auth.startLogoutTimer())
@@ -505,6 +516,7 @@ export default ({ api, coreSagas }) => {
   }
 
   return {
+    authNabu,
     checkAndHandleVulnerableAddress,
     checkDataErrors,
     deauthorizeBrowser,
