12401be01c29109242d0da5eb08010e77bdc9cba
plondon <philip@blockchain.com>
2018-10-25 17:49:14 -0400

    fix(BCH): Check if bch change index got behind btc by gap limit

--- packages/blockchain-wallet-v4-frontend/src/data/modules/addressesBch/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/modules/addressesBch/sagas.js
@@ -1,9 +1,12 @@
-import { call, put } from 'redux-saga/effects'
+import { select, call, put } from 'redux-saga/effects'
 import * as actions from '../../actions.js'
+import * as selectors from '../../selectors.js'
 import * as C from 'services/AlertService'
 import { promptForInput } from 'services/SagaService'
+import { utils } from 'blockchain-wallet-v4/src'
+const { toCashAddr } = utils.bch
 
-export default ({ coreSagas }) => {
+export default ({ coreSagas, networks }) => {
   const logLocation = 'modules/addressesBch/sagas'
 
   const editBchAccountLabel = function*(action) {
@@ -24,7 +27,51 @@ export default ({ coreSagas }) => {
     }
   }
 
+  // FIX: if change was sent to users btc change index instead of bch
+  const showBchChangeAddrs = function*(action) {
+    const { index, xpub } = action.payload
+    const GAP_LIMIT = 20
+    const state = yield select()
+    const btcChangeIndex = selectors.core.data.bitcoin
+      .getChangeIndex(xpub, state)
+      .getOrElse(0)
+    const bchChangeIndex = selectors.core.data.bch
+      .getChangeIndex(xpub, state)
+      .getOrElse(0)
+
+    let addrs = []
+    // check if bch change index got behind btc by gap limit
+    if (bchChangeIndex + GAP_LIMIT < btcChangeIndex) {
+      for (
+        var i = bchChangeIndex + GAP_LIMIT;
+        i < btcChangeIndex;
+        i += GAP_LIMIT
+      ) {
+        // get address at GAP_LIMIT points
+        const addr = selectors.core.common.bch.getAddress(
+          networks.btc,
+          `${index}/${1}/${i}`,
+          state
+        )
+        addrs.push(addr)
+      }
+      // if there are addrs to display show them else carry on
+      /* eslint-disable */
+      if (addrs.length) {
+        window.alert(
+          addrs.map((addr, i) => `${i + 1}. ${toCashAddr(addr, true)}`)
+        )
+      } else {
+        window.alert('Nothing to see here!')
+      }
+    } else {
+      window.alert('Nothing to see here!')
+    }
+    /* eslint-enable */
+  }
+
   return {
-    editBchAccountLabel
+    editBchAccountLabel,
+    showBchChangeAddrs
   }
 }
