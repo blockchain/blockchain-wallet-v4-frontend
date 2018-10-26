66ca8c946ec52039f6438ece8501c86909dd9427
plondon <philip@blockchain.com>
2018-10-26 09:54:53 -0400

    test(BCH): Change address fix

--- packages/blockchain-wallet-v4-frontend/src/data/modules/addressesBch/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/modules/addressesBch/sagas.js
@@ -5,6 +5,7 @@ import * as C from 'services/AlertService'
 import { promptForInput } from 'services/SagaService'
 import { utils } from 'blockchain-wallet-v4/src'
 const { toCashAddr } = utils.bch
+const GAP_LIMIT = 20
 
 export default ({ coreSagas, networks }) => {
   const logLocation = 'modules/addressesBch/sagas'
@@ -27,10 +28,23 @@ export default ({ coreSagas, networks }) => {
     }
   }
 
-  // FIX: if change was sent to users btc change index instead of bch
+  const selectChangeAddresses = (accountIndex, start, end, state) => {
+    let addrs = []
+    for (var i = start + GAP_LIMIT; i < end; i += GAP_LIMIT) {
+      // get address at GAP_LIMIT points
+      const addr = selectors.core.common.bch.getAddress(
+        networks.btc,
+        `${accountIndex}/${1}/${i}`,
+        state
+      )
+      addrs.push(addr)
+    }
+    return addrs
+  }
+
+  // if change was sent to users btc change index instead of bch
   const showBchChangeAddrs = function*(action) {
     const { index, xpub } = action.payload
-    const GAP_LIMIT = 20
     const state = yield select()
     const btcChangeIndex = selectors.core.data.bitcoin
       .getChangeIndex(xpub, state)
@@ -39,31 +53,20 @@ export default ({ coreSagas, networks }) => {
       .getChangeIndex(xpub, state)
       .getOrElse(0)
 
-    let addrs = []
     // check if bch change index got behind btc by gap limit
-    if (bchChangeIndex + GAP_LIMIT < btcChangeIndex) {
-      for (
-        var i = bchChangeIndex + GAP_LIMIT;
-        i < btcChangeIndex;
-        i += GAP_LIMIT
-      ) {
-        // get address at GAP_LIMIT points
-        const addr = selectors.core.common.bch.getAddress(
-          networks.btc,
-          `${index}/${1}/${i}`,
-          state
-        )
-        addrs.push(addr)
-      }
-      // if there are addrs to display show them else carry on
-      /* eslint-disable */
-      if (addrs.length) {
-        window.alert(
-          addrs.map((addr, i) => `${i + 1}. ${toCashAddr(addr, true)}`)
-        )
-      } else {
-        window.alert('Nothing to see here!')
-      }
+    const addrs = selectChangeAddresses(
+      index,
+      bchChangeIndex,
+      btcChangeIndex,
+      state
+    )
+
+    // if there are addrs to display show them else carry on
+    /* eslint-disable */
+    if (addrs.length) {
+      window.alert(
+        addrs.map((addr, i) => `${toCashAddr(addr, true)}`).join(', ')
+      )
     } else {
       window.alert('Nothing to see here!')
     }
@@ -71,7 +74,8 @@ export default ({ coreSagas, networks }) => {
   }
 
   return {
+    showBchChangeAddrs,
     editBchAccountLabel,
-    showBchChangeAddrs
+    selectChangeAddresses
   }
 }
