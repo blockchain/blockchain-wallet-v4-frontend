d91a91520d5b7bf7ae7cd96cb7e7a599370554d0
TheLeoB <TheLeoB@users.noreply.github.com>
2018-11-01 10:58:52 -0400

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 module.exports = {
-  ROOT_URL: 'https://explorer.dev.blockchain.info',
-  API_DOMAIN: 'https://api.dev.blockchain.info',
-  WEB_SOCKET_URL: 'wss://ws.dev.blockchain.info',
+  ROOT_URL: 'https://blockchain.info',
+  API_DOMAIN: 'https://api.blockchain.info',
+  WEB_SOCKET_URL: 'wss://ws.blockchain.info',
   COM_WALLET_APP: 'https://login.blockchain.com',
   COM_ROOT: 'https://blockchain.com',
   WALLET_HELPER_DOMAIN: 'https://wallet-helper.blockchain.info',
