5f194755376cb97f0f0196c1ca58a076fae7cd77
schnogz <andrew@blockchain.com>
2018-11-02 10:09:22 -0400

    update selectors

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/Header/Announcements/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/Header/Announcements/selectors.js
@@ -8,20 +8,18 @@ export const getData = createDeepEqualSelector(
     selectors.core.settings.getEmail,
     selectors.core.settings.getEmailVerified,
     selectors.modules.profile.getUserData
-],
+  ],
   (emailR, emailVerifiedR, userDataR) => {
     return lift((email, emailVerified, userData) => {
       debugger
-      const kycState  = prop('kycState', userData)
+      const kycState = prop('kycState', userData)
       debugger
-      const needsSunRiverKyc = path(['tags', 'SUNRIVER'], userData)
-      && (!equals(kycState, KYC_STATES.VERIFIED) || !equals(kycState, KYC_STATES.PENDING))
+      const needsSunRiverKyc =
+        path(['tags', 'SUNRIVER'], userData) &&
+        (!equals(kycState, KYC_STATES.VERIFIED) ||
+          !equals(kycState, KYC_STATES.PENDING))
       debugger
       return { email, announcement: emailVerified ? needsSunRiverKyc : 'email' }
-    })(
-      emailR,
-      emailVerifiedR,
-      userDataR
-    )
+    })(emailR, emailVerifiedR, userDataR)
   }
 )
