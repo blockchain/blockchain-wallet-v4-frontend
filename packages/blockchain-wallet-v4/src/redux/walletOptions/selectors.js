fc339a7e2a8def602dd1e07566f07d1a0062a542
plondon <philip@blockchain.com>
2018-11-08 14:49:20 +0000

    refactor(Wallet Options): Remove unused wallet-options and clean up naming
    1241

--- packages/blockchain-wallet-v4/src/redux/walletOptions/selectors.js
+++ packages/blockchain-wallet-v4/src/redux/walletOptions/selectors.js
@@ -1,21 +1,22 @@
 import { path } from 'ramda'
 import { walletOptionsPath } from '../paths'
 
+// general
 export const getOptions = path([walletOptionsPath])
 export const getDomains = state => getOptions(state).map(path(['domains']))
+export const getWebOptions = state =>
+  getOptions(state).map(path(['platforms', 'web']))
+
+// specific
 export const getBtcNetwork = state =>
-  getOptions(state).map(
-    path(['platforms', 'web', 'bitcoin', 'config', 'network'])
-  )
+  getWebOptions(state).map(path(['btc', 'config', 'network']))
+export const getEthTxFuse = state =>
+  getWebOptions(state).map(path(['eth', 'lastTxFuse']))
+export const getAnnouncements = state =>
+  getWebOptions(state).map(path(['application', 'announcements']))
 export const getMigrationRedirects = state =>
-  getOptions(state).map(
-    path(['platforms', 'web', 'application', 'enableDomainMigrationRedirects'])
+  getWebOptions(state).map(
+    path(['application', 'enableDomainMigrationRedirects'])
   )
 export const getShapeshiftStates = state =>
-  getOptions(state).map(path(['platforms', 'web', 'shapeshift', 'states']))
-export const getAnnouncements = state =>
-  getOptions(state).map(
-    path(['platforms', 'web', 'application', 'announcements'])
-  )
-export const getEthereumTxFuse = state =>
-  getOptions(state).map(path(['platforms', 'web', 'ethereum', 'lastTxFuse']))
+  getWebOptions(state).map(path(['shapeshift', 'states']))
