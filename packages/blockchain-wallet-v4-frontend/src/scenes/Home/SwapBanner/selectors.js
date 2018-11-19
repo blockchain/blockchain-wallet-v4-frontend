2170ebceda1f6bc870cb655849827a2c6e9d85de
tony-blockchain <tony@blockchain.com>
2018-11-19 20:14:52 +0300

    fix(Swap): swap banner shown when kyc state is none

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/SwapBanner/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/SwapBanner/selectors.js
@@ -1,12 +1,17 @@
-import { selectors } from 'data'
+import { equals } from 'ramda'
+
+import { selectors, model } from 'data'
+
+const { NONE } = model.profile.KYC_STATES
 
 export const getData = state => {
   const showKycGetStarted = selectors.preferences.getShowKycGetStarted(state)
-  const userVerified = selectors.modules.profile
-    .isUserVerified(state)
-    .getOrElse(true)
+  const kycNotFinished = selectors.modules.profile
+    .getUserKYCState(state)
+    .map(equals(NONE))
+    .getOrElse(false)
 
   return {
-    showBanner: !showKycGetStarted && !userVerified
+    showBanner: !showKycGetStarted && kycNotFinished
   }
 }
