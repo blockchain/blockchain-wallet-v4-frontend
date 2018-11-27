974680ed1cbd3c93d229483fdf82cb065925a702
plondon <philip@blockchain.com>
2018-11-27 15:54:00 -0500

    test(BCH - Split): transformTx for bch inputs/outputs

--- packages/blockchain-wallet-v4/src/transactions/bch.spec.js
+++ packages/blockchain-wallet-v4/src/transactions/bch.spec.js
@@ -5,15 +5,17 @@ import { Types } from '../../src'
 
 describe('transformTx', () => {
   const mockState = createMockState(walletV3)
+  const tx = _transformTx(
+    mockState.walletPath.wallet,
+    558418,
+    Types.HDAccountList.fromJS([]),
+    () => '',
+    () => '',
+    bchTx
+  )
 
-  it('should transformTx', () => {
-    // _transformTx = (
-    //   wallet,
-    //   currentBlockHeight,
-    //   accountList,
-    //   getDescription,
-    //   getPartnerLabel,
-    //   tx
-    // )
+  it('should not include dust transactions', () => {
+    expect(tx.inputs.length).toBe(1)
+    expect(tx.outputs.length).toBe(2)
   })
 })
