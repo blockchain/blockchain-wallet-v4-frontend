e479de00a0f501ac02d59dd2534b56270269077d
plondon <philip@blockchain.com>
2018-11-27 13:26:37 -0500

    test(BCH): setup bch tx mock for test

---
+++ packages/blockchain-wallet-v4/src/transactions/bch.spec.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { bchTx, createMockState, walletV3 } from '../../data'
+import { _transformTx } from './bch'
+import { Types } from '../../src'
+
+describe('transformTx', () => {
+  const mockState = createMockState(walletV3)
+
+  it('should transformTx', () => {
+    // _transformTx = (
+    //   wallet,
+    //   currentBlockHeight,
+    //   accountList,
+    //   getDescription,
+    //   getPartnerLabel,
+    //   tx
+    // )
+  })
+})
