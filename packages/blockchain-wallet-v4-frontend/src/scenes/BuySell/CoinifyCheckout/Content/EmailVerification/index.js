829ee11eda343c0ad19783c59a0807b61636438d
Philip Welber <welber@blockchain.com>
2018-11-27 13:43:21 -0500

    feature(Coinify): remove unnecessary EmailVerificationWrapper

--- packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/CoinifyCheckout/Content/EmailVerification/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/BuySell/CoinifyCheckout/Content/EmailVerification/index.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import EmailVerification from 'modals/Coinify/CoinifyExchangeData/Create'
-import styled from 'styled-components'
 
-const EmailVerificationWrapper = styled.div`
-  padding: 30px;
-`
-const emailVerification = props => {
-  return (
-    <EmailVerificationWrapper>
-      <EmailVerification country={props.country} />
-    </EmailVerificationWrapper>
-  )
-}
+const emailVerification = props => <EmailVerification country={props.country} />
 
 export default emailVerification
