e01f5442b6041930d6836fa04e868e62339a0c99
schnogz <andrew@blockchain.com>
2018-11-28 16:38:13 -0500

    ability to disable eth send/receive lockbox

--- packages/blockchain-wallet-v4-frontend/src/modals/Eth/SendEth/FirstStep/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/Eth/SendEth/FirstStep/selectors.js
@@ -11,10 +11,22 @@ export const getData = createDeepEqualSelector(
     selectors.components.sendEth.getFeeToggled,
     selectors.core.data.ethereum.getCurrentBalance,
     selectors.core.kvStore.lockbox.getDevices,
-    selectors.form.getFormValues(model.components.sendEth.FORM)
+    selectors.form.getFormValues(model.components.sendEth.FORM),
+    selectors.core.walletOptions.getCoinAvailability
   ],
-  (paymentR, toToggled, feeToggled, balanceR, lockboxDevicesR, formValues) => {
+  (
+    paymentR,
+    toToggled,
+    feeToggled,
+    balanceR,
+    lockboxDevicesR,
+    formValues,
+    getCoinAvailability
+  ) => {
     const enableToggle = !isEmpty(lockboxDevicesR.getOrElse([]))
+    const excludeLockbox = !getCoinAvailability('ETH')
+      .map(propOr(true, 'lockbox'))
+      .getOrElse(true)
 
     const transform = payment => {
       const effectiveBalance = propOr('0', 'effectiveBalance', payment)
@@ -68,7 +80,8 @@ export const getData = createDeepEqualSelector(
         minFee,
         maxFee,
         feeElements,
-        balanceStatus: balanceR
+        balanceStatus: balanceR,
+        excludeLockbox
       }
     }
     return paymentR.map(transform)
