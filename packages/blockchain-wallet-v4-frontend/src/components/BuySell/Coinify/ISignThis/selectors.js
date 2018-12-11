6b5589555e20d2a3183555048209cb95a3d923b6
Philip Welber <welber@blockchain.com>
2018-12-11 13:06:43 -0500

    fix(Coinify): remove unneeded selectors

--- packages/blockchain-wallet-v4-frontend/src/components/BuySell/Coinify/ISignThis/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/components/BuySell/Coinify/ISignThis/selectors.js
@@ -2,7 +2,5 @@ import { selectors } from 'data'
 
 export const getData = state => ({
   quoteR: selectors.core.data.coinify.getQuote(state),
-  trade: selectors.core.data.coinify.getTrade(state),
-  iSignThisDomainR: selectors.core.walletOptions.getISignThisDomain(state),
-  coinifyPaymentDomainR: selectors.core.walletOptions.getCoinifyPaymentDomain(state)
+  trade: selectors.core.data.coinify.getTrade(state)
 })
