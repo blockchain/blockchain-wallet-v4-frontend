1b8a7e0fdf207e70364ba160085d9166ea3bb8c2
plondon <philip@blockchain.com>
2018-12-13 10:41:46 -0500

    refactor(Tiers): Remove duplicative ...rest from tiercard

--- packages/blockchain-wallet-v4-frontend/src/components/IdentityVerification/TierCard/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/IdentityVerification/TierCard/index.js
@@ -20,7 +20,10 @@ const Wrapper = styled.div`
   flex-direction: column;
   border: 1px solid ${props => props.theme['gray-1']};
   box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.21);
-  width: ${props => (props.column ? '260px' : '375px')};
+  width: 375px;
+  &.column {
+    width: 260px;
+  }
   ${media.laptop`
     width: 100%;
   `};
@@ -39,41 +42,35 @@ const Header = styled(Text)`
   align-items: center
   letter-spacing: 2px;
   width: 50%;
-  ${props =>
-    props.column &&
-    `
+  ${Wrapper}.column & {
     width: 100%;
     justify-content: flex-center;
-  `}
+  }
 `
 const Row = styled.div`
   display: flex;
-  ${props =>
-    props.column &&
-    `
+  ${Wrapper}.column & {
     flex-direction: column;
-  `};
+  }
 `
 const Column = styled.div`
   display: flex;
   flex-direction: column;
   width: ${props => props.width || '100%'};
-  text-align: ${props => (props.column ? 'center' : 'left')};
   &:first-child {
     margin-right: 20px;
     border-right: 1px solid ${props => props.theme['gray-1']};
   }
-  ${props =>
-    props.column &&
-    `
+  ${Wrapper}.column & {
+    text-align: center;
     &:first-child {
       margin-right: 0px;
       border-right: 0px;
-      border-bottom: 1px solid ${props.theme['gray-1']};
+      border-bottom: 1px solid ${props => props.theme['gray-1']};
       padding-bottom: 15px;
       margin-bottom: 15px;
     }
-  `};
+  }
 `
 const Announcement = styled(Text)`
   display: flex;
@@ -92,10 +89,10 @@ const ActionButton = styled(Button)`
 `
 
 export const TierCard = ({ userData, userTiers, tier, ...rest }) => {
-  const { verifyIdentity } = rest
+  const { verifyIdentity, column } = rest
   const tierData = head(userTiers.filter(propEq('index', tier)))
   return (
-    <Wrapper {...rest}>
+    <Wrapper className={column ? 'column' : ''}>
       {tier === 2 && (
         <Announcement uppercase weight={500} size='16px' color='white'>
           <FormattedMessage
@@ -105,13 +102,7 @@ export const TierCard = ({ userData, userTiers, tier, ...rest }) => {
         </Announcement>
       )}
       <Container>
-        <Header
-          size='16px'
-          weight={400}
-          color='marketing-primary'
-          uppercase
-          {...rest}
-        >
+        <Header size='16px' weight={400} color='marketing-primary' uppercase>
           <FormattedMessage
             id='components.identityverification.tiercard.tierheader'
             defaultMessage='Tier {tier} Verification'
@@ -119,8 +110,8 @@ export const TierCard = ({ userData, userTiers, tier, ...rest }) => {
           />
         </Header>
         <Content>
-          <Row {...rest}>
-            <Column {...rest}>
+          <Row>
+            <Column>
               <Text size='28px' color='marketing-secondary'>
                 {Exchange.getSymbol(tierData.limits.currency) +
                   Currency.formatFiat(
@@ -135,7 +126,7 @@ export const TierCard = ({ userData, userTiers, tier, ...rest }) => {
                 {getTiming(tierData.state, tier)}
               </Text>
             </Column>
-            <Column {...rest}>
+            <Column>
               {path([tier, 'requirements'], TIERS).map((requirement, i) => (
                 <TextGroup inline key={i} style={{ marginBottom: '8px' }}>
                   {requirement.message}
