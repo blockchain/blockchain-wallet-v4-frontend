0e06aa461294cc69058e04716398a03a5ce388c6
schnogz <andrew@blockchain.com>
2018-12-12 19:10:21 -0500

    update tests

--- packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Menu/index.spec.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/Menu/index.spec.js
@@ -3,7 +3,12 @@ import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
 import { MenuTop } from './index'
 
-jest.mock('blockchain-info-components', () => ({ Images: '', TextGroup: '' }))
+jest.mock('blockchain-info-components', () => ({
+  Images: '',
+  TextGroup: '',
+  TabMenu: '',
+  TabMenuItem: ''
+}))
 
 describe('MenuTop Component', () => {
   it('should render with correct links and without error', () => {
@@ -17,23 +22,5 @@ describe('MenuTop Component', () => {
     const component = shallow(<MenuTop />)
     const tree = toJson(component)
     expect(tree).toMatchSnapshot()
-  })
-
-  it('should have correct item selected', () => {
-    const component = shallow(<MenuTop historySelected={false} />)
-    const exchangeItem = component
-      .find('LinkContainer')
-      .at(0)
-      .children()
-      .first()
-    expect(exchangeItem.prop('selected')).toBe(true)
-
-    component.setProps({ historySelected: true })
-    const historyItem = component
-      .find('LinkContainer')
-      .at(1)
-      .children()
-      .first()
-    expect(historyItem.prop('selected')).toBe(true)
   })
 })
