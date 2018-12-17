34a3315a2cbb096d38c09306939f493516683811
tony-blockchain <tony@blockchain.com>
2018-12-17 16:16:23 +0000

    fix(Account): renamed Profile to Account

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Footer/template.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Footer/template.js
@@ -41,11 +41,11 @@ const Footer = props => {
               />
             </SubMenuItem>
           </LinkContainer>
-          <LinkContainer to='/settings/profile' activeClassName='active'>
+          <LinkContainer to='/settings/account' activeClassName='active'>
             <SubMenuItem data-e2e='settings_profileLink'>
               <FormattedMessage
-                id='layouts.wallet.menuleft.footer.profile'
-                defaultMessage='Profile'
+                id='layouts.wallet.menuleft.footer.account'
+                defaultMessage='Account'
               />
             </SubMenuItem>
           </LinkContainer>
