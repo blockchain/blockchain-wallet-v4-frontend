277eea3fdc9dea5548fec65798e801454c75f9d1
schnogz <andrew@blockchain.com>
2018-12-29 18:57:07 -0600

    update bsv send saga specs

--- packages/blockchain-wallet-v4-frontend/src/data/components/sendBsv/sagas.spec.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/sendBsv/sagas.spec.js
@@ -229,18 +229,16 @@ describe('sendBsv sagas', () => {
     const saga = testSaga(firstStepSubmitClicked)
     const beforeError = 'beforeError'
 
+    it('should put loading action', () => {
+      saga.next().put(A.sendBsvPaymentUpdated(Remote.Loading))
+    })
+
     it('should select payment', () => {
       saga.next().select(S.getPayment)
     })
 
-    it('should put loading action', () => {
-      saga
-        .next(Remote.of(paymentMock))
-        .put(A.sendBsvPaymentUpdated(Remote.Loading))
-    })
-
     it('should create payment from state value', () => {
-      saga.next()
+      saga.next(Remote.of(paymentMock))
       expect(coreSagas.payment.bsv.create).toHaveBeenCalledTimes(1)
       expect(coreSagas.payment.bsv.create).toHaveBeenCalledWith({
         payment: paymentMock,
