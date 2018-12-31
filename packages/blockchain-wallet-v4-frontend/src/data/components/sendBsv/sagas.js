2bc25909c3c93092f78faf7e3ee4dd6295479f9b
plondon <philip@blockchain.com>
2018-12-31 13:43:30 -0500

    fix(BSV): Populate from with selected account

--- packages/blockchain-wallet-v4-frontend/src/data/components/sendBsv/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/sendBsv/sagas.js
@@ -21,8 +21,10 @@ export const logLocation = 'components/sendBsv/sagas'
 export const bsvDefaultFee = 4
 
 export default ({ coreSagas }) => {
-  const initialized = function*() {
+  const initialized = function*(action) {
     try {
+      const { payload } = action
+      const { index } = payload
       yield put(A.sendBsvPaymentUpdated(Remote.Loading))
       let payment = coreSagas.payment.bsv.create({
         network: settings.NETWORK_BSV
@@ -34,7 +36,7 @@ export default ({ coreSagas }) => {
       const defaultIndexR = yield select(
         selectors.core.kvStore.bsv.getDefaultAccountIndex
       )
-      const defaultIndex = defaultIndexR.getOrElse(0)
+      const defaultIndex = index || defaultIndexR.getOrElse(0)
       const defaultAccountR = accountsR.map(nth(defaultIndex))
       payment = yield payment.from(defaultIndex, ADDRESS_TYPES.ACCOUNT)
       payment = yield payment.fee(bsvDefaultFee)
