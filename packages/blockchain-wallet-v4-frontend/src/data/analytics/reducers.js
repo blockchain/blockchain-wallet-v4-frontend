a72f4cf33d46b373bbec85f9a365b58bdcbcccb6
Philip Welber <welber@blockchain.com>
2019-01-02 16:22:02 -0500

    fix(Analytics): remove assert from reducer

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/reducers.js
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/reducers.js
@@ -1,6 +1,5 @@
 import * as AT from './actionTypes'
-import { insert, path, contains, assoc } from 'ramda'
-import assert from 'assert'
+import { insert, path, contains, assoc, hasPath } from 'ramda'
 import { TRACKING_ACTIONS } from './model'
 import { SHOW_CONFIRMATION } from '../components/exchange/actionTypes'
 import moment from 'moment'
@@ -15,12 +14,16 @@ const analytics = (state = INITIAL_STATE, action) => {
 
   switch (type) {
     case AT.LOG_EVENT: {
-      assert(path(['trackingData', 'length'], payload), 'trackingData cannot be empty')
-      const trackingAction = path(['trackingData', 1], payload)
-      assert(contains(trackingAction, TRACKING_ACTIONS), `tracking action: ${trackingAction} is not recognized`)
-      const newEvents = insert(0, payload, state.events)
-
-      return assoc('events', newEvents, state)
+      const pathIsValid = hasPath(['trackingData', 'length'], payload)
+      const actionIsValid = contains(
+        path(['trackingData', 1], payload),
+        TRACKING_ACTIONS
+      )
+      if (pathIsValid && actionIsValid) {
+        const newEvents = insert(0, payload, state.events)
+        return assoc('events', newEvents, state)
+      }
+      return state
     }
     case AT.SET_SWAP_START_TIME: {
       return assoc('swapTiming', payload, state)
