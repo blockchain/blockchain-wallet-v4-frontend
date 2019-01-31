75057f0c4ada1c3a3fc2e55c2aae4d5e96895047
schnogz <andrew@blockchain.com>
2019-01-31 09:33:30 -0500

    ensure user is authed before starting analytics

--- packages/blockchain-wallet-v4-frontend/src/providers/AnalyticsTracker/index.js
+++ packages/blockchain-wallet-v4-frontend/src/providers/AnalyticsTracker/index.js
@@ -14,19 +14,25 @@ const Iframe = styled.iframe`
 
 class AnalyticsTracker extends React.PureComponent {
   render () {
-    return (
-      <Iframe
-        id='matomo-iframe'
-        src={this.props.domains.walletHelper + '/wallet-helper/matomo/#/'}
-      />
-    )
+    const { domains, isAuthenticated } = this.props
+    // ensure iframe is added only once
+    if (isAuthenticated && !document.getElementById('matomo-iframe')) {
+      return (
+        <Iframe
+          id='matomo-iframe'
+          src={domains.walletHelper + '/wallet-helper/matomo/#/'}
+        />
+      )
+    }
+    return null
   }
 }
 
 const mapStateToProps = state => ({
   domains: selectors.core.walletOptions.getDomains(state).getOrElse({
     walletHelper: 'https://wallet-helper.blockchain.com'
-  })
+  }),
+  isAuthenticated: selectors.auth.isAuthenticated(state)
 })
 
 export default connect(mapStateToProps)(AnalyticsTracker)
