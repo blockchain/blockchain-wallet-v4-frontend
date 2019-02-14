018fba1d5cf4911dca095928f1e610c2c62f8946
tony-blockchain <tony@blockchain.com>
2019-02-14 18:45:04 +0000

    fix(Swap): restyled swap getstarted modal

--- packages/blockchain-wallet-v4-frontend/src/data/components/swapGetStarted/sagaRegister.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/swapGetStarted/sagaRegister.js
@@ -2,14 +2,10 @@ import { takeLatest } from 'redux-saga/effects'
 import * as AT from './actionTypes'
 import sagas from './sagas'
 
-export default ({ coreSagas }) => {
-  const swapGetStartedSagas = sagas({ coreSagas })
+export default () => {
+  const swapGetStartedSagas = sagas()
 
   return function* swapGetStartedSaga () {
-    yield takeLatest(
-      AT.SWAP_GET_STARTED_INITIALIZED,
-      swapGetStartedSagas.swapGetStartedInitialized
-    )
     yield takeLatest(
       AT.SWAP_GET_STARTED_SUBMIT_CLICK,
       swapGetStartedSagas.swapGetStartedSubmitClicked
