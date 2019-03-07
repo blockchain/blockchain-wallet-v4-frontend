ac24d10ff33373b0d07c9293f8d906330e9ea037
plondon <philip@blockchain.com>
2019-03-07 16:27:54 -0500

    fix(Onboarding): Add amount back to airdrop msging

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/AirdropReminder/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/AirdropReminder/index.js
@@ -79,8 +79,8 @@ export const AirdropReminderBanner = ({
     <Column>
       <LargeText>
         <FormattedMessage
-          defaultMessage='Complete your profile today and we will airdrop free {coinName} ({coinCode}) in your Wallet.'
-          id='scenes.home.banners.airdrop.title_1'
+          defaultMessage='Complete your profile today and we will airdrop $25 of free {coinName} ({coinCode}) in your Wallet.'
+          id='scenes.home.banners.airdrop.completeprofile'
           values={{
             coinName: prop('coinName', CAMPAIGNS[campaign]),
             coinCode: prop('coinCode', CAMPAIGNS[campaign])
