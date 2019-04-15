95604a8af0fef1e4794b1656c7fbf65a122ce76d
schnogz <andrew@blockchain.com>
2019-04-15 14:46:24 -0400

    update tests

--- packages/blockchain-wallet-v4/src/redux/data/eth/sagas.spec.js
+++ packages/blockchain-wallet-v4/src/redux/data/eth/sagas.spec.js
@@ -1,14 +1,12 @@
-import { select } from 'redux-saga/effects'
-import { path, sum, values, length } from 'ramda'
+import { path, length } from 'ramda'
 import * as A from './actions'
 import * as AT from './actionTypes'
 import * as S from './selectors'
 import * as selectors from '../../selectors'
 
 import { Remote } from 'blockchain-wallet-v4/src'
-import { expectSaga, testSaga } from 'redux-saga-test-plan'
+import { testSaga } from 'redux-saga-test-plan'
 import sagas from './sagas'
-import reducers from '../reducers'
 
 const ethFetchData = {
   '0x8790143B84A1A12ADA4aF057D7096A937143a476': {
@@ -69,19 +67,6 @@ describe('eth data sagas', () => {
       '0x4e7943357bbd52afd8d317fa7974abf8bb64beffe906bb4ab4d42e7ef5ac6af1'
     )
 
-    const ethInfo = {
-      n_tx: sum(values(ethFetchData).map(obj => obj.txn_count)),
-      total_received: sum(values(ethFetchData).map(obj => obj.totalReceived)),
-      total_sent: sum(values(ethFetchData).map(obj => obj.totalSent)),
-      final_balance: sum(values(ethFetchData).map(obj => obj.balance))
-    }
-
-    const ethData = {
-      addresses: ethFetchData,
-      info: ethInfo,
-      latest_block: latest_block
-    }
-
     const saga = testSaga(dataEthSagas.fetchData)
 
     it('should put loading state', () => {
@@ -99,10 +84,14 @@ describe('eth data sagas', () => {
     })
 
     it('should retrieve latest_block info and end', () => {
+      saga.next(ethFetchData).call(api.getEthLatestBlock)
+    })
+
+    it('should check for low eth balance and end', () => {
       saga
-        .next(ethFetchData)
-        .call(api.getEthLatestBlock)
         .next()
+        .next()
+        .call(dataEthSagas.checkForLowEthBalance)
         .next()
         .isDone()
     })
@@ -117,43 +106,6 @@ describe('eth data sagas', () => {
         .put(A.fetchDataFailure(error.message))
         .next()
         .isDone()
-    })
-
-    describe('state change', () => {
-      it('should add eth data to the state', () => {
-        return expectSaga(dataEthSagas.fetchData)
-          .withReducer(reducers)
-          .provide([[select(S.getContext), mockContext]])
-          .run()
-          .then(result => {
-            expect(result.storeState.eth).toMatchObject({
-              addresses: Remote.Success(ethData.addresses),
-              info: {
-                eth: Remote.Success(ethData.info),
-                pax: Remote.NotAsked
-              },
-              latest_block: Remote.Success(ethData.latest_block),
-              current_balance: {
-                eth: Remote.NotAsked,
-                pax: Remote.NotAsked
-              },
-              fee: Remote.NotAsked,
-              legacy_balance: Remote.NotAsked,
-              rates: {
-                eth: Remote.NotAsked,
-                pax: Remote.NotAsked
-              },
-              transactions_at_bound: {
-                eth: false,
-                pax: false
-              },
-              transactions: {
-                eth: [],
-                pax: []
-              }
-            })
-          })
-      })
     })
   })
 
