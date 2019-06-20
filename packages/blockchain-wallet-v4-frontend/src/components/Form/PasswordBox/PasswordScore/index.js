203da69b48b47c6e94bb8decd08a8671d87d519f
schnogz <andrew@blockchain.com>
2019-06-20 10:54:23 -0400

    simplify and fix password gauge component

--- packages/blockchain-wallet-v4-frontend/src/components/Form/PasswordBox/PasswordScore/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { PasswordGauge } from 'blockchain-info-components'
-
-const PasswordScore = props => {
-  return <PasswordGauge score={props.passwordScore} />
-}
-
-export default PasswordScore
