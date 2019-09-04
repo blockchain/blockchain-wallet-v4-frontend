2bb541a4d3983f7e3012d75edf32dfd533853610
schnogz <andrew@blockchain.com>
2019-09-04 17:24:20 -0500

    feat(bitpay): add more analytic events

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/model.js
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/model.js
@@ -109,8 +109,9 @@ export const TRANSACTION_EVENTS = {
   PAYMENT_REQUEST: ['transactions', 'payment_request'],
   SEND_FAILURE: ['send_failure'],
   VIEW_TX_ON_EXPLORER: ['transactions', 'view_tx_explorer'],
-  BITPAY_INITIALIZED: ['transactions', 'bitpay', 'initialized'],
-  BITPAY_SENT: ['transactions', 'bitpay', 'sent']
+  BITPAY_URL_DEEPLINK: ['transactions', 'bitpay', 'bitpay_url_deeplink'],
+  BITPAY_FAILURE: ['transactions', 'bitpay', 'bitpay_payment_failure'],
+  BITPAY_SUCCESS: ['transactions', 'bitpay', 'bitpay_payment_success']
 }
 export const WALLET_EVENTS = {
   ADD_NEW: ['wallets', 'add_new'],
