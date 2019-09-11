77a63cbc5f006df330858fa6b6e6849ef37d8215
Sean Pollock <sean@blockchain.com>
2019-09-11 12:02:51 -0400

    adding checkmark svg when alert nature is 'success'

--- packages/blockchain-wallet-v4-frontend/src/components/Alerts/template.js
+++ packages/blockchain-wallet-v4-frontend/src/components/Alerts/template.js
@@ -43,11 +43,29 @@ const Alerts = props => {
             persist={persist}
             onClose={() => handleClose(id)}
           >
+            {nature === 'success' ? <SuccessCheckmark /> : null}
             {getAlertContent(message, data, handleClose, id)}
           </Toast>
         )
       })}
     </Wrapper>
+  )
+}
+
+const SuccessCheckmark = () => {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M21.3333 0H2.66667C1.18667 0 0 1.2 0 2.66667V21.3333C0 22.8 1.18667 24 2.66667 24H21.3333C22.8133 24 24 22.8 24 21.3333V2.66667C24 1.2 22.8133 0 21.3333 0ZM9.33333 18.6667L2.66667 12L4.54667 10.12L9.33333 14.8933L19.4533 4.77333L21.3333 6.66667L9.33333 18.6667Z'
+        fill='#0C6CF2'
+      />
+    </svg>
   )
 }
 
