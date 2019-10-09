df5b2f494247bd9a5867d8d683e879aacab435c9
plondon <philip@blockchain.com>
2019-10-09 21:50:33 +0200

    feat(Signup): PIT callout cleanup

--- packages/blockchain-wallet-v4-frontend/src/scenes/Register/PitCallout/index.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Register/PitCallout/index.js
@@ -1,17 +1,10 @@
-import React from 'react'
-import styled, { keyframes, css } from 'styled-components'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { FormattedMessage } from 'react-intl'
 import { prop } from 'ramda'
 import { Icon, Image, Link, Text } from 'blockchain-info-components'
 import { Wrapper } from 'components/Public'
 
-const popoutKeyFrame = keyframes`
-  0%, 10%, 20%, 30%, 70%, 80%, 90%, 100% { right: -40px; }
-  40%, 50%, 60% { right: -70px; }
-`
-const popoutAnimation = css`
-  animation: ${popoutKeyFrame} 5s infinite
-    cubic-bezier(0.68, -0.55, 0.265, 1.55);
-`
 const InnerWrapperWidth = 280
 const CopyWrapperWidth = 200
 const WrapperPaddingX = 30
@@ -20,16 +13,15 @@ const WrapperPaddingY = 20
 const PitCalloutWrapper = styled(Wrapper)`
   width: auto;
   top: 50%;
-  right: -40px;
+  right: -20px;
   position: absolute;
   flex-direction: column;
   transform: translateY(-50%);
   padding: ${WrapperPaddingY}px ${WrapperPaddingX}px;
-  ${popoutAnimation};
-  transition: all 0.3s;
-  &:hover {
-    transform: translateY(-50%) translateX(${InnerWrapperWidth - 10}px);
-    animation: initial;
+  transition: transform 0.3s 0.7s, opacity 0.3s 0.7s;
+  &.active {
+    transform: translateY(-50%)
+      translateX(${InnerWrapperWidth + WrapperPaddingX - 20}px);
     .footer {
       opacity: 1;
     }
@@ -72,51 +64,66 @@ const GoLink = styled.div`
 `
 
 const PitCallout = ({ abTestR, domainsR }) => {
+  const [isCalloutVisible, showCallout] = useState(false)
   const domain = prop(
     'thePit',
     domainsR.getOrElse({ thePit: 'https://pit.blockchain.com' })
   )
-  const campaign = abTestR.getOrElse('original')
+  const campaign = 'pit_signup'
+
+  setTimeout(() => showCallout(true), 1)
 
   return campaign === 'original' ? null : (
-    <PitCalloutWrapper>
-      <InnerWrapper>
-        <CopyWrapper>
-          <Text color='gray-6' size='20px' weight={600}>
-            Trade Like the Pros.
-          </Text>
-          <Text color='gray-6' size='20px' weight={600}>
-            Trade in The PIT.
-          </Text>
-          <Copy>
-            <Text color='textBlack' size='12px' weight={500}>
-              We built a Crypto Exchange. Instantly Exchange over 26 pairs of
-              Crypto and Cash.
+    <PitCalloutWrapper className={isCalloutVisible && 'active'}>
+      <Link
+        href={`${domain}${
+          campaign === 'pit_signup' ? '/trade/signup' : ''
+        }?utm_source=web_wallet&utm_medium=wallet_signup&utm_campaign=${campaign}`}
+        rel='noopener noreferrer'
+        target='_blank'
+      >
+        <InnerWrapper>
+          <CopyWrapper>
+            <Text color='gray-6' size='20px' weight={600}>
+              <FormattedMessage
+                id='components.pitcallout.tradeinthepit'
+                defaultMessage='Trade in The PIT.'
+              />
             </Text>
-          </Copy>
-        </CopyWrapper>
-        <Image
-          width='100%'
-          name='pit-macbook-gold'
-          srcset={{
-            'pit-macbook-gold2': '2x',
-            'pit-macbook-gold3': '3x'
-          }}
-        />
-        <FooterLink
-          href={`${domain}/?utm_source=web_wallet&utm_medium=wallet_signup&utm_campaign=${campaign}`}
-          className='footer'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Text color='blue' weight={600}>
-            Check It Out
-          </Text>
-          <GoLink>
-            <Icon name='down-arrow-filled' color='brand-secondary' />
-          </GoLink>
-        </FooterLink>
-      </InnerWrapper>
+            <Copy>
+              <Text color='textBlack' size='12px' weight={500}>
+                <FormattedMessage
+                  id='components.pitcallout.tradecrypto'
+                  defaultMessage='Trade crypto & fiat in The PIT, Blockchain’s new lightning fast exchange.'
+                />
+              </Text>
+            </Copy>
+          </CopyWrapper>
+          <Image
+            width='100%'
+            name='pit-macbook-gold'
+            srcset={{
+              'pit-macbook-gold2': '2x',
+              'pit-macbook-gold3': '3x'
+            }}
+          />
+          <FooterLink
+            href={`${domain}${
+              campaign === 'pit_signup' ? '/trade/signup' : ''
+            }?utm_source=web_wallet&utm_medium=wallet_signup&utm_campaign=${campaign}`}
+            className='footer'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <Text color='blue' weight={600}>
+              Check It Out
+            </Text>
+            <GoLink>
+              <Icon name='down-arrow-filled' color='brand-secondary' />
+            </GoLink>
+          </FooterLink>
+        </InnerWrapper>
+      </Link>
     </PitCalloutWrapper>
   )
 }
