8b25757ae8f314a7ef21f06ff59fb69b8f61e486
jjBlockchain <jj@blockchain.com>
2019-10-21 09:24:28 -0400

    fix(qr-code-dark-mode)

--- packages/blockchain-wallet-v4-frontend/src/components/QRCodeWrapper/index.js
+++ packages/blockchain-wallet-v4-frontend/src/components/QRCodeWrapper/index.js
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+  height: 150px;
+  background-color: white;
+  border-radius: 6px;
   canvas {
     padding: 12px;
     border: 1px solid ${props => props.theme['gray-2']};
