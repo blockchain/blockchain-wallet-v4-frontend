a738303f513e3e65cab78aa6bbe12295d0f4597f
Sean Pollock <sean@blockchain.com>
2019-12-12 14:35:59 -0500

    quick fix to error handling

--- packages/blockchain-wallet-v4-frontend/src/data/components/signMessageBch/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/signMessageBch/sagas.js
@@ -1,21 +1,20 @@
 import * as A from './actions.js'
 import * as actions from '../../actions.js'
+import * as C from 'services/AlertService'
 import { put } from 'redux-saga/effects'
 import { signMessage as sign } from 'blockchain-wallet-v4/src/signer/btc'
 
 export default ({ coreSagas }) => {
-  const logLocation = 'components/bchSignMessage/sagas'
+  const logLocation = 'components/signMessageBch/sagas'
 
   const signMessage = function * (action) {
     try {
-      // TODO: handle second password
       const { addr, priv, message } = action.payload
-      // sign message with private key
       const signed = yield sign(priv, addr, message)
       yield put(A.messageSigned(signed))
     } catch (error) {
-      yield put(actions.logs.logErrorMessage(logLocation, 'signedMessageBch'))
-      // TODO: display error message
+      yield put(actions.logs.logErrorMessage(logLocation, 'signMessage', error))
+      yield put(actions.alerts.displayError(C.MESSAGE_SIGN_ERROR))
     }
   }
 
