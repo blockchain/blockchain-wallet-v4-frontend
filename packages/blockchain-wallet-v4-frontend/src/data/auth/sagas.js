3f22462d89e41f8950fa667573a4d7fe63d362db
schnogz <andrew@blockchain.com>
2020-01-05 11:06:18 -0500

    remove register for stx airdrop goals

--- packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.js
+++ packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.js
@@ -83,7 +83,6 @@ export default ({ api, coreSagas }) => {
 
   const saveGoals = function * (firstLogin) {
     yield put(actions.goals.saveGoal('walletTour', { firstLogin }))
-    yield put(actions.goals.saveGoal('registerForBlockstackAirdrop'))
     yield put(actions.goals.saveGoal('coinifyUpgrade'))
     yield put(actions.goals.saveGoal('coinifyBuyViaCard'))
     yield put(actions.goals.saveGoal('upgradeForAirdrop'))
