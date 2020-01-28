b1ffb98536e6d5f02b379c54e6e663b38a282298
schnogz <andrew@blockchain.com>
2020-01-28 08:21:44 -0500

    feat(balances): style currency toggle button

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Balances/CurrencySwitch/template.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Balances/CurrencySwitch/template.js
@@ -1,36 +1,54 @@
+import { Button, Text } from 'blockchain-info-components'
 import { FormattedMessage } from 'react-intl'
-import { Text } from 'blockchain-info-components'
 import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  padding: 7px 0;
-  margin-top: 15px;
-  text-align: center;
-  border-top: 1px solid ${props => props.theme['gray-1']};
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+  margin: 12px 0 20px;
+`
+
+const SwitchButton = styled(Button)`
+  width: 200px;
+  height: 30px;
+  padding: 0;
+
+  &:hover {
+    border: 1px solid ${props => props.theme.blue600};
+    background-color: transparent;
+  }
+`
+const ButtonText = styled(Text)`
+  color: ${props => props.theme.blue600};
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 150%;
 `
 
 const Template = props => (
   <Wrapper>
-    <Text
-      size='11px'
-      color='blue600'
+    <SwitchButton
       onClick={props.toggleCoinDisplayed}
       data-e2e='balanceDropdown-currency-switch'
     >
-      {props.coinDisplayed ? (
-        <FormattedMessage
-          id='layouts.wallet.menutop.balance.walletbalance.showfiat'
-          defaultMessage='Show {currency}'
-          values={{ currency: props.currency }}
-        />
-      ) : (
-        <FormattedMessage
-          id='layouts.wallet.menutop.balance.walletbalance.showcrypto'
-          defaultMessage='Show Crypto'
-        />
-      )}
-    </Text>
+      <ButtonText>
+        {props.coinDisplayed ? (
+          <FormattedMessage
+            id='layouts.wallet.menutop.balance.walletbalance.showfiat'
+            defaultMessage='Show {currency}'
+            values={{ currency: props.currency }}
+          />
+        ) : (
+          <FormattedMessage
+            id='layouts.wallet.menutop.balance.walletbalance.showcrypto'
+            defaultMessage='Show Crypto'
+          />
+        )}
+      </ButtonText>
+    </SwitchButton>
   </Wrapper>
 )
 
