88ad59374c72fd3f29da1898d34e566709acf38a
Philip London <philip@blockchain.com>
2020-02-04 17:21:25 +0100

    fix(Airdrops): fix airdrop page if no user has been created

--- packages/blockchain-wallet-v4-frontend/src/scenes/Airdrops/StxAirdrop/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Airdrops/StxAirdrop/index.tsx
@@ -1,9 +1,15 @@
 import { Box } from '../AirdropInfo'
-import { CampaignInfoType, TagsType } from 'data/types'
+import { CampaignInfoType } from 'data/types'
 import { Icon, Text } from 'blockchain-info-components'
-import { KycStatesType } from 'data/components/identityVerification/types'
 import { LinkDispatchPropsType } from '..'
-import { StxDateOrAmount, StxFooterCta, StxHeader, StxInfo, StxStatus } from './model'
+import { Props } from '../template.success'
+import {
+  StxDateOrAmount,
+  StxFooterCta,
+  StxHeader,
+  StxInfo,
+  StxStatus
+} from './model'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -21,19 +27,20 @@ const StatusContainer = styled.div`
   }
 `
 
-export type Props = {
-  userCampaignsInfoResponseList: Array<CampaignInfoType>,
-  kycState: KycStatesType,
-  tags: TagsType
-}
-
 const StxAirdrop = (props: Props & LinkDispatchPropsType) => {
+  if (props.userDoesNotExistYet === true) return null
+
   const stxCampaign = props.userCampaignsInfoResponseList.find(
     (campaign: CampaignInfoType) => campaign.campaignName === 'BLOCKSTACK'
   )
 
   // do not show card if user did not sign up for airdrop
-  if (!stxCampaign || !stxCampaign.userCampaignState || stxCampaign.userCampaignState === 'NONE') return null
+  if (
+    !stxCampaign ||
+    !stxCampaign.userCampaignState ||
+    stxCampaign.userCampaignState === 'NONE'
+  )
+    return null
 
   return (
     <Box>
