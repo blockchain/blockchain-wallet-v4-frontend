dfea45ffdd1b189d43b4dcc0f3040932d0b39585
TheLeoB <leora@blockchain.com>
2020-02-06 12:38:03 -0500

    fix(sfox): remove part 3

--- packages/blockchain-wallet-v4-frontend/src/services/ExchangeService/index.js
+++ packages/blockchain-wallet-v4-frontend/src/services/ExchangeService/index.js
@@ -1,7 +1,6 @@
 import { isNil, path } from 'ramda'
 
 export const hasAccount = partners => {
-  if (!isNil(path(['sfox', 'account_token'], partners))) return 'sfox'
   if (!isNil(path(['coinify', 'offline_token'], partners))) return 'coinify'
 }
 
@@ -9,16 +8,10 @@ export const findMatch = (settings, options) => {
   /* eslint-disable */
   const { country_code, state } = settings
   /* eslint-enable */
-  const { sfox, coinify } = options.platforms.web
+  const { coinify } = options.platforms.web
 
   // if user's location matches a partner country code set match
   if (coinify.countries.indexOf(country_code) > -1) return 'coinify'
-  if (
-    sfox.countries.indexOf(country_code) > -1 &&
-    (sfox.states.indexOf(state) > -1 || !state)
-  ) {
-    return 'sfox'
-  }
 }
 
 // settings, options, buySell, type ('Buy' || 'Sell') => 'partner' || false
@@ -38,16 +31,6 @@ export const canTrade = (settings, options, buySell, type) => {
   if (!invited) return false
 
   switch (match) {
-    case 'sfox':
-      return (
-        (type
-          ? type === 'Buy'
-            ? // check sfoxBuyV4 to avoid conflict with v3
-              // which also checks invited.sfoxBuy
-              invited['sfoxBuyV4']
-            : invited['sfoxSell']
-          : invited.sfoxBuyV4 || invited.sfoxSell) && 'sfox'
-      )
     case 'coinify':
       return (
         (type
