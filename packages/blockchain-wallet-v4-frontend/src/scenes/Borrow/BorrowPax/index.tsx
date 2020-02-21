53bc1fc8dcd61de3b5839ac6df7ee32d2e5c1ba8
Philip London <philip@blockchain.com>
2020-02-21 14:24:57 +0100

    feat(borrow): implement tier 2 check

--- packages/blockchain-wallet-v4-frontend/src/scenes/Borrow/BorrowPax/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Borrow/BorrowPax/index.tsx
@@ -1,11 +1,9 @@
 import { Box } from 'components/Box'
 import { Button, Icon, Text } from 'blockchain-info-components'
 import { FormattedMessage } from 'react-intl'
+import { Props, State } from '..'
 import React, { PureComponent } from 'react'
 import styled from 'styled-components'
-
-interface Props {}
-interface State {}
 
 const CustomBox = styled(Box)`
   background-image: url('/img/buy-sell-learn-more.png');
@@ -16,48 +14,66 @@ const CustomBox = styled(Box)`
   );
   /* stylelint-enable */
   background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `
 
-class BorrowPax extends PureComponent<Props, State> {
-  state = {}
-
+class BorrowPax extends PureComponent<Props & State> {
   render () {
     return (
       <CustomBox>
-        <Icon name='borrow' color='blue600' size='32px' />
-        <Text
-          size='20px'
-          color='grey800'
-          weight={600}
-          style={{ marginTop: '16px' }}
-        >
-          <FormattedMessage
-            id='scenes.borrow.borrowusd'
-            defaultMessage='Borrow USD Pax Today'
-          />
-        </Text>
-        <Text
-          size='14px'
-          color='grey600'
-          weight={500}
-          style={{ marginTop: '4px', lineHeight: 1.5 }}
-        >
-          <FormattedMessage
-            id='scenes.borrow.getusdpax'
-            defaultMessage='Get USD Pax directly from your Blockchain Wallet, use your bitcoin as collateral. You need to be Gold level to benefit from this new offering.'
-          />
-        </Text>
-        <Button
-          style={{ marginTop: '16px' }}
-          nature='light'
-          fullwidth
-          data-e2e='paxLearnMore'
-        >
-          <FormattedMessage
-            id='scenes.borrow.learnmore'
-            defaultMessage='Learn More'
-          />
-        </Button>
+        <div>
+          <Icon name='borrow' color='blue600' size='32px' />
+          <Text
+            size='20px'
+            color='grey800'
+            weight={600}
+            style={{ marginTop: '16px' }}
+          >
+            <FormattedMessage
+              id='scenes.borrow.borrowusd'
+              defaultMessage='Borrow USD Pax Today'
+            />
+          </Text>
+          <Text
+            size='14px'
+            color='grey600'
+            weight={500}
+            style={{ marginTop: '4px', lineHeight: 1.5 }}
+          >
+            <FormattedMessage
+              id='scenes.borrow.getusdpax'
+              defaultMessage='Get USD Pax directly from your Blockchain Wallet, use your bitcoin as collateral. You need to be Gold level to benefit from this new offering.'
+            />
+          </Text>
+        </div>
+        {this.props.isDisabled ? (
+          <Button
+            nature='primary'
+            data-e2e='verifyIdentityBorrow'
+            onClick={() =>
+              this.props.identityVerificationActions.verifyIdentity(2)
+            }
+          >
+            <FormattedMessage
+              id='scenes.borrow.verifyid'
+              defaultMessage='Upgrade Now'
+            />
+          </Button>
+        ) : (
+          <Button
+            style={{ marginTop: '16px' }}
+            nature='light'
+            fullwidth
+            data-e2e='paxLearnMore'
+          >
+            <FormattedMessage
+              id='scenes.borrow.learnmore'
+              defaultMessage='Learn More'
+            />
+          </Button>
+        )}
       </CustomBox>
     )
   }
