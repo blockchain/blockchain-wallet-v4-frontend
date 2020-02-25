cc54b3cbbdad0c38ebb3c9669c8fcc8ab047902b
jjBlockchain <jj@blockchain.com>
2020-02-25 15:01:35 -0500

    feat(input-redesign): changed these input to new design

--- packages/blockchain-info-components/src/Form/NumberInput.js
+++ packages/blockchain-info-components/src/Form/NumberInput.js
@@ -2,6 +2,14 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
+import {
+  hasValue,
+  isValid,
+  selectBackgroundColor,
+  selectBorderColor,
+  selectFocusBorderColor
+} from './helper'
+
 const BaseNumberInput = styled.input.attrs({
   type: 'number'
 })`
@@ -15,17 +23,25 @@ const BaseNumberInput = styled.input.attrs({
   font-weight: 500;
   color: ${props =>
     props.color ? props.theme[props.color] : props.theme['gray-5']};
-  background-color: ${props => props.theme.white};
+  background-color: ${({ bgColor, hasValue, isValid, theme }) =>
+    hasValue && isValid ? theme.white : theme[bgColor]};
   font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   background-image: none;
   outline-width: 0;
   user-select: text;
-  border-radius: 4px;
-  border: 1px solid ${props => props.theme[props.borderColor]};
+  border-radius: 8px;
+  border: ${({ borderColor, hasValue, isValid, theme }) =>
+    hasValue ? (isValid ? `1px solid ${theme[borderColor]}` : 'none') : 'none'};
   border-right: ${props => (props.borderRightNone ? 'none' : '')};
   border-top: ${props => (props.borderTopNone ? 'none' : '')};
   cursor: ${props => props.disabled && 'not-allowed'};
+
+  &:focus {
+    background-color: ${({ theme }) => theme.white};
+    border: 1px solid
+      ${({ focusedBorderColor, theme }) => theme[focusedBorderColor]};
+  }
   &::placeholder {
     color: ${props => props.theme['gray-3']};
     opacity: 0.4;
@@ -37,25 +53,17 @@ const BaseNumberInput = styled.input.attrs({
   }
 `
 
-const selectBorderColor = state => {
-  switch (state) {
-    case 'initial':
-      return 'grey100'
-    case 'invalid':
-      return 'error'
-    case 'valid':
-      return 'success'
-    default:
-      return 'grey100'
-  }
-}
-
-const NumberInput = props => {
-  const { errorState, ...rest } = props
-  const borderColor = selectBorderColor(errorState)
-
-  return <BaseNumberInput borderColor={borderColor} {...rest} />
-}
+const NumberInput = ({ errorState, value, ...rest }) => (
+  <BaseNumberInput
+    bgColor={selectBackgroundColor(errorState)}
+    borderColor={selectBorderColor(errorState)}
+    focusedBorderColor={selectFocusBorderColor(errorState)}
+    hasValue={hasValue(value)}
+    isValid={isValid(errorState)}
+    value={value}
+    {...rest}
+  />
+)
 
 NumberInput.propTypes = {
   disabled: PropTypes.bool,
