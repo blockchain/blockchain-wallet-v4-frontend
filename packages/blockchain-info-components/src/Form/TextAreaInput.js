2925955bb90c7ada48e758cba30d41234cce69ed
jjBlockchain <jj@blockchain.com>
2020-03-05 11:25:17 -0500

    fix(input): textAreaInput

--- packages/blockchain-info-components/src/Form/TextAreaInput.js
+++ packages/blockchain-info-components/src/Form/TextAreaInput.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import {
-  hasValue,
-  isValid,
-  selectBackgroundColor,
-  selectBorderColor,
-  selectFocusBorderColor
-} from './helper'
+import { selectBorderColor, selectFocusBorderColor } from './helper'
 
 const BaseTextAreaInput = styled.textarea`
   display: block;
@@ -16,8 +10,7 @@ const BaseTextAreaInput = styled.textarea`
   box-sizing: border-box;
   font-size: 14px;
   color: ${props => props.theme['gray-6']};
-  background-color: ${({ bgColor, hasValue, isValid, theme }) =>
-    hasValue && isValid ? theme.white : theme[bgColor]};
+  background-color: ${({ theme }) => theme.white};
   background-image: none;
   outline-width: 0;
   user-select: text;
@@ -25,22 +18,15 @@ const BaseTextAreaInput = styled.textarea`
   font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   resize: ${props => (props.resize ? 'initial' : 'none')};
-  border: ${({ borderColor, hasValue, isValid, theme }) =>
-    hasValue && isValid
-      ? `1px solid ${theme[borderColor]}`
-      : '1px solid transparent'};
+  border: ${({ borderColor, theme }) => `1px solid ${theme[borderColor]}`};
   border-radius: 8px;
 
-  &:focus {
-    padding: 5px 11px;
-    background-color: ${({ theme }) => theme.white};
-    border: 1px solid
-      ${({ focusedBorderColor, theme }) => theme[focusedBorderColor]};
+  &:focus::placeholder {
+    opacity: 0.25;
   }
 
-  &::-webkit-input-placeholder {
-    opacity: 0.4;
-    color: ${props => props.theme['gray-3']};
+  &::placeholder {
+    color: ${props => props.theme.grey400};
     font-size: 14px;
     font-weight: 500;
   }
@@ -48,11 +34,8 @@ const BaseTextAreaInput = styled.textarea`
 
 const TextAreaInput = ({ errorState, value, ...rest }) => (
   <BaseTextAreaInput
-    bgColor={selectBackgroundColor(errorState)}
     borderColor={selectBorderColor(errorState)}
     focusedBorderColor={selectFocusBorderColor(errorState)}
-    hasValue={hasValue(value)}
-    isValid={isValid(errorState)}
     value={value}
     {...rest}
   />
