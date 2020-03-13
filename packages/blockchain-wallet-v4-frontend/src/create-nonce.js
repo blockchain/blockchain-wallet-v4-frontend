e9cc74ab41bd1643f0bfffd8d1aa31128072d303
Philip London <philip@blockchain.com>
2020-03-13 16:26:35 +0100

    fix(react-select): pass nonce to emotion/cache

--- packages/blockchain-wallet-v4-frontend/src/create-nonce.js
+++ packages/blockchain-wallet-v4-frontend/src/create-nonce.js
@@ -1,3 +1,9 @@
+import createCache from '@emotion/cache'
+
 /* eslint-disable */
 __webpack_nonce__ = window.NONCE
 /* eslint-enable */
+
+export const myCache = createCache({
+  nonce: window.NONCE
+})
