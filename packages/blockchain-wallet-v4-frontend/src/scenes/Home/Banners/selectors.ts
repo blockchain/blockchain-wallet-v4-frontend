0c284cf79a7ac81cad960827bf82f22ea816cfff
Philip London <philip@blockchain.com>
2020-04-08 15:10:25 +0200

    feat(simple buy): if coinify user and no sb order history show banner

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/selectors.ts
@@ -1,19 +1,20 @@
 import { anyPass, equals } from 'ramda'
 import { model, selectors } from 'data'
+import { Remote } from 'blockchain-wallet-v4/src'
 import { SBOrderType } from 'core/types'
 
 const { GENERAL, EXPIRED } = model.profile.DOC_RESUBMISSION_REASONS
+export type BannerType = 'resubmit' | 'sbOrder' | 'finishKyc' | 'coinifyToSb'
 
-export const getData = state => {
+export const getData = (state): { bannerToShow: BannerType } => {
   // @ts-ignore
   const showDocResubmitBanner = selectors.modules.profile
     .getKycDocResubmissionStatus(state)
     .map(anyPass([equals(GENERAL), equals(EXPIRED)]))
     .getOrElse(false)
 
-  const orders: Array<SBOrderType> = selectors.components.simpleBuy
-    .getSBOrders(state)
-    .getOrElse([])
+  const ordersR = selectors.components.simpleBuy.getSBOrders(state)
+  const orders: Array<SBOrderType> = ordersR.getOrElse([])
   const isSimpleBuyOrderPending = orders.find(
     order =>
       order.state === 'PENDING_CONFIRMATION' ||
@@ -28,10 +29,19 @@ export const getData = state => {
     // @ts-ignore
     selectors.modules.profile.getUserKYCState(state).getOrElse('') === 'NONE'
   const isFirstLogin = selectors.auth.getFirstLogin(state)
+  const isCoinifyUser = selectors.core.kvStore.buySell
+    .getCoinifyUser(state)
+    .getOrElse(false)
 
   let bannerToShow
   if (showDocResubmitBanner) {
     bannerToShow = 'resubmit'
+  } else if (
+    isCoinifyUser &&
+    !isSimpleBuyOrderPending &&
+    Remote.Success.is(ordersR)
+  ) {
+    bannerToShow = 'coinifyToSb'
   } else if (isKycStateNone && isUserActive && !isFirstLogin) {
     bannerToShow = 'finishKyc'
   } else if (isSimpleBuyOrderPending) {
