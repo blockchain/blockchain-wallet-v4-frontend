107c552616baa9267659134b9dd87c563b4b56aa
schnogz <andrew@blockchain.com>
2020-04-18 10:18:26 -0400

    feat(zendesk): add z-index fixes and convert to ts

--- packages/blockchain-wallet-v4-frontend/src/components/ZendeskWidget/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/components/ZendeskWidget/index.tsx
@@ -1,26 +1,37 @@
 import { any } from 'ramda'
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import React from 'react'
+import { RootState } from 'data/rootReducer'
+import { SBOrderType } from 'core/types'
 
-import { actions, selectors } from 'data'
+import { selectors } from 'data'
+import { UserDataType } from 'data/types'
+import React from 'react'
 import styled from 'styled-components'
+
+type LinkStatePropsType = {
+  domains: { [key in string]: string }
+  sbOrders: Array<SBOrderType>
+  userData: UserDataType
+}
+type State = { widgetOpen: boolean }
+type CustomIframe = HTMLIFrameElement & State
+type Props = LinkStatePropsType & CustomIframe
 
 const Wrapper = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
-  z-index: 9999;
+  z-index: 10;
 `
-const Iframe = styled.iframe`
-  height: 550px;
-  width: 375px;
+const Iframe = styled.iframe<CustomIframe>`
+  height: ${props => (props.widgetOpen ? '800px' : '60px')};
+  width: ${props => (props.widgetOpen ? '400px' : '150px')};
   border: none;
 `
 
 const postMsgToWalletHelper = (methodName, data) => {
-  const frame = document.getElementById('zendesk-iframe')
-  if (frame) {
+  const frame = document.getElementById('zendesk-iframe') as CustomIframe
+  if (frame && frame.contentWindow) {
     frame.contentWindow.postMessage(
       {
         method: methodName,
@@ -31,17 +42,27 @@ const postMsgToWalletHelper = (methodName, data) => {
   }
 }
 
-class ZendeskWidget extends React.PureComponent {
+class ZendeskWidget extends React.PureComponent<Props, State> {
+  state = {
+    widgetOpen: false
+  }
+  componentDidMount () {
+    window.addEventListener('message', this.updateWidgetState, false)
+  }
+
+  updateWidgetState = e => {
+    this.setState({ widgetOpen: e.data.widgetOpen })
+  }
+
   render () {
     const { domains, sbOrders, userData } = this.props
-
     const pendingSbOrder = any(
       o => o.state === 'PENDING_CONFIRMATION' || o.state === 'PENDING_DEPOSIT',
       sbOrders
     )
 
     // only show chat to users with pending sb orders for now
-    if (pendingSbOrder) {
+    if (userData && pendingSbOrder) {
       postMsgToWalletHelper('showChat', {
         fullName: `${userData.firstName} ${userData.lastName}`,
         email: userData.email
@@ -52,28 +73,25 @@ class ZendeskWidget extends React.PureComponent {
 
     return (
       <Wrapper>
+        {/*
+          // @ts-ignore */}
         <Iframe
           id='zendesk-iframe'
           src={domains.walletHelper + '/wallet-helper/zendesk/#/'}
+          widgetOpen={this.state.widgetOpen}
         />
       </Wrapper>
     )
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  analyticsActions: bindActionCreators(actions.analytics, dispatch)
-})
-
-const mapStateToProps = state => ({
+const mapStateToProps = (state: RootState): LinkStatePropsType => ({
   domains: selectors.core.walletOptions.getDomains(state).getOrElse({
     walletHelper: 'https://wallet-helper.blockchain.com'
   }),
   sbOrders: selectors.components.simpleBuy.getSBOrders(state).getOrElse([]),
+  // @ts-ignore
   userData: selectors.modules.profile.getUserData(state).getOrElse({})
 })
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ZendeskWidget)
+export default connect(mapStateToProps)(ZendeskWidget)
