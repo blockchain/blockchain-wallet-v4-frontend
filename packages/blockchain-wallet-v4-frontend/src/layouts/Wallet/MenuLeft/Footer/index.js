bdf0a555e6da8550e07d1547770d3eec32ad958f
jjBlockchain <jj@blockchain.com>
2020-04-28 14:15:55 -0400

    feat(bitcoin-ad): add new ad for bitcoin.com

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Footer/index.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Footer/index.js
@@ -1,6 +1,6 @@
 import { actions, model } from 'data'
 import { bindActionCreators } from 'redux'
-import { Button, Icon, Link, Text } from 'blockchain-info-components'
+import { Button, Icon, Image, Link, Text } from 'blockchain-info-components'
 import { connect } from 'react-redux'
 import { FormattedHTMLMessage, FormattedMessage } from 'react-intl'
 import { includes, not } from 'ramda'
@@ -13,6 +13,7 @@ const Wrapper = styled.div`
   text-align: center;
 `
 const AdsButton = styled(Button)`
+  padding: 0;
   margin: 4px auto;
   margin-top: 12px;
   transition: background 0.3s;
@@ -35,9 +36,6 @@ const ArrowIcon = styled(Icon)`
   margin-left: 4px;
   margin-top: 2px;
   font-size: 16px;
-`
-const SpadeIcon = styled(Icon)`
-  margin-right: 8px;
 `
 
 const Footer = ({ actions, countryCode, adsBlacklist, adsUrl }) => {
@@ -82,17 +80,17 @@ const Footer = ({ actions, countryCode, adsBlacklist, adsUrl }) => {
             />
           </Text>
           <AdsButton
-            height='48px'
+            height='fit-content'
             onClick={() => actions.logEvent(ADS_EVENTS.CLICK_AD)}
           >
             <Link href={adsUrl} rel='noopener noreferrer' target='_blank'>
               <ButtonText color='grey500' size='14px' weight={500}>
-                <SpadeIcon name='spade' color='gray4' />
-                <FormattedHTMLMessage
-                  id='layouts.wallet.menuleft.footer.bitcoingames2'
-                  defaultMessage='<span>Bitcoin Games.</span> <span>Play Now</span>'
+                <Image
+                  name='ad-bitcoin-games'
+                  srcset={{
+                    'ad-bitcoin-games2': '2x'
+                  }}
                 />
-                <ArrowIcon name='arrow-right' color='blue600' />
               </ButtonText>
             </Link>
           </AdsButton>
