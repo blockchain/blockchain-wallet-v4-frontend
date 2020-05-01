f678b9efc7a358122213d221198af826ee0f4e47
Philip London <philip@blockchain.com>
2020-05-01 13:44:07 +0200

    feat(eth): retry eth fee requires higher fee

--- packages/blockchain-wallet-v4-frontend/src/modals/Eth/SendEth/FirstStep/selectors.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/Eth/SendEth/FirstStep/selectors.js
@@ -54,6 +54,7 @@ export const getData = createDeepEqualSelector(
       const maxFee = path(['fees', 'limits', 'max'], payment)
       const isSufficientEthForErc20 = path(['isSufficientEthForErc20'], payment)
       const isRetryAttempt = path(['isRetryAttempt'], payment)
+      const minFeeRequiredForRetry = path(['minFeeRequiredForRetry'], payment)
       const isContractChecked = Remote.Success.is(isContractR)
       const feeElements = [
         {
@@ -97,6 +98,7 @@ export const getData = createDeepEqualSelector(
         isSufficientEthForErc20,
         maxFee,
         minFee,
+        minFeeRequiredForRetry,
         priorityFee,
         regularFee,
         unconfirmedTx
