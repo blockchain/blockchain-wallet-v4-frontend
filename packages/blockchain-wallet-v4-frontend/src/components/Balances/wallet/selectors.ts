5230c5737ccb96765750186de67f7a9771540e7d
Philip London <philip@blockchain.com>
2020-05-14 16:35:23 +0200

    feat(savings): add interest balances to total

--- packages/blockchain-wallet-v4-frontend/src/components/Balances/wallet/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/components/Balances/wallet/selectors.ts
@@ -2,7 +2,11 @@ import { add, lift, pathOr, prop, reduce } from 'ramda'
 import { createDeepEqualSelector } from 'services/ReselectHelper'
 import { Exchange, Remote } from 'blockchain-wallet-v4/src'
 import { formatFiat } from 'core/exchange/currency'
-import { RemoteDataType, SBBalancesType } from 'core/types'
+import {
+  InterestAccountBalanceType,
+  RemoteDataType,
+  SBBalancesType
+} from 'core/types'
 import { selectors } from 'data'
 import BigNumber from 'bignumber.js'
 
@@ -10,23 +14,29 @@ export const getBtcBalance = createDeepEqualSelector(
   [
     selectors.core.wallet.getSpendableContext,
     selectors.core.data.btc.getAddresses,
+    selectors.components.interest.getInterestAccountBalance,
     selectors.components.simpleBuy.getSBBalances
   ],
-  (context, addressesR, sbBalancesR) => {
+  (context, addressesR, interestAccountBalanceR, sbBalancesR) => {
     const contextToBalances = (
       context,
       balances,
+      interestAccountBalance: InterestAccountBalanceType,
       sbBalances: SBBalancesType
     ): Array<number> => {
       const walletBalances: Array<number> = context.map(a =>
         pathOr(0, [a, 'final_balance'], balances)
       )
+      const interestBalance = interestAccountBalance.BTC
+        ? interestAccountBalance.BTC.balance
+        : 0
       const sbBalance = Number(sbBalances.BTC ? sbBalances.BTC.available : 0)
-      return walletBalances.concat(sbBalance)
+      return walletBalances.concat(sbBalance).concat(interestBalance)
     }
     const balancesR = lift(contextToBalances)(
       Remote.of(context),
       addressesR,
+      interestAccountBalanceR,
       sbBalancesR
     )
     return balancesR.map(reduce<number, number>(add, 0))
@@ -37,23 +47,29 @@ export const getBchBalance = createDeepEqualSelector(
   [
     selectors.core.kvStore.bch.getSpendableContext,
     selectors.core.data.bch.getAddresses,
+    selectors.components.interest.getInterestAccountBalance,
     selectors.components.simpleBuy.getSBBalances
   ],
-  (context, addressesR, sbBalancesR) => {
+  (context, addressesR, interestAccountBalanceR, sbBalancesR) => {
     const contextToBalances = (
       context,
       balances,
+      interestAccountBalance: InterestAccountBalanceType,
       sbBalances: SBBalancesType
     ) => {
       const walletBalances: Array<number> = context.map(a =>
         pathOr(0, [a, 'final_balance'], balances)
       )
+      const interestBalance = interestAccountBalance.BCH
+        ? interestAccountBalance.BCH.balance
+        : 0
       const sbBalance = Number(sbBalances.BCH ? sbBalances.BCH.available : 0)
-      return walletBalances.concat(sbBalance)
+      return walletBalances.concat(sbBalance).concat(interestBalance)
     }
     const balancesR = lift(contextToBalances)(
       Remote.of(context),
       addressesR,
+      interestAccountBalanceR,
       sbBalancesR
     )
     return balancesR.map(reduce<number, number>(add, 0))
@@ -64,16 +80,22 @@ export const getEthBalance = createDeepEqualSelector(
   [
     selectors.core.kvStore.eth.getContext,
     selectors.core.data.eth.getAddresses,
+    selectors.components.interest.getInterestAccountBalance,
     selectors.components.simpleBuy.getSBBalances
   ],
   (
     context,
     addressesR,
+    interestAccountBalanceR: RemoteDataType<string, InterestAccountBalanceType>,
     sbBalancesR: RemoteDataType<string, SBBalancesType>
   ) => {
     const contextToBalances = (context, balances) =>
       context.map(a => pathOr(0, [a, 'balance'], balances))
 
+    const interestEthBalance = interestAccountBalanceR.getOrElse({
+      ETH: { balance: 0 }
+    }).ETH
+    const interestBalance = interestEthBalance ? interestEthBalance.balance : 0
     const sbEthBalance = sbBalancesR.getOrElse({ ETH: { available: '0' } }).ETH
     const sbBalance = sbEthBalance ? sbEthBalance.available : '0'
 
@@ -82,7 +104,9 @@ export const getEthBalance = createDeepEqualSelector(
       addressesR
     )
     return balancesR.map(b => {
-      return new BigNumber(b.getOrElse(0)).plus(new BigNumber(sbBalance))
+      return new BigNumber(b.getOrElse(0))
+        .plus(new BigNumber(sbBalance))
+        .plus(new BigNumber(interestBalance))
     })
   }
 )
@@ -90,14 +114,27 @@ export const getEthBalance = createDeepEqualSelector(
 export const getPaxBalance = createDeepEqualSelector(
   [
     state => selectors.core.data.eth.getErc20Balance(state, 'pax'),
+    selectors.components.interest.getInterestAccountBalance,
     selectors.components.simpleBuy.getSBBalances
   ],
-  (balanceR, sbBalancesR) => {
+  (
+    balanceR,
+    interestAccountBalanceR: RemoteDataType<string, InterestAccountBalanceType>,
+    sbBalancesR: RemoteDataType<string, SBBalancesType>
+  ) => {
+    const interestPaxBalance = interestAccountBalanceR.getOrElse({
+      PAX: { balance: '0' }
+    }).PAX
+    const interestBalance = interestPaxBalance
+      ? interestPaxBalance.balance
+      : '0'
     const sbPaxBalance = sbBalancesR.getOrElse({ PAX: { available: '0' } }).PAX
     const sbBalance = sbPaxBalance ? sbPaxBalance.available : '0'
 
     return Remote.of(
-      new BigNumber(balanceR.getOrElse(0)).plus(new BigNumber(sbBalance))
+      new BigNumber(balanceR.getOrElse(0))
+        .plus(new BigNumber(sbBalance))
+        .plus(new BigNumber(interestBalance))
     )
   }
 )
@@ -110,14 +147,27 @@ export const getXlmBalance = createDeepEqualSelector(
         .map(accountId =>
           selectors.core.data.xlm.getBalance(state, accountId).getOrElse(0)
         ),
+    selectors.components.interest.getInterestAccountBalance,
     selectors.components.simpleBuy.getSBBalances
   ],
-  (balanceR, sbBalancesR: RemoteDataType<string, SBBalancesType>) => {
+  (
+    balanceR,
+    interestAccountBalanceR: RemoteDataType<string, InterestAccountBalanceType>,
+    sbBalancesR: RemoteDataType<string, SBBalancesType>
+  ) => {
+    const interestXlmBalance = interestAccountBalanceR.getOrElse({
+      PAX: { balance: '0' }
+    }).PAX
+    const interestBalance = interestXlmBalance
+      ? interestXlmBalance.balance
+      : '0'
     const sbXlmBalance = sbBalancesR.getOrElse({ XLM: { available: '0' } }).XLM
     const sbBalance = sbXlmBalance ? sbXlmBalance.available : '0'
 
     return Remote.of(
-      new BigNumber(balanceR.getOrElse(0)).plus(new BigNumber(sbBalance))
+      new BigNumber(balanceR.getOrElse(0))
+        .plus(new BigNumber(sbBalance))
+        .plus(new BigNumber(interestBalance))
     )
   }
 )
