7ab66d60fe0ed297045948c335eadb5d6a581164
TheLeoB <leora@blockchain.com>
2020-05-29 14:59:46 -0400

    fix(footer): remove bitcoin.com ad

--- packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Footer/index.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Wallet/MenuLeft/Footer/index.js
@@ -3,7 +3,6 @@ import { bindActionCreators } from 'redux'
 import { Button, Icon, Link, Text } from 'blockchain-info-components'
 import { connect } from 'react-redux'
 import { FormattedHTMLMessage, FormattedMessage } from 'react-intl'
-import { includes, not } from 'ramda'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -37,70 +36,40 @@ const ArrowIcon = styled(Icon)`
   margin-top: 2px;
   font-size: 16px;
 `
-const SpadeIcon = styled(Icon)`
-  margin-right: 8px;
-`
 
-const Footer = ({ actions, countryCode, adsBlacklist, adsUrl }) => {
-  switch (true) {
-    case countryCode === 'US':
-      return (
-        <Wrapper>
-          <Text color='grey400' size='12px' weight={500}>
-            <FormattedMessage
-              id='layouts.wallet.menuleft.footer.adtax'
-              defaultMessage='Offer by LukkaTax'
-            />
-          </Text>
-          <AdsButton
-            height='48px'
-            onClick={() => actions.logEvent(ADS_EVENTS.CLICK_AD_TAX)}
+const Footer = ({ actions, countryCode }) => {
+  if (countryCode === 'US') {
+    return (
+      <Wrapper>
+        <Text color='grey400' size='12px' weight={500}>
+          <FormattedMessage
+            id='layouts.wallet.menuleft.footer.adtax'
+            defaultMessage='Offer by LukkaTax'
+          />
+        </Text>
+        <AdsButton
+          height='48px'
+          onClick={() => actions.logEvent(ADS_EVENTS.CLICK_AD_TAX)}
+        >
+          <Link
+            href='https://diy.lukkatax.com/?referralCode=1blockchain'
+            rel='noopener noreferrer'
+            target='_blank'
           >
-            <Link
-              href='https://diy.lukkatax.com/?referralCode=1blockchain'
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              <ButtonText color='grey500' size='14px' weight={500}>
-                <span>💰</span>
-                <FormattedHTMLMessage
-                  id='layouts.wallet.menuleft.footer.adtaxsave'
-                  defaultMessage='Save this tax season'
-                />
-                <ArrowIcon name='arrow-right' color='blue600' />
-              </ButtonText>
-            </Link>
-          </AdsButton>
-        </Wrapper>
-      )
-    case not(includes(countryCode, adsBlacklist)):
-      return (
-        <Wrapper>
-          <Text color='grey400' size='12px' weight={500}>
-            <FormattedMessage
-              id='layouts.wallet.menuleft.footer.ad'
-              defaultMessage='Ad by bitcoin.com'
-            />
-          </Text>
-          <AdsButton
-            height='48px'
-            onClick={() => actions.logEvent(ADS_EVENTS.CLICK_AD)}
-          >
-            <Link href={adsUrl} rel='noopener noreferrer' target='_blank'>
-              <ButtonText color='grey500' size='14px' weight={500}>
-                <SpadeIcon color='gray4' name='spade' />
-                <FormattedHTMLMessage
-                  id='layouts.wallet.menuleft.footer.bitcoingames2'
-                  defaultMessage='<span>Bitcoin Games.</span> <span>Play Now</span>'
-                />
-                <ArrowIcon color='blue600' name='arrow-right' />
-              </ButtonText>
-            </Link>
-          </AdsButton>
-        </Wrapper>
-      )
-    default:
-      return null
+            <ButtonText color='grey500' size='14px' weight={500}>
+              <span>💰</span>
+              <FormattedHTMLMessage
+                id='layouts.wallet.menuleft.footer.adtaxsave'
+                defaultMessage='Save this tax season'
+              />
+              <ArrowIcon name='arrow-right' color='blue600' />
+            </ButtonText>
+          </Link>
+        </AdsButton>
+      </Wrapper>
+    )
+  } else {
+    return null
   }
 }
 
