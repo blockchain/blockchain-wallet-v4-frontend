23d8524eaa3f88862185fd5094bca135d5548771
Philip London <philip@blockchain.com>
2020-06-02 11:25:22 +0200

    feat(lazy): remove awesome-phonenumber

--- packages/blockchain-wallet-v4-frontend/src/components/Alerts/template.tsx
+++ packages/blockchain-wallet-v4-frontend/src/components/Alerts/template.tsx
@@ -1,10 +1,13 @@
 import { Toast } from 'blockchain-info-components'
 import React from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import styled from 'styled-components'
 
 import media from 'services/ResponsiveService'
 
 import { getAlertContent } from './messages'
+
+const duration = 200
 
 const Wrapper = styled.div`
   position: absolute;
@@ -13,9 +16,22 @@ const Wrapper = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   z-index: 1050;
-  & > * {
-    margin-top: 5px;
+
+  .toast-enter {
+    opacity: 0.01;
+    position: absolute;
   }
+
+  .toast-enter.toast-enter-active {
+    opacity: 1;
+    transition: opacity ${duration}ms;
+  }
+
+  .toast-leave.toast-leave-active {
+    opacity: 0.01;
+    transition: opacity ${duration}ms;
+  }
+
   ${media.tablet`
     bottom: 10px;
     right: 10px;
@@ -33,22 +49,28 @@ const Alerts = props => {
 
   return (
     <Wrapper>
-      {alerts.map(alert => {
-        const { id, nature, message, data, coin, persist, timeout } = alert
-        const dismissTimer = timeout || 7000
-        return (
-          <Toast
-            key={id}
-            nature={nature}
-            coin={coin}
-            timeout={dismissTimer}
-            persist={persist}
-            onClose={() => handleClose(id)}
-          >
-            {getAlertContent(message, data)}
-          </Toast>
-        )
-      })}
+      <ReactCSSTransitionGroup
+        transitionName='toast'
+        transitionEnterTimeout={duration}
+        transitionLeaveTimeout={duration}
+      >
+        {alerts.map(alert => {
+          const { id, nature, message, data, coin, persist, timeout } = alert
+          const dismissTimer = timeout || 7000
+          return (
+            <Toast
+              key={id}
+              nature={nature}
+              coin={coin}
+              timeout={dismissTimer}
+              persist={persist}
+              onClose={() => handleClose(id)}
+            >
+              {getAlertContent(message, data)}
+            </Toast>
+          )
+        })}
+      </ReactCSSTransitionGroup>
     </Wrapper>
   )
 }
