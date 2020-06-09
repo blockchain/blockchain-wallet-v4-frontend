f3d2493e22207208dd99d60199ae918519876f1c
Philip London <philip@blockchain.com>
2020-06-09 10:31:32 +0200

    refactor(testing): remove saga tests and snapshots

--- packages/blockchain-wallet-v4-frontend/src/components/Form/CountdownTimer/index.spec.js
+++
@@ -1,9 +0,0 @@
-import { getCountdownColor } from './template'
-
-describe('countdownTimer', () => {
-  describe('get countdown color', () => {
-    it('should return correct color', () => {
-      expect(getCountdownColor('2012-03-29T10:05:45.455Z')).toEqual('error')
-    })
-  })
-})
