a348b9c3b70c2287e1a6b36dee63408f87c9475d
TheLeoB <leora@blockchain.com>
2020-06-11 18:13:03 -0400

    fix(data-e2e): toastMessage tag on right element

--- packages/blockchain-wallet-v4-frontend/src/components/Alerts/messages.tsx
+++ packages/blockchain-wallet-v4-frontend/src/components/Alerts/messages.tsx
@@ -27,7 +27,12 @@ const IconColumn = styled.div`
 `
 
 const buildMessageTemplate = messageText => (
-  <Text size='14px' weight={600} style={{ marginLeft: '10px' }}>
+  <Text
+    size='14px'
+    weight={600}
+    style={{ marginLeft: '10px' }}
+    data-e2e='toastMessage'
+  >
     {messageText}
   </Text>
 )
