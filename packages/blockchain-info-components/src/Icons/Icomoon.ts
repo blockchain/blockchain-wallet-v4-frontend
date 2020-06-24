6a121143583dbc0ea8eaa1708caccb4dd4d289be
Philip London <philip@blockchain.com>
2020-06-24 19:06:34 +0200

    feat(coins): add logos for usdt and algo

--- packages/blockchain-info-components/src/Icons/Icomoon.ts
+++ packages/blockchain-info-components/src/Icons/Icomoon.ts
@@ -1,4 +1,5 @@
 const IcoMoon = {
+  algo: '\\e939',
   'alert-filled': '\\e900',
   'arrow-down': '\\e91d',
   'arrow-down-circle': '\\e935',
@@ -95,6 +96,7 @@ const IcoMoon = {
   trash: '\\e951',
   twitter: '\\e916',
   'usd-d': '\\e948',
+  usdt: '\\e94a',
   user: '\\e955',
   users: '\\e928',
   'vertical-arrow-switch': '\\e980',
