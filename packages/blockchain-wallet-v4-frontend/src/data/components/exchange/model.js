c6f87d87ae353300a7569b802af6776e820b566e
Philip London <philip@blockchain.com>
2020-06-30 11:11:14 +0200

    fix(test): ignore extracted-ts

--- packages/blockchain-wallet-v4-frontend/src/data/components/exchange/model.js
+++ packages/blockchain-wallet-v4-frontend/src/data/components/exchange/model.js
@@ -53,7 +53,7 @@ export const SWAP_ERROR_CODES = {
   47: ANNUAL_LIMIT_EXCEEDED
 }
 
-const currenciesOrder = ['BTC', 'PAX', 'ETH', 'BCH', 'XLM']
+const currenciesOrder = ['BTC', 'ETH', 'BCH', 'XLM', 'PAX']
 export const sortByOrder = sortBy(flip(indexOf)(currenciesOrder))
 
 const getPairedCoins = curry(
