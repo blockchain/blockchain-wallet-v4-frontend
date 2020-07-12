9e1370ea899036f0c27324372d2b3333cff64257
Philip London <philip@blockchain.com>
2020-07-12 18:13:19 +0200

    fix(lift): map, getOrElse, getOrFail, typings

--- typings/ramda.d.ts
+++ typings/ramda.d.ts
@@ -1,15 +1,11 @@
 /* eslint-disable */
 import * as ramda from 'ramda'
-import { ExtractFailure, ExtractRemoteArray, RemoteDataType } from 'core/types'
+import { RemoteDataType } from 'core/types'
 
 declare module 'ramda' {
   export function lift<T>(
     f: (...args: any[]) => T
-  ): <A>(
-    ...Fx: ExtractRemoteArray<A[]>
-  ) => // @ts-ignore
-  RemoteDataType<ExtractFailure<A>, T> {
-    // @ts-ignore
+  ): (...Fx) => RemoteDataType<any, ReturnType<typeof f>> {
     return Fx => Fx.map(f)
   }
 }
