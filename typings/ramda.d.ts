9d32e59e4ae8a44dd4844ceefe206c85442239e0
Philip London <philip@blockchain.com>
2020-07-27 13:17:04 +0200

    chore(Release): v4.38.0

--- typings/ramda.d.ts
+++ typings/ramda.d.ts
@@ -5,6 +5,7 @@ import { RemoteDataType } from 'core/types'
 declare module 'ramda' {
   export function lift<T>(
     f: (...args: any[]) => T
+    // @ts-ignore
   ): (...Fx) => RemoteDataType<any, ReturnType<typeof f>> {
     return Fx => Fx.map(f)
   }
