2cec9f273d4165b9c28440d0c57da51ec8eb4276
Milan <milan@blockchain.com>
2020-08-17 16:04:29 +0200

    feat: modal moved to flyout

--- packages/blockchain-wallet-v4-frontend/src/modals/Onboarding/LinkFromExchangeAccount/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Onboarding/LinkFromExchangeAccount/index.tsx
@@ -1,16 +1,21 @@
 import { actions, selectors } from 'data'
 import { bindActionCreators, compose, Dispatch } from 'redux'
 import { connect } from 'react-redux'
+import { ModalPropsType } from '../../types'
 import { RemoteDataType } from 'core/types'
 import { UserTiersType } from 'data/types'
-import LinkFromExchangeAccount from './template'
+import Failure from './template.failure'
+import Flyout, { duration, FlyoutChild } from 'components/Flyout'
+import Loading from './template.loading'
 import modalEnhancer from 'providers/ModalEnhancer'
+import NotAsked from './template.notasked'
 import React from 'react'
+import Success from './template.success'
 
 type OwnPropsType = {
   close: () => void
   linkId: string
-}
+} & ModalPropsType
 
 export type LinkStatePropsType = {
   email: string
@@ -27,14 +32,81 @@ export type LinkDispatchPropsType = {
 
 export type Props = OwnPropsType & LinkStatePropsType & LinkDispatchPropsType
 
-class LinkFromExchangeAccountContainer extends React.PureComponent<Props> {
+type State = { direction: 'left' | 'right'; show: boolean }
+
+class LinkFromExchangeAccountContainer extends React.PureComponent<
+  Props,
+  State
+> {
+  state: State = { show: true, direction: 'left' }
   componentDidMount () {
     const { linkId } = this.props
     this.props.actions.linkFromExchangeAccount(linkId)
   }
 
+  handleClose = () => {
+    this.setState({ show: false })
+    setTimeout(() => {
+      this.props.close()
+    }, duration)
+  }
+
   render () {
-    return <LinkFromExchangeAccount {...this.props} />
+    return this.props.linkFromExchangeAccountStatus.cata({
+      Success: val => (
+        <Flyout
+          {...this.props}
+          onClose={this.handleClose}
+          in={this.state.show}
+          direction={this.state.direction}
+          data-e2e='infoModalLinkFromExchangeAccount'
+        >
+          <FlyoutChild>
+            <Success {...val} {...this.props} close={this.handleClose} />
+          </FlyoutChild>
+        </Flyout>
+      ),
+
+      Failure: error => (
+        <Flyout
+          {...this.props}
+          onClose={this.handleClose}
+          in={this.state.show}
+          direction={this.state.direction}
+          data-e2e='infoModalLinkFromExchangeAccount'
+        >
+          <FlyoutChild>
+            <Failure {...this.props} close={this.handleClose} {...error} />
+          </FlyoutChild>
+        </Flyout>
+      ),
+      Loading: () => (
+        <Flyout
+          {...this.props}
+          onClose={this.handleClose}
+          in={this.state.show}
+          direction={this.state.direction}
+          data-e2e='infoModalLinkFromExchangeAccount'
+        >
+          <FlyoutChild>
+            <Loading {...this.props} close={this.handleClose} />
+          </FlyoutChild>
+        </Flyout>
+      ),
+      NotAsked: () => (
+        <Flyout
+          {...this.props}
+          onClose={this.handleClose}
+          in={this.state.show}
+          direction={this.state.direction}
+          data-e2e='infoModalLinkFromExchangeAccount'
+        >
+          <FlyoutChild>
+            <NotAsked {...this.props} close={this.handleClose} />
+          </FlyoutChild>
+        </Flyout>
+      )
+    })
   }
 }
 
