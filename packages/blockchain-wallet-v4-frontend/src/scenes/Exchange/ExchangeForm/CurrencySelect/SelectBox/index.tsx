9a60d3bc3bd6fc1a99f6235a1d5a3873c353f0b1
Philip London <philip@blockchain.com>
2020-08-18 12:31:55 +0200

    feat(swap): style options

--- packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/ExchangeForm/CurrencySelect/SelectBox/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Exchange/ExchangeForm/CurrencySelect/SelectBox/index.tsx
@@ -1,10 +1,22 @@
 import { Icon, Text as TextComponent } from 'blockchain-info-components'
 import { toLower } from 'ramda'
 import React from 'react'
-import styled, { DefaultTheme } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 
+import { convertBaseToStandard } from 'data/components/exchange/services'
 import { GroupHeadingLabelType, SwapAccountType } from '../types'
 import { SelectBox } from 'components/Form'
+
+const option = css`
+  padding: 12px 20px;
+  &:hover {
+    background-color: ${props => props.theme['blue000']} !important;
+  }
+`
+
+const width = css`
+  width: 240px;
+`
 
 const ExchangeSelect = styled(SelectBox)`
   .bc__control {
@@ -18,8 +30,7 @@ const ExchangeSelect = styled(SelectBox)`
   }
 
   .bc__menu {
-    width: auto;
-    min-width: 240px;
+    ${width}
   }
 
   .bc__menu-list {
@@ -30,8 +41,7 @@ const ExchangeSelect = styled(SelectBox)`
 
   .bc__group {
     position: relative;
-    padding: 12px 20px;
-
+    ${option}
     ${`/* 
       🚨🚨🚨
       > div:last-child
@@ -43,9 +53,10 @@ const ExchangeSelect = styled(SelectBox)`
       position: absolute;
       left: 100%;
       top: 0;
+      border-radius: 8px;
+      box-shadow: 0px 4px 16px rgba(5, 24, 61, 0.2);
     }
     &:hover {
-      background-color: ${props => props.theme['blue000']};
       > div:last-child {
         display: block;
       }
@@ -64,8 +75,12 @@ const ExchangeSelect = styled(SelectBox)`
     padding: 0;
   }
   .bc__option {
-    padding: 8px 0;
-    font-weight: 600;
+    border-radius: 0px;
+    &.bc__option--is-focused {
+      background-color: ${props => props.theme.white};
+    }
+    ${width}
+    ${option}
   }
 
   .bc__single-value {
@@ -90,11 +105,9 @@ const DisplayWrapper = styled.div<{ coin: keyof DefaultTheme }>`
 const ItemWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: flex-start;
   align-items: center;
   width: 100%;
   height: 100%;
-  padding: 5px;
   box-sizing: border-box;
   cursor: pointer;
   min-width: 0;
@@ -102,7 +115,6 @@ const ItemWrapper = styled.div`
 const GroupHeadingWrapper = styled.div`
   display: flex;
   align-items: center;
-  cursor: pointer;
 `
 const GroupContent = styled.div`
   width: 100%;
@@ -114,7 +126,6 @@ const Text = styled(TextComponent)<{
   color?: keyof DefaultTheme
   uppercase?: boolean
 }>`
-  cursor: pointer;
   width: 100%;
   white-space: nowrap;
   overflow: hidden;
@@ -122,22 +133,21 @@ const Text = styled(TextComponent)<{
   color: ${props => props.theme[props.color || 'grey700']};
   text-transform: ${props => (props.uppercase ? 'uppercase' : 'none')};
 `
-
+const Title = styled(Text)`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${props => props.theme['grey800']};
+`
+const Value = styled(Text)`
+  font-size: 14px;
+  font-weight: 500;
+  margin-top: 2px;
+  color: ${props => props.theme['grey600']};
+`
 const DisplayIcon = styled(Icon)`
   font-size: 26px;
   margin-right: 8px;
   color: ${props => props.theme.white};
-`
-
-const selectItemIconColor = props => props.theme[props.coin]
-
-const ItemIcon = styled(Icon)<{ coin: keyof DefaultTheme }>`
-  font-size: 20px;
-  margin-right: 8px;
-  color: ${selectItemIconColor} !important;
-  span {
-    color: ${selectItemIconColor} !important;
-  }
 `
 
 const renderDisplay = (item: SwapAccountType, children) => {
@@ -158,15 +168,16 @@ const renderDisplay = (item: SwapAccountType, children) => {
 
 const renderItem = (item: SwapAccountType) => {
   if (!item.value) return
-  const coin = item.value.coin
-  const icon = item.value.icon
 
   return (
     <ItemWrapper>
-      <ItemIcon coin={toLower(coin) as keyof DefaultTheme} name={icon} />
-      <Text size='13px' weight={600}>
-        {item.value.label}
-      </Text>
+      <div>
+        <Title>{item.value.label}</Title>
+        <Value>
+          {convertBaseToStandard(item.value.coin, item.value.balance)}{' '}
+          {item.value.coin}
+        </Value>
+      </div>
     </ItemWrapper>
   )
 }
@@ -182,12 +193,8 @@ const renderGroupHeading = (props, heading: GroupHeadingLabelType) => {
       />
       <GroupContent>
         <div>
-          <Text size='16px' weight={600} color='grey800'>
-            {heading.name}
-          </Text>
-          <Text size='14px' weight={500} color='grey600'>
-            {heading.coin}
-          </Text>
+          <Title>{heading.name}</Title>
+          <Value>{heading.coin}</Value>
         </div>
         <Icon size='24px' color={'grey400'} name='chevron-right' />
       </GroupContent>
