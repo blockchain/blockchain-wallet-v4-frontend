b981a6b0af3f36dc92229013821cc9aa1a130247
TheLeoB <leora@blockchain.com>
2020-09-02 13:04:03 -0700

    feat(interest): custodial send amount from number to string

--- packages/blockchain-wallet-v4/src/network/api/interest/index.ts
+++ packages/blockchain-wallet-v4/src/network/api/interest/index.ts
@@ -123,6 +123,7 @@ export default ({ nabuUrl, authorizedGet, authorizedPost }) => {
   ): CustodialTransferResponseType =>
     authorizedPost({
       contentType: 'application/json',
+      endpoint: '/user/balance/transfer',
       data: {
         amount,
         currency,
