0ebb1b2befac9483a610d5a68f0f4a24952318aa
Milan <milan@blockchain.com>
2020-10-05 09:32:11 +0200

    feat(verify email): implemented starting page design to meet figma

--- packages/blockchain-wallet-v4-frontend/src/layouts/Public/index.js
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Public/index.js
@@ -28,7 +28,7 @@ const FooterInner = styled.div`
 `
 
 const Wrapper = styled.div`
-  background-color: ${props => props.theme.blue900};
+  background-color: ${props => props.theme.grey900};
   height: auto;
   min-height: 100%;
   width: 100%;
