edb94082c8b8b96508957acceb0e13f9c1ec1b2f
Milan <milan@blockchain.com>
2020-12-17 19:55:47 +0100

    feat(sdd-flow): fixed failing veriff screen

--- packages/blockchain-wallet-v4-frontend/src/modals/Onboarding/KycVerification/Veriff/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Onboarding/KycVerification/Veriff/index.tsx
@@ -12,13 +12,11 @@ class Veriff extends React.PureComponent<Props> {
   state = { loading: false }
 
   componentDidMount () {
-    const { data, onClose } = this.props
+    const { data } = this.props
     // close the main flyout when veriff iframe takes over
     if (!Remote.Success.is(data)) {
       this.props.actions.fetchVeriffUrl()
-      return onClose()
     }
-    onClose()
   }
 
   componentWillUnmount () {
