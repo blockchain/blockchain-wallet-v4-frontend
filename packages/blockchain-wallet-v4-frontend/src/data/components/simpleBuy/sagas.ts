5ee8174c8c7254f04ddb3b2300c2f6ea2d786d11
leora <leora@blockchain.com>
2021-02-01 15:07:04 -0700

    fix(sell): refresh balances after sell is submitted

--- packages/blockchain-wallet-v4-frontend/src/data/components/simpleBuy/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/components/simpleBuy/sagas.ts
@@ -322,6 +322,7 @@ export default ({
           })
         )
         yield put(actions.form.stopSubmit('previewSell'))
+        yield put(actions.components.refresh.refreshClicked())
         return yield put(actions.components.swap.fetchTrades())
       }
 
