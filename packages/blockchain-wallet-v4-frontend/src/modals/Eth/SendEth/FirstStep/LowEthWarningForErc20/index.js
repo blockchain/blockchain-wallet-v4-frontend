00647a4ef24698c6adfdf2e08d1cf6b4d4e8a9ed
leora <leora@blockchain.com>
2021-02-01 10:42:35 -0700

    fix(erc20-send): link to correct fee article for each coin

--- packages/blockchain-wallet-v4-frontend/src/modals/Eth/SendEth/FirstStep/LowEthWarningForErc20/index.js
+++ packages/blockchain-wallet-v4-frontend/src/modals/Eth/SendEth/FirstStep/LowEthWarningForErc20/index.js
@@ -8,7 +8,16 @@ import {
 } from '../Components'
 import React from 'react'
 
-const LowEthWarningForErc20 = () => {
+const LowEthWarningForErc20 = props => {
+  const { coin } = props
+
+  const supportArticle =
+    coin === 'PAX'
+      ? 'https://support.blockchain.com/hc/en-us/articles/360027492092-Why-do-I-need-ETH-to-send-USD-Digital-previously-USD-PAX-'
+      : coin === 'USDT'
+      ? 'https://support.blockchain.com/hc/en-us/articles/360045724832-Why-do-I-need-ETH-to-send-USDT-'
+      : 'https://support.blockchain.com/hc/en-us/articles/360052984751-Why-do-I-need-ETH-to-send-wDGLD-'
+
   return (
     <WarningWrapper>
       <WarningLeftColumn>
@@ -26,12 +35,7 @@ const LowEthWarningForErc20 = () => {
         </Text>
       </WarningLeftColumn>
       <WarningRightColumn>
-        <Link
-          size='13px'
-          weight={500}
-          href='https://support.blockchain.com/hc/en-us/sections/360004368351-USD-Pax-FAQ'
-          target='_blank'
-        >
+        <Link size='13px' weight={500} href={supportArticle} target='_blank'>
           <FormattedMessage
             id='buttons.learn_more'
             defaultMessage='Learn More'
