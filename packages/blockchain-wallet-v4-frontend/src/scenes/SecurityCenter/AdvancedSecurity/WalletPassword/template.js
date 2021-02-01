11ab02d937f773516b6bdfb8649fb4e9da602669
leora <leora@blockchain.com>
2021-02-01 09:29:59 -0700

    fix(copy): updates backup phrase or recovery phrase to secret key recovery phrase

--- packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/AdvancedSecurity/WalletPassword/template.js
+++ packages/blockchain-wallet-v4-frontend/src/scenes/SecurityCenter/AdvancedSecurity/WalletPassword/template.js
@@ -21,12 +21,12 @@ const WalletPassword = props => {
         </SettingHeader>
         <SettingDescription>
           <FormattedMessage
-            id='scenes.securitysettings.advanced.walletpassword.description'
-            defaultMessage='Your password is never shared with Blockchain or stored on our servers. We cannot access or reset your password. The only way to restore your wallet is through your backup phrase.'
+            id='scenes.securitysettings.advanced.walletpassword.description_one'
+            defaultMessage='Your password is never shared with Blockchain or stored on our servers. We cannot access or reset your password. The only way to restore your wallet is through your Secret Private Key Recovery Phrase .'
           />
           <FormattedMessage
-            id='scenes.securitysettings.advanced.walletpassword.description2'
-            defaultMessage='Make sure you write down your backup phrase which is the only way to restore access to your wallet in the event of a lost password.'
+            id='scenes.securitysettings.advanced.walletpassword.description_two'
+            defaultMessage='Make sure you write down your Secret Private Key Recovery Phrasewhich is the only way to restore access to your wallet in the event of a lost password.'
           />
         </SettingDescription>
       </SettingSummary>
