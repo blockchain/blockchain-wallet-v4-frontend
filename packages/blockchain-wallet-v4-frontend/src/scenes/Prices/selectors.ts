9ad0f2586fdc66fd108714eb6a5a4f2d1d49ab12
Andrew Schneider <andrew@blockchain.com>
2021-03-18 22:07:44 -0400

    fix(assets): use memoized selector to avoid table rerenders

--- packages/blockchain-wallet-v4-frontend/src/scenes/Prices/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Prices/selectors.ts
@@ -5,46 +5,48 @@ import {
   ExtractSuccess,
   SupportedCoinType
 } from 'blockchain-wallet-v4/src/types'
+import { createDeepEqualSelector } from 'blockchain-wallet-v4/src/utils'
 import { getAllCoinsBalancesSelector } from 'components/Balances/selectors'
 import { selectors } from 'data'
 
-export const getData = state => {
-  const coinPricesR = selectors.prices.getAllCoinPrices(state)
-  const coinPricesPreviousR = selectors.prices.getAllCoinPricesPreviousDay(
-    state
-  )
-  const supportedCoinsR = selectors.core.walletOptions.getSupportedCoins(state)
-  const coinBalances = getAllCoinsBalancesSelector(state)
-
-  const transform = (
-    coinPrices: ExtractSuccess<typeof coinPricesR>,
-    coinPricesPrevious: ExtractSuccess<typeof coinPricesPreviousR>,
-    supportedCoins: ExtractSuccess<typeof supportedCoinsR>
-  ) => {
-    // logic to exclude fiat currencies from list
-    return reject(
-      not,
-      values(
-        // @ts-ignore
-        map((coin: SupportedCoinType) => {
-          const currentPrice = coinPrices[coin.coinCode]
-          const yesterdayPrice = coinPricesPrevious[coin.coinCode]
-          return (
-            coin.coinCode in CoinTypeEnum && {
-              coin: coin.coinCode,
-              coinModel: coin,
-              name: `${coin.displayName} (${coin.coinTicker})`,
-              price: currentPrice,
-              priceChange: Number(
-                ((yesterdayPrice - currentPrice) / yesterdayPrice) * 100
-              ).toPrecision(2),
-              balance: coinBalances[coin.coinCode]
-            }
-          )
-        }, supportedCoins)
+export const getData = createDeepEqualSelector(
+  [
+    selectors.prices.getAllCoinPrices,
+    selectors.prices.getAllCoinPricesPreviousDay,
+    selectors.core.walletOptions.getSupportedCoins,
+    getAllCoinsBalancesSelector
+  ],
+  (coinPricesR, coinPricesPreviousR, supportedCoinsR, coinBalances) => {
+    const transform = (
+      coinPrices: ExtractSuccess<typeof coinPricesR>,
+      coinPricesPrevious: ExtractSuccess<typeof coinPricesPreviousR>,
+      supportedCoins: ExtractSuccess<typeof supportedCoinsR>
+    ) => {
+      // logic to exclude fiat currencies from list
+      return reject(
+        not,
+        values(
+          // @ts-ignore
+          map((coin: SupportedCoinType) => {
+            const currentPrice = coinPrices[coin.coinCode]
+            const yesterdayPrice = coinPricesPrevious[coin.coinCode]
+            return (
+              coin.coinCode in CoinTypeEnum && {
+                coin: coin.coinCode,
+                coinModel: coin,
+                name: `${coin.displayName} (${coin.coinTicker})`,
+                price: currentPrice,
+                priceChange: Number(
+                  ((yesterdayPrice - currentPrice) / yesterdayPrice) * 100
+                ).toPrecision(2),
+                balance: coinBalances[coin.coinCode]
+              }
+            )
+          }, supportedCoins)
+        )
       )
-    )
-  }
+    }
 
-  return lift(transform)(coinPricesR, coinPricesPreviousR, supportedCoinsR)
-}
+    return lift(transform)(coinPricesR, coinPricesPreviousR, supportedCoinsR)
+  }
+)
