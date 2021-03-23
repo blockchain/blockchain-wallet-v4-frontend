ecaacd613b34a795265d0f8473070c0b1ad194e6
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2021-03-23 08:24:42 -0700

    feat(open banking): style bank search page, implement simple search filtering, add yapily host to img-src CSP list

--- packages/blockchain-wallet-v4-frontend/utils/webpackBuilder.js
+++ packages/blockchain-wallet-v4-frontend/utils/webpackBuilder.js
@@ -278,7 +278,7 @@ const buildDevServerConfig = (
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Content-Security-Policy': [
-        `img-src 'self' *.googleusercontent.com *.zendesk.com data: blob:`,
+        `img-src 'self' *.googleusercontent.com *.zendesk.com *.yapily.com data: blob:`,
         allowUnsafeScripts
           ? `script-src 'nonce-${CSP_NONCE}' 'self' 'unsafe-eval'`
           : `script-src 'nonce-${CSP_NONCE}' 'self'`,
