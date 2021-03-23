6ae4f1e118914d7487283ce9974366ab1f31a643
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2021-03-23 16:46:09 -0700

    feat(open banking): merge in dev, fix conflicts, merge in authorize screen, connect them

--- packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/AddBankYapily/SelectBank/template.success.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/AddBankYapily/SelectBank/template.success.tsx
@@ -10,6 +10,7 @@ import {
   BankWrapper,
   ModalNavWithBackArrow,
   SimpleBankRow
+<<<<<<< HEAD
 } from '../../../../components'
 import {
   LinkDispatchPropsType as _LD,
@@ -17,6 +18,12 @@ import {
   SuccessStateType as _SS} from '.'
 
 type Props = _SS & _LD & _O
+=======
+} from '../../../components'
+import { LinkDispatchPropsType as _LD,SuccessStateType as _SS } from '.'
+
+type OwnProps = _SS & _LD
+>>>>>>> feat(open banking): merge in dev, fix conflicts, merge in authorize screen, connect them
 
 const Success = (props: Props) => {
   const [banks, setBanks] = useState<OBInstitution[]>(
