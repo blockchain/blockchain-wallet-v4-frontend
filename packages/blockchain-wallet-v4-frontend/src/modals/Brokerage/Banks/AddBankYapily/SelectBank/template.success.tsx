604efec576c3a9807379502c3ce1e06d3323b888
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2021-03-23 08:24:42 -0700

    feat(open banking): style bank search page, implement simple search filtering, add yapily host to img-src CSP list

--- packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/AddBankYapily/SelectBank/template.success.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/AddBankYapily/SelectBank/template.success.tsx
@@ -1,40 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 
-import { Icon } from 'blockchain-info-components'
-import { FlyoutWrapper } from 'components/Flyout'
+import { OBInstitution } from 'data/types'
 
-import { NavText, SimpleBankRow } from '../../../components'
+import {
+  BankSearchInput,
+  BankWrapper,
+  ModalNavWithBackArrow,
+  SimpleBankRow
+} from '../../../components'
 import { SuccessStateType as _SS } from '.'
 
 type OwnProps = _SS
+
 const Success = (props: OwnProps) => {
+  const [banks, setBanks] = useState<OBInstitution[]>(
+    props.bankCredentials.attributes.institutions
+  )
+
+  const simpleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    const searchResults = props.bankCredentials.attributes.institutions.filter(
+      bank => bank.name.toLowerCase().match(value.toLowerCase())
+    )
+    setBanks(searchResults)
+  }
+
   return (
-    <FlyoutWrapper>
-      <NavText color='grey800' size='20px' weight={600}>
-        <Icon
-          cursor
-          name='arrow-back'
-          size='20px'
-          color='grey600'
-          role='button'
-          style={{ marginRight: '24px' }}
-          onClick={() => {}}
-        />
+    <BankWrapper>
+      <ModalNavWithBackArrow>
         <FormattedMessage
           id='copy.find_your_bank'
           defaultMessage='Find Your Bank'
         />
-      </NavText>
-      {props.bankCredentials.attributes.institutions.map(institution => {
-        return (
-          <div>
-            <SimpleBankRow institution={institution} />
-            <span>{institution.name}</span>
-          </div>
-        )
+      </ModalNavWithBackArrow>
+      <BankSearchInput
+        onChange={simpleSearch}
+        placeholder='Search'
+        type='text'
+      />
+      {banks.map(bank => {
+        return <SimpleBankRow key={bank.id} institution={bank} />
       })}
-    </FlyoutWrapper>
+    </BankWrapper>
   )
 }
 
