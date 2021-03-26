477fece3936ffee41b9cf07fcf4814e6a2c7610b
Andrew Schneider <andrew@blockchain.com>
2021-03-26 14:22:00 -0400

    feat(assets): rethink icon display in CoinAccountIcon

--- packages/blockchain-info-components/src/Icons/CoinAccountIcon.js
+++ packages/blockchain-info-components/src/Icons/CoinAccountIcon.js
@@ -20,64 +20,39 @@ const SubIconWrapper = styled.div`
 `
 const SubIcon = styled.span`
   display: flex;
-  position: relative;
-  width: 12px;
-  height: 12px;
-  border-radius: 12px;
-  margin: 1px;
-  font-weight: ${props => props.weight};
-  font-size: ${props => props.size};
-  color: white;
-  -webkit-font-smoothing: antialiased;
+  font-size: 12px;
+  margin: 0.05rem;
+  color: ${props => props.theme[props.color] || props.color};
   cursor: ${props => (props.cursorEnabled ? 'pointer' : 'inherit')};
-  background: ${props => props.theme[props.color] || props.color};
+  -webkit-font-smoothing: antialiased;
 
+  * {
+    color: red !important;
+  }
+  
   &:before {
-    position: absolute;
-    bottom: ${props => props.offsetBottom};
-    right: ${props => props.offsetRight};
     font-family: 'icomoon', sans-serif;
     content: '${props => props.icon}';
   }
 `
 
-// hardcoded for 32px size icons... 😕
 const CoinAccountIcon = props => {
   const { accountType, coin } = props
-  let subIcon
+
+  let icon
   switch (true) {
     case accountType === 'CUSTODIAL':
-      subIcon = {
-        offsetBottom: '0.21rem',
-        offsetRight: '0.17rem',
-        icon: Icomoon['arrow-top-right-bottom-left'],
-        size: '6px'
-      }
+      icon = Icomoon['arrow-top-right-bottom-left-circle']
       break
     case accountType === 'EXCHANGE':
-      subIcon = {
-        offsetBottom: '0.06rem',
-        offsetRight: '0.06rem',
-        icon: Icomoon['blockchain-logo'],
-        size: '9px'
-      }
+      icon = Icomoon['blockchain-logo-circle']
       break
     case accountType === 'INTEREST':
-      subIcon = {
-        offsetBottom: '0.05rem',
-        offsetRight: '0.1rem',
-        icon: Icomoon['percentage'],
-        size: '9px'
-      }
+      icon = Icomoon['percentage']
       break
     case accountType === 'ACCOUNT':
     default:
-      subIcon = {
-        offsetBottom: '0.25rem',
-        offsetRight: '0.05rem',
-        icon: Icomoon['key'],
-        size: '5px'
-      }
+      icon = Icomoon['key-circle']
       break
   }
 
@@ -85,7 +60,7 @@ const CoinAccountIcon = props => {
     <Wrapper {...props}>
       <Image height='32px' name={coin.toLowerCase()} width='32px' />
       <SubIconWrapper>
-        <SubIcon {...subIcon} color={coin} />
+        <SubIcon icon={icon} color={coin} />
       </SubIconWrapper>
     </Wrapper>
   )
