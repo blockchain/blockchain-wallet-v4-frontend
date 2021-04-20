b279a0a9bac0074d5decbbf6f05d1524cd8daea9
plondon <philip@blockchain.com>
2021-04-20 07:50:32 -0400

    feat(segwit): remove warning and banner

--- packages/blockchain-info-components/src/Images/Images.ts
+++ packages/blockchain-info-components/src/Images/Images.ts
@@ -122,7 +122,6 @@ import recoverPadlock from './img/recover-padlock.svg'
 import refresh from './img/refresh.svg'
 import safeConnect from './img/safe-connect.svg'
 import search from './img/search.svg'
-import segwitLogo from './img/segwit-logo.png'
 import selectArrow from './img/select-arrow.png'
 import silverApproved from './img/silver-approved.svg'
 import smartphone from './img/smartphone.png'
@@ -273,7 +272,6 @@ const Images = {
   refresh,
   'safe-connect': safeConnect,
   search: search,
-  'segwit-logo': segwitLogo,
   'select-arrow': selectArrow,
   'silver-approved': silverApproved,
   smartphone: smartphone,
