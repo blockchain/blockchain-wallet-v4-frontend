d7b5919723a6034beb658997e653564183a54a08
Andrew Schneider <andrew@blockchain.com>
2021-04-22 10:13:55 -0400

    chore(deps): fix tests

--- packages/blockchain-wallet-v4-frontend/babel.config.js
+++ packages/blockchain-wallet-v4-frontend/babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
     ['module-resolver', { root: ['./src'] }],
     ['react-intl', { messagesDir: './build/extractedMessages' }]
   ],
