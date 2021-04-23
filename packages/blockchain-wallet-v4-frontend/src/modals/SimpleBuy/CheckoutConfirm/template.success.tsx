38961c0d269e92efbec0402dbfcf933f1272c563
Milan <milan@blockchain.com>
2021-04-23 10:39:24 +0200

    feat(ob): fixed lock time from 14 to 7 days for ACH

--- packages/blockchain-wallet-v4-frontend/src/modals/SimpleBuy/CheckoutConfirm/template.success.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/SimpleBuy/CheckoutConfirm/template.success.tsx
@@ -235,8 +235,8 @@ const Success: React.FC<InjectedFormProps<{}, Props> & Props> = props => {
           <Info>
             <Text size='12px' weight={500} color='grey900'>
               <FormattedMessage
-                id='modals.simplebuy.confirm.ach'
-                defaultMessage='For your security, buy orders with a bank account are subject up to a 14 day holding period. You can Swap or Sell during this time. We will notify you once the funds are fully available.'
+                id='modals.simplebuy.confirm.ach_lock'
+                defaultMessage='For your security, buy orders with a bank account are subject up to a 7 day holding period. You can Swap or Sell during this time. We will notify you once the funds are fully available.'
               />
             </Text>
           </Info>
