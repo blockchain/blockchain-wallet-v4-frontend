8c3bbfda302716460984ce05aa30dde991a39bf3
Pedro Filho <pedro@filho.me>
2021-04-28 11:39:40 -0300

    fix(segment): adding KYC step and dictionaries

--- packages/blockchain-wallet-v4-frontend/src/middleware/analyticsMiddleware.ts
+++ packages/blockchain-wallet-v4-frontend/src/middleware/analyticsMiddleware.ts
@@ -1,10 +1,11 @@
 import { v4 } from 'uuid'
 
 import { actionTypes as AT } from 'data'
-// import { ModalNamesType, ModalOriginType } from 'data/types'
+import { ModalNamesType, ModalOriginType } from 'data/types'
 import queuevent from 'utils/queuevent'
 
 enum AnalyticsKey {
+  KYC_INITIALIZE_VERIFICATION = 'KYC Initialized',
   MODAL_VIEW = 'Page View',
   PAGE_VIEW = 'Page View'
 }
@@ -30,7 +31,11 @@ type PageViewPayload = BasePayload & {
   referrer: string
 }
 
-type AnalyticsPayload = ModalViewPayload | PageViewPayload
+type KYCPayload = BasePayload & {
+  language: string
+}
+
+type AnalyticsPayload = ModalViewPayload | PageViewPayload | KYCPayload
 
 const analyticsURL = 'https://api.dev.blockchain.info/events/publish'
 
@@ -68,19 +73,23 @@ const analytics = queuevent<AnalyticsKey, AnalyticsPayload>({
   }
 })
 
-// const modalNameDictionary = (modalType: ModalNamesType) => {
-//   switch (modalType) {
-//     case 'SEND_BCH_MODAL':
-//       return '/send'
-//   }
-// }
+const modalNameDictionary = (modalType: ModalNamesType): string => {
+  switch (modalType) {
+    case 'SEND_BCH_MODAL':
+      return '/send'
+    default:
+      return modalType
+  }
+}
 
-// const modalOriginDictionary = (modalOrigin: ModalOriginType) => {
-//   switch (modalOrigin) {
-//     case 'AddBankModal':
-//       return '/send'
-//   }
-// }
+const modalOriginDictionary = (modalOrigin: ModalOriginType) => {
+  switch (modalOrigin) {
+    case 'AddBankModal':
+      return 'ADD_BANK_MODAL'
+    default:
+      return modalOrigin
+  }
+}
 
 const getOriginalTimestamp = () => new Date().toISOString()
 
@@ -88,9 +97,7 @@ const analyticsMiddleware = () => store => next => action => {
   const referrer = document.referrer
   const location = window.location.href
   const state = store.getState()
-
-  // eslint-disable-next-line no-console
-  console.log(state)
+  const language = state.preferences.language
 
   switch (action.type) {
     case '@@INIT':
@@ -109,8 +116,15 @@ const analyticsMiddleware = () => store => next => action => {
         type: AnalyticsType.VIEW,
         originalTimestamp: getOriginalTimestamp(),
         referrer: location,
-        origin: action.payload.props.origin,
-        page: action.payload.type
+        origin: modalOriginDictionary(action.payload.props.origin),
+        page: modalNameDictionary(action.payload.type)
+      })
+      break
+    case AT.components.identityVerification.INITIALIZE_VERIFICATION:
+      analytics.push(AnalyticsKey.KYC_INITIALIZE_VERIFICATION, {
+        type: AnalyticsType.VIEW,
+        originalTimestamp: getOriginalTimestamp(),
+        language: language
       })
       break
   }
