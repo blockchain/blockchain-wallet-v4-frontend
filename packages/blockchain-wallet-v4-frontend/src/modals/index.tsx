29ea1d817947cf6b76198f59f419d4df75d27326
Milan <milan@blockchain.com>
2021-05-21 14:38:05 +0200

    feat(react intl): fixed rendering of html translations

--- packages/blockchain-wallet-v4-frontend/src/modals/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/index.tsx
@@ -1,15 +1,9 @@
 import React, { Suspense } from 'react'
 
 // ADDRESSES
-const DeleteAddressLabel = React.lazy(() =>
-  import('./Addresses/DeleteAddressLabel')
-)
-const ShowUsedAddresses = React.lazy(() =>
-  import('./Addresses/ShowUsedAddresses')
-)
-const UpgradeAddressLabels = React.lazy(() =>
-  import('./Addresses/UpgradeAddressLabels')
-)
+const DeleteAddressLabel = React.lazy(() => import('./Addresses/DeleteAddressLabel'))
+const ShowUsedAddresses = React.lazy(() => import('./Addresses/ShowUsedAddresses'))
+const UpgradeAddressLabels = React.lazy(() => import('./Addresses/UpgradeAddressLabels'))
 
 // BITPAY
 const BitpayInformational = React.lazy(() => import('./BitPay/Informational'))
@@ -32,9 +26,7 @@ const TransferEth = React.lazy(() => import('./Eth/TransferEth'))
 
 // XLM
 const SendXlm = React.lazy(() => import('./Xlm/SendXlm'))
-const XlmCreateAccountLearn = React.lazy(() =>
-  import('./Xlm/XlmCreateAccountLearn')
-)
+const XlmCreateAccountLearn = React.lazy(() => import('./Xlm/XlmCreateAccountLearn'))
 const XlmReserveLearn = React.lazy(() => import('./Xlm/XlmReserveLearn'))
 
 // CRYPTO
@@ -48,65 +40,39 @@ const Support = React.lazy(() => import('./Generic/Support'))
 // ONBOARDING
 const AirdropClaim = React.lazy(() => import('./Onboarding/AirdropClaim'))
 const AirdropSuccess = React.lazy(() => import('./Onboarding/AirdropSuccess'))
-const LinkFromExchangeAccount = React.lazy(() =>
-  import('./Onboarding/LinkFromExchangeAccount')
-)
-const LinkToExchangeAccount = React.lazy(() =>
-  import('./Onboarding/LinkToExchangeAccount')
-)
-const IdentityVerification = React.lazy(() =>
-  import('./Onboarding/KycVerification')
-)
+const LinkFromExchangeAccount = React.lazy(() => import('./Onboarding/LinkFromExchangeAccount'))
+const LinkToExchangeAccount = React.lazy(() => import('./Onboarding/LinkToExchangeAccount'))
+const IdentityVerification = React.lazy(() => import('./Onboarding/KycVerification'))
 const KycDocResubmit = React.lazy(() => import('./Onboarding/KycDocResubmit'))
 const KycTierUpgrade = React.lazy(() => import('./Onboarding/KycTierUpgrade'))
 const SwapGetStarted = React.lazy(() => import('./Onboarding/SwapGetStarted'))
-const UpgradeForAirdrop = React.lazy(() =>
-  import('./Onboarding/UpgradeForAirdrop')
-)
+const UpgradeForAirdrop = React.lazy(() => import('./Onboarding/UpgradeForAirdrop'))
 const Welcome = React.lazy(() => import('./Onboarding/Welcome'))
 
 // LOCKBOX
-const LockboxAppManager = React.lazy(() =>
-  import('./Lockbox/LockboxAppManager')
-)
-const LockboxConnectionPrompt = React.lazy(() =>
-  import('./Lockbox/LockboxConnectionPrompt')
-)
+const LockboxAppManager = React.lazy(() => import('./Lockbox/LockboxAppManager'))
+const LockboxConnectionPrompt = React.lazy(() => import('./Lockbox/LockboxConnectionPrompt'))
 const LockboxFirmware = React.lazy(() => import('./Lockbox/LockboxFirmware'))
 const LockboxSetup = React.lazy(() => import('./Lockbox/LockboxSetup'))
 const LockboxShowXPubs = React.lazy(() => import('./Lockbox/LockboxShowXPubs'))
 
 // MOBILE
-const MobileNumberChange = React.lazy(() =>
-  import('./Mobile/MobileNumberChange')
-)
-const MobileNumberVerify = React.lazy(() =>
-  import('./Mobile/MobileNumberVerify')
-)
+const MobileNumberChange = React.lazy(() => import('./Mobile/MobileNumberChange'))
+const MobileNumberVerify = React.lazy(() => import('./Mobile/MobileNumberVerify'))
 
 // SETTINGS
-const AutoDisconnection = React.lazy(() =>
-  import('./Settings/AutoDisconnection')
-)
-const ConfirmDisable2FA = React.lazy(() =>
-  import('./Settings/ConfirmDisable2FA')
-)
+const AutoDisconnection = React.lazy(() => import('./Settings/AutoDisconnection'))
+const ConfirmDisable2FA = React.lazy(() => import('./Settings/ConfirmDisable2FA'))
 const RecoveryPhrase = React.lazy(() => import('./Settings/RecoveryPhrase'))
 const SecondPassword = React.lazy(() => import('./Settings/SecondPassword'))
 const TradingLimits = React.lazy(() => import('./Settings/TradingLimits'))
-const TwoStepGoogleAuthenticator = React.lazy(() =>
-  import('./Settings/TwoStepGoogleAuthenticator')
-)
+const TwoStepGoogleAuthenticator = React.lazy(() => import('./Settings/TwoStepGoogleAuthenticator'))
 const TwoStepSetup = React.lazy(() => import('./Settings/TwoStepSetup'))
 const TwoStepYubico = React.lazy(() => import('./Settings/TwoStepYubico'))
 
 // TRANSACTIONS
-const DownloadTransactions = React.lazy(() =>
-  import('./Transactions/DownloadTransactions')
-)
-const EditTxDescription = React.lazy(() =>
-  import('./Transactions/EditDescription')
-)
+const DownloadTransactions = React.lazy(() => import('./Transactions/DownloadTransactions'))
+const EditTxDescription = React.lazy(() => import('./Transactions/EditDescription'))
 
 // WALLET
 const PairingCode = React.lazy(() => import('./Wallet/PairingCode'))
@@ -124,12 +90,8 @@ const Swap = React.lazy(() => import('./Swap'))
 // BROKERAGE
 const BankDetails = React.lazy(() => import('./Brokerage/Banks/BankDetails'))
 const RemoveBank = React.lazy(() => import('./Brokerage/Banks/RemoveBank'))
-const AddBankYapily = React.lazy(() =>
-  import('./Brokerage/Banks/AddBankYapily')
-)
-const AddBankYodlee = React.lazy(() =>
-  import('./Brokerage/Banks/AddBankYodlee')
-)
+const AddBankYapily = React.lazy(() => import('./Brokerage/Banks/AddBankYapily'))
+const AddBankYodlee = React.lazy(() => import('./Brokerage/Banks/AddBankYodlee'))
 const Deposit = React.lazy(() => import('./Brokerage/Banks/Deposit'))
 const Withdraw = React.lazy(() => import('./Brokerage/Banks/Withdraw'))
 
