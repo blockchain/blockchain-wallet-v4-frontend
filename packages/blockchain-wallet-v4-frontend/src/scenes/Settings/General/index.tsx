ce5a98369bec87c52c88cd113fdb771d2fb0ec48
leora <leora@blockchain.com>
2021-06-01 10:59:01 -0400

    feat(secuirty): add showPairing code feature flag to wallet options

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import { connect, ConnectedProps } from 'react-redux'
 import styled from 'styled-components'
 
 import { Text } from 'blockchain-info-components'
+import { selectors } from 'data'
 
 import About from './About'
 import LinkedBanks from './LinkedBanks'
@@ -23,7 +25,7 @@ const Title = styled(Text)`
   margin: 4px 0;
 `
 
-const General = () => {
+const General = (props: Props) => {
   return (
     <section>
       <MenuHeader>
@@ -41,10 +43,9 @@ const General = () => {
         </Text>
       </MenuHeader>
       <WalletId />
-      {/* We are hiding the pairing code as part of SSO authorization
-      squad work. We may bring it back in some form later
-      so just commenting it out for now */}
-      {/* <PairingCode /> */}
+      {/* We are hiding the pairing code on production as
+      for wallet security */}
+      {props.showPairingCode && <PairingCode />}
       <LinkedBanks />
       <LinkedWireBanks />
       <LinkedCards />
@@ -56,4 +57,12 @@ const General = () => {
   )
 }
 
-export default General
+const mapStateToProps = (state) => ({
+  showPairingCode: selectors.core.walletOptions.getPairingCodeFlag(state).getOrElse(false)
+})
+
+const connector = connect(mapStateToProps)
+
+type Props = ConnectedProps<typeof connector>
+
+export default connector(General)
