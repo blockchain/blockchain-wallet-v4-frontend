84c9c1e434f1101ff790123416a10878f54bdae1
Andrew Schneider <andrew@blockchain.com>
2021-06-17 16:31:10 -0400

    fix(ts): remove unused mobileLogin flag and fix ts errors

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/actions.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/actions.ts
@@ -2,43 +2,39 @@ import * as AT from './actionTypes'
 import { ABTestCmdType, ABTestNameType } from './types'
 
 export const createABTest = (test: ABTestNameType) => ({
-  type: AT.CREATE_AB_TEST,
-  payload: { test }
+  payload: { test },
+  type: AT.CREATE_AB_TEST
 })
 
-export const createABTestSuccess = (
-  test: ABTestNameType,
-  result: ABTestCmdType
-) => ({
-  type: AT.CREATE_AB_TEST_SUCCESS,
-  payload: { test, result }
+export const createABTestSuccess = (test: ABTestNameType, result: ABTestCmdType) => ({
+  payload: { result, test },
+  type: AT.CREATE_AB_TEST_SUCCESS
 })
 
-export const logEvent = event => ({
-  type: AT.LOG_EVENT,
-  payload: { event }
+export const logEvent = (event) => ({
+  payload: { event },
+  type: AT.LOG_EVENT
 })
 
-export const logPageView = route => ({
-  type: AT.LOG_PAGE_VIEW,
-  payload: { route }
+export const logPageView = (route) => ({
+  payload: { route },
+  type: AT.LOG_PAGE_VIEW
 })
 
-export const logGoal = data => ({
-  type: AT.LOG_GOAL,
-  payload: { data }
+export const logGoal = (data) => ({
+  payload: { data },
+  type: AT.LOG_GOAL
 })
 
 export const initUserSession = () => ({
   type: AT.INIT_USER_SESSION
 })
 
-export const startSession = guid => ({
-  type: AT.START_SESSION,
-  payload: { guid }
+export const startSession = (guid) => ({
+  payload: { guid },
+  type: AT.START_SESSION
 })
 
-export const stopSession = data => ({
-  type: AT.STOP_SESSION,
-  payload: { data }
+export const stopSession = () => ({
+  type: AT.STOP_SESSION
 })
