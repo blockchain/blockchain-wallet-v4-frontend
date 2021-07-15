a1fe67636b6d9f529a89dd9f79fbf8cb4b9ff5d4
leora <leora@blockchain.com>
2021-07-15 09:45:28 -0400

    fix(error-template): remove duplicate wrapper

--- packages/blockchain-wallet-v4-frontend/src/scenes/RecoverWallet/RecoveryPhrase/SecondStep/error.template.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/RecoverWallet/RecoveryPhrase/SecondStep/error.template.tsx
@@ -14,26 +14,18 @@ const Body = styled.div`
 
 const Error = (props) => {
   return (
-    <Wrapper>
-      <Body>
-        <Icon color='error' name='close-circle' size='40px' />
-        <Text size='20px' weight={600} color='black' style={{ marginTop: '8px' }}>
-          <FormattedMessage
-            id='scenes.recover.restore_fails'
-            defaultMessage='Restoring Account Failed'
-          />
-        </Text>
-        <Text
-          color='blue600'
-          size='16px'
-          weight={500}
-          cursor='pointer'
-          onClick={props.previousStep}
-        >
-          <FormattedMessage id='scenes.exchange.exchangeform.tryagain' defaultMessage='Try Again' />
-        </Text>
-      </Body>
-    </Wrapper>
+    <Body>
+      <Icon color='error' name='close-circle' size='40px' />
+      <Text size='20px' weight={600} color='black' style={{ marginTop: '8px' }}>
+        <FormattedMessage
+          id='scenes.recover.restore_fails'
+          defaultMessage='Restoring Account Failed'
+        />
+      </Text>
+      <Text color='blue600' size='16px' weight={500} cursor='pointer' onClick={props.previousStep}>
+        <FormattedMessage id='scenes.exchange.exchangeform.tryagain' defaultMessage='Try Again' />
+      </Text>
+    </Body>
   )
 }
 
