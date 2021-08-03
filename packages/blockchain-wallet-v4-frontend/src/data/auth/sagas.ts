1efd954e93235d87b6d33d4bcf4257e55a2ce5fb
leora <leora@blockchain.com>
2021-08-03 08:19:44 -0400

    feat(account-reset): reset failed modal part one

--- packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.ts
@@ -744,29 +744,33 @@ export default ({ api, coreSagas, networks }) => {
     // If user is resetting their custodial account
     // Creating a new wallet and assigning an existing custodial account
     // to that wallet
-    const { email, language, password } = action.payload
-    // We get recovery token and nabu ID
-    const magicLinkData = yield select(S.getMagicLinkData)
-    const recoveryToken = magicLinkData.wallet?.nabu?.recoveryToken
-    const userId = magicLinkData.wallet?.nabu?.userId
-    // create a new wallet
-    yield call(register, actions.auth.register(email, password, language))
-    const guid = yield select(selectors.core.wallet.getGuid)
-    // generate a retail token for new wallet
-    const retailToken = yield call(generateRetailToken)
-    // call the reset nabu user endpoint, receive new lifetime
-    // token for nabu user
-    const { token: lifetimeToken } = yield call(
-      api.resetUserAccount,
-      userId,
-      recoveryToken,
-      retailToken
-    )
-    // set new lifetime token for user in metadata
-    yield put(actions.core.kvStore.userCredentials.setUserCredentials(userId, lifetimeToken))
-    // fetch user in new wallet
-    yield call(setSession, userId, lifetimeToken, email, guid)
-    // TODOs - how do we handle failure?
+    try {
+      const { email, language, password } = action.payload
+      // We get recovery token and nabu ID
+      const magicLinkData = yield select(S.getMagicLinkData)
+      const recoveryToken = magicLinkData.wallet?.nabu?.recoveryToken
+      const userId = magicLinkData.wallet?.nabu?.userId
+      // create a new wallet
+      yield call(register, actions.auth.register(email, password, language))
+      const guid = yield select(selectors.core.wallet.getGuid)
+      // generate a retail token for new wallet
+      const retailToken = yield call(generateRetailToken)
+      // call the reset nabu user endpoint, receive new lifetime
+      // token for nabu user
+      const { token: lifetimeToken } = yield call(
+        api.resetUserAccount,
+        userId,
+        recoveryToken,
+        retailToken
+      )
+      // set new lifetime token for user in metadata
+      yield put(actions.core.kvStore.userCredentials.setUserCredentials(userId, lifetimeToken))
+      // fetch user in new wallet
+      yield call(setSession, userId, lifetimeToken, email, guid)
+      // TODOs - how do we handle failure?
+    } catch (e) {
+      yield put(actions.modals.showModal('RESET_ACCOUNT_FAILED', { origin: 'ResetAccount' }))
+    }
   }
   return {
     authNabu,
---
+++ packages/blockchain-wallet-v4-frontend/src/modals/Wallet/ResetAccountFailed/index.tsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import { connect, ConnectedProps } from 'react-redux'
+import { compose } from 'redux'
+import styled from 'styled-components'
+
+import { Icon, Modal, Text } from 'blockchain-info-components'
+import { actions } from 'data'
+import modalEnhancer from 'providers/ModalEnhancer'
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  margin: 30px 0 10px;
+  overflow: hidden;
+  text-align: center;
+`
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+  text-align: center;
+  margin: 4px 0 14px;
+
+  & > :last-child {
+    margin: 15px 0;
+  }
+`
+
+const RectangleBackground = styled.div`
+  height: 64px;
+  width: 100%;
+  background-color: ${(props) => props.theme.grey000};
+  border-radius: 8px;
+  margin-top: 24px;
+`
+
+class ResetAccountFailed extends React.PureComponent<Props> {
+  render() {
+    return (
+      <Modal size='medium'>
+        <Header>
+          <Icon
+            name='alert-filled'
+            size='48px'
+            color='orange600'
+            style={{ marginBottom: '16px' }}
+          />
+          <Text size='20px' weight={600} color='grey900'>
+            <FormattedMessage
+              defaultMessage='Fund Recovery Failed'
+              id='modals.resetaccountfailure.title'
+            />
+          </Text>
+        </Header>
+        <Body>
+          <Text size='16px' weight={500} color='grey900'>
+            <FormattedMessage
+              defaultMessage='Don’t worry, your account is safe.  Please contact support to finish the Account Recovery process. Your account will not show balances or transaction history until you complete the recovery process.'
+              id='modals.resetaccountfailure.body'
+            />
+          </Text>
+          <RectangleBackground>
+            <Text size='12px' color='grey600' weight={500}>
+              <FormattedMessage
+                defaultMessage='Fund recovery failures can happen for a number of reasons. Our support team is able to help recover your account. <a>Learn more</a>'
+                id='modals.resetaccountfailure.information'
+                values={{
+                  a: (msg) => (
+                    <a
+                      // TODO get correct support article
+                      href='https://support.blockchain.com/hc/en-us/'
+                      rel='noopener noreferrer'
+                      target='_blank'
+                    >
+                      {msg}
+                    </a>
+                  )
+                }}
+              />
+            </Text>
+          </RectangleBackground>
+          {/* Add contact support button */}
+        </Body>
+      </Modal>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  closeModal: () => dispatch(actions.modals.closeModal())
+})
+
+const connector = connect(null, mapDispatchToProps)
+
+type Props = ConnectedProps<typeof connector>
+
+const enhance = compose<any>(
+  connect(null, mapDispatchToProps),
+  modalEnhancer('RESET_ACCOUNT_FAILED'),
+  connector
+)
+
+export default enhance(ResetAccountFailed)
