200eb39d56c8d4a076af983518a214144189eb55
leora <leora@blockchain.com>
2021-08-04 09:58:02 -0400

    fix(kyc-reset): handle case where tier 2 kycstate is none

--- packages/blockchain-wallet-v4-frontend/src/data/components/identityVerification/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/components/identityVerification/sagas.ts
@@ -145,8 +145,11 @@ export default ({ api, coreSagas, networks }) => {
       selectors.modules.profile.getKycDocResubmissionStatus
     )).getOrElse({})
     const tiersState = (yield select(selectors.modules.profile.getTiers)).getOrElse({})
+    // Edge case where a user profile is set to tier two
+    // but kycState is none after nabu reset
+    const tierTwoKycNone = kycState === KYC_STATES.NONE && tiers.current > 1
     if (kycDocResubmissionStatus === 1) {
-      if (tiers.current === 0 || kycState === KYC_STATES.NONE) {
+      if (tiers.current === 0) {
         // case where user already went through first step
         // of verfication but was rejected, want to set
         // next to 2
@@ -155,7 +158,7 @@ export default ({ api, coreSagas, networks }) => {
         } else {
           tiers = { current: 0, next: 1, selected: 2 }
         }
-      } else if (tiers.current === 1 || tiers.current === 3) {
+      } else if (tierTwoKycNone || tiers.current === 1 || tiers.current === 3) {
         tiers = { current: 1, next: 2, selected: 2 }
       } else {
         return
