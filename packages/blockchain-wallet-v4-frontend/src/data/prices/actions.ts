3f32439a7389e4cce23130a96f1c843239f7cb25
Mitch Malinin <mitchmalinin@gmail.com>
2021-08-08 18:51:57 -0400

    test: testing inital commit flow

--- packages/blockchain-wallet-v4-frontend/src/data/prices/actions.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/prices/actions.ts
@@ -3,36 +3,34 @@ import { CoinPricesRequestType } from './types'
 
 // FETCH_COIN_PRICES
 export const fetchCoinPrices = (request?: CoinPricesRequestType) => ({
-  type: AT.FETCH_COIN_PRICES,
-  payload: request || {}
+  payload: request || {},
+  type: AT.FETCH_COIN_PRICES
 })
 export const fetchCoinPricesLoading = () => ({
   type: AT.FETCH_COIN_PRICES_LOADING
 })
-export const fetchCoinPricesSuccess = data => ({
-  type: AT.FETCH_COIN_PRICES_SUCCESS,
-  payload: data
+export const fetchCoinPricesSuccess = (data) => ({
+  payload: data,
+  type: AT.FETCH_COIN_PRICES_SUCCESS
 })
-export const fetchCoinPricesFailure = data => ({
-  type: AT.FETCH_COIN_PRICES_FAILURE,
-  payload: data
+export const fetchCoinPricesFailure = (data) => ({
+  payload: data,
+  type: AT.FETCH_COIN_PRICES_FAILURE
 })
 
 // FETCH_COIN_PRICES_PREVIOUS_DAY
-export const fetchCoinPricesPreviousDay = (
-  request?: CoinPricesRequestType
-) => ({
-  type: AT.FETCH_COIN_PRICES_PREVIOUS_DAY,
-  payload: request || {}
+export const fetchCoinPricesPreviousDay = (request?: CoinPricesRequestType) => ({
+  payload: request || {},
+  type: AT.FETCH_COIN_PRICES_PREVIOUS_DAY
 })
 export const fetchCoinPricesPreviousDayLoading = () => ({
   type: AT.FETCH_COIN_PRICES_PREVIOUS_DAY_LOADING
 })
-export const fetchCoinPricesPreviousDaySuccess = data => ({
-  type: AT.FETCH_COIN_PRICES_PREVIOUS_DAY_SUCCESS,
-  payload: data
+export const fetchCoinPricesPreviousDaySuccess = (data) => ({
+  payload: data,
+  type: AT.FETCH_COIN_PRICES_PREVIOUS_DAY_SUCCESS
 })
-export const fetchCoinPricesPreviousDayFailure = data => ({
-  type: AT.FETCH_COIN_PRICES_PREVIOUS_DAY_FAILURE,
-  payload: data
+export const fetchCoinPricesPreviousDayFailure = (data) => ({
+  payload: data,
+  type: AT.FETCH_COIN_PRICES_PREVIOUS_DAY_FAILURE
 })
