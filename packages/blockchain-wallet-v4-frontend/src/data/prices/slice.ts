0a10744eca7402617e70b57da13a3ed845891bc7
Mitch Malinin <mitchmalinin@gmail.com>
2021-08-09 20:55:39 -0400

    chore(prices): prices toolkit migration

---
+++ packages/blockchain-wallet-v4-frontend/src/data/prices/slice.ts
@@ -0,0 +1,75 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { keys, map, mergeAll } from 'ramda'
+
+import Remote from 'blockchain-wallet-v4/src/remote/remote'
+
+import { CoinPricesRequestType, PricesStateType } from './types'
+
+const createPricesKvPairs = (prices) => {
+  return mergeAll(
+    map(
+      (x) => ({
+        // @ts-ignore
+        [x.split('-')[0]]: prices[x].price
+      }),
+      keys(prices)
+    )
+  )
+}
+
+const initialState: PricesStateType = {
+  current: Remote.NotAsked,
+  previousDay: Remote.NotAsked
+}
+
+// This combines the action with the reducer in one slice
+const pricesSlice = createSlice({
+  initialState,
+  name: 'prices',
+  reducers: {
+    fetchCoinPrices: {
+      reducer: (state, action: PayloadAction<CoinPricesRequestType>) => {},
+      prepare: (request?: CoinPricesRequestType) => ({ payload: request || {} })
+    },
+    fetchCoinPricesFailure: (state, action: PayloadAction<string>) => {
+      state.current = Remote.Failure(action.payload)
+    },
+    fetchCoinPricesLoading: (state) => {
+      state.current = Remote.Loading
+    },
+    fetchCoinPricesSuccess: (state, action: PayloadAction<CoinPricesRequestType>) => {
+      const pricesPairing = createPricesKvPairs(action.payload)
+      state.current = Remote.Success(pricesPairing)
+    },
+    fetchCoinPricesPreviousDay: {
+      reducer: (state, action: PayloadAction<CoinPricesRequestType>) => {},
+      prepare: (request?: CoinPricesRequestType) => ({ payload: request || {} })
+    },
+    fetchCoinPricesPreviousDayFailure: (state, action: PayloadAction<string>) => {
+      state.previousDay = Remote.Failure(action.payload)
+    },
+    fetchCoinPricesPreviousDayLoading: (state) => {
+      state.previousDay = Remote.Loading
+    },
+    fetchCoinPricesPreviousDaySuccess: (state, action: PayloadAction<CoinPricesRequestType>) => {
+      const pricesPairing = createPricesKvPairs(action.payload)
+      state.previousDay = Remote.Success(pricesPairing)
+    }
+  }
+})
+
+export const {
+  fetchCoinPrices,
+  fetchCoinPricesFailure,
+  fetchCoinPricesLoading,
+  fetchCoinPricesPreviousDay,
+  fetchCoinPricesPreviousDayFailure,
+  fetchCoinPricesPreviousDayLoading,
+  fetchCoinPricesPreviousDaySuccess,
+  fetchCoinPricesSuccess
+} = pricesSlice.actions
+
+const { actions } = pricesSlice
+const pricesReducer = pricesSlice.reducer
+export { actions, pricesReducer }
