8aa81488fa5595008555a6fd822081b61ccc83e0
plondon <philip@blockchain.com>
2021-08-19 14:35:48 -0400

    feat(error boundary): use error stack instead of component stack

--- packages/blockchain-wallet-v4-frontend/src/data/components/recurringBuy/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/components/recurringBuy/selectors.ts
@@ -11,12 +11,10 @@ export const getMethods = (state: RootState) => state.components.recurringBuy.me
 export const getPeriod = (state: RootState) => state.components.recurringBuy.period
 export const getRegisteredList = (state: RootState) => state.components.recurringBuy.registeredList
 
-export const isAvailableMethod = (method?: SBPaymentMethodType) =>
+export const isAvailableMethod = (method) =>
   createSelector(
     getMethods,
-    (methodsR) =>
-      methodsR.getOrElse([]).filter((m) => m === (method?.type as unknown as RecurringBuyPeriods))
-        .length > 0
+    (methodsR) => methodsR.getOrElse([]).filter((m) => m === method.type).length > 0
   )
 
 export const getRegisteredListByCoin = (coin) =>
