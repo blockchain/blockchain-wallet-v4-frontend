5286fe424a69b42c48e0fdbb96065ad5bcb152fd
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2021-09-14 20:38:21 -0700

    feat(deposit): add withdrawal lock copy to deposit confirm screen, convert confirm screen modal to shared comps, add arkansas to credit card address dropdown list (#3623)

--- packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/Deposit/Confirm/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/Deposit/Confirm/index.tsx
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
-import { getFormValues } from 'redux-form'
 
+import { ExtractSuccess } from 'core/types'
 import { actions, selectors } from 'data'
 import { RootState } from 'data/rootReducer'
+import { BrokerageTxFormValuesType } from 'data/types'
 
+import Failure from '../template.failure'
+import Loading from '../template.loading'
+import { getData } from './selectors'
 import Success from './template.success'
 
 const DepositMethods = (props: Props) => {
-  return <Success {...props} />
+  useEffect(() => {
+    props.sendActions.getLockRule()
+  }, [])
+
+  return props.data.cata({
+    Failure: () => <Failure {...props} handleClose={props.handleClose} />,
+    Loading: () => <Loading />,
+    NotAsked: () => <Loading />,
+    Success: (val) => <Success {...val} {...props} />
+  })
 }
 
 const mapStateToProps = (state: RootState) => ({
+  data: getData(state),
   defaultMethod: selectors.components.brokerage.getAccount(state),
   fiatCurrency: selectors.core.settings.getCurrency(state),
-  formValues: getFormValues('brokerageTx')(state)
+  formValues: selectors.form.getFormValues('brokerageTx')(state) as BrokerageTxFormValuesType
 })
 
 export const mapDispatchToProps = (dispatch: Dispatch) => ({
   analyticsActions: bindActionCreators(actions.analytics, dispatch),
-  brokerageActions: bindActionCreators(actions.components.brokerage, dispatch)
+  brokerageActions: bindActionCreators(actions.components.brokerage, dispatch),
+  sendActions: bindActionCreators(actions.components.send, dispatch)
 })
 
 export type OwnProps = {
@@ -28,6 +43,7 @@ export type OwnProps = {
 }
 const connector = connect(mapStateToProps, mapDispatchToProps)
 
+export type SuccessStateType = ExtractSuccess<ReturnType<typeof getData>>
 export type Props = OwnProps & ConnectedProps<typeof connector>
 
 export default connector(DepositMethods)
