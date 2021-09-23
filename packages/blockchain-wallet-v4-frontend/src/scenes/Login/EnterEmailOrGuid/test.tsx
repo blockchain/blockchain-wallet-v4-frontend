b5548a175abb9de984af46affb355ae0368881a7
leora <leora@blockchain.com>
2021-09-23 18:38:16 -0400

    feat(product-picker): product picker

--- packages/blockchain-wallet-v4-frontend/src/scenes/Login/EnterEmailOrGuid/test.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Login/EnterEmailOrGuid/test.tsx
@@ -5,27 +5,12 @@ import styled from 'styled-components'
 import { Icon, Image, Text } from 'blockchain-info-components'
 
 import { Props } from '..'
-import { CenteredColumn } from '../model'
 
-const FormBody = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
 const IconTextRow = styled.div`
   display: flex;
   align-items: center;
+  position: relative;
   cursor: pointer;
-`
-
-const TextStack = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 24px;
   &:after {
     position: absolute;
     bottom: 0;
@@ -33,7 +18,15 @@ const TextStack = styled.div`
     height: 1px;
     background: ${(props) => props.theme.grey100};
     content: '';
-  }
+`
+
+// TODO add media tag for
+// min width
+const TextStack = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 24px;
+  min-width: 312px;
 `
 
 const Test = (props: Props) => {
@@ -41,30 +34,27 @@ const Test = (props: Props) => {
     <>
       <IconTextRow>
         <Image name='wallet-logo' height='48px' width='48px' />
-        <div>
-          <Row>
-            <Text color='grey900' size='24px' weight={600} lineHeight='1.5'>
-              <FormattedMessage id='copy.wallet' defaultMessage='Wallet' />
-            </Text>
-            <Icon name='chevron-right' size='32px' color='grey400' />
-          </Row>
+        <TextStack>
+          <Text color='grey900' size='24px' weight={600} lineHeight='1.5'>
+            <FormattedMessage id='copy.wallet' defaultMessage='Wallet' />
+          </Text>
+
           <Text color='grey600' size='16px' weight={500} lineHeight='1.5'>
             <FormattedMessage
               id='scenes.login.productpicker.wallet'
               defaultMessage='Easily buy and sell Bitcoin, Ether and more.'
             />
           </Text>
-        </div>
+        </TextStack>
+        <Icon name='chevron-right' size='32px' color='grey400' />
       </IconTextRow>
       <IconTextRow>
         <Image name='exchange-logo' height='48px' width='48px' />
         <TextStack>
-          <Row>
-            <Text color='grey900' size='24px' weight={600} lineHeight='1.5'>
-              <FormattedMessage id='copy.exchange' defaultMessage='Exchange' />
-            </Text>
-            <Icon name='chevron-right' size='32px' color='grey400' />
-          </Row>
+          <Text color='grey900' size='24px' weight={600} lineHeight='1.5'>
+            <FormattedMessage id='copy.exchange' defaultMessage='Exchange' />
+          </Text>
+
           <Text color='grey600' size='16px' weight={500} lineHeight='1.5'>
             <FormattedMessage
               id='scenes.login.productpicker.exchange'
@@ -72,16 +62,15 @@ const Test = (props: Props) => {
             />
           </Text>
         </TextStack>
+        <Icon name='chevron-right' size='32px' color='grey400' />
       </IconTextRow>
       <IconTextRow>
         <Image name='explorer-logo' height='48px' width='48px' />
         <TextStack>
-          <Row>
-            <Text color='grey900' size='24px' weight={600} lineHeight='1.5'>
-              <FormattedMessage id='copy.explorer' defaultMessage='Explorer' />
-            </Text>
-            <Icon name='chevron-right' size='32px' color='grey400' />
-          </Row>
+          <Text color='grey900' size='24px' weight={600} lineHeight='1.5'>
+            <FormattedMessage id='copy.explorer' defaultMessage='Explorer' />
+          </Text>
+
           <Text color='grey600' size='16px' weight={500} lineHeight='1.5'>
             <FormattedMessage
               id='scenes.login.productpicker.explorer'
@@ -89,6 +78,7 @@ const Test = (props: Props) => {
             />
           </Text>
         </TextStack>
+        <Icon name='chevron-right' size='32px' color='grey400' />
       </IconTextRow>
     </>
   )
