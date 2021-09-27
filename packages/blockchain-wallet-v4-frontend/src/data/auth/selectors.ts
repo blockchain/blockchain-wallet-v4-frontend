da159ea6c35e35976bc30335382ed2e9f622c13e
toki-sean <seanhan94@gmail.com>
2021-09-27 11:07:49 -0400

    chore(sso): auth selectors typed

--- packages/blockchain-wallet-v4-frontend/src/data/auth/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/auth/selectors.ts
@@ -2,21 +2,55 @@ import { path } from 'ramda'
 
 import { RootState } from 'data/rootReducer'
 
-import { WalletDataFromMagicLink } from './types'
+import { AuthStateType } from './types'
 
-export const isAuthenticated = path(['auth', 'isAuthenticated'])
-export const getRegistering = path(['auth', 'registering'])
-export const getFirstLogin = path(['auth', 'firstLogin'])
-export const getRestoring = path(['auth', 'restoring'])
-export const getAuthType = path(['auth', 'auth_type'])
-export const getDesignatedProduct = path(['auth', 'designatedProduct'])
-export const getSecureChannelLogin = path(['auth', 'secureChannelLogin'])
-export const getLogin = path(['auth', 'login'])
-export const getMobileLoginStarted = path(['auth', 'mobileLoginStarted'])
-export const getRegisterEmail = path(['auth', 'registerEmail'])
-export const getMetadataRestore = path(['auth', 'metadataRestore'])
-export const getKycResetStatus = path(['auth', 'kycReset'])
-export const getAccountReset = path(['auth', 'resetAccount'])
+export const isAuthenticated: (state: RootState) => AuthStateType['isAuthenticated'] | undefined =
+  path(['auth', 'isAuthenticated'])
+export const getRegistering: (state: RootState) => AuthStateType['registering'] = path([
+  'auth',
+  'registering'
+])
+export const getFirstLogin: (state: RootState) => AuthStateType['firstLogin'] | undefined = path([
+  'auth',
+  'firstLogin'
+])
+export const getRestoring: (state: RootState) => AuthStateType['restoring'] = path([
+  'auth',
+  'restoring'
+])
+export const getAuthType: (state: RootState) => AuthStateType['auth_type'] | undefined = path([
+  'auth',
+  'auth_type'
+])
+export const getDesignatedProduct: (
+  state: RootState
+) => AuthStateType['designatedProduct'] | undefined = path(['auth', 'designatedProduct'])
+export const getSecureChannelLogin: (state: RootState) => AuthStateType['secureChannelLogin'] =
+  path(['auth', 'secureChannelLogin'])
+export const getLogin: (state: RootState) => AuthStateType['login'] = path(['auth', 'login'])
+export const getMobileLoginStarted: (
+  state: RootState
+) => AuthStateType['mobileLoginStarted'] | undefined = path(['auth', 'mobileLoginStarted'])
+export const getRegisterEmail: (state: RootState) => AuthStateType['registerEmail'] = path([
+  'auth',
+  'registerEmail'
+])
+export const getMetadataRestore: (state: RootState) => AuthStateType['metadataRestore'] = path([
+  'auth',
+  'metadataRestore'
+])
+export const getKycResetStatus: (state: RootState) => AuthStateType['kycReset'] = path([
+  'auth',
+  'kycReset'
+])
+export const getAccountReset: (state: RootState) => AuthStateType['resetAccount'] | undefined =
+  path(['auth', 'resetAccount'])
 export const getMagicLinkData = (state: RootState) => state.auth.magicLinkData
-export const getUserGeoData = path(['auth', 'userGeoData'])
-export const getManifest = path(['auth', 'manifestFile'])
+export const getUserGeoData: (state: RootState) => AuthStateType['userGeoData'] = path([
+  'auth',
+  'userGeoData'
+])
+export const getManifest: (state: RootState) => AuthStateType['manifestFile'] = path([
+  'auth',
+  'manifestFile'
+])
