3b609182f7cd3b4d6b61f169c1c393db23b61fd5
Milan <milan@blockchain.com>
2021-09-30 09:51:21 +0200

    chore(storybook): fixed rendering of storybook

--- packages/blockchain-wallet-v4-frontend/src/components/Rows/index.ts
+++ packages/blockchain-wallet-v4-frontend/src/components/Rows/index.ts
@@ -2,4 +2,5 @@ import CheckoutRow from './Checkout'
 import type { Props as OptionRightActionRowProps } from './OptionRightAction'
 import OptionRightActionRow from './OptionRightAction'
 
-export { CheckoutRow, OptionRightActionRow, OptionRightActionRowProps }
+export { CheckoutRow, OptionRightActionRow }
+export type { OptionRightActionRowProps }
