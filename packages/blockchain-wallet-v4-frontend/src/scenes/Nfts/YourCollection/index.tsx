5d768884e87258a551fcd0b8b4c98089a3276d42
plondon <philip@blockchain.com>
2021-10-14 13:51:48 -0400

    feat(nfts): continue stylin

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/YourCollection/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/YourCollection/index.tsx
@@ -6,14 +6,29 @@ import { NftAssetsType } from '@core/network/api/nfts/types'
 import { RemoteDataType } from '@core/types'
 import { Text } from 'blockchain-info-components'
 import { PageTitle, SubTitle, Title } from 'components/Layout'
+import { media } from 'services/styles'
 
 const CollectionWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(8, minmax(0, 1fr));
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  margin-top: 8px;
+  gap: 20px;
+  margin-bottom: 100px;
+  ${media.laptopM`
+    gap: 16px;
+    margin-bottom: 24px;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  `}
+  ${media.tabletL`
+    gap: 12px;
+    margin-bottom: 24px;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  `}
 `
 const Asset = styled.div`
   border-radius: 8px;
-  height: 285px;
+  overflow: hidden;
+  border: ${(props) => `1px solid ${props.theme.grey100}`};
 `
 const AssetImage = styled.img`
   object-fit: contain;
@@ -22,6 +37,19 @@ const AssetImage = styled.img`
   max-width: 100%;
   max-height: 100%;
   border-radius: 0px;
+`
+const AssetDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  background: ${(props) => props.theme.white};
+  > div {
+    width: 50%;
+  }
+`
+const AssetCollection = styled.div``
+const PriceInfo = styled.div`
+  text-align: right;
 `
 
 const YourCollection: React.FC<Props> = ({ assetsR }) => {
@@ -52,16 +80,31 @@ const YourCollection: React.FC<Props> = ({ assetsR }) => {
           <CollectionWrapper>
             {assets.map((asset) => {
               return (
-                <Asset
-                  style={{ backgroundColor: asset.background_color || '#fff' }}
-                  key={asset.token_id}
-                >
-                  <AssetImage
-                    alt={asset.image_url}
-                    style={{ height: '285px', width: '100%' }}
-                    src={asset.image_url}
-                  />
-                  {asset.name}
+                <Asset key={asset.token_id}>
+                  <div style={{ backgroundColor: `#${asset.background_color}` || '#fff' }}>
+                    <AssetImage
+                      alt={asset.image_url}
+                      style={{ width: '100%' }}
+                      src={asset.image_url}
+                    />
+                  </div>
+                  <AssetDetails>
+                    <div>
+                      <AssetCollection>
+                        <Text size='14px' color='grey600' weight={600}>
+                          {asset.collection.name}
+                        </Text>
+                      </AssetCollection>
+                      <Text size='14px' color='grey800' weight={600}>
+                        {asset.name}
+                      </Text>
+                    </div>
+                    <PriceInfo>
+                      <Text size='14px' color='grey600'>
+                        <FormattedMessage id='copy.price' defaultMessage='Price' />
+                      </Text>
+                    </PriceInfo>
+                  </AssetDetails>
                 </Asset>
               )
             })}
