4cfdc4d75ad04734211d252bcb7cd02050744408
plondon <philip@blockchain.com>
2021-11-04 17:33:41 -0400

    feat(nfts): add a few more to wallet options

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Marketplace/MarketForm/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Marketplace/MarketForm/index.tsx
@@ -1,29 +1,37 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import styled from 'styled-components'
 
 import { Form } from 'components/Form'
 
 import { Props as OwnProps } from '../..'
 
+const FormWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`
+
 const MarketForm: React.FC<Props> = (props: Props) => {
   return (
     <Form>
       <div>Collections</div>
-      {props.collections.map((collection) => (
-        <div key={collection.opensea_slug}>
-          <Field
-            component='input'
-            type='radio'
-            id={collection.opensea_slug}
-            name='collection'
-            value={collection.opensea_slug}
-            onChange={() =>
-              props.formActions.change('nftMarketplace', 'collection', collection.opensea_slug)
-            }
-          />
-          <label htmlFor={collection.opensea_slug}>{collection.display_name}</label>
-        </div>
-      ))}
+      <FormWrapper>
+        {props.collections.map((collection) => (
+          <div key={collection.opensea_slug}>
+            <Field
+              component='input'
+              type='radio'
+              id={collection.opensea_slug}
+              name='collection'
+              value={collection.opensea_slug}
+              onChange={() =>
+                props.formActions.change('nftMarketplace', 'collection', collection.opensea_slug)
+              }
+            />
+            <label htmlFor={collection.opensea_slug}>{collection.display_name}</label>
+          </div>
+        ))}
+      </FormWrapper>
     </Form>
   )
 }
