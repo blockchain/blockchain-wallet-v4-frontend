19f010a5411022b82451b4153f59e54c935432a3
Sean Han <shan@blockchain.com>
2021-11-19 12:49:45 -0500

    feat(styling): styled modals

--- packages/blockchain-wallet-v4-frontend/src/modals/Eth/WalletConnect/Steps/ApproveTransactionStep/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Eth/WalletConnect/Steps/ApproveTransactionStep/index.tsx
@@ -6,11 +6,16 @@ import { Button, Text } from 'blockchain-info-components'
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
-  align-items: center;
   justify-content: space-between;
   display: flex;
   flex-direction: column;
   overflow: hidden;
+`
+
+const TxBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 15px 0px 15px 0px;
 `
 
 const ApproveTransactionStep = (props) => {
@@ -23,26 +28,90 @@ const ApproveTransactionStep = (props) => {
     props.walletConnectActions.respondToTxSendRequest({ action: 'REJECT', requestDetails })
   }
 
+  const { from } = txDetails // add in later
+  const { to } = txDetails // add in later
+  const amount = txDetails.value
+  const { gas } = txDetails
+  const { gasPrice } = txDetails
+  const message = txDetails.data
+
   return (
     <Wrapper>
-      <div style={{ height: '33%' }} />
-      <div style={{ height: '33%' }}>
-        <img
-          alt='Dapp Logo'
-          height='100px'
-          width='auto'
-          style={{ marginBottom: '0.5rem' }}
-          src={sessionDetails.peerMeta.icons[0]}
-        />
-        <Text weight={600} color='grey900' size='20px' style={{ marginBottom: '1rem' }}>
-          {sessionDetails.peerMeta.name} wants to connect
-        </Text>
-        <Text weight={500} color='grey600' size='14px' style={{ marginBottom: '0.5rem' }}>
-          url
-        </Text>
-        {/* <Text weight={600} color='grey600' size='12px' style={{ marginBottom: '3.5rem' }}>
-          <pre>{JSON.stringify(txDetails, null, 2)}</pre>
-        </Text> */}
+      <div>
+        <div
+          style={{
+            alignItems: 'center',
+            display: 'flex',
+            marginBottom: '1rem'
+          }}
+        >
+          <img
+            alt='Dapp Logo'
+            height='50px'
+            width='auto'
+            src={sessionDetails.peerMeta.icons[0]}
+            style={{ marginRight: '15px' }}
+          />
+          <Text weight={600} color='grey900' size='20px'>
+            Signature Request
+          </Text>
+        </div>
+        <div>
+          <hr />
+          <TxBlock>
+            <Text weight={500} color='grey900' size='16px'>
+              From:
+            </Text>
+            <Text weight={500} color='grey900' size='16px'>
+              ETH Private Wallet
+            </Text>
+          </TxBlock>
+          <hr />
+          <TxBlock>
+            <Text weight={500} color='grey900' size='16px'>
+              To:
+            </Text>
+            <Text weight={500} color='grey900' size='16px'>
+              {sessionDetails.peerMeta.name}
+            </Text>
+          </TxBlock>
+          <hr />
+          <TxBlock>
+            <Text weight={500} color='grey900' size='16px'>
+              Amount:
+            </Text>
+            <Text weight={500} color='grey900' size='16px'>
+              {amount}
+            </Text>
+          </TxBlock>
+          <hr />
+          <TxBlock>
+            <Text weight={500} color='grey900' size='16px'>
+              Gas
+            </Text>
+            <Text weight={500} color='grey900' size='16px'>
+              {gas}
+            </Text>
+          </TxBlock>
+          <hr />
+          <TxBlock>
+            <Text weight={500} color='grey900' size='16px'>
+              Gas Price:
+            </Text>
+            <Text weight={500} color='grey900' size='16px'>
+              {gasPrice}
+            </Text>
+          </TxBlock>
+          <hr />
+          <div>
+            <Text weight={500} color='grey900' size='16px'>
+              Message from {sessionDetails.peerMeta.name}
+            </Text>
+            <Text weight={500} color='grey600' size='14px' style={{ marginTop: '10px' }}>
+              {message}
+            </Text>
+          </div>
+        </div>
       </div>
       <div style={{ height: '33%', width: '100%' }}>
         <Button
