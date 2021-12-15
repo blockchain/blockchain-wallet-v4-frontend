f404db46c6e5c72c170cb7a5ffc91497a7a3e8f5
Sean Han <shan@blockchain.com>
2021-12-15 10:24:31 -0500

    feat: simplify conditional feature flag quoteR in selectors

--- packages/blockchain-wallet-v4-frontend/src/modals/BuySell/EnterAmount/Checkout/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/modals/BuySell/EnterAmount/Checkout/selectors.ts
@@ -9,15 +9,18 @@ import { OwnProps } from '.'
 const { FORM_BS_CHECKOUT } = model.components.buySell
 
 const getData = (state: RootState, ownProps: OwnProps) => {
+  const isBuyOrder = ownProps.orderType === 'BUY'
+  const isFlexiblePricingModel = selectors.core.walletOptions
+    .getFlexiblePricingModel(state)
+    .getOrElse(false)
+
   const coin = selectors.components.buySell.getCryptoCurrency(state) || 'BTC'
   const formErrors = selectors.form.getFormSyncErrors(FORM_BS_CHECKOUT)(state)
   const paymentR = selectors.components.buySell.getPayment(state)
   const quoteR =
-    ownProps.orderType === 'BUY' &&
-    selectors.core.walletOptions.getFlexiblePricingModel(state).getOrElse(false)
+    isBuyOrder && isFlexiblePricingModel
       ? selectors.components.buySell.getBuyQuote(state)
-      : ownProps.orderType === 'BUY' &&
-        !selectors.core.walletOptions.getFlexiblePricingModel(state).getOrElse(false)
+      : isBuyOrder && !isFlexiblePricingModel
       ? selectors.components.buySell.getBSQuote(state)
       : selectors.components.buySell.getSellQuote(state)
   const ratesR = selectors.core.data.misc.getRatesSelector(coin, state)
