6e0ec3043234a13364c76a2035fc1deb1d11ecb2
Terence Buchannan <terence@blockchain.com>
2021-12-22 17:01:14 -0500

    fix(asset): correct asset sorting

--- packages/blockchain-wallet-v4-frontend/src/modals/RequestCrypto/CoinSelect/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/modals/RequestCrypto/CoinSelect/selectors.ts
@@ -1,8 +1,6 @@
-import { isEmpty, map, prop, sortBy } from 'ramda'
+import { map } from 'ramda'
 
-import { CoinfigType } from '@core/types'
 import { createDeepEqualSelector } from '@core/utils'
-import { getCoinsSortedByBalance } from 'components/Balances/selectors'
 import { selectors } from 'data'
 import { REQUEST_ACCOUNTS_SELECTOR } from 'data/coins/model/request'
 import { getCoinAccounts } from 'data/coins/selectors'
@@ -11,33 +9,55 @@ import { SwapAccountType, SwapBaseCounterTypes } from 'data/components/swap/type
 
 import { REQUEST_FORM } from '../model'
 
-const fuzzyAlgorithm = (a: string, b: string): number => {
-  if (a === b) return 0
-  let smallStr = a.toLowerCase()
-  let bigStr = b.toLowerCase()
+// https://stackoverflow.com/a/11958496
+const levDist = (s: string, t: string): number => {
+  const d: Array<Array<number>> = [] // 2d matrix
 
-  if (a.length !== b.length) {
-    if (a.length > b.length) {
-      const temp = smallStr
-      smallStr = bigStr
-      bigStr = temp
+  // Step 1
+  const n = s.length
+  const m = t.length
+
+  if (n === 0) return m
+  if (m === 0) return n
+
+  // Create an array of arrays in javascript (a descending loop is quicker)
+  for (let i = n; i >= 0; i -= 1) d[i] = []
+
+  // Step 2
+  for (let i = n; i >= 0; i -= 1) d[i][0] = i
+  for (let j = m; j >= 0; j -= 1) d[0][j] = j
+
+  // Step 3
+  for (let i = 1; i <= n; i += 1) {
+    const s_i = s.charAt(i - 1)
+
+    // Step 4
+    for (let j = 1; j <= m; j += 1) {
+      // Check the jagged ld total so far
+      if (i === j && d[i][j] > 4) return n
+
+      const t_j = t.charAt(j - 1)
+      const cost = s_i === t_j ? 0 : 1 // Step 5
+
+      // Calculate the minimum
+      let mi = d[i - 1][j] + 1
+      const b = d[i][j - 1] + 1
+      const c = d[i - 1][j - 1] + cost
+
+      if (b < mi) mi = b
+      if (c < mi) mi = c
+
+      d[i][j] = mi // Step 6
+
+      // Damerau transposition
+      if (i > 1 && j > 1 && s_i === t.charAt(j - 2) && s.charAt(i - 2) === t_j) {
+        d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost)
+      }
     }
   }
 
-  if (!isEmpty(smallStr) && bigStr.startsWith(a.substr(0, Math.floor(smallStr.length / 1.5) + 1))) {
-    return 0
-  }
-  if (isEmpty(smallStr)) return isEmpty(bigStr) ? 0 : 1
-
-  let j = 0
-  for (let i = 0; i < bigStr.length; i += 1) {
-    const char = bigStr[i]
-    const char2 = smallStr[j]
-    if (char === char2) j += 1
-    if (j >= smallStr.length) return 0.1
-  }
-
-  return bigStr.length > 1 ? 1 : 0
+  // Step 7
+  return d[n][m]
 }
 
 export const getData = createDeepEqualSelector(
@@ -47,19 +67,14 @@ export const getData = createDeepEqualSelector(
         coins: ownProps.requestableCoins,
         ...REQUEST_ACCOUNTS_SELECTOR
       } as CoinAccountSelectorType),
-    getCoinsSortedByBalance,
     selectors.modules.profile.isSilverOrAbove,
     selectors.form.getFormValues(REQUEST_FORM)
   ],
-  (accounts, sortedCoinsR, isSilverOrAbove, formValues: { coinSearch?: string }) => {
+  (accounts, isSilverOrAbove, formValues: { coinSearch?: string }) => {
     const search = formValues?.coinSearch || 'ALL'
     const prunedAccounts = [] as Array<SwapAccountType>
     const isAtLeastTier1 = isSilverOrAbove
     const lowerSearch = search.toLowerCase()
-    const sortedCoins = sortedCoinsR
-      .getOrElse([] as CoinfigType[])
-      .map((coinfig) => coinfig.symbol)
-      .reverse()
     // @ts-ignore
     const ethAccount = accounts.ETH.find(({ type }) => type === SwapBaseCounterTypes.ACCOUNT)
 
@@ -81,11 +96,21 @@ export const getData = createDeepEqualSelector(
       accounts
     )
 
-    const sortedAccounts = prunedAccounts.sort(
-      (acc1, acc2) =>
-        fuzzyAlgorithm(window.coins[acc1.coin].coinfig.name, lowerSearch) -
-        fuzzyAlgorithm(window.coins[acc2.coin].coinfig.name, lowerSearch)
-    )
+    const sortedAccounts = prunedAccounts.sort((acc1, acc2) => {
+      // if the coin names are the same, sort by descending balance
+      if (window.coins[acc1.coin].coinfig.name === window.coins[acc2.coin].coinfig.name) {
+        return Number(acc2.balance) - Number(acc1.balance)
+      }
+      // if the coin name is what the user searched put in front of list
+      if (acc1.coin.toLowerCase() === lowerSearch || acc2.coin.toLowerCase() === lowerSearch) {
+        return 100
+      }
+
+      return (
+        levDist(window.coins[acc1.coin].coinfig.name, lowerSearch) -
+        levDist(window.coins[acc2.coin].coinfig.name, lowerSearch)
+      )
+    })
 
     return { accounts: sortedAccounts, ethAccount, formValues, isAtLeastTier1 }
   }
