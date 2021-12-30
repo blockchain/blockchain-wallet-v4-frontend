f36c8b0a801f5b488afb7c16a04e500d8ab4a108
plondon <philip@blockchain.com>
2021-12-30 16:27:15 +0100

    feat(nfts): set up some events to monitor

--- packages/blockchain-wallet-v4-frontend/src/middleware/analyticsMiddleware/index.ts
+++ packages/blockchain-wallet-v4-frontend/src/middleware/analyticsMiddleware/index.ts
@@ -3269,6 +3269,72 @@ const analyticsMiddleware = () => (store) => (next) => (action) => {
         }
         break
       }
+      case actions.components.nfts.createOrderSuccess.type: {
+        const state = store.getState()
+
+        const nabuId = state.profile.userData.getOrElse({})?.id ?? null
+
+        const email = state.profile.userData.getOrElse({})?.emailVerified
+          ? state.profile.userData.getOrElse({})?.email
+          : null
+        const tier = state.profile.userData.getOrElse({})?.tiers?.current ?? null
+        analytics.push(AnalyticsKey.NFT_ORDER_SUCCESS, {
+          properties: {
+            originalTimestamp: getOriginalTimestamp(),
+            site_redirect: 'WALLET'
+          },
+          traits: {
+            email,
+            nabuId,
+            tier
+          }
+        })
+        break
+      }
+      case actions.components.nfts.createOrderFailure.type: {
+        const state = store.getState()
+
+        const nabuId = state.profile.userData.getOrElse({})?.id ?? null
+
+        const email = state.profile.userData.getOrElse({})?.emailVerified
+          ? state.profile.userData.getOrElse({})?.email
+          : null
+        const tier = state.profile.userData.getOrElse({})?.tiers?.current ?? null
+        analytics.push(AnalyticsKey.NFT_ORDER_FAILURE, {
+          properties: {
+            originalTimestamp: getOriginalTimestamp(),
+            site_redirect: 'WALLET'
+          },
+          traits: {
+            email,
+            nabuId,
+            tier
+          }
+        })
+        break
+      }
+      case actions.components.nfts.nftOrderFlowOpen.type: {
+        const state = store.getState()
+
+        const nabuId = state.profile.userData.getOrElse({})?.id ?? null
+
+        const email = state.profile.userData.getOrElse({})?.emailVerified
+          ? state.profile.userData.getOrElse({})?.email
+          : null
+        const tier = state.profile.userData.getOrElse({})?.tiers?.current ?? null
+        analytics.push(AnalyticsKey.NFT_ORDER_STARTED, {
+          properties: {
+            originalTimestamp: getOriginalTimestamp(),
+            site_redirect: 'WALLET'
+          },
+          traits: {
+            email,
+            nabuId,
+            tier
+          }
+        })
+        break
+      }
       default: {
         break
       }
