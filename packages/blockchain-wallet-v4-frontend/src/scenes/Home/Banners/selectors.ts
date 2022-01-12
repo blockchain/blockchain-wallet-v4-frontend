0f363be5630163db1c041203016ebdb94dc6eea6
Andrew Schneider <andrew@blockchain.com>
2022-01-12 08:30:08 -0500

    feat(ceur): remove kyc tier check

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/selectors.ts
@@ -92,7 +92,6 @@ export const getData = (state: RootState): { bannerToShow: BannerType } => {
   const showCEURBanner =
     showBanner(cEURAnnouncement, cEURAnnouncementAnnouncement, announcementState) &&
     userData &&
-    userData.tiers?.current >= 1 &&
     userData.address &&
     userData.address.country &&
     ['US', 'DE', 'IT', 'FR', 'NL'].indexOf(userData.address.country) === -1
