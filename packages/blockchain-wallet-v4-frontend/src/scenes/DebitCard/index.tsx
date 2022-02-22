76317f0bd342edb7eeb1b35e4eeef105b012c166
Julian Romero <julianromero@blockchain.com>
2022-02-22 13:16:01 -0300

    fix: remove unnecesary withRouter

--- packages/blockchain-wallet-v4-frontend/src/scenes/DebitCard/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/DebitCard/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect, ConnectedProps } from 'react-redux'
-import { withRouter } from 'react-router-dom'
 import { bindActionCreators, Dispatch } from 'redux'
 
 import { actions } from 'data'
@@ -17,4 +16,4 @@ const connector = connect(undefined, mapDispatchToProps)
 
 export type Props = ConnectedProps<typeof connector>
 
-export default withRouter(connector(DebitCardContainer))
+export default connector(DebitCardContainer)
