10e9cf000ece1f617d276156192a44704303a9d0
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-03-04 19:51:55 -0300

    feat(Tabs): add tabs

--- packages/blockchain-wallet-v4-frontend/src/components/Card/Card.tsx
+++ packages/blockchain-wallet-v4-frontend/src/components/Card/Card.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import styled, { css } from 'styled-components'
 
-import { Container } from './styles'
-import { CardComponent } from './types'
+import { CardComponent, CardProps } from './types'
 
-export const Card: CardComponent = ({ children }) => {
-  return <Container>{children}</Container>
-}
+export const Card: CardComponent = styled.div<CardProps>`
+  ${({ theme }) => css`
+    border: 1px solid ${theme.grey100};
+    border-radius: 12px;
+  `}
+`
---
+++ packages/blockchain-wallet-v4-frontend/src/components/Tabs/Tabs/index.ts
@@ -0,0 +1,2 @@
+export { Tabs } from './Tabs'
+export type { TabsComponent, TabsProps } from './types'
