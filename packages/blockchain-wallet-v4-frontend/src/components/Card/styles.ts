cdd9efba9bef4bedb6e08df8fb71a500e6a71d01
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-03-04 19:51:55 -0300

    feat(Tabs): add tabs

--- packages/blockchain-wallet-v4-frontend/src/components/Card/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled, { css } from 'styled-components'
-
-export const Container = styled.div`
-  ${({ theme }) => css`
-    border: 1px solid ${theme.grey100};
-    border-radius: 12px;
-  `}
-`
