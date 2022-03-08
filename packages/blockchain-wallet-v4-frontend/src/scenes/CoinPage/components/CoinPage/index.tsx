32bef0f2b3227bfd50794973687e016cee485453
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2022-03-08 14:21:39 -0800

    feat(coin page): add redux and coin specific chart data to template

--- packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/index.tsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { memo, useEffect } from 'react'
+import { connect, ConnectedProps } from 'react-redux'
+import { bindActionCreators } from '@reduxjs/toolkit'
+
+import { TimeRange } from '@core/types'
+import { actions } from 'data'
 
 import { AboutSection } from '../AboutSection'
 import { CoinPage } from './CoinPage'
+import { getData } from './selectors'
+import { CoinPageContainerComponent } from './types'
 
 export type { CoinPageComponent, CoinPageProps } from './types'
 
-const CoinPageContainer = () => {
-  return (
-    <CoinPage
-      about={<AboutSection content='some stuff' title='Bitcoin' actions={[<></>]} />}
-      chart={undefined}
-      header={undefined}
-    />
-  )
-}
+const CoinPageContainer: CoinPageContainerComponent<Props> = memo(
+  ({ coin, data, priceChartActions }) => {
+    useEffect(() => {
+      priceChartActions.initialized(coin, TimeRange.WEEK)
+    }, [coin, priceChartActions])
 
-export default CoinPageContainer
+    return data.cata({
+      Failure: () => <></>,
+      Loading: () => <></>,
+      NotAsked: () => <></>,
+      Success: (value) => (
+        <CoinPage
+          about={<AboutSection content='' title={coin} actions={[<></>]} />}
+          chart={JSON.stringify(value)}
+          header={undefined}
+        />
+      )
+    })
+  }
+)
+
+const mapStateToProps = (state, ownProps) => getData(state, ownProps)
+const mapDispatchToProps = (dispatch) => ({
+  priceChartActions: bindActionCreators(actions.components.priceChart, dispatch)
+})
+const connector = connect(mapStateToProps, mapDispatchToProps)
+
+type Props = ConnectedProps<typeof connector>
+
+export default connector(CoinPageContainer)
