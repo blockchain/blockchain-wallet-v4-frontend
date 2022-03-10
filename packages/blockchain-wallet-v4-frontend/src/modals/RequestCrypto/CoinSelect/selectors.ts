83625beb83b253ff5920a1ba17114109eb5f5a32
plondon <philip@blockchain.com>
2022-03-10 10:12:03 -0500

    feat(request): order by balance

--- packages/blockchain-wallet-v4-frontend/src/modals/RequestCrypto/CoinSelect/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/modals/RequestCrypto/CoinSelect/selectors.ts
@@ -1,6 +1,8 @@
 import { map } from 'ramda'
 
+import { CoinfigType } from '@core/types'
 import { createDeepEqualSelector } from '@core/utils'
+import { getCoinsSortedByBalance } from 'components/Balances/selectors'
 import { selectors } from 'data'
 import { REQUEST_ACCOUNTS_SELECTOR } from 'data/coins/model/request'
 import { getCoinAccounts } from 'data/coins/selectors'
@@ -17,16 +19,26 @@ export const getData = createDeepEqualSelector(
         coins: ownProps.requestableCoins,
         ...REQUEST_ACCOUNTS_SELECTOR
       } as CoinAccountSelectorType),
+    getCoinsSortedByBalance,
     selectors.modules.profile.isSilverOrAbove,
     selectors.form.getFormValues(REQUEST_FORM)
   ],
-  (accounts, isSilverOrAbove, formValues: { coinSearch?: string }) => {
+  (
+    accounts,
+    sortedCoinsR: ReturnType<typeof getCoinsSortedByBalance>,
+    isSilverOrAbove,
+    formValues: { coinSearch?: string }
+  ) => {
     const search = formValues?.coinSearch || 'ALL'
     const prunedAccounts = [] as Array<SwapAccountType>
     const isAtLeastTier1 = isSilverOrAbove
     const lowerSearch = search.toLowerCase()
     // @ts-ignore
     const ethAccount = accounts.ETH.find(({ type }) => type === SwapBaseCounterTypes.ACCOUNT)
+    const sortedCoins = sortedCoinsR
+      .getOrElse([] as CoinfigType[])
+      .map(({ symbol }) => symbol)
+      .reverse()
 
     // @ts-ignore
     map(
@@ -46,21 +58,25 @@ export const getData = createDeepEqualSelector(
       accounts
     )
 
-    const sortedAccounts = prunedAccounts.sort((acc1, acc2) => {
-      // if the coin names are the same, sort by descending balance
-      if (window.coins[acc1.coin].coinfig.name === window.coins[acc2.coin].coinfig.name) {
-        return Number(acc2.balance) - Number(acc1.balance)
-      }
-      // if the coin name is what the user searched put in front of list
-      if (acc1.coin.toLowerCase() === lowerSearch || acc2.coin.toLowerCase() === lowerSearch) {
-        return 100
-      }
+    const sortedAccounts = prunedAccounts
+      .sort((acc1, acc2) => {
+        // if the coin names are the same, sort by descending balance
+        if (window.coins[acc1.coin].coinfig.name === window.coins[acc2.coin].coinfig.name) {
+          return Number(acc2.balance) - Number(acc1.balance)
+        }
+        // if the coin name is what the user searched put in front of list
+        if (acc1.coin.toLowerCase() === lowerSearch || acc2.coin.toLowerCase() === lowerSearch) {
+          return 100
+        }
 
-      return (
-        levenshteinDistanceSearch(window.coins[acc1.coin].coinfig.name, lowerSearch) -
-        levenshteinDistanceSearch(window.coins[acc2.coin].coinfig.name, lowerSearch)
-      )
-    })
+        return (
+          levenshteinDistanceSearch(window.coins[acc1.coin].coinfig.name, lowerSearch) -
+          levenshteinDistanceSearch(window.coins[acc2.coin].coinfig.name, lowerSearch)
+        )
+      })
+      .sort((acc1, acc2) => {
+        return sortedCoins.indexOf(acc1.coin) < sortedCoins.indexOf(acc2.coin) ? 1 : -1
+      })
 
     return { accounts: sortedAccounts, ethAccount, formValues, isAtLeastTier1 }
   }
