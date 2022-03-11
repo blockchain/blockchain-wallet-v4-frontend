d4fc6a06bbcf86951e0571c2499d8695d32b76e3
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2022-03-10 17:24:47 -0800

    feat(coin page): add charge and data to coin page

--- packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/index.tsx
@@ -1,12 +1,16 @@
-import React, { memo, useEffect, useMemo } from 'react'
+import React, { memo, useCallback, useEffect, useMemo, useState } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
+import { colors } from '@blockchain-com/constellation'
 import { bindActionCreators } from '@reduxjs/toolkit'
+import moment, { MomentInput } from 'moment'
 
 import { TimeRange } from '@core/types'
+import { Tab, Tabs } from 'components/Tabs'
 import { actions } from 'data'
 
 import { CoinHeader } from '..'
 import { AboutSection } from '../AboutSection'
+import { CoinChart } from '../CoinChart'
 import { CoinPage } from './CoinPage'
 import { getData } from './selectors'
 import { CoinPageContainerComponent } from './types'
@@ -16,28 +20,81 @@ export type { CoinPageComponent, CoinPageProps } from './types'
 const CoinPageContainer: CoinPageContainerComponent<Props> = memo(
   ({ coin, data, priceChartActions }) => {
     const displayName = useMemo(() => window.coins[coin].coinfig.name, [coin])
+    const [selectedTab, setSelectedTab] = useState<TimeRange>(TimeRange.DAY)
 
     useEffect(() => {
       priceChartActions.initialized(coin, TimeRange.WEEK)
     }, [coin, priceChartActions])
 
+    const handleTabChange = useCallback(
+      (timeRange: TimeRange) => {
+        setSelectedTab(timeRange)
+        priceChartActions.initialized(coin, timeRange)
+      },
+      [coin, priceChartActions]
+    )
+
     return data.cata({
       Failure: () => <></>,
       Loading: () => <>Loading...</>,
       NotAsked: () => <>Not Asked</>,
-      Success: (value) => (
-        <CoinPage
-          about={<AboutSection content='' title={coin} actions={[<></>]} />}
-          chart={JSON.stringify(value)}
-          header={
-            <CoinHeader
-              coinCode={coin}
-              coinDescription="The internet's first and largest crypto currency."
-              coinName={displayName}
-            />
-          }
-        />
-      )
+      Success: (value) => {
+        return (
+          <CoinPage
+            chartTabs={
+              <Tabs>
+                <Tab
+                  onClick={() => handleTabChange(TimeRange.DAY)}
+                  selected={selectedTab === TimeRange.DAY}
+                >
+                  1D
+                </Tab>
+
+                <Tab
+                  onClick={() => handleTabChange(TimeRange.WEEK)}
+                  selected={selectedTab === TimeRange.WEEK}
+                >
+                  1W
+                </Tab>
+
+                <Tab
+                  onClick={() => handleTabChange(TimeRange.MONTH)}
+                  selected={selectedTab === TimeRange.MONTH}
+                >
+                  1M
+                </Tab>
+
+                <Tab
+                  onClick={() => handleTabChange(TimeRange.YEAR)}
+                  selected={selectedTab === TimeRange.YEAR}
+                >
+                  1Y
+                </Tab>
+
+                <Tab
+                  onClick={() => handleTabChange(TimeRange.ALL)}
+                  selected={selectedTab === TimeRange.ALL}
+                >
+                  All
+                </Tab>
+              </Tabs>
+            }
+            about={<AboutSection content='' title={coin} actions={[<></>]} />}
+            chart={
+              <CoinChart
+                data={value.data}
+                backgroundColor={colors.white060}
+                primaryColor={colors.blue600}
+                textColor={colors.grey400}
+                x={0}
+                y={1}
+                xFormatter={(date) => moment(date as MomentInput).format('hh:mm')}
+              />
+            }
+            header={<CoinHeader coinCode={coin} coinDescription='' coinName={displayName} />}
+          />
+        )
+      }
     })
   }
 )
