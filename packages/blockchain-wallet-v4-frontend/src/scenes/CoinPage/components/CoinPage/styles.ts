b3c7c7afba4f283cdf06d1f2a142563f94d04d02
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2022-03-11 10:48:34 -0800

    feat(coin page): connect real data to chart

--- packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/styles.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex: 1;
+  justify-content: space-between;
 `
 
 export const Grid = styled.div`
