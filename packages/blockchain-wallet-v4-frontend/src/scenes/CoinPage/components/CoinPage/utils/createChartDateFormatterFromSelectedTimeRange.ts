961a01bc9f4aff2ef0cd01a3ce700deafebe4d49
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-03-14 18:32:48 -0300

    feat(CoinView): add formats to chart
    This adds the formatting for all chart
    queries

--- packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/utils/createChartDateFormatterFromSelectedTimeRange.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/utils/createChartDateFormatterFromSelectedTimeRange.ts
@@ -5,11 +5,11 @@ import { TimeRange } from '@core/types'
 type DateFormatter = (date: number) => string
 
 const dateFormatters: Record<TimeRange, DateFormatter> = {
-  [TimeRange.ALL]: (date) => moment(date).format('DD/MM/YYYY'),
-  [TimeRange.DAY]: (date) => moment(date).format('HH:mm'),
-  [TimeRange.MONTH]: (date) => moment(date).format('DD/MM/YYYY'),
-  [TimeRange.WEEK]: (date) => moment(date).format('DD/MM/YYYY'),
-  [TimeRange.YEAR]: (date) => moment(date).format('DD/MM/YYYY')
+  [TimeRange.ALL]: (date) => moment(date).format('YYYY'),
+  [TimeRange.DAY]: (date) => moment(date).format('h:mm a').toUpperCase(),
+  [TimeRange.MONTH]: (date) => moment(date).format('MMM D'),
+  [TimeRange.WEEK]: (date) => moment(date).format('MMM D'),
+  [TimeRange.YEAR]: (date) => moment(date).format('MMMM')
 }
 
 export const createDateFormatterFromSelectedTimeRange = (timeRange: TimeRange): DateFormatter =>
