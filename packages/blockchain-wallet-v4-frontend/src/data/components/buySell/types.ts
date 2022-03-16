7251476fe00697fd773c8e8a6e75441606445784
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2022-03-15 17:09:31 -0700

    feat(coin page): connecting Holdings action buttons

--- packages/blockchain-wallet-v4-frontend/src/data/components/buySell/types.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/components/buySell/types.ts
@@ -97,6 +97,7 @@ export enum BuySellStepType {
   'VERIFY_EMAIL'
 }
 export type BSShowModalOriginType =
+  | 'CoinPageHoldings'
   | 'CompleteProfileBanner'
   | 'CompleteProfile'
   | 'EmptyFeed'
--- packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/model.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/CoinPage/model.tsx
@@ -1,22 +1,36 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useCallback } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Icon } from '@blockchain-com/constellation'
 import { product } from 'ramda'
 
-import { CoinfigType } from '@core/types'
+import { CoinfigType, CoinType } from '@core/types'
 import { Button } from 'blockchain-info-components'
 import { Flex } from 'components/Flex'
 
 export const HoldingsCardActions = ({
   amount,
-  products
+  buyButtonCallback,
+  products,
+  receiveButtonCallback,
+  sellButtonCallback,
+  sendButtonCallback
 }: {
   amount: number
+  buyButtonCallback: () => void
   products: CoinfigType['products']
+  receiveButtonCallback: () => void
+  sellButtonCallback: () => void
+  sendButtonCallback: () => void
 }): [ReactElement, ReactElement] => {
   const isBroke = amount <= 0
   const SendButton = (): ReactElement => (
-    <Button nature='primary' data-e2e='sendButton' fullwidth disabled={isBroke}>
+    <Button
+      nature='primary'
+      data-e2e='sendButton'
+      fullwidth
+      disabled={isBroke}
+      onClick={sendButtonCallback}
+    >
       <Flex gap={10} alignItems='center'>
         <Icon name='send' color='white900' size='sm' />
         <FormattedMessage id='buttons.send' defaultMessage='Send' />
@@ -24,7 +38,7 @@ export const HoldingsCardActions = ({
     </Button>
   )
   const ReceiveButton = (): ReactElement => (
-    <Button nature='dark-grey' data-e2e='receiveButton' fullwidth>
+    <Button nature='dark-grey' data-e2e='receiveButton' fullwidth onClick={receiveButtonCallback}>
       <Flex gap={10} alignItems='center'>
         <Icon color='white900' name='qrCode' size='sm' />
         <FormattedMessage id='buttons.receive' defaultMessage='Receive' />
@@ -32,7 +46,7 @@ export const HoldingsCardActions = ({
     </Button>
   )
   const BuyButton = (): ReactElement => (
-    <Button nature='primary' key={1} data-e2e='buyButton' fullwidth>
+    <Button nature='primary' key={1} data-e2e='buyButton' fullwidth onClick={buyButtonCallback}>
       <Flex gap={10} alignItems='center'>
         <Icon name='cart' color='white900' size='sm' />
         <FormattedMessage id='buttons.buy' defaultMessage='Buy' />
@@ -40,7 +54,14 @@ export const HoldingsCardActions = ({
     </Button>
   )
   const SellButton = (): ReactElement => (
-    <Button nature='dark-grey' key={1} data-e2e='sellButton' fullwidth disabled={isBroke}>
+    <Button
+      nature='dark-grey'
+      key={1}
+      data-e2e='sellButton'
+      fullwidth
+      disabled={isBroke}
+      onClick={sellButtonCallback}
+    >
       <Flex gap={10} alignItems='center'>
         <Icon name='sell' color='white900' size='sm' />
         <FormattedMessage id='buttons.sell' defaultMessage='Sell' />
