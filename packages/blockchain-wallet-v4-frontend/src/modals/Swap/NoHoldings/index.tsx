9b37c0a0d12b6283e7cc05fe9181809cbca14708
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2022-03-24 12:36:32 -0700

    fix(swap): dont show all payment methods when user is not gold tier

--- packages/blockchain-wallet-v4-frontend/src/modals/Swap/NoHoldings/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Swap/NoHoldings/index.tsx
@@ -7,7 +7,9 @@ import styled, { css } from 'styled-components'
 import { Icon, Text } from 'blockchain-info-components'
 import { SuccessCartridge } from 'components/Cartridge'
 import { FlyoutWrapper } from 'components/Flyout'
-import { actions } from 'data'
+import { actions, selectors } from 'data'
+import { RootState } from 'data/rootReducer'
+import { UserDataType } from 'data/types'
 
 import { Border, IconBackground, TopText } from '../components'
 import { Props as BaseProps } from '../index'
@@ -55,7 +57,7 @@ const DisplayContainer = styled(FlyoutWrapper)`
     `}
 `
 
-const NoHoldings: React.FC<Props> = ({ buySellActions, handleClose, modalActions }) => {
+const NoHoldings: React.FC<Props> = ({ buySellActions, handleClose, modalActions, userData }) => {
   return (
     <>
       <FlyoutWrapper>
@@ -76,81 +78,83 @@ const NoHoldings: React.FC<Props> = ({ buySellActions, handleClose, modalActions
         <Text size='16px' color='grey800' weight={600} style={{ marginTop: '24px' }}>
           <FormattedMessage
             id='swap.no_holdings.own_some_first'
-            defaultMessage='But you’ll need to own some first...'
+            defaultMessage="But you'll need to own some first..."
           />
         </Text>
         <Text size='14px' color='grey500' weight={500}>
           <FormattedMessage
             id='swap.no_holdings.get_started'
-            defaultMessage='You’re all set to Swap but we don’t see any crypto in this Wallet. Below are 3 ways to get started.'
+            defaultMessage="You're all set to Swap but we don't see any crypto in this Wallet. Below are ways to get started."
           />
         </Text>
       </FlyoutWrapper>
 
-      <Border />
+      {userData?.tiers?.current >= 2 && (
+        <>
+          <Border />
 
-      <DisplayContainer
-        onClick={() => {
-          buySellActions.showModal({ origin: 'SwapNoHoldings' })
-        }}
-      >
-        <FlexTopRow>
-          <IconBackground size='32px' style={{ marginRight: '16px' }}>
-            <IconContainer>
-              <Icon size='16px' color='blue600' name='credit-card-sb' />
-            </IconContainer>
-          </IconBackground>
-          <div>
-            <Text color='grey900' size='14px' weight={600} lineHeight='150%'>
-              <FormattedMessage
-                id='swap.no_holdings.buy_with_card.title'
-                defaultMessage='Buy with a Card'
-              />
-            </Text>
-            <Text size='12px' lineHeight='150%' weight={500}>
-              <FormattedMessage
-                id='swap.no_holdings.buy_with_card.description'
-                defaultMessage='Instantly buy crypto with any Visa or Mastercard. You will need to verify your identiy to link a card'
-              />
-            </Text>
-            <CartridgeContainer>
-              <SuccessCartridge>
-                <FormattedMessage id='copy.most_popular' defaultMessage='Most Popular' />
-              </SuccessCartridge>
-            </CartridgeContainer>
-          </div>
-        </FlexTopRow>
-      </DisplayContainer>
-
-      <Border />
-
-      <DisplayContainer
-        onClick={() => {
-          buySellActions.showModal({ origin: 'SwapNoHoldings' })
-        }}
-      >
-        <FlexTopRow>
-          <IconBackground size='32px' style={{ marginRight: '16px' }}>
-            <IconContainer>
-              <Icon size='16px' color='blue600' name='bank-filled' />
-            </IconContainer>
-          </IconBackground>
-          <div>
-            <Text color='grey900' size='14px' weight={600} lineHeight='150%'>
-              <FormattedMessage
-                id='swap.no_holdings.buy_with_bank.title'
-                defaultMessage='Buy with a Bank Deposit'
-              />
-            </Text>
-            <Text size='12px' lineHeight='150%' weight={500}>
-              <FormattedMessage
-                id='swap.no_holdings.buy_with_bank.description'
-                defaultMessage='Send cash directly from your bank. Once received, we’ll use use that balance to buy the crypto of your choice.'
-              />
-            </Text>
-          </div>
-        </FlexTopRow>
-      </DisplayContainer>
+          <DisplayContainer
+            onClick={() => {
+              buySellActions.showModal({ origin: 'SwapNoHoldings' })
+            }}
+          >
+            <FlexTopRow>
+              <IconBackground size='32px' style={{ marginRight: '16px' }}>
+                <IconContainer>
+                  <Icon size='16px' color='blue600' name='credit-card-sb' />
+                </IconContainer>
+              </IconBackground>
+              <div>
+                <Text color='grey900' size='14px' weight={600} lineHeight='150%'>
+                  <FormattedMessage
+                    id='swap.no_holdings.buy_with_card.title'
+                    defaultMessage='Buy with a Card'
+                  />
+                </Text>
+                <Text size='12px' lineHeight='150%' weight={500}>
+                  <FormattedMessage
+                    id='swap.no_holdings.buy_with_card.description'
+                    defaultMessage='Instantly buy crypto with any Visa or Mastercard. You will need to verify your identiy to link a card'
+                  />
+                </Text>
+                <CartridgeContainer>
+                  <SuccessCartridge>
+                    <FormattedMessage id='copy.most_popular' defaultMessage='Most Popular' />
+                  </SuccessCartridge>
+                </CartridgeContainer>
+              </div>
+            </FlexTopRow>
+          </DisplayContainer>
+          <Border />
+          <DisplayContainer
+            onClick={() => {
+              buySellActions.showModal({ origin: 'SwapNoHoldings' })
+            }}
+          >
+            <FlexTopRow>
+              <IconBackground size='32px' style={{ marginRight: '16px' }}>
+                <IconContainer>
+                  <Icon size='16px' color='blue600' name='bank-filled' />
+                </IconContainer>
+              </IconBackground>
+              <div>
+                <Text color='grey900' size='14px' weight={600} lineHeight='150%'>
+                  <FormattedMessage
+                    id='swap.no_holdings.buy_with_bank.title'
+                    defaultMessage='Buy with a Bank Deposit'
+                  />
+                </Text>
+                <Text size='12px' lineHeight='150%' weight={500}>
+                  <FormattedMessage
+                    id='swap.no_holdings.buy_with_bank.description'
+                    defaultMessage="Send cash directly from your bank. Once received, we'll use use that balance to buy the crypto of your choice."
+                  />
+                </Text>
+              </div>
+            </FlexTopRow>
+          </DisplayContainer>
+        </>
+      )}
 
       <Border />
 
@@ -193,16 +197,24 @@ const NoHoldings: React.FC<Props> = ({ buySellActions, handleClose, modalActions
   )
 }
 
+const mapStateToProps = (state: RootState): LinkStatePropsType => ({
+  userData: selectors.modules.profile.getUserData(state).getOrElse({} as UserDataType)
+})
+
 const mapDispatchToProps = (dispatch) => ({
   buySellActions: bindActionCreators(actions.components.buySell, dispatch),
   modalActions: bindActionCreators(actions.modals, dispatch)
 })
 
-const connector = connect(undefined, mapDispatchToProps)
+const connector = connect(mapStateToProps, mapDispatchToProps)
 
 const enhance = compose(connector)
 
 type OwnProps = BaseProps & { handleClose: () => void }
+
+type LinkStatePropsType = {
+  userData: UserDataType
+}
 
 export type Props = OwnProps & ConnectedProps<typeof connector>
 
