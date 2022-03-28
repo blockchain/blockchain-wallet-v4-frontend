e8c3c5c13c9b3d462c5a4f8ecaa4f23ddc9adf50
Ryan S <rsteubs@blockchain.com>
2022-03-28 11:35:09 -0700

    feat(nfts): styling

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/components/workInProgressBanner.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/components/workInProgressBanner.tsx
@@ -2,26 +2,26 @@ import React from 'react'
 import { connect, ConnectedProps } from 'react-redux'
 import styled from 'styled-components'
 
+import { Text } from 'blockchain-info-components'
+
 const ItemWrapper = styled.div`
   margin: 1em 0em;
   display: block;
   align-items: center;
-  border-color: rgb(242, 153, 74);
-  background: rgb(239 166 101);
+  background: ${(props) => props.theme.orange400};
   border-radius: 8px;
   padding: 1em;
 `
-const Title = styled.div`
-  color: white;
-  size: 24px;
+const TitleText = styled(Text)`
   text-align: center;
-  font-family: Inter, sans-serif;
 `
 const WorkInProgressComponent: React.FC<Props> = () => {
   return (
     <>
       <ItemWrapper>
-        <Title>NFT BETA: Work in progress, you may notice some intermittent issues.</Title>
+        <TitleText size='18px' color='white'>
+          NFT BETA: Work in progress, you may notice some intermittent issues.
+        </TitleText>
       </ItemWrapper>
     </>
   )
