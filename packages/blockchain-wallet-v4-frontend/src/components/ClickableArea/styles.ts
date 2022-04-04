ab7a2c3fdac922fda6fd615e43f9c481381caa83
blockdylanb <57680122+blockdylanb@users.noreply.github.com>
2022-04-04 16:23:53 -0700

    feat(coin page): add subtle animation to standard row clickable area and add new coin route

--- packages/blockchain-wallet-v4-frontend/src/components/ClickableArea/styles.ts
+++ packages/blockchain-wallet-v4-frontend/src/components/ClickableArea/styles.ts
@@ -6,11 +6,17 @@ type ContainerProps = {
 export const Container = styled.div<ContainerProps>`
   ${({ showBackgroundOnHover, theme }) => css`
     cursor: pointer;
+    & > div {
+      transition: transform 0.05s;
+    }
 
     ${showBackgroundOnHover &&
     css`
       &:hover {
         background-color: ${theme.grey000};
+      }
+      &:active > div {
+        transform: scale(0.98);
       }
     `}
   `}
