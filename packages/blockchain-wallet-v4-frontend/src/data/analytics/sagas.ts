91faa51adff55b440a58361e99dc23ec176bbe54
Andrew Schneider <andrew@blockchain.com>
2022-04-05 13:54:03 +0100

    fix(analytics): dont assume nabu and email exist

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/sagas.ts
@@ -6,8 +6,8 @@ import analytics from './analytics'
 import { trackEvent as trackEventAction } from './slice'
 
 export const trackEvent = function* ({ payload }: ReturnType<typeof trackEventAction>) {
-  const nabuId = (yield select(selectors.core.kvStore.userCredentials.getUserId)).getOrFail()
-  const email = yield select(selectors.core.settings.getEmailVerified)
+  const nabuId = (yield select(selectors.core.kvStore.userCredentials.getUserId)).getOrElse('')
+  const email = (yield select(selectors.core.settings.getEmailVerified)).getOrElse('')
   const tiersState = (yield select(selectors.modules.profile.getTiers)).getOrElse({})
   const originalTimestamp = new Date().toISOString()
   const properties = { originalTimestamp, ...payload.properties }
