0a134607c9f4b491924e3d191525f16360a2fa17
plondon <philip@blockchain.com>
2022-04-05 10:59:36 -0400

    feat(nfts): display vol in fiat

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Explore/TrendingCollectionsTable/volume.column.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Explore/TrendingCollectionsTable/volume.column.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
+import { convertCoinToCoin } from '@core/exchange'
+import FiatDisplay from 'components/Display/FiatDisplay'
 import { CellHeaderText, CellText } from 'components/Table'
 
 export const getVolumeColumn = () => ({
   Cell: ({ row: { original: values } }) => {
-    return <CellText>{values.one_day_volume}</CellText>
+    return (
+      <CellText>
+        <FiatDisplay weight={600} color='black' size='14px' coin='ETH' currency='USD'>
+          {convertCoinToCoin({ baseToStandard: false, coin: 'ETH', value: values.one_day_volume })}
+        </FiatDisplay>
+      </CellText>
+    )
   },
   Header: () => (
     <CellHeaderText>
