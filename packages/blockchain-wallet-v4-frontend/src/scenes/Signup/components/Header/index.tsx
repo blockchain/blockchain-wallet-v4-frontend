8fe84479bec801d9430ebe883b282d8feba6c245
Fernando Gonzalez <fgonzalez@blockchain.com>
2022-04-11 09:45:24 -0300

    fix conditions from texts translations if latam

--- packages/blockchain-wallet-v4-frontend/src/scenes/Signup/components/Header/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Signup/components/Header/index.tsx
@@ -42,13 +42,12 @@ const Header = (props: SubviewProps) => (
   <Wrapper>
     <Column>
       <HeaderText color='whiteFade900' weight={700} data-e2e='signupSecureHeader'>
-        {!props.isLatam && (
+        {!props.isLatam ? (
           <FormattedMessage
             id='scenes.register.securelybuy'
             defaultMessage='Securely Buy, Sell, and Store Crypto.'
           />
-        )}
-        {props.isLatam && (
+        ) : (
           <FormattedMessage
             id='scenes.register.sendreceiveearn'
             defaultMessage='Send, receive and get up to 14% profit with your cryptos.'
@@ -57,13 +56,12 @@ const Header = (props: SubviewProps) => (
       </HeaderText>
 
       <SubHeader color='whiteFade800' weight={500} data-e2e='signupSecureSubHeader'>
-        {!props.isLatam && (
+        {!props.isLatam ? (
           <FormattedMessage
             id='scenes.register.getstarted'
             defaultMessage='Get Started by Signing Up Now.'
           />
-        )}
-        {props.isLatam && (
+        ) : (
           <FormattedMessage
             id='scenes.register.getstartedandverify'
             defaultMessage='Get Started by Signing Up and verify your profile.'
