e81582e72f036e737783e5f959f38607ce7f11ee
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-04-18 16:05:24 -0300

    feat(CoinView): remove the selectors from wallet card

---
+++ packages/blockchain-wallet-v4-frontend/src/hooks/useWalletsForCoin/useWalletsForCoin.types.ts
@@ -0,0 +1,34 @@
+import { RemoteHookState } from '../useRemote'
+
+export type AddressData = {
+  data: {
+    label: string
+    options: {
+      label: string
+      value: {
+        address: string
+        balance: number
+        coin: string
+        label: string
+        type: string
+      }
+    }[]
+    value: string
+  }[]
+}
+
+export type WalletsForCoinHookData = {
+  address: string
+  balance: number
+  coin: string
+  label: string
+  type: string
+}[]
+
+export type WalletsForCoinHookProps = {
+  coin: string
+}
+
+export type WalletsForCoinHook = (
+  props: WalletsForCoinHookProps
+) => RemoteHookState<string, WalletsForCoinHookData>
