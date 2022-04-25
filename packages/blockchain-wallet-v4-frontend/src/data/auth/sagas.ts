5c3ca1a82681f01726285941a7331740dcaa44ac
Andrew Schneider <andrew@blockchain.com>
2022-04-25 13:16:41 -0400

    hotfix(exchange): add captcha to exchange institutional login

--- packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/auth/sagas.ts
@@ -78,7 +78,7 @@ export default ({ api, coreSagas, networks }) => {
   }
 
   const exchangeLogin = function* (action) {
-    const { code, password, username } = action.payload
+    const { captchaToken, code, password, username } = action.payload
     const { redirect, userType } = yield select(selectors.auth.getProductAuthMetadata)
     const unificationFlowType = yield select(selectors.auth.getAccountUnificationFlowType)
     const { platform } = yield select(selectors.auth.getProductAuthMetadata)
@@ -101,7 +101,7 @@ export default ({ api, coreSagas, networks }) => {
     }
     // start signin flow
     try {
-      const response = yield call(api.exchangeSignIn, code, password, username)
+      const response = yield call(api.exchangeSignIn, captchaToken, code, password, username)
       const { csrfToken, sessionExpirationTime, token: jwtToken } = response
       yield put(actions.auth.setJwtToken(jwtToken))
       // determine login flow
@@ -782,6 +782,7 @@ export default ({ api, coreSagas, networks }) => {
         // i.e. creating a new wallet and merging it to their exchange account
         yield put(
           actions.auth.exchangeLogin({
+            captchaToken,
             code: exchangeTwoFA,
             password: exchangePassword,
             username: exchangeEmail
