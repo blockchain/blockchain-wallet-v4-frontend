03a4c1c2d12a9f1bbf9722e6890b6797ee099435
Ryan S <rsteubs@blockchain.com>
2022-04-29 14:14:27 -0700

    feat: add token symbol

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Offers/OffersTableColumns/OffersTableColumns.price.column.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Offers/OffersTableColumns/OffersTableColumns.price.column.tsx
@@ -2,20 +2,25 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
 import CoinDisplay from 'components/Display/CoinDisplay'
+import { Flex } from 'components/Flex'
 import { CellHeaderText, CellText } from 'components/Table'
+import { CoinIcon } from 'layouts/Wallet/components'
 
 export const getPriceColumn = () => ({
   Cell: ({ row: { original: offer } }) => {
     return (
       <CellText>
-        <CoinDisplay
-          coin={offer.payment_token_contract.symbol}
-          weight={600}
-          size='14px'
-          color='grey900'
-        >
-          {offer.base_price}
-        </CoinDisplay>
+        <Flex gap={6} alignItems='center'>
+          <CoinIcon name={offer.payment_token_contract.symbol} />
+          <CoinDisplay
+            coin={offer.payment_token_contract.symbol}
+            weight={600}
+            size='14px'
+            color='grey900'
+          >
+            {offer.base_price}
+          </CoinDisplay>
+        </Flex>
       </CellText>
     )
   },
