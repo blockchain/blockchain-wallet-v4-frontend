7d682cd95c3fa5ad2ea57ae7c9dd1d9063fd006c
Ryan S <97060047+rsteubs-bc@users.noreply.github.com>
2022-05-06 17:32:48 -0700

    Update Asset.tsx

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Asset/Asset.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Asset/Asset.tsx
@@ -874,6 +874,90 @@ const NftAsset: React.FC<Props> = ({
               ) : null}
             </RightColWrapper>
           </Top>
+                    <div style={{ display: 'flex' }}>
+            <MoreAssets>
+              <div
+                style={{
+                  alignItems: 'center',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  marginTop: '40px'
+                }}
+              >
+                <Text color={colors.grey700} weight={600} capitalize>
+                  More from this collection
+                </Text>
+                <CustomLink to={`/nfts/collection/${currentAsset.collection?.slug}`}>
+                  <Button data-e2e='goToCollection' nature='empty-blue' padding='1em'>
+                    See All
+                  </Button>
+                </CustomLink>
+              </div>
+              <MoreAssetsList>
+                {assets?.data?.assets?.length
+                  ? assets?.data?.assets?.map((asset) => {
+                      const link = `/nfts/asset/${currentAsset.contract?.address}/${asset.token_id}`
+                      return (
+                        <MoreAssetsListItem key={asset.token_id}>
+                          <CustomLink
+                            to={link}
+                            style={{
+                              border: `1px solid ${colors.grey100}`,
+                              borderRadius: '10%',
+                              borderWidth: '1px',
+                              boxSizing: 'border-box',
+                              justifyContent: 'center',
+                              margin: '1em',
+                              padding: '10px'
+                            }}
+                          >
+                            <div>
+                              <CollectionName
+                                style={{ justifyContent: 'center', paddingBottom: 'unset' }}
+                              >
+                                <img
+                                  alt='Dapp Logo'
+                                  height='30px'
+                                  width='auto'
+                                  style={{
+                                    borderRadius: '50%',
+                                    marginBottom: '0.5rem',
+                                    paddingRight: '2px'
+                                  }}
+                                  src={asset.collection?.image_url || ''}
+                                />
+                                <div style={{ lineHeight: '2em', paddingLeft: '0.5em' }}>
+                                  {asset.collection?.name}
+                                </div>
+                              </CollectionName>
+                              <img
+                                alt='Asset Logo'
+                                width='100%'
+                                height='auto'
+                                style={{
+                                  borderRadius: '10%',
+                                  boxSizing: 'border-box',
+                                  marginBottom: '0.5rem'
+                                }}
+                                src={asset.image_url || ''}
+                              />
+                              <Text
+                                style={{ textAlign: 'center' }}
+                                size='14px'
+                                weight={600}
+                                capitalize
+                              >
+                                {asset.name || asset.token_id}
+                              </Text>
+                            </div>
+                          </CustomLink>
+                        </MoreAssetsListItem>
+                      )
+                    })
+                  : null}
+              </MoreAssetsList>
+            </MoreAssets>
+          </div>
         </div>
       </>
     </Wrapper>
