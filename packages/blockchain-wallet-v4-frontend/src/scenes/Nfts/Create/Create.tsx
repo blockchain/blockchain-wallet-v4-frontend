5d33d7eaa400fdbe3cfbcdb9447aa031cea318d2
Ryan S <rsteubs@blockchain.com>
2022-05-16 10:58:56 -0700

    feat: create page

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Create/Create.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Create/Create.tsx
@@ -8,7 +8,6 @@ import { bindActionCreators } from 'redux'
 import styled from 'styled-components'
 
 import { Button, Image, Text } from 'blockchain-info-components'
-import { Flex } from 'components/Flex'
 import { actions } from 'data'
 import { media, useMedia } from 'services/styles'
 
@@ -21,23 +20,33 @@ const Banner = styled.div`
   box-sizing: border-box;
   display: block;
   align-items: center;
-  padding: 100px 64px;
+  padding: 50px 64px;
+  ${media.mobile`
+    height: unset;
+    padding: 50px 32px;
+  `};
 `
-
-const Footer = styled.div`
-  background: linear-gradient(0deg, #e6e1ff 0%, rgba(250, 251, 255, 0) 100%);
-  height: 200px;
-  width: 100%;
-  box-sizing: border-box;
-  display: block;
-  align-items: center;
-  padding: 100px 64px;
-`
-
 const Body = styled.div`
   box-sizing: border-box;
   display: block;
   align-items: center;
+  padding: 100px 64px;
+  ${media.tablet`
+    padding: 0px 32px;
+  `};
+`
+const Footer = styled.div`
+  background: linear-gradient(0deg, #e6e1ff 0%, rgba(250, 251, 255, 0) 100%);
+  height: 100px;
+  width: 100%;
+  box-sizing: border-box;
+  display: block;
+  align-items: center;
+  padding: 0px 64px;
+  ${media.tablet`
+    height: unset;
+    padding: 32px 32px;
+  `};
 `
 
 const Create: React.FC<Props> = (props) => {
@@ -46,7 +55,13 @@ const Create: React.FC<Props> = (props) => {
   return (
     <NftPageV2 style={{ padding: 'unset' }}>
       <Banner>
-        <Flex gap={24} justifyContent='space-around'>
+        <div
+          style={
+            !isMobile && !isTablet
+              ? { alignItems: 'center', display: 'flex', justifyContent: 'space-evenly' }
+              : {}
+          }
+        >
           <div>
             <div style={{ alignItems: 'center', display: 'flex', marginBottom: '16px' }}>
               <Icon label='logo'>
@@ -57,7 +72,7 @@ const Create: React.FC<Props> = (props) => {
                 NFT
               </Text>
             </div>
-            <Text size={isTablet || isMobile ? '20px' : '40px'} weight={600} color='black'>
+            <Text size={isTablet || isMobile ? '20px' : '54px'} weight={600} color='black'>
               Create NFTs.
             </Text>
 
@@ -83,16 +98,21 @@ const Create: React.FC<Props> = (props) => {
           </div>
           <Image
             // @ts-ignore
-            name='cryptoad-2456'
-            width='155px'
-            style={{ padding: '5em 3em' }}
+            name='cryptoad-large'
+            width={!isMobile && !isTablet ? '380px' : '250px'}
           />
-        </Flex>
+        </div>
       </Banner>
       <Body>
-        <Flex justifyContent='space-around'>
-          <div style={{ alignItems: 'center', display: 'flex', width: '33%' }}>
-            <div style={{ display: 'block' }}>
+        <div
+          style={!isMobile && !isTablet ? { display: 'flex', justifyContent: 'space-evenly' } : {}}
+        >
+          <div
+            style={
+              !isMobile && !isTablet ? { alignItems: 'center', display: 'flex', width: '33%' } : {}
+            }
+          >
+            <div style={!isTablet || !isMobile ? { width: '90%' } : {}}>
               <Text size={isTablet || isMobile ? '20px' : '40px'} weight={600} color='black'>
                 Bring Your Creations To Digital Life.
               </Text>
@@ -113,44 +133,84 @@ const Create: React.FC<Props> = (props) => {
           </div>
           <Image
             // @ts-ignore
-            name='cryptoad-2456'
-            width='155px'
-            style={{ padding: '5em 3em' }}
+            name='nft-collections'
+            width={!isMobile && !isTablet ? '400px' : '250px'}
           />
-        </Flex>
-        <Flex justifyContent='space-around'>
-          <Image
-            // @ts-ignore
-            name='cryptoad-2456'
-            width='155px'
-            style={{ padding: '5em 3em' }}
-          />
-          <div style={{ alignItems: 'center', display: 'flex' }}>
-            <div style={{ display: 'block' }}>
-              <Text size={isTablet || isMobile ? '20px' : '40px'} weight={600} color='black'>
-                Get Paid What You’re Worth.
-              </Text>
-              <Text
-                style={{
-                  lineHeight: '20px',
-                  marginTop: '8px',
-                  maxWidth: '500px',
-                  textAlign: 'left'
-                }}
-                size='16px'
-                weight={500}
-                color='grey900'
-              >
-                NFTs give you the chance to receive a cut of secondary market sales. With us,
-                everytime your work is bought and sold, you receive a percentage of the sales.
-              </Text>
+        </div>
+        {!isMobile && !isTablet ? (
+          <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+            <Image
+              // @ts-ignore
+              name='nft-nouns'
+              width='400px'
+            />
+            <div style={{ alignItems: 'center', display: 'flex', width: '33%' }}>
+              <div style={{ display: 'block' }}>
+                <Text size='40px' weight={600} color='black'>
+                  Get Paid What You’re Worth.
+                </Text>
+                <Text
+                  style={{
+                    lineHeight: '20px',
+                    marginTop: '8px',
+                    maxWidth: '500px',
+                    textAlign: 'left'
+                  }}
+                  size='16px'
+                  weight={500}
+                  color='grey900'
+                >
+                  NFTs give you the chance to receive a cut of secondary market sales. With us,
+                  everytime your work is bought and sold, you receive a percentage of the sales.
+                </Text>
+              </div>
             </div>
           </div>
-        </Flex>
-        <Flex justifyContent='space-around'>
+        ) : (
+          <div style={{ paddingTop: '50px' }}>
+            <div style={{ display: 'block', paddingBottom: '1em' }}>
+              <div style={{ display: 'block' }}>
+                <Text size='20px' weight={600} color='black'>
+                  Get Paid What You’re Worth.
+                </Text>
+                <Text
+                  style={{
+                    lineHeight: '20px',
+                    marginTop: '8px',
+                    maxWidth: '500px',
+                    textAlign: 'left'
+                  }}
+                  size='16px'
+                  weight={500}
+                  color='grey900'
+                >
+                  NFTs give you the chance to receive a cut of secondary market sales. With us,
+                  everytime your work is bought and sold, you receive a percentage of the sales.
+                </Text>
+              </div>
+            </div>
+            <Image
+              // @ts-ignore
+              name='nft-nouns'
+              width='250px'
+            />
+          </div>
+        )}
+        <div
+          style={
+            !isMobile && !isTablet
+              ? { display: 'flex', justifyContent: 'space-evenly' }
+              : { paddingTop: '50px' }
+          }
+        >
           <div style={{ alignItems: 'center', display: 'flex' }}>
-            <div style={{ display: 'block' }}>
-              <Text size={isTablet || isMobile ? '20px' : '40px'} weight={600} color='black'>
+            <div style={{ display: 'block', paddingBottom: '1em' }}>
+              <Text
+                style={!isTablet || !isMobile ? { width: '60%' } : {}}
+                size={isTablet || isMobile ? '20px' : '40px'}
+                weight={600}
+                color='black'
+              >
                 You Create It, We Build It.
               </Text>
               <Text
@@ -172,19 +232,26 @@ const Create: React.FC<Props> = (props) => {
           </div>
           <Image
             // @ts-ignore
-            name='cryptoad-2456'
-            width='155px'
-            style={{ padding: '5em 3em' }}
+            name='nft-paint-hyperlink'
+            width='300px'
           />
-        </Flex>
+        </div>
       </Body>
       <Footer>
-        <Flex gap={24} justifyContent='center'>
+        <div
+          style={
+            !isMobile && !isTablet ? { display: 'flex', gap: '24', justifyContent: 'center' } : {}
+          }
+        >
           <Text
             color='grey900'
-            size='40px'
+            size={!isMobile && !isTablet ? '40px' : '20px'}
             weight={600}
-            style={{ alignItems: 'center', display: 'flex' }}
+            style={
+              !isMobile && !isTablet
+                ? { alignItems: 'center', display: 'flex', paddingRight: '1em' }
+                : { alignItems: 'center', display: 'flex', paddingBottom: '1em' }
+            }
           >
             Ready To Stand Out?
           </Text>
@@ -193,7 +260,7 @@ const Create: React.FC<Props> = (props) => {
               <FormattedMessage id='copy.explore' defaultMessage='Apply Now' />
             </Button>
           </LinkContainer>
-        </Flex>
+        </div>
       </Footer>
     </NftPageV2>
   )
