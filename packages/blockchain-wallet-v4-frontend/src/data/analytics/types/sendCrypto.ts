80a451dc1ff4a0edc2938047554e6e11454a60ff
plondon <philip@blockchain.com>
2022-05-16 13:59:27 -0400

    feat(send): track failures

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/types/sendCrypto.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/types/sendCrypto.ts
@@ -1,11 +1,12 @@
 // Send Crypto Events
 export enum Events {
   SEND_AMOUNT_ENTERED = 'Send Amount Entered',
+  SEND_FAILED = 'Send Failed Client',
   SEND_SUBMITTED = 'Send Submitted'
 }
 
 type LoginActions = {
-  key: Events.SEND_AMOUNT_ENTERED | Events.SEND_SUBMITTED
+  key: Events.SEND_AMOUNT_ENTERED | Events.SEND_SUBMITTED | Events.SEND_FAILED
   properties: {}
 }
 
