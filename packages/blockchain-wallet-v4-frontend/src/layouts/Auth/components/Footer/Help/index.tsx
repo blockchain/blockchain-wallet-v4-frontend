3535586737b7373fb4a72aae7782b597001b4cad
Andrew Schneider <andrew@blockchain.com>
2022-05-16 10:51:59 -0400

    fix(help): use wallet support page link

--- packages/blockchain-wallet-v4-frontend/src/layouts/Auth/components/Footer/Help/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/layouts/Auth/components/Footer/Help/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
-import { LinkContainer } from 'react-router-bootstrap'
 import styled from 'styled-components'
 
 import { Link, Text } from 'blockchain-info-components'
@@ -10,21 +9,20 @@ const HelpContainer = styled.div`
   cursor: pointer;
 `
 
-const Help = () => {
-  return (
-    <HelpContainer>
-      <Link
-        color='grey400'
-        href='https://support.blockchain.com/'
-        size='14px'
-        target='_blank'
-        weight={500}
-      >
-        <Text color='grey400' size='16px' weight={500}>
-          <FormattedMessage id='scenes.login.need.help' defaultMessage='Need Help?' />
-        </Text>
-      </Link>
-    </HelpContainer>
-  )
-}
+const Help = () => (
+  <HelpContainer>
+    <Link
+      color='grey400'
+      href='https://support.blockchain.com/hc/en-us/categories/4416659837460-Wallet'
+      size='14px'
+      target='_blank'
+      weight={500}
+    >
+      <Text color='grey400' size='16px' weight={500}>
+        <FormattedMessage id='scenes.login.need.help' defaultMessage='Need Help?' />
+      </Text>
+    </Link>
+  </HelpContainer>
+)
+
 export default Help
