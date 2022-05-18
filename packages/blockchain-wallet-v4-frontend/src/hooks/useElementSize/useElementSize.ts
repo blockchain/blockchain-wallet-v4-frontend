a63a65ce754fa5ab61319cc22e11be6794ded5f4
Andrew Schneider <andrew@blockchain.com>
2022-05-18 10:10:25 -0400

    feat(hooks): add useElementSize hook and its dependencies

---
+++ packages/blockchain-wallet-v4-frontend/src/hooks/useElementSize/useElementSize.ts
@@ -0,0 +1,42 @@
+import { useCallback, useState } from 'react'
+
+import { useEventListener } from '../useEventListener'
+import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
+
+interface Size {
+  height: number
+  width: number
+}
+
+export const useElementSize = <T extends HTMLElement = HTMLDivElement>(): [
+  (node: T | null) => void,
+  Size
+] => {
+  // Mutable values like 'ref.current' aren't valid dependencies
+  // because mutating them doesn't re-render the component.
+  // Instead, we use a state as a ref to be reactive.
+  const [ref, setRef] = useState<T | null>(null)
+  const [size, setSize] = useState<Size>({
+    height: 0,
+    width: 0
+  })
+
+  // Prevent too many rendering using useCallback
+  const handleSize = useCallback(() => {
+    setSize({
+      height: ref?.offsetHeight || 0,
+      width: ref?.offsetWidth || 0
+    })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref?.offsetHeight, ref?.offsetWidth])
+
+  useEventListener('resize', handleSize)
+
+  useIsomorphicLayoutEffect(() => {
+    handleSize()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref?.offsetHeight, ref?.offsetWidth])
+
+  return [setRef, size]
+}
