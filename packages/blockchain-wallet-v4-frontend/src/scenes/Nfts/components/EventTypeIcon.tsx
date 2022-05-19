60313d8afd6b16e19eabcc40993bfb7747cc6df5
plondon <philip@blockchain.com>
2022-05-18 21:21:10 -0400

    feat(nfts): change listing icon

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/components/EventTypeIcon.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/components/EventTypeIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { colors } from '@blockchain-com/constellation'
-import { IconActivity, IconCart, IconCloseCircle, IconSell, IconSend } from '@blockchain-com/icons'
+import { IconActivity, IconCart, IconCloseCircle, IconSend, IconTag } from '@blockchain-com/icons'
 
 import { opensea_event_types } from '.'
 
@@ -14,7 +14,7 @@ const EventTypeIcon: React.FC<Props> = ({ event_type }) => {
     case 'offer_entered':
       return <IconActivity color={colors.grey400} />
     case 'created':
-      return <IconSell color={colors.grey400} />
+      return <IconTag color={colors.grey400} />
     case 'cancelled':
     case 'bid_withdrawn':
       return <IconCloseCircle color={colors.grey400} />
