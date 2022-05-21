8e1e122a120266665160a163a5ec84306c1e392e
Ryan S <rsteubs@blockchain.com>
2022-05-20 18:11:22 -0700

    fix: cleanup

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Home/NftCollectionTable.template.loading.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Home/NftCollectionTable.template.loading.tsx
@@ -1,49 +1,65 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import { SkeletonCircle, SkeletonRectangle } from 'blockchain-info-components'
+import { SkeletonRectangle } from 'blockchain-info-components'
 import { Flex } from 'components/Flex'
-import { useMedia } from 'services/styles'
+import { media, useMedia } from 'services/styles'
 
 const NftCollectionTableLoading: React.FC<Props> = () => {
   const isTablet = useMedia('tablet')
+  const CustomRectangleHeader = styled(SkeletonRectangle)`
+    height: 56px;
+    width: 100%;
+    border-radius: 24px 24px 0px 0px;
+    ${media.mobile`
+      border-radius: unset;
+  `}
+  `
 
+  const CustomRectangle = styled(SkeletonRectangle)`
+    height: 36px;
+    margin-top: 12px;
+    width: 100%;
+    background: linear-gradient(270.04deg, #f0f2f7 42.26%, #ffffff 51.67%, #f0f2f7 63.26%);
+    border-radius: 2px;
+  `
   return (
     <div style={{ paddingTop: '0px', position: 'relative' }}>
       <div style={{ marginTop: '20px', width: '100%' }}>
-        <SkeletonRectangle height='48px' width='100%'>
+        <CustomRectangleHeader>
           <Flex alignItems='center' style={{ gap: '15%', height: '100%', margin: '0em 1em' }} />
-        </SkeletonRectangle>
-        <Flex flexDirection='column' gap={8} style={{ marginTop: '8px' }}>
-          <Flex gap={8}>
-            <SkeletonRectangle width='100%' height='48px' />
-            <SkeletonRectangle width='100%' height='48px' />
+        </CustomRectangleHeader>
+        <Flex flexDirection='column' gap={16} style={{ marginTop: '8px' }}>
+          <Flex gap={36}>
+            <CustomRectangle />
+            <CustomRectangle />
             {!isTablet && (
               <>
-                <SkeletonRectangle width='100%' height='48px' />
-                <SkeletonRectangle width='100%' height='48px' />
-                <SkeletonRectangle width='100%' height='48px' />
+                <CustomRectangle />
+                <CustomRectangle />
+                <CustomRectangle />
               </>
             )}
           </Flex>
-          <Flex gap={8}>
-            <SkeletonRectangle width='100%' height='48px' />
-            <SkeletonRectangle width='100%' height='48px' />
+          <Flex gap={36}>
+            <CustomRectangle />
+            <CustomRectangle />
             {!isTablet && (
               <>
-                <SkeletonRectangle width='100%' height='48px' />
-                <SkeletonRectangle width='100%' height='48px' />
-                <SkeletonRectangle width='100%' height='48px' />
+                <CustomRectangle />
+                <CustomRectangle />
+                <CustomRectangle />
               </>
             )}
           </Flex>
-          <Flex gap={8}>
-            <SkeletonRectangle width='100%' height='48px' />
-            <SkeletonRectangle width='100%' height='48px' />
+          <Flex gap={36}>
+            <CustomRectangle />
+            <CustomRectangle />
             {!isTablet && (
               <>
-                <SkeletonRectangle width='100%' height='48px' />
-                <SkeletonRectangle width='100%' height='48px' />
-                <SkeletonRectangle width='100%' height='48px' />
+                <CustomRectangle />
+                <CustomRectangle />
+                <CustomRectangle />
               </>
             )}
           </Flex>
