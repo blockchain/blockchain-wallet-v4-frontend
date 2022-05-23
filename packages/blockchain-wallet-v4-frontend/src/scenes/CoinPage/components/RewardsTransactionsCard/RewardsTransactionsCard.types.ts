7fa9d01ed8c9bd41d0161f072b3d5029e39df7af
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-05-23 10:51:47 -0300

    feat(CoinView): add wallet filter to transactions list
    Adds a wallet filter to the transactions list in the Coin view v2 page

---
+++ packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/RewardsTransactionsCard/RewardsTransactionsCard.types.ts
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+type RewardsTransactionsCardProps = {}
+
+type RewardsTransactionsCardComponent = FC<RewardsTransactionsCardProps>
+
+export type { RewardsTransactionsCardComponent, RewardsTransactionsCardProps }
