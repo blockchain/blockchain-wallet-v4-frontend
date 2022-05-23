05617e0a148999f7a622551bb25ec9b662171528
Lucas Sena <lsena@blockchain.com>
2022-05-23 16:27:46 -0300

    feat: renaming to coins.ars.intro

--- packages/blockchain-wallet-v4-frontend/src/data/coins/selectors/coins/ars.tsx
+++ packages/blockchain-wallet-v4-frontend/src/data/coins/selectors/coins/ars.tsx
@@ -4,7 +4,7 @@ import { FormattedMessage } from 'react-intl'
 // retrieves introduction text for coin on its transaction page
 export const getTransactionPageHeaderText = () => (
   <FormattedMessage
-    id='coins.usd.intro'
+    id='coins.ars.intro'
     defaultMessage='Store Argentine peso in your wallet and use it to Buy crypto.'
   />
 )
