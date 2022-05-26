e11ef54e0fdd5a0e9d91eb49baf15732d5a62b8b
Ryan S <rsteubs@blockchain.com>
2022-05-26 16:50:25 -0700

    fix: countdown timer

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Asset/components/NftAssetCountdown.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/Asset/components/NftAssetCountdown.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { colors } from '@blockchain-com/constellation'
 import styled from 'styled-components'
+
+import { Text } from 'blockchain-info-components'
 
 import { EthText } from '.'
 
 const CountdownText = styled(EthText)`
-  font-size: 20px;
+  font-size: 24px;
+  line-height: 32px;
 `
-
+const GreyLetter = styled(Text)`
+  color: ${colors.grey400};
+  padding-right: 8px;
+`
 const NftAssetCountdown: React.FC<Props> = ({ countDownDate }) => {
   const [Countdown, setCountdown] = useState('')
 
@@ -14,16 +21,12 @@ const NftAssetCountdown: React.FC<Props> = ({ countDownDate }) => {
     const updateCountdown = () => {
       const now = new Date().getTime()
       const duration = countDownDate - now
-      const days = Math.floor(duration / (1000 * 60 * 60 * 24))
-      const hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((duration % (1000 * 60)) / 1000)
-      setCountdown(
-        () => `${days ? `${days} days` : ''} ${hours} hours ${minutes} minutes ${seconds} seconds`
-      )
+      const days = Math.floor(duration / (1000 * 60 * 60 * 24)).toString()
+      const hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString()
+      const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60)).toString()
+      setCountdown(() => `${days},${hours},${minutes}`)
       return { duration }
     }
-
     const interval = setInterval(function () {
       const { duration } = updateCountdown()
       // if duration < 0, expired
@@ -37,7 +40,22 @@ const NftAssetCountdown: React.FC<Props> = ({ countDownDate }) => {
     return () => clearInterval(interval)
   }, [Countdown])
 
-  return <CountdownText>{Countdown}</CountdownText>
+  return (
+    <CountdownText>
+      {Countdown.split(',')[0]}
+      <GreyLetter size='24px' lineHeight='32px' weight={600}>
+        d
+      </GreyLetter>{' '}
+      {Countdown.split(',')[1]}
+      <GreyLetter size='24px' lineHeight='32px' weight={600}>
+        h
+      </GreyLetter>{' '}
+      {Countdown.split(',')[2]}
+      <GreyLetter size='24px' lineHeight='32px' weight={600}>
+        m
+      </GreyLetter>
+    </CountdownText>
+  )
 }
 
 type Props = {
