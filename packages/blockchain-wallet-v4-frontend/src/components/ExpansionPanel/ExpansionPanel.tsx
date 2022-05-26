9bba751a694b0738fb6b613689a159cc1630a72d
Lucas Sena <lsena@blockchain.com>
2022-05-26 15:06:44 -0300

    feat: removing style property and adding center alignment

--- packages/blockchain-wallet-v4-frontend/src/components/ExpansionPanel/ExpansionPanel.tsx
+++ packages/blockchain-wallet-v4-frontend/src/components/ExpansionPanel/ExpansionPanel.tsx
@@ -15,27 +15,28 @@ export const ExpansionPanel: ExpansionPanelComponent = ({ children }) => {
   return (
     <>
       {isToggled ? <div>{children}</div> : ''}
-      <Padding top={10}>
-        <Button
-          nature='white-blue'
-          small
-          style={{ margin: '8px auto 0px' }}
-          data-e2e={isToggled ? 'showLess' : 'showMore'}
-          onClick={() => {
-            handleToggle(!isToggled)
-          }}
-        >
-          <Flex gap={8} alignItems='center'>
-            <FormattedMessage
-              id={`buttons.${isToggled ? 'show_less' : 'show_more'}`}
-              defaultMessage={isToggled ? 'Show less' : 'Show more'}
-            />
-            <Icon label={isToggled ? 'chevron-up' : 'chevron-down'} color='blue600' size='sm'>
-              {isToggled ? <IconChevronUp /> : <IconChevronDown />}
-            </Icon>
-          </Flex>
-        </Button>
-      </Padding>
+      <Flex justifyContent='center'>
+        <Padding top={10}>
+          <Button
+            nature='white-blue'
+            small
+            data-e2e={isToggled ? 'showLess' : 'showMore'}
+            onClick={() => {
+              handleToggle(!isToggled)
+            }}
+          >
+            <Flex gap={8} alignItems='center'>
+              <FormattedMessage
+                id={`buttons.${isToggled ? 'show_less' : 'show_more'}`}
+                defaultMessage={isToggled ? 'Show less' : 'Show more'}
+              />
+              <Icon label={isToggled ? 'chevron-up' : 'chevron-down'} color='blue600' size='sm'>
+                {isToggled ? <IconChevronUp /> : <IconChevronDown />}
+              </Icon>
+            </Flex>
+          </Button>
+        </Padding>
+      </Flex>
     </>
   )
 }
