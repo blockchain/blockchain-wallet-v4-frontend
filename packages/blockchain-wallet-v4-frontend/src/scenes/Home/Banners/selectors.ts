5fae4e47cce5450ac3ea3370e69f5cc6188a1cef
plondon <philip@blockchain.com>
2022-06-06 15:19:10 +0200

    fix(stx): show to all users despite banner priority

--- packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Home/Banners/selectors.ts
@@ -24,7 +24,6 @@ export type BannerType =
   | 'coinRename'
   | 'servicePriceUnavailable'
   | 'completeYourProfile'
-  | 'stxAirdropFundsAvailable'
   | 'taxCenter'
   | null
 
@@ -160,15 +159,11 @@ export const getData = (state: RootState): { bannerToShow: BannerType } => {
 
   const isProfileCompleted = isVerifiedId && isBankOrCardLinked && isBuyCrypto
 
-  const isStxSelfCustodyAvailable = selectors.coins.getStxSelfCustodyAvailablity(state)
-
   let bannerToShow: BannerType = null
   if (showTaxCenterBanner && taxCenterEnabled) {
     bannerToShow = 'taxCenter'
   } else if (showCompleteYourProfileBanner && !isProfileCompleted) {
     bannerToShow = 'completeYourProfile'
-  } else if (isStxSelfCustodyAvailable) {
-    bannerToShow = 'stxAirdropFundsAvailable'
   } else if (showDocResubmitBanner && !isKycPendingOrVerified) {
     bannerToShow = 'resubmit'
   } else if (
