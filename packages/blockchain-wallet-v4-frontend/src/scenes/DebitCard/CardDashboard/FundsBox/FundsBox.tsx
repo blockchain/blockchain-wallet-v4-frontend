a485ad9b7ae02b32f9f84a828e9c076a50df2d0e
Julian Romero <julianromero@blockchain.com>
2022-06-09 15:55:41 -0300

    feat: remove unnecesary map props to state

--- packages/blockchain-wallet-v4-frontend/src/scenes/DebitCard/CardDashboard/FundsBox/FundsBox.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/DebitCard/CardDashboard/FundsBox/FundsBox.tsx
@@ -8,15 +8,13 @@ import FundsBox from './FundsBox.template'
 
 const FundsBoxContainer = (props) => <FundsBox {...props} />
 
-const mapStateToProps = (state) => ({})
-
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   brokerageActions: bindActionCreators(actions.components.brokerage, dispatch),
   buySellActions: bindActionCreators(actions.components.buySell, dispatch),
   modalActions: bindActionCreators(actions.modals, dispatch)
 })
 
-const connector = connect(mapStateToProps, mapDispatchToProps)
+const connector = connect(null, mapDispatchToProps)
 
 export type Props = ConnectedProps<typeof connector>
 
