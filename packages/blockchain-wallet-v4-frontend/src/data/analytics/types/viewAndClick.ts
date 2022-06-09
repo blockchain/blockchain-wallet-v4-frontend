2920c77b81d30b702375e60ae450ffcd6dd6e80a
Charly Monti <cmonti@blockchain.com>
2022-06-09 11:19:08 -0300

    feat: adding earn rewards banner click & view events

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/types/viewAndClick.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/types/viewAndClick.ts
@@ -1,6 +1,24 @@
 // View and Click Events
 export enum Events {
+  BANNER_REWARDS_CLICKED = 'Banner Rewards Clicked',
+  BANNER_REWARDS_VIEWED = 'Banner Rewards Viewed',
   VIEW_AND_CLICK_FIAT_CURRENCY_SELECTED = 'Fiat Currency Selected'
+}
+
+type BannerEarnRewardsButtonView = {
+  key: Events.BANNER_REWARDS_VIEWED
+  properties: {
+    device: 'APP-Android' | 'APP-iOS' | 'WEB'
+    platform: 'EXCHANGE' | 'WALLET'
+  }
+}
+
+type BannerEarnRewardsButtonClick = {
+  key: Events.BANNER_REWARDS_CLICKED
+  properties: {
+    device: 'APP-Android' | 'APP-iOS' | 'WEB'
+    platform: 'EXCHANGE' | 'WALLET'
+  }
 }
 
 type ClickFiatCurrencyAction = {
@@ -11,7 +29,10 @@ type ClickFiatCurrencyAction = {
 }
 
 // track event actions to be used inside codebase when we do trigger event
-export type TrackEventAction = ClickFiatCurrencyAction
+export type TrackEventAction =
+  | ClickFiatCurrencyAction
+  | BannerEarnRewardsButtonView
+  | BannerEarnRewardsButtonClick
 
 // shared types
 type BasePayload = {
