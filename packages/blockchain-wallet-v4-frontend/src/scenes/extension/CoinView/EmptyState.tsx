3b875167ed7e29a93f853896ddf21fac9b67f8e1
bakhrom-akbarov <bakhrom.akbarov@gmail.com>
2022-06-09 13:09:53 +0500

    feat: coinview screen empty state

---
+++ packages/blockchain-wallet-v4-frontend/src/scenes/extension/CoinView/EmptyState.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Button, Text } from 'blockchain-info-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
+const ButtonStyled = styled(Button)`
+  background: #65a5ff;
+  border-color: #65a5ff;
+  color: ${(props) => props.theme.exchangeLogin};
+`
+
+const EmptyState = () => {
+  return (
+    <Wrapper>
+      <div>
+        <Text style={{ color: '#ffffff' }} size='14px' weight={500}>
+          Receive
+        </Text>
+      </div>
+      <ButtonStyled data-e2e=''>Add crypto</ButtonStyled>
+    </Wrapper>
+  )
+}
+
+export default EmptyState
