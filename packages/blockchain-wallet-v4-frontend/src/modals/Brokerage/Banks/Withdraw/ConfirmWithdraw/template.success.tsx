e17c5e5180da217d9c91a6d976edf3454bba7481
Lucas Sena <lsena@blockchain.com>
2022-06-13 14:40:03 -0300

    feat: using copies from ARS to USD in Argentina in the withdrawal flow

--- packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/Withdraw/ConfirmWithdraw/template.success.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/Withdraw/ConfirmWithdraw/template.success.tsx
@@ -45,24 +45,128 @@ const ErrorContainer = styled(FlyoutWrapper)`
 
 const Success: React.FC<InjectedFormProps<WithdrawCheckoutFormValuesType, Props> & Props> = (
   props
-) => (
-  <Form
-    onSubmit={(e) => {
-      e.preventDefault()
-      props.withdrawActions.handleCustodyWithdraw({
-        amount: props.formValues.amount,
-        beneficiary: props.beneficiary || props.defaultMethod || null,
-        fiatCurrency: props.fiatCurrency
-      })
-    }}
-  >
-    <FlyoutWrapper>
-      <Top>
-        <Icon
-          name='arrow-left'
-          size='20px'
-          role='button'
-          style={{ marginRight: '16px' }}
+) => {
+  const userCountryCode = props.userData?.address?.country || 'default'
+
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault()
+        props.withdrawActions.handleCustodyWithdraw({
+          amount: props.formValues.amount,
+          beneficiary: props.beneficiary || props.defaultMethod || null,
+          fiatCurrency: props.fiatCurrency
+        })
+      }}
+    >
+      <FlyoutWrapper>
+        <Top>
+          <Icon
+            name='arrow-left'
+            size='20px'
+            role='button'
+            style={{ marginRight: '16px' }}
+            onClick={() =>
+              props.withdrawActions.setStep({
+                beneficiary: props.beneficiary,
+                fiatCurrency: props.fiatCurrency,
+                step: WithdrawStepEnum.ENTER_AMOUNT
+              })
+            }
+          />
+          <Text size='20px' weight={600} color='grey800'>
+            <FormattedMessage id='copy.confirm_withdrawal' defaultMessage='Confirm Withdrawal' />
+          </Text>
+        </Top>
+        <AmountContainer>
+          <CoinDisplay color='grey800' size='32px' weight={600} coin={props.fiatCurrency}>
+            {props.amount}
+          </CoinDisplay>
+        </AmountContainer>
+      </FlyoutWrapper>
+      <Row>
+        <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
+          <FormattedMessage id='copy.from' defaultMessage='From' />
+        </Text>
+        <Value>
+          <FormattedMessage
+            id='modals.brokerage.fiat_account'
+            defaultMessage='{currency} Account'
+            values={{
+              currency: props.fiatCurrency
+            }}
+          />
+        </Value>
+      </Row>
+      <Row>
+        <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
+          <FormattedMessage id='copy.to' defaultMessage='To' />
+        </Text>
+        <Value>{props.beneficiary?.name || props.defaultMethod?.details.bankName}</Value>
+        <Value>{props.defaultMethod?.details.accountName || ''}</Value>
+      </Row>
+      <Row>
+        <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
+          <FormattedMessage id='copy.fee' defaultMessage='Fee' />
+        </Text>
+        <Value>
+          {props.fees.value} {props.fiatCurrency}
+        </Value>
+      </Row>
+      <Row>
+        <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
+          <FormattedMessage id='copy.total' defaultMessage='Total' />
+        </Text>
+        <Value>
+          {fiatToString({
+            digits: 0,
+            unit: props.fiatCurrency || ('USD' as FiatType),
+            value: props.amount
+          })}
+        </Value>
+      </Row>
+      <RowDisclaimer>
+        <SubTitle size='14px' color='grey600' weight={500}>
+          {userCountryCode === 'AR' ? (
+            <FormattedMessage
+              id='modals.simplebuy.withdrawal.arrive_description_ARS'
+              defaultMessage='The funds can take up to 3 business days to arrive. Check the status of your Withdrawal at anytime from your Activity screen.'
+            />
+          ) : (
+            <FormattedMessage
+              id='modals.simplebuy.withdrawal.arrive_description_default'
+              defaultMessage='The funds can take up to 5 business days to arrive. Check the status of your Withdrawal at anytime from your Activity screen.'
+            />
+          )}
+        </SubTitle>
+      </RowDisclaimer>
+      <FlyoutWrapper>
+        <Button
+          data-e2e='withdrawCustody'
+          disabled={props.submitting}
+          fullwidth
+          height='48px'
+          nature='primary'
+          size='16px'
+          type='submit'
+        >
+          {props.submitting ? (
+            <HeartbeatLoader height='20px' width='20px' color='white' />
+          ) : (
+            <FormattedMessage
+              id='modals.simplebuy.withdrawal.withdrawal_button'
+              defaultMessage='Withdraw Now'
+            />
+          )}
+        </Button>
+        <Button
+          data-e2e='cancelCustody'
+          disabled={props.submitting}
+          fullwidth
+          height='48px'
+          nature='empty-red'
+          size='16px'
+          margin='20px 0 0'
           onClick={() =>
             props.withdrawActions.setStep({
               beneficiary: props.beneficiary,
@@ -70,125 +174,25 @@ const Success: React.FC<InjectedFormProps<WithdrawCheckoutFormValuesType, Props>
               step: WithdrawStepEnum.ENTER_AMOUNT
             })
           }
-        />
-        <Text size='20px' weight={600} color='grey800'>
-          <FormattedMessage id='copy.confirm_withdrawal' defaultMessage='Confirm Withdrawal' />
-        </Text>
-      </Top>
-      <AmountContainer>
-        <CoinDisplay color='grey800' size='32px' weight={600} coin={props.fiatCurrency}>
-          {props.amount}
-        </CoinDisplay>
-      </AmountContainer>
-    </FlyoutWrapper>
-    <Row>
-      <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
-        <FormattedMessage id='copy.from' defaultMessage='From' />
-      </Text>
-      <Value>
-        <FormattedMessage
-          id='modals.brokerage.fiat_account'
-          defaultMessage='{currency} Account'
-          values={{
-            currency: props.fiatCurrency
-          }}
-        />
-      </Value>
-    </Row>
-    <Row>
-      <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
-        <FormattedMessage id='copy.to' defaultMessage='To' />
-      </Text>
-      <Value>{props.beneficiary?.name || props.defaultMethod?.details.bankName}</Value>
-      <Value>{props.defaultMethod?.details.accountName || ''}</Value>
-    </Row>
-    <Row>
-      <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
-        <FormattedMessage id='copy.fee' defaultMessage='Fee' />
-      </Text>
-      <Value>
-        {props.fees.value} {props.fiatCurrency}
-      </Value>
-    </Row>
-    <Row>
-      <Text color='grey600' size='14px' weight={500} lineHeight='21px'>
-        <FormattedMessage id='copy.total' defaultMessage='Total' />
-      </Text>
-      <Value>
-        {fiatToString({
-          digits: 0,
-          unit: props.fiatCurrency || ('USD' as FiatType),
-          value: props.amount
-        })}
-      </Value>
-    </Row>
-    <RowDisclaimer>
-      <SubTitle size='14px' color='grey600' weight={500}>
-        {props.fiatCurrency === WalletFiatEnum[WalletFiatEnum.ARS] ? (
-          <FormattedMessage
-            id='modals.simplebuy.withdrawal.arrive_description_ARS'
-            defaultMessage='The funds can take up to 3 business days to arrive. Check the status of your Withdrawal at anytime from your Activity screen.'
-          />
-        ) : (
-          <FormattedMessage
-            id='modals.simplebuy.withdrawal.arrive_description_default'
-            defaultMessage='The funds can take up to 5 business days to arrive. Check the status of your Withdrawal at anytime from your Activity screen.'
-          />
-        )}
-      </SubTitle>
-    </RowDisclaimer>
-    <FlyoutWrapper>
-      <Button
-        data-e2e='withdrawCustody'
-        disabled={props.submitting}
-        fullwidth
-        height='48px'
-        nature='primary'
-        size='16px'
-        type='submit'
-      >
-        {props.submitting ? (
-          <HeartbeatLoader height='20px' width='20px' color='white' />
-        ) : (
-          <FormattedMessage
-            id='modals.simplebuy.withdrawal.withdrawal_button'
-            defaultMessage='Withdraw Now'
-          />
-        )}
-      </Button>
-      <Button
-        data-e2e='cancelCustody'
-        disabled={props.submitting}
-        fullwidth
-        height='48px'
-        nature='empty-red'
-        size='16px'
-        margin='20px 0 0'
-        onClick={() =>
-          props.withdrawActions.setStep({
-            beneficiary: props.beneficiary,
-            fiatCurrency: props.fiatCurrency,
-            step: WithdrawStepEnum.ENTER_AMOUNT
-          })
-        }
-      >
-        {props.submitting ? (
-          <HeartbeatLoader height='20px' width='20px' color='white' />
-        ) : (
-          <FormattedMessage
-            id='modals.simplebuy.withdrawal.cancel_button'
-            defaultMessage='Cancel'
-          />
-        )}
-      </Button>
-    </FlyoutWrapper>
-    {props.error && !props.submitting && (
-      <ErrorContainer>
-        <ErrorCartridge>{props.error}</ErrorCartridge>
-      </ErrorContainer>
-    )}
-  </Form>
-)
+        >
+          {props.submitting ? (
+            <HeartbeatLoader height='20px' width='20px' color='white' />
+          ) : (
+            <FormattedMessage
+              id='modals.simplebuy.withdrawal.cancel_button'
+              defaultMessage='Cancel'
+            />
+          )}
+        </Button>
+      </FlyoutWrapper>
+      {props.error && !props.submitting && (
+        <ErrorContainer>
+          <ErrorCartridge>{props.error}</ErrorCartridge>
+        </ErrorContainer>
+      )}
+    </Form>
+  )
+}
 
 export type Props = OwnProps & SuccessStateType & { fees: NabuSymbolNumberType }
 
