35c6ec15d747afc0dbe2755f7c1f9a303816d4a4
bakhrom-akbarov <bakhrom.akbarov@gmail.com>
2022-06-21 19:28:59 +0500

    feat: useCoinBalances refactored

--- packages/blockchain-wallet-v4-frontend/src/hooks/useCoinBalances/useCoinBalances.tsx
+++ packages/blockchain-wallet-v4-frontend/src/hooks/useCoinBalances/useCoinBalances.tsx
@@ -13,26 +13,31 @@ export const useCoinBalances = () => {
   const allowedChains = ['ETH', 'BTC', 'XLM', 'BCH', 'STX']
 
   useEffect(() => {
-    setTimeout(() => {
-      if (window.coins) {
-        const coinsArr: Array<any> = []
-        Object.entries(window.coins).forEach((coin: any) => {
-          const balance = getBalanceSelector(coin[0])(state).getOrElse(0).valueOf()
-          const { coinfig } = coin[1]
-          if (
-            (allowedChains.includes(coinfig.symbol) ||
-              allowedChains.includes(coinfig.type.parentChain)) &&
-            balance > 0
-          ) {
-            coinsArr.push({ balance, coinfig })
-          }
-        })
-        coinsArr.sort(function (a, b) {
-          return a.balance + b.balance
-        })
-        setCoins(coinsArr)
-      }
-    }, 500)
+    const getCoins = () => {
+      const coinsArr: Array<any> = []
+      Object.entries(window.coins).forEach((coin: any) => {
+        const balance = getBalanceSelector(coin[0])(state).getOrElse(0).valueOf()
+        const { coinfig } = coin[1]
+        if (
+          (allowedChains.includes(coinfig.symbol) ||
+            allowedChains.includes(coinfig.type.parentChain)) &&
+          balance > 0
+        ) {
+          coinsArr.push({ balance, coinfig })
+        }
+      })
+      coinsArr.sort(function (a, b) {
+        return a.balance + b.balance
+      })
+      setCoins(coinsArr)
+    }
+    if (window.coins) {
+      getCoins()
+    } else {
+      setTimeout(() => {
+        getCoins()
+      }, 500)
+    }
   }, [window.coins])
 
   return coins
