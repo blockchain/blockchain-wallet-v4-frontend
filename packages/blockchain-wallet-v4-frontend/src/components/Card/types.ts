79775f66fda4cf6d688dde60e1ee58bcc0140420
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-06-22 09:09:13 -0300

    feat(AddCard): adds card to incentivize use of debit card

--- packages/blockchain-wallet-v4-frontend/src/components/Card/types.ts
+++ packages/blockchain-wallet-v4-frontend/src/components/Card/types.ts
@@ -1,7 +1,14 @@
 import { FC } from 'react'
+import { colors } from '@blockchain-com/constellation'
+
+export type CardBorderRadius = 'lg' | 'md'
+export type CardElevation = 1
 
 export type CardProps = {
-  elevation?: number
+  backgroundColor?: keyof typeof colors
+  borderRadius?: CardBorderRadius
+  borderWidth?: number
+  elevation?: CardElevation
 }
 
 export type CardComponent = FC<CardProps>
