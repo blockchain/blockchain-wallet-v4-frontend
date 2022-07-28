17495a955684faeae2ad23c8bbccab3a0323ce94
Тарас <potsikaylot@gmail.com>
2022-07-28 14:15:20 +0300

    feat(plugin): refactored request account rpc method (#50)

--- packages/blockchain-wallet-v4-frontend/src/plugin/internal/background.ts
+++ packages/blockchain-wallet-v4-frontend/src/plugin/internal/background.ts
@@ -1,6 +1,4 @@
-// @ts-nocheck
 import { openPopup } from 'plugin/internal/browser'
-import { clearSession, isSessionActive } from 'plugin/internal/chromeStorage'
 import { TabMetadata } from 'plugin/internal/index'
 import { ConnectionEvents, ProviderMessage, RequestArguments } from 'plugin/provider/types'
 import { SupportedRPCMethods } from 'plugin/provider/utils'
@@ -10,46 +8,51 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.tabs.create({ url: chrome.runtime.getURL('index-tab.html') }).catch((err) => console.log(err))
 })
 
-chrome.runtime.onConnect.addListener(function (port: chrome.runtime.Port) {
-  port.postMessage({ data: { chainId: '1' }, type: ConnectionEvents.Connected })
+chrome.runtime.onConnect.addListener(async (port: chrome.runtime.Port) => {
+  await port.postMessage({ data: { chainId: '1' }, type: ConnectionEvents.Connected })
 
   const metadata: TabMetadata = {
     favicon: port.sender?.tab?.favIconUrl,
     origin: port.name
   }
 
-  const listener = (msg: ProviderMessage) => {
-    port.postMessage(msg)
+  const listener = async (msg: ProviderMessage) => {
+    await port.postMessage(msg)
 
-    chrome.runtime.onMessage.removeListener(listener)
+    await chrome.runtime.onMessage.removeListener(listener)
   }
 
-  port.onMessage.addListener(async function (msg: RequestArguments) {
-    // try {
-    //   const isCurrentSessionActive = await isSessionActive()
-    //   if (!isCurrentSessionActive) {
-    //     await clearSession()
-    //     port.postMessage({
-    //       data: 'BCDC session expired. Please reauthenticate',
-    //       type: ConnectionEvents.Error
-    //     })
-    //     await chrome.tabs.create({ url: chrome.runtime.getURL('index-tab.html') })
-    //   }
-    // } catch (e) {
-    //   port.postMessage({
-    //     data: e.message,
-    //     type: ConnectionEvents.Error
-    //   })
-    // }
+  await port.onMessage.addListener(async (msg: RequestArguments) => {
+    try {
+      const { sessionExpiresAt } = await chrome.storage.local.get('sessionExpiresAt')
+      const isSessionActive = sessionExpiresAt ? new Date(sessionExpiresAt) > new Date() : false
 
-    switch (msg.method) {
-      case SupportedRPCMethods.RequestAccounts:
-        chrome.runtime.onMessage.addListener(listener)
-
-        // eslint-disable-next-line
-        openPopup(`/plugin/connect-dapp?domain=${metadata.origin}&favicon=${metadata.favicon}`).catch((e) => console.log(e))
-        break
-      default:
+      switch (msg.method) {
+        case SupportedRPCMethods.RequestAccounts:
+          await chrome.runtime.onMessage.addListener(listener)
+          try {
+            if (isSessionActive) {
+              // eslint-disable-next-line
+              openPopup(`/plugin/connect-dapp?domain=${metadata.origin}&favicon=${metadata.favicon}`).catch((e) => console.log(e))
+            } else {
+              await chrome.storage.session.clear()
+              // eslint-disable-next-line
+              await chrome.tabs.create({ url: chrome.runtime.getURL('index-tab.html') }).catch((err) => console.log(err))
+            }
+          } catch (e) {
+            await port.postMessage({
+              data: e.message,
+              type: ConnectionEvents.Error
+            })
+          }
+          break
+        default:
+      }
+    } catch (e) {
+      await port.postMessage({
+        data: e.message,
+        type: ConnectionEvents.Error
+      })
     }
   })
 })
