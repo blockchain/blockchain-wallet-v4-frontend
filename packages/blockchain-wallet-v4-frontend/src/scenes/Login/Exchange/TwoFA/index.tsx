2f90572e65a11e2b36e4de198d5a3f77c04f60b4
Pedro Filho <pedro@filho.me>
2022-08-05 16:57:13 +0100

    fix(2fa): making minimum as 6 on exchange screen

--- packages/blockchain-wallet-v4-frontend/src/scenes/Login/Exchange/TwoFA/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Login/Exchange/TwoFA/index.tsx
@@ -91,7 +91,7 @@ const TwoFAExchange = (props: Props) => {
             nature='primary'
             fullwidth
             height='48px'
-            disabled={submitting || invalid || busy || (formValues?.exchangeTwoFA?.length || 0) < 5}
+            disabled={submitting || invalid || busy || (formValues?.exchangeTwoFA?.length || 0) < 6}
             data-e2e='loginButton'
             style={{ marginBottom: '16px' }}
           >
