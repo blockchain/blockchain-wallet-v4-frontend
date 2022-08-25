79f05254326765a256c28d11ec734ee63fea4d7d
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-08-25 09:39:24 -0300

    refactor(IconCircularBackground): move styles to styled components

--- packages/blockchain-wallet-v4-frontend/src/components/IconCircularBackground/IconCircularBackground.tsx
+++ packages/blockchain-wallet-v4-frontend/src/components/IconCircularBackground/IconCircularBackground.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { colors } from '@blockchain-com/constellation'
 
-import { IconCircularBackgroundComponent, IconCircularBackgroundSizes } from './types'
+import { CircularBackground, Container, ContentContainer } from './IconCircularBackground.styles'
+import { IconCircularBackgroundComponent } from './IconCircularBackground.types'
 
 export const IconCircularBackground: IconCircularBackgroundComponent = ({
   backgroundOpacity = 1,
@@ -9,42 +9,11 @@ export const IconCircularBackground: IconCircularBackgroundComponent = ({
   color = 'default',
   size = 'default'
 }) => {
-  const sizeToPixels: Record<IconCircularBackgroundSizes, number> = {
-    default: 24,
-    large: 32
-  }
-
   return (
-    <div
-      style={{
-        height: sizeToPixels[size] ?? size,
-        position: 'relative',
-        width: sizeToPixels[size] ?? size
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: colors[color] ?? color,
-          borderRadius: '50%',
-          height: sizeToPixels[size] ?? size,
-          opacity: backgroundOpacity,
-          position: 'absolute',
-          width: sizeToPixels[size] ?? size
-        }}
-      />
+    <Container size={size}>
+      <CircularBackground color={color} size={size} backgroundOpacity={backgroundOpacity} />
 
-      <div
-        style={{
-          alignItems: 'center',
-          display: 'flex',
-          height: sizeToPixels[size] ?? size,
-          justifyContent: 'center',
-          position: 'absolute',
-          width: sizeToPixels[size] ?? size
-        }}
-      >
-        {children}
-      </div>
-    </div>
+      <ContentContainer size={size}>{children}</ContentContainer>
+    </Container>
   )
 }
--- packages/blockchain-wallet-v4-frontend/src/components/IconCircularBackground/IconCircularBackground.types.ts
+++ packages/blockchain-wallet-v4-frontend/src/components/IconCircularBackground/IconCircularBackground.types.ts
@@ -1,11 +1,13 @@
 import { FC } from 'react'
 import { colors } from '@blockchain-com/constellation'
 
+export type IconCircularBackgroundColor = keyof typeof colors | string
+
 export type IconCircularBackgroundSizes = 'default' | 'large'
 
 export type IconCircularBackgroundProps = {
   backgroundOpacity?: number
-  color: keyof typeof colors | string
+  color: IconCircularBackgroundColor
   size?: IconCircularBackgroundSizes | number
 }
 
