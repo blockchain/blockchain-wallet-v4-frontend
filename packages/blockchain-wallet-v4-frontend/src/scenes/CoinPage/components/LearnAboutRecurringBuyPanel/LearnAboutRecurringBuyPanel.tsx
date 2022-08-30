f2002b689e69347d727a53a781774acca428d838
jjBlockchain <jj@blockchain.com>
2022-08-30 17:14:16 -0500

    chore(constellation-0.1): update card component

--- packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/LearnAboutRecurringBuyPanel/LearnAboutRecurringBuyPanel.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/CoinPage/components/LearnAboutRecurringBuyPanel/LearnAboutRecurringBuyPanel.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
-import { Icon } from '@blockchain-com/constellation'
-import { IconRepeat } from '@blockchain-com/icons'
+import { IconRepeat, PaletteColors } from '@blockchain-com/constellation'
 
 import { Button, Text } from 'blockchain-info-components'
 import { Card } from 'components/Card'
@@ -17,9 +16,7 @@ export const LearnAboutRecurringBuyPanel: LearnAboutRecurringBuyPanelComponent =
       <Padding all={16}>
         <Flex flexDirection='column' gap={12}>
           <IconCircularBackground color='blue000'>
-            <Icon label='repeat' size='sm' color='blue600'>
-              <IconRepeat />
-            </Icon>
+            <IconRepeat color={PaletteColors['blue-600']} size='small' />
           </IconCircularBackground>
 
           <Flex flexDirection='column'>
