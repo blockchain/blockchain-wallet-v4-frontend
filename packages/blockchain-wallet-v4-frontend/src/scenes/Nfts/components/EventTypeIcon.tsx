8fe46946baa0f19c2b4e9caf8d361a3ab385d0b0
jjBlockchain <jj@blockchain.com>
2022-08-31 00:23:17 -0500

    chore(constellation): update EventTypeIcon

--- packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/components/EventTypeIcon.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Nfts/components/EventTypeIcon.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
-import { PaletteColors } from '@blockchain-com/constellation'
-import { IconActivity, IconCart, IconCloseCircle, IconSend, IconTag } from '@blockchain-com/icons'
+import {
+  IconActivity,
+  IconCart,
+  IconCloseCircle,
+  IconSend,
+  IconTag,
+  PaletteColors
+} from '@blockchain-com/constellation'
 
 import { opensea_event_types } from '.'
 
-const EventTypeIcon: React.FC<Props> = ({ event_type }) => {
+const EventTypeIcon: React.FC<Props> = ({ event_type, label, size }) => {
   switch (event_type) {
     case 'successful':
-      return <IconCart color={PaletteColors['grey-400']} />
+      return <IconCart color={PaletteColors['grey-400']} label={label} size={size} />
     case 'transfer':
-      return <IconSend color={PaletteColors['grey-400']} />
+      return <IconSend color={PaletteColors['grey-400']} label={label} size={size} />
     case 'bid_entered':
     case 'offer_entered':
-      return <IconActivity color={PaletteColors['grey-400']} />
+      return <IconActivity color={PaletteColors['grey-400']} label={label} size={size} />
     case 'created':
-      return <IconTag color={PaletteColors['grey-400']} />
+      return <IconTag color={PaletteColors['grey-400']} label={label} size={size} />
     case 'cancelled':
     case 'bid_withdrawn':
-      return <IconCloseCircle color={PaletteColors['grey-400']} />
+      return <IconCloseCircle color={PaletteColors['grey-400']} label={label} size={size} />
     default:
       return null
   }
@@ -25,6 +31,8 @@ const EventTypeIcon: React.FC<Props> = ({ event_type }) => {
 
 type Props = {
   event_type: keyof typeof opensea_event_types
+  label: string
+  size: 'small' | 'medium' | 'large'
 }
 
 export default EventTypeIcon
