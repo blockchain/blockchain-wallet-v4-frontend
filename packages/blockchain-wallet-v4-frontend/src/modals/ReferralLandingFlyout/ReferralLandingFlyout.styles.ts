ab8100731029d92451a99e74e46004852f1523fa
Milan <milan@blockchain.com>
2022-08-31 15:26:45 +0200

    feat(referral modal): improved referal modal to render per figma

--- packages/blockchain-wallet-v4-frontend/src/modals/ReferralLandingFlyout/ReferralLandingFlyout.styles.ts
+++ packages/blockchain-wallet-v4-frontend/src/modals/ReferralLandingFlyout/ReferralLandingFlyout.styles.ts
@@ -43,3 +43,50 @@ export const Description = styled(Text)`
   font-weight: 500;
   max-width: 345px;
 `
+
+export const ReferralId = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  height: 120px;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  background: #fafbff;
+`
+
+export const CircleBackground = styled.div<{ color?: string; size?: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => (props.size ? props.size : '48px')};
+  height: ${(props) => (props.size ? props.size : '48px')};
+  min-width: ${(props) => (props.size ? props.size : '48px')};
+  background-color: ${(props) => (props.color ? props.theme[props.color] : props.theme.blue000)};
+  border-radius: ${(props) => (props.size ? props.size : '48px')};
+  margin: 8px 8px 8px 0;
+`
+
+export const ReferralItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+
+export const ReferralItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  justify-content: center;
+`
+
+export const ReferralItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+`
+export const ReferralItemDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
--- packages/blockchain-wallet-v4-frontend/src/modals/ReferralLandingFlyout/ReferralLandingFlyout.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/ReferralLandingFlyout/ReferralLandingFlyout.tsx
@@ -1,24 +1,29 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useSelector } from 'react-redux'
-import { Icon, Padding } from '@blockchain-com/constellation'
+import { Icon, Padding, useCopyToClipboard } from '@blockchain-com/constellation'
 import { IconCloseCircleV2 } from '@blockchain-com/icons'
 
-import { Button, Image } from 'blockchain-info-components'
+import { Button, Image, Text } from 'blockchain-info-components'
 import Flyout, { duration, FlyoutChild } from 'components/Flyout'
 import { FlyoutContent, FlyoutFooter } from 'components/Flyout/Layout'
-// import { selectors } from 'data'
 import { RootState } from 'data/rootReducer'
 import { ModalName, UserDataType } from 'data/types'
 import { useFetchUserReferrals } from 'hooks'
 import ModalEnhancer from 'providers/ModalEnhancer'
 
 import {
+  CircleBackground,
   CloseIconContainer,
   Description,
   DescriptionWrapper,
   HeaderWrapper,
   ImageWrapper,
+  ReferralId,
+  ReferralItem,
+  ReferralItemDescription,
+  ReferralItems,
+  ReferralItemWrapper,
   Title
 } from './ReferralLandingFlyout.styles'
 import { ReferralLandingFlyoutComponent } from './ReferralLandingFlyout.types'
@@ -33,6 +38,7 @@ const ReferralLanding: ReferralLandingFlyoutComponent = ({
 }) => {
   const [isOpen, setOpen] = useState<boolean>(true)
   const fetchUserReferrals = useFetchUserReferrals()
+  const [v, copy] = useCopyToClipboard()
 
   const userTags = useSelector(
     (state: RootState) => state.profile.userData.getOrElse({} as UserDataType)?.tags
@@ -49,6 +55,10 @@ const ReferralLanding: ReferralLandingFlyoutComponent = ({
       }, duration)
     })
   }, [close])
+
+  const handleOnClickToCopyText = useCallback(() => {
+    copy('12345678')
+  }, [copy])
 
   useEffect(() => {
     // load user referrals
@@ -99,6 +109,92 @@ const ReferralLanding: ReferralLandingFlyoutComponent = ({
               </Description>
             </Padding>
           </DescriptionWrapper>
+
+          <Padding top={32} left={40} right={40} bottom={8}>
+            <Text color='grey400' style={{ textAlign: 'center' }} size='14px'>
+              <FormattedMessage
+                id='modals.referralLanding.referralCode'
+                defaultMessage='Your referral code'
+              />
+            </Text>
+          </Padding>
+          <Padding top={0} left={40} right={40} bottom={8}>
+            <ReferralId>
+              <Text
+                color='grey900'
+                style={{ fontFamily: 'Inter', letterSpacing: '0.4em', textAlign: 'center' }}
+                size='24px'
+                weight={500}
+              >
+                12345678
+              </Text>
+              <Button
+                data-e2e='copyReferralNumber'
+                nature='white-blue'
+                onClick={handleOnClickToCopyText}
+              >
+                <FormattedMessage id='buttons.copy' defaultMessage='Copy' />
+              </Button>
+            </ReferralId>
+            <Padding top={16} left={40} right={40} bottom={8}>
+              <Text color='grey400' style={{ textAlign: 'center' }} size='14px'>
+                <FormattedMessage
+                  id='modals.referralLanding.toQualify'
+                  defaultMessage='To qualify, your friends must:'
+                />
+              </Text>
+            </Padding>
+
+            <ReferralItemWrapper>
+              <ReferralItems>
+                <ReferralItem>
+                  <CircleBackground size='24px' color='blue000'>
+                    <Text color='blue600' size='16px'>
+                      1
+                    </Text>
+                  </CircleBackground>
+                  <ReferralItemDescription>
+                    <Text color='grey800' style={{ textAlign: 'center' }} size='14px'>
+                      <FormattedMessage
+                        id='modals.referralLanding.signUpUsingCode'
+                        defaultMessage='Sign up using your code'
+                      />
+                    </Text>
+                  </ReferralItemDescription>
+                </ReferralItem>
+                <ReferralItem>
+                  <CircleBackground size='24px' color='blue000'>
+                    <Text color='blue600' size='16px'>
+                      2
+                    </Text>
+                  </CircleBackground>
+                  <ReferralItemDescription>
+                    <Text color='grey800' style={{ textAlign: 'center' }} size='14px'>
+                      <FormattedMessage
+                        id='modals.referralLanding.verifyIdentity'
+                        defaultMessage='Verify their identity'
+                      />
+                    </Text>
+                  </ReferralItemDescription>
+                </ReferralItem>
+                <ReferralItem>
+                  <CircleBackground size='24px' color='blue000'>
+                    <Text color='blue600' size='16px'>
+                      3
+                    </Text>
+                  </CircleBackground>
+                  <ReferralItemDescription>
+                    <Text color='grey800' style={{ textAlign: 'center' }} size='14px'>
+                      <FormattedMessage
+                        id='modals.referralLanding.buy20'
+                        defaultMessage='Buy $20+ of crypto'
+                      />
+                    </Text>
+                  </ReferralItemDescription>
+                </ReferralItem>
+              </ReferralItems>
+            </ReferralItemWrapper>
+          </Padding>
         </FlyoutContent>
 
         <FlyoutFooter collapsed>
