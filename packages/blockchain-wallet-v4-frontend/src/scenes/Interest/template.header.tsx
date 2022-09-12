fd5c1e90cd2154f88fec58dd19eb8567a51ccd6a
jjBlockchain <jj@blockchain.com>
2022-09-11 20:45:44 -0500

    feat(earn-landing): update copy

--- packages/blockchain-wallet-v4-frontend/src/scenes/Interest/template.header.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Interest/template.header.tsx
@@ -2,18 +2,9 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import styled from 'styled-components'
 
-import { Icon, Link, Text, TooltipHost } from 'blockchain-info-components'
-import { IconBackground, SceneHeader, SceneHeaderText, SceneSubHeaderText } from 'components/Layout'
+import { Icon, Text, TooltipHost } from 'blockchain-info-components'
+import { SceneHeader, SceneHeaderText, SceneSubHeaderText } from 'components/Layout'
 
-const LearnMoreLink = styled(Link)`
-  display: inline-flex;
-`
-const LearnMoreText = styled(Text)`
-  margin-left: 3px;
-  font-size: 15px;
-  font-weight: 500;
-  color: ${(props) => props.theme.blue600};
-`
 const DisclaimerText = styled(Text)`
   display: flex;
   align-items: center;
@@ -30,26 +21,15 @@ const InterestHeader = () => {
   return (
     <>
       <SceneHeader>
-        <IconBackground>
-          <Icon color='blue600' name='percentage' size='24px' />
-        </IconBackground>
         <SceneHeaderText>
-          <FormattedMessage id='scenes.interest.interestaccount' defaultMessage='Rewards Account' />
+          <FormattedMessage id='copy.earn' defaultMessage='Earn' />
         </SceneHeaderText>
       </SceneHeader>
       <SceneSubHeaderTextCustom>
         <FormattedMessage
-          id='scenes.interest.subheader'
-          defaultMessage='Deposit crypto and watch it grow. Rewards are paid by the end of the day on the 1st of each month.'
+          defaultMessage='Get daily or monthly rewards on your crypto.'
+          id='scenes.earn.subheader'
         />
-        <LearnMoreLink
-          href='https://support.blockchain.com/hc/en-us/articles/360043658491-How-the-Interest-Account-works'
-          target='_blank'
-        >
-          <LearnMoreText size='16px'>
-            <FormattedMessage id='buttons.learn_more' defaultMessage='Learn More' />
-          </LearnMoreText>
-        </LearnMoreLink>
         <SubheaderSeparator />
         <DisclaimerText>
           <TooltipHost id='scenes.interest.legaldisclaimer'>
