899678291888187a32d852efad1e8211dd80835b
Milan <67264242+milan-bc@users.noreply.github.com>
2022-10-31 22:05:08 +0100

    feat(sardine): added logic to allow sardine script to be loaded with nonce (#5480)
    * feat(sardine): added logic to allow sardine script to be loaded with nonce
    
    * feat(sardine): improved loading of sardine scripts

--- config/env/production.js
+++ config/env/production.js
@@ -8,6 +8,7 @@ module.exports = {
   OPENSEA_API: 'https://api.opensea.io',
   RECAPTCHA_KEY: '6LcxysYaAAAAAOf5QgMWz-GXzoXjpvSrSXETmtlU',
   SARDINE_CLIENT_ID: '01ac52dd-f1ed-4715-be81-1023407cdd82',
+  SARDINE_ENVIRONMENT: 'production',
   ROOT_URL: 'https://blockchain.info',
   VERIFF_URL: 'https://magic.veriff.me',
   WALLET_HELPER_DOMAIN: 'https://wallet-helper.blockchain.com',
