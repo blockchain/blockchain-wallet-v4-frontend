ed40e54a32bd82e76ab97599c4b2e238ac3876fc
jjBlockchain <jj@blockchain.com>
2022-11-22 09:17:29 -0600

    feat(earn-filter): move earn filter and add mobile scene

--- packages/blockchain-wallet-v4-frontend/src/scenes/Earn/Earn.types.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Earn/Earn.types.ts
@@ -1,7 +1,5 @@
-import { ChangeEvent } from 'react'
-
 import { EarnEDDStatus, RewardsRatesType, StakingRatesType } from '@core/types'
-import { EarnTabsType, UserDataType } from 'data/types'
+import { UserDataType } from 'data/types'
 
 export type SuccessStateType = {
   earnEDDStatus: EarnEDDStatus
@@ -13,13 +11,4 @@ export type SuccessStateType = {
 
 export type HTMLElementEvent<T extends HTMLElement> = Event & {
   target: T
-}
-
-export type EarnFilterPropsType = {
-  earnTab: EarnTabsType
-  handleAssetClick: (status: boolean) => void
-  handleHistoryClick: () => void
-  handleSearch: (e: ChangeEvent<HTMLInputElement>) => void
-  handleTabClick: (tab: string) => void
-  showAvailableAssets: boolean
 }
