ac09df9054d9e7e0f4a245f6cd8eaee6a6c1dc49
jjBlockchain <jj@blockchain.com>
2022-12-02 00:01:04 -0600

    style(active-rewards-learn-header): link width fit content

--- packages/blockchain-wallet-v4-frontend/src/scenes/ActiveRewardsLearn/Header/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/ActiveRewardsLearn/Header/index.tsx
@@ -2,20 +2,25 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Button, Flex, IconChevronLeft, SemanticColors, Text } from '@blockchain-com/constellation'
+import styled from 'styled-components'
 
 import { Link } from 'blockchain-info-components'
+
+const CustomLink = styled(Link)`
+  width: fit-content;
+`
 
 const Header = () => (
   <Flex flexDirection='column' gap={24}>
     <LinkContainer to='/earn'>
-      <Link>
+      <CustomLink>
         <Flex alignItems='center'>
           <IconChevronLeft color={SemanticColors.primary} size='medium' />
           <Text color={SemanticColors.primary} variant='caption1'>
             <FormattedMessage id='copy.earn' defaultMessage='Earn' />
           </Text>
         </Flex>
-      </Link>
+      </CustomLink>
     </LinkContainer>
     <Flex alignItems='center' gap={16} justifyContent='space-between'>
       <Text color={SemanticColors.title} variant='title1'>
