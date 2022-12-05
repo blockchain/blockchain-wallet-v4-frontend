655dae77efcb0f2fb182624364585ed805f17a84
leora <leora@blockchain.com>
2022-12-05 09:29:44 -0700

    fix(polling): look for approved instead of true

--- packages/blockchain-wallet-v4-frontend/src/data/signup/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/signup/sagas.ts
@@ -410,9 +410,7 @@ export default ({ api, coreSagas, networks }) => {
     try {
       yield delay(2000)
       const response = yield call(api.pollForResetApprovalStatus, sessionToken)
-      // temo should revert back to approved
-      if (response?.status === 'true') {
-        // if (response?.status === AccountRecoveryApprovalStatusType.APPROVED) {
+      if (response?.status === AccountRecoveryApprovalStatusType.APPROVED) {
         yield put(actions.signup.setAccountRecoveryMagicLinkData(response))
         yield put(actions.form.change(RECOVER_FORM, 'step', RecoverSteps.RECOVERY_OPTIONS))
         return true
