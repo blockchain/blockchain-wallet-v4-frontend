827f77a08ba5286f25c78ce376ca78ea3394c34b
Diego Sisto <dsisto@blockchain.com>
2022-12-14 13:56:04 -0300

    feat(exchange-promo): refactor saga network call

--- packages/blockchain-wallet-v4-frontend/src/data/experiments/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/experiments/sagas.ts
@@ -6,11 +6,10 @@ export default ({ api }) => {
   const fetchExperimentsSaga = function* () {
     try {
       yield put(A.fetchLoading())
-      const experiements: ReturnType<typeof api.getExperiments> = yield call(api.getExperiments)
+      const experiements = yield call(api.getExperiments)
 
       yield put(A.fetchSuccess(experiements))
     } catch (error) {
-      // const error = errorHandler(e)
       yield put(A.fetchFailure(error))
     }
   }
