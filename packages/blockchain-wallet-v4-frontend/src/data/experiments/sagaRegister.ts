2657d2057cb75b2149e11c015e33a0b15b2401cf
leora <leora@blockchain.com>
2022-12-14 08:53:20 -0700

    fix(user-data): wait for user data before calling exp

--- packages/blockchain-wallet-v4-frontend/src/data/experiments/sagaRegister.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/experiments/sagaRegister.ts
@@ -3,8 +3,8 @@ import { takeEvery } from 'redux-saga/effects'
 import sagas from './sagas'
 import { actions } from './slice'
 
-export default ({ api }) => {
-  const { fetchExperimentsSaga } = sagas({ api })
+export default ({ api, coreSagas, networks }) => {
+  const { fetchExperimentsSaga } = sagas({ api, coreSagas, networks })
 
   return function* experimentsSaga() {
     yield takeEvery(actions.fetch.type, fetchExperimentsSaga)
--- packages/blockchain-wallet-v4-frontend/src/data/experiments/sagas.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/experiments/sagas.ts
@@ -1,11 +1,20 @@
 import { call, put } from 'redux-saga/effects'
 
+import profileSagas from 'data/modules/profile/sagas'
+
 import { actions as A } from './slice'
 
-export default ({ api }) => {
+export default ({ api, coreSagas, networks }) => {
+  const { waitForUserData } = profileSagas({
+    api,
+    coreSagas,
+    networks
+  })
+
   const fetchExperimentsSaga = function* () {
     try {
       yield put(A.fetchLoading())
+      yield call(waitForUserData)
       const experiements: ReturnType<typeof api.getExperiments> = yield call(api.getExperiments)
 
       yield put(A.fetchSuccess(experiements))
