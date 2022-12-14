7c6270a63dd7c90e26157425efed5e4db49798ee
jjBlockchain <jj@blockchain.com>
2022-12-14 00:35:49 -0600

    feat(active-rewards-warning): create getActions

--- packages/blockchain-wallet-v4-frontend/src/modals/ActiveRewards/Warning/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/ActiveRewards/Warning/index.tsx
@@ -2,28 +2,28 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { CoinType } from '@core/types'
-import { actions } from 'data'
 import { Analytics } from 'data/types'
 
+import { getActions } from './Warning.selectors'
 import Warning from './Warning.template'
 
 const WarningContainer = ({ coin, handleClose }: OwnProps) => {
   const dispatch = useDispatch()
+  const { analyticsActions, earnActions } = getActions(dispatch)
 
   useEffect(() => {
-    dispatch(actions.components.interest.fetchActiveRewardsLimits())
+    earnActions.fetchActiveRewardsLimits()
   }, [])
 
   const handleClick = () => {
-    dispatch(
-      actions.analytics.trackEvent({
-        key: Analytics.WALLET_REWARDS_DETAIL_DEPOSIT_CLICKED,
-        properties: {
-          currency: coin
-        }
-      })
-    )
-    dispatch(actions.components.interest.showStakingModal({ coin, step: 'DEPOSIT' }))
+    analyticsActions.trackEvent({
+      key: Analytics.WALLET_REWARDS_DETAIL_DEPOSIT_CLICKED,
+      properties: {
+        currency: coin
+      }
+    })
+
+    earnActions.showActiveRewardsModal({ coin, step: 'DEPOSIT' })
   }
 
   return <Warning coin={coin} handleClick={handleClick} handleClose={handleClose} />
