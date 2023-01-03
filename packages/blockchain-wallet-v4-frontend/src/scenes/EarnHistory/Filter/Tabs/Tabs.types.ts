2559ed1e2af8393ae07d2f4b0668244e2d4b99d2
TheLeoB <TheLeoB@users.noreply.github.com>
2023-01-03 12:14:57 -0700

    Release/v4.80 (#5656)
    * fix(analytics): change middleware queuevent name
    
    * chore(Release): v4.80.6
    
    * fix(active-rewards-learn-faq): add missing link
    
    * feat(active-rewards-activity): add active rewards feature flag to filter tabs
    
    * fix: exchange promo shows for non SSO users
    
    * chore(Release): v4.80.7
    
    Co-authored-by: jjBlockchain <53186076+jjBlockchain@users.noreply.github.com>
    Co-authored-by: Diego Sisto <dsisto@blockchain.com>
    Co-authored-by: dsisto-bc <67278135+dsisto-bc@users.noreply.github.com>
    Co-authored-by: jjBlockchain <jj@blockchain.com>
    Co-authored-by: Milan <67264242+milan-bc@users.noreply.github.com>

--- packages/blockchain-wallet-v4-frontend/src/scenes/EarnHistory/Filter/Tabs/Tabs.types.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/EarnHistory/Filter/Tabs/Tabs.types.ts
@@ -1,6 +1,11 @@
 import { EarnTabsType } from 'data/types'
 
-export type TabsProps = {
+export type TabsPropsType = {
   earnTab: EarnTabsType
   handleTabClick: (tab: EarnTabsType) => void
+}
+
+export type TabType = {
+  key: string | EarnTabsType
+  titleContent: JSX.Element
 }
