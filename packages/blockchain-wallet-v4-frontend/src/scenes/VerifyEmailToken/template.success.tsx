3ee6cd5a0121d2ae228aabcc0b86999479c18e96
Milan <milan@blockchain.com>
2023-01-05 10:14:03 +0100

    feat(email verification): added button to redirect user to app once when they verified email

--- packages/blockchain-wallet-v4-frontend/src/scenes/VerifyEmailToken/template.success.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/VerifyEmailToken/template.success.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import { Padding } from '@blockchain-com/constellation'
 import styled from 'styled-components'
 
-import { Image, Text } from 'blockchain-info-components'
+import { Button, Image, Link, Text } from 'blockchain-info-components'
 import { isMobile } from 'services/styles'
 
 const Wrapper = styled.div`
@@ -12,7 +13,13 @@ const Wrapper = styled.div`
   flex-direction: column;
 `
 
+const APP_URL = 'https://blockchainwallet.page.link/dashboard'
+
 const Success = () => {
+  const returnToApp = () => {
+    window.location.href = APP_URL
+  }
+
   return (
     <Wrapper>
       <Image name='email-verified' size='24px' />
@@ -38,6 +45,23 @@ const Success = () => {
           />
         </Text>
       )}
+      <Padding top={1}>
+        <Button
+          data-e2e='verifyEmail'
+          fullwidth
+          height='48px'
+          nature='light'
+          onClick={returnToApp}
+          style={{ marginBottom: '5px', marginTop: '32px' }}
+        >
+          <Text color='blue600' size='16px' weight={600}>
+            <FormattedMessage
+              id='buttons.go_back_to_bc_app'
+              defaultMessage='Go back to Blockchain.com App'
+            />
+          </Text>
+        </Button>
+      </Padding>
     </Wrapper>
   )
 }
