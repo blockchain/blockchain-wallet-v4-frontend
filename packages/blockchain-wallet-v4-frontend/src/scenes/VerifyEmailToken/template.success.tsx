cec2aea5ce44e871c9c674876bbc877a813d7021
Milan <milan@blockchain.com>
2023-01-09 16:20:29 +0100

    feat(verify email): use constelation components

--- packages/blockchain-wallet-v4-frontend/src/scenes/VerifyEmailToken/template.success.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/VerifyEmailToken/template.success.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
-import { Padding } from '@blockchain-com/constellation'
+import { Button, Padding } from '@blockchain-com/constellation'
 import styled from 'styled-components'
 
-import { Button, Image, Link, Text } from 'blockchain-info-components'
+import { Image, Text } from 'blockchain-info-components'
 import { isMobile } from 'services/styles'
 
 const Wrapper = styled.div`
@@ -45,22 +45,21 @@ const Success = () => {
           />
         </Text>
       )}
-      <Padding top={1}>
+      <Padding top={3} bottom={0.5}>
         <Button
-          data-e2e='verifyEmail'
-          fullwidth
-          height='48px'
-          nature='light'
+          data-e2e='goBackToApp'
+          width='full'
+          variant='minimal'
           onClick={returnToApp}
-          style={{ marginBottom: '5px', marginTop: '32px' }}
-        >
-          <Text color='blue600' size='16px' weight={600}>
-            <FormattedMessage
-              id='buttons.go_back_to_bc_app'
-              defaultMessage='Go back to Blockchain.com App'
-            />
-          </Text>
-        </Button>
+          text={
+            <Text color='blue600' size='16px' weight={600}>
+              <FormattedMessage
+                id='buttons.go_back_to_bc_app'
+                defaultMessage='Go back to Blockchain.com App'
+              />
+            </Text>
+          }
+        />
       </Padding>
     </Wrapper>
   )
