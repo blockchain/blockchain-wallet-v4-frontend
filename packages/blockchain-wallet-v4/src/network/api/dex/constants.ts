98d1f64544c5b7f67f31a596c5a0ef61ea05ee96
jjBlockchain <jj@blockchain.com>
2023-05-29 10:32:36 -0500

    chore(dex-api): temporarily use prod api until new staging apis are implemented

--- packages/blockchain-wallet-v4/src/network/api/dex/constants.ts
+++ packages/blockchain-wallet-v4/src/network/api/dex/constants.ts
@@ -1,4 +1,4 @@
 export const DEX_NABU_GATEWAY_PREFIX = '/nabu-gateway/dex'
-export const DEX_GATEWAY_PREFIX = '/dex-gateway/public/v1'
-// export const DEX_GATEWAY_PREFIX = '/dex-gateway/v1'
+// export const DEX_GATEWAY_PREFIX = '/dex-gateway/public/v1'
+export const DEX_GATEWAY_PREFIX = '/dex-gateway/v1'
 export const DEX_URL = 'https://dex-gateway.traefik/public/'
