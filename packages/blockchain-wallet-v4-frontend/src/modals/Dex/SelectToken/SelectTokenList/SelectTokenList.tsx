9a1d495ca34cb02dfd6e72f7342b2925f03f7a3c
jjBlockchain <jj@blockchain.com>
2023-06-21 09:09:55 -0500

    feat(token-select): remove 0 balance tokens from input

--- packages/blockchain-wallet-v4-frontend/src/modals/Dex/SelectToken/SelectTokenList/SelectTokenList.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Dex/SelectToken/SelectTokenList/SelectTokenList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Flex, Padding, SemanticColors, Text } from '@blockchain-com/constellation'
+import { BigNumber } from 'ethers'
 
 import type { CoinType } from '@core/types'
 import CoinDisplay from 'components/Display/CoinDisplay'
@@ -13,6 +14,7 @@ import { SelectTokenListProps } from './types'
 import ViewEtherscan from './ViewEtherscan'
 
 const NATIVE_TOKEN = 'ETH'
+const BASE = 'BASE'
 
 const SelectTokenList = ({
   items,
@@ -27,49 +29,54 @@ const SelectTokenList = ({
 
   return (
     <TokenListWrapper>
-      {filteredItems.map(({ address, balance, name, symbol }) => (
-        <TokenRow key={symbol} onClick={() => onTokenSelect(symbol)}>
-          <TokenIcon name={symbol as CoinType} size='24px' />
-          <TokenDetails>
-            <Flex flexDirection='column'>
-              <TokenName name={name} />
-              <Flex alignItems='center'>
-                <Text color={SemanticColors.muted} variant='paragraph1'>
-                  {symbol}
-                </Text>
-                <Padding left={0.5} />
-                {symbol !== NATIVE_TOKEN && <ViewEtherscan tokenAddress={address} />}
+      {filteredItems.map(({ address, balance, name, symbol }) => {
+        const balanceString = (typeof balance !== 'number' ? balance : balance).toString()
+
+        if (swapSide === BASE && balanceString === '0') return null
+        return (
+          <TokenRow key={symbol} onClick={() => onTokenSelect(symbol)}>
+            <TokenIcon name={symbol as CoinType} size='24px' />
+            <TokenDetails>
+              <Flex flexDirection='column'>
+                <TokenName name={name} />
+                <Flex alignItems='center'>
+                  <Text color={SemanticColors.muted} variant='paragraph1'>
+                    {symbol}
+                  </Text>
+                  <Padding left={0.5} />
+                  {symbol !== NATIVE_TOKEN && <ViewEtherscan tokenAddress={address} />}
+                </Flex>
               </Flex>
-            </Flex>
-            <TokenBalanceColumn>
-              <FiatDisplay
-                coin={symbol}
-                color='textBlack'
-                currency={walletCurrency}
-                cursor='pointer'
-                data-e2e={`${symbol}FiatBalance`}
-                lineHeight='150%'
-                loadingHeight='20px'
-                size='16px'
-                weight={600}
-              >
-                {balance}
-              </FiatDisplay>
-              <CoinDisplay
-                coin={symbol}
-                color='grey600'
-                cursor='pointer'
-                data-e2e={`${symbol}Balance`}
-                lineHeight='20px'
-                size='14px'
-                weight={500}
-              >
-                {balance}
-              </CoinDisplay>
-            </TokenBalanceColumn>
-          </TokenDetails>
-        </TokenRow>
-      ))}
+              <TokenBalanceColumn>
+                <FiatDisplay
+                  coin={symbol}
+                  color='textBlack'
+                  currency={walletCurrency}
+                  cursor='pointer'
+                  data-e2e={`${symbol}FiatBalance`}
+                  lineHeight='150%'
+                  loadingHeight='20px'
+                  size='16px'
+                  weight={600}
+                >
+                  {balance}
+                </FiatDisplay>
+                <CoinDisplay
+                  coin={symbol}
+                  color='grey600'
+                  cursor='pointer'
+                  data-e2e={`${symbol}Balance`}
+                  lineHeight='20px'
+                  size='14px'
+                  weight={500}
+                >
+                  {balance}
+                </CoinDisplay>
+              </TokenBalanceColumn>
+            </TokenDetails>
+          </TokenRow>
+        )
+      })}
     </TokenListWrapper>
   )
 }
