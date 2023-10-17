3f7726e8a928d5a10d6eda83dfeade910ff8896a
mperdomo-bc <97296851+mperdomo-bc@users.noreply.github.com>
2023-10-17 20:38:05 -0300

    Finprom text updates (#6087)
    * chore(finprom): add text for UK users in Prices
    
    * chore(finprom): add text to Earn page

--- packages/blockchain-wallet-v4-frontend/src/scenes/Prices/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Prices/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
 import { FormattedMessage } from 'react-intl'
-import { connect, ConnectedProps } from 'react-redux'
+import { connect, ConnectedProps, useSelector } from 'react-redux'
 import { bindActionCreators, compose } from 'redux'
 import { BaseFieldProps, Field, reduxForm } from 'redux-form'
 import styled from 'styled-components'
 
 import { ExtractSuccess } from '@core/types'
-import { Icon, Text } from 'blockchain-info-components'
+import { Icon, Link, Text } from 'blockchain-info-components'
+import { getData as getUserCountry } from 'components/Banner/selectors'
 import TextBox from 'components/Form/TextBox'
 import { PageTitle, SceneWrapper, StickyHeader, SubTitle, Title } from 'components/Layout'
 import { actions, selectors } from 'data'
@@ -32,43 +33,64 @@ const SearchIconWrapper = styled.div`
   left: 10px;
 `
 
-const Scene = ({ children }) => (
-  <SceneWrapper>
-    <StickyHeader style={{ paddingBottom: '20px' }}>
-      <PageTitle>
-        <div>
-          <Title>
-            <Icon size='36px' color='blue600' name='compass' />
-            <Text color='grey800' size='32px' weight={600}>
-              <FormattedMessage id='copy.prices' defaultMessage='Prices' />
-            </Text>
-          </Title>
-          <SubTitle>
-            <Text color='grey600' size='16px' weight={500}>
-              <FormattedMessage
-                id='scenes.prices.subtitle'
-                defaultMessage='Buy, Sell and Swap all of the assets offered by our wallet.'
-              />
-            </Text>
-          </SubTitle>
-        </div>
-        <TextFilterWrapper>
-          <IconField
-            component={TextBox}
-            height='42px'
-            data-e2e='pricesTextFilter'
-            name='textFilter'
-            placeholder='Search'
-          />
-          <SearchIconWrapper>
-            <Icon name='magnifier' size='20px' color='grey400' />
-          </SearchIconWrapper>
-        </TextFilterWrapper>
-      </PageTitle>
-    </StickyHeader>
-    {children}
-  </SceneWrapper>
-)
+const Scene = ({ children }) => {
+  const isUserFromUK = useSelector(getUserCountry)?.country === 'GB'
+
+  return (
+    <SceneWrapper>
+      <StickyHeader style={{ paddingBottom: '20px' }}>
+        <PageTitle>
+          <div>
+            <Title>
+              <Icon size='36px' color='blue600' name='compass' />
+              <Text color='grey800' size='32px' weight={600}>
+                <FormattedMessage id='copy.prices' defaultMessage='Prices' />
+              </Text>
+            </Title>
+            <SubTitle>
+              <Text color='grey600' size='16px' weight={500}>
+                <FormattedMessage
+                  id='scenes.prices.subtitle'
+                  defaultMessage='Buy, Sell and Swap all of the assets offered by our wallet.'
+                />
+              </Text>
+            </SubTitle>
+            {isUserFromUK && (
+              <SubTitle>
+                <Text color='grey600' size='14px' weight={500}>
+                  Real-time data is obtained from multiple sources and may sometimes be delayed due
+                  to system performance issues. Past performance is not a reliable indicator of
+                  future results. Find out more about various crypto assets and their risks{' '}
+                  <Link
+                    size='14px'
+                    href='https://support.blockchain.com/hc/en-us/articles/10857167024156-Various-Cryptoassets-and-Their-Risks-'
+                    target='_blank'
+                  >
+                    here
+                  </Link>
+                  .
+                </Text>
+              </SubTitle>
+            )}
+          </div>
+          <TextFilterWrapper>
+            <IconField
+              component={TextBox}
+              height='42px'
+              data-e2e='pricesTextFilter'
+              name='textFilter'
+              placeholder='Search'
+            />
+            <SearchIconWrapper>
+              <Icon name='magnifier' size='20px' color='grey400' />
+            </SearchIconWrapper>
+          </TextFilterWrapper>
+        </PageTitle>
+      </StickyHeader>
+      {children}
+    </SceneWrapper>
+  )
+}
 
 const PricesContainer = (props: OwnProps) => {
   const { priceActions, rowDataR, ...rest } = props
