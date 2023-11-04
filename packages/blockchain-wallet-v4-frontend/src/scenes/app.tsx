b7a5d4bb2abbabcae14bd62102b197acfda2aabb
mperdomo-bc <97296851+mperdomo-bc@users.noreply.github.com>
2023-11-04 14:38:08 -0300

    Add Prove redirection page (#6126)
    * feat(prove): initial prove handling page
    
    * feat(prove): add link back to app
    
    * feat(prove): send prove fingerprint to backend

--- packages/blockchain-wallet-v4-frontend/src/scenes/app.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/app.tsx
@@ -43,6 +43,7 @@ const RecoverWallet = React.lazy(() => import('./RecoverWallet'))
 const Signup = React.lazy(() => import('./Signup'))
 const ResetWallet2fa = React.lazy(() => import('./ResetWallet2fa'))
 const ResetWallet2faToken = React.lazy(() => import('./ResetWallet2faToken'))
+const Prove = React.lazy(() => import('./Prove'))
 // need to be authed to see this, but uses public layout
 const ContinueOnPhone = React.lazy(() => import('./ContinueOnPhone'))
 const TwoStepVerification = React.lazy(() => import('./TwoStepVerification'))
@@ -220,6 +221,11 @@ const App = ({
                                 path='/verify-email-step'
                                 component={VerifyEmail}
                                 pageTitle={`${BLOCKCHAIN_TITLE} | Verify Email`}
+                              />
+                              <AuthLayout
+                                path='/prove/instant-link/callback'
+                                component={Prove}
+                                pageTitle={`${BLOCKCHAIN_TITLE} | Verify Device`}
                               />
 
                               {/* DEX routes */}
