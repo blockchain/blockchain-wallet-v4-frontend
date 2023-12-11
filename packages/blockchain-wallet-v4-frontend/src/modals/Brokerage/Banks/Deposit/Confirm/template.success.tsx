88769a37f534d0027b36e9555e8d3b65d2110200
Mariano Perdomo <mperdomo@blockchain.com>
2023-12-11 19:25:36 -0300

    feat(deposit): add warning message

--- packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/Deposit/Confirm/template.success.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Brokerage/Banks/Deposit/Confirm/template.success.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useState } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { addDays, format } from 'date-fns'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
 
 import { fiatToString } from '@core/exchange/utils'
 import { FiatType } from '@core/types'
-import { Button, HeartbeatLoader } from 'blockchain-info-components'
+import { Button, HeartbeatLoader, Text } from 'blockchain-info-components'
 import AvailabilityRows from 'components/Brokerage/AvailabilityRows'
 import {
   FlyoutContainer,
@@ -15,10 +17,23 @@ import {
 } from 'components/Flyout/Layout'
 import { CheckoutRow } from 'components/Rows'
 import { BankDWStepType, BankPartners } from 'data/types'
+import { getUserLegalEntity } from 'data/modules/profile/selectors'
+import { getFiatTransformAlertEnabled } from '@core/redux/walletOptions/selectors'
 
 import { Props as _P, SuccessStateType as _S } from '.'
 import { FormattedBank } from './model'
 import { getCurrencyName } from './utils'
+
+// Auto margin top so it gets pushed to the bottom
+const FiatNoticeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 1rem;
+  background-color: ${(props) => props.theme.grey000};
+  border-radius: 0.5rem;
+  margin: auto 2rem 0;
+`
 
 const Success = (props: Props) => {
   const [submitting, setSubmitting] = useState<boolean>(false)
@@ -36,7 +51,12 @@ const Success = (props: Props) => {
     props.brokerageActions.createFiatDeposit()
   }, [])
 
+  const fiatTransformAlertEnabled = useSelector(getFiatTransformAlertEnabled)
+  const userLegalEntity = useSelector(getUserLegalEntity)
+
   const amount = props.formValues?.amount || 0
+
+  const showChangeAlert = fiatTransformAlertEnabled && true;
 
   return (
     <FlyoutContainer>
@@ -80,6 +100,25 @@ const Success = (props: Props) => {
           title={<FormattedMessage id='copy.total' defaultMessage='Total' />}
         />
         {props.availableToTradeWithdraw && <AvailabilityRows depositTerms={props.depositTerms} />}
+
+        {showChangeAlert && (
+          <FiatNoticeWrapper>
+            <Text
+              weight={600}
+              size='14px'
+              lineHeight='21px'
+              style={{ marginBottom: '8px' }}
+              color='grey900'
+            >
+              Changes to {props.defaultMethod?.currency} Balances
+            </Text>
+            <Text size='12px' color='grey900'>
+              Your {targetCoinName} ({props.defaultMethod?.currency}) balance will be converted to
+              USDC daily at 12:00 am UTC. To avoid any inconvenience, buy crypto or initiate a
+              withdrawal before the specified time.
+            </Text>
+          </FiatNoticeWrapper>
+        )}
       </FlyoutContent>
       <FlyoutFooter collapsed>
         <Button
