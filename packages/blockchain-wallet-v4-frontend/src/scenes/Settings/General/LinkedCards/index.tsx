d9ef7618ea9e046876c14224fbc6b76b1ef071f3
mperdomo-bc <97296851+mperdomo-bc@users.noreply.github.com>
2023-12-12 16:37:19 -0300

    chore(settings): refactor linked cards (#6163)
    * chore(settings): refactor linked cards
    
    * chore(settings): address pr comments

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/LinkedCards/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/LinkedCards/index.tsx
@@ -1,79 +1,64 @@
-import React, { PureComponent } from 'react'
-import { connect, ConnectedProps } from 'react-redux'
-import { bindActionCreators, Dispatch } from 'redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { BSCardType, BSPaymentMethodsType, FiatType, RemoteDataType } from '@core/types'
+import { BSCardType, BSPaymentMethodsType } from '@core/types'
 import { actions, selectors } from 'data'
 import { ModalName } from 'data/modals/types'
-import { RootState } from 'data/rootReducer'
 import { UserDataType } from 'data/types'
+import { useRemote } from 'hooks'
 
 import { getData } from './selectors'
 import Loading from './template.loading'
 import Success from './template.success'
 
-class LinkedCards extends PureComponent<Props> {
-  componentDidMount() {
-    this.props.buySellActions.fetchCards(false)
-    this.props.buySellActions.fetchPaymentMethods(this.props.fiatCurrency)
+const LinkedCards = () => {
+  const dispatch = useDispatch()
+
+  const fiatCurrency = useSelector(selectors.core.settings.getCurrency) ?? 'USD'
+
+  const handleCreditCardClick = () => {
+    dispatch(
+      actions.components.buySell.showModal({
+        origin: 'SettingsGeneral',
+        step: 'DETERMINE_CARD_PROVIDER'
+      })
+    )
+
+    dispatch(actions.components.buySell.setFiatCurrency(fiatCurrency))
   }
 
-  handleCreditCardClick = () => {
-    this.props.buySellActions.showModal({
-      origin: 'SettingsGeneral',
-      step: 'DETERMINE_CARD_PROVIDER'
-    })
-    this.props.buySellActions.setFiatCurrency(this.props.fiatCurrency || 'USD')
+  const proceedToUserVerification = () => {
+    dispatch(
+      actions.modals.showModal(ModalName.COMPLETE_USER_PROFILE, {
+        origin: 'SettingsGeneral'
+      })
+    )
   }
 
-  proceedToUserVerification = () => {
-    this.props.modalActions.showModal(ModalName.COMPLETE_USER_PROFILE, {
-      origin: 'SettingsGeneral'
-    })
-  }
+  useEffect(() => {
+    dispatch(actions.components.buySell.fetchCards(false))
+    dispatch(actions.components.buySell.fetchPaymentMethods(fiatCurrency))
+  }, [])
 
-  render() {
-    return this.props.data.cata({
-      Failure: () => null,
-      Loading: () => <Loading />,
-      NotAsked: () => null,
-      Success: (val) => {
-        const isUserVerified = val.userData.tiers && val.userData.tiers.current > 0
-        return (
-          <Success
-            {...val}
-            {...this.props}
-            handleCreditCardClick={
-              isUserVerified ? this.handleCreditCardClick : this.proceedToUserVerification
-            }
-          />
-        )
-      }
-    })
-  }
+  const { data, hasError, isLoading, isNotAsked } = useRemote(getData)
+
+  if (isLoading) return <Loading />
+  if (hasError || isNotAsked || !data) return null
+
+  const isUserVerified = data.userData.tiers?.current > 0
+
+  return (
+    <Success
+      {...(data as SuccessStateType)}
+      handleCreditCardClick={isUserVerified ? handleCreditCardClick : proceedToUserVerification}
+    />
+  )
 }
-
-const mapStateToProps = (state: RootState): LinkStatePropsType => ({
-  data: getData(state),
-  fiatCurrency: selectors.core.settings.getCurrency(state).getOrElse('USD')
-})
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  buySellActions: bindActionCreators(actions.components.buySell, dispatch),
-  modalActions: bindActionCreators(actions.modals, dispatch)
-})
-
-const connector = connect(mapStateToProps, mapDispatchToProps)
 
 export type SuccessStateType = {
   cards: Array<BSCardType>
   paymentMethods: BSPaymentMethodsType
   userData: UserDataType
 }
-type LinkStatePropsType = {
-  data: RemoteDataType<string, SuccessStateType>
-  fiatCurrency?: FiatType
-}
-export type Props = ConnectedProps<typeof connector>
 
-export default connector(LinkedCards)
+export default LinkedCards
