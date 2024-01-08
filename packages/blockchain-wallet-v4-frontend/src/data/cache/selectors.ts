e6e9daae2b03a59d3d2a93a47f0472310160fdd1
mperdomo-bc <97296851+mperdomo-bc@users.noreply.github.com>
2024-01-08 10:48:57 -0300

    chore(refactor): clean up around login folder (#6218)

--- packages/blockchain-wallet-v4-frontend/src/data/cache/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/cache/selectors.ts
@@ -1,46 +1,32 @@
-import { path, prop } from 'ramda'
-
 import { crypto as wCrypto } from '@core'
+import { RootState } from 'data/rootReducer'
 
-export const getLastAnnouncementState = (state): object | undefined =>
-  path(['cache', 'announcements'], state)
-export const getCache = (state) => prop('cache', state)
-export const getEmail = (state): string | undefined => path(['cache', 'lastEmail'], state)
-export const getExchangeEmail = (state): string | undefined =>
-  path(['cache', 'exchangeEmail'], state)
-export const getExchangeWalletGuid = (state): string | undefined =>
-  path(['cache', 'exchangeWalletGuid'], state)
-export const getStoredGuid = (state): string | undefined => path(['cache', 'guidStored'], state)
-export const getMobileConnected = (state): string | undefined =>
-  path(['cache', 'mobileConnected'], state)
-export const getLastGuid = (state): string | undefined => path(['cache', 'lastGuid'], state)
-export const getChannelChannelId = (state): string | undefined =>
-  path(['cache', 'channelChannelId'], state)
-export const getChannelPrivKey = (state): string | undefined =>
-  path(['cache', 'channelPrivKey'], state)
-export const getPhonePubkey = (state): string | undefined =>
-  path(['cache', 'channelPhonePubkey'], state)
-export const getHasCloudBackup = (state): boolean | undefined =>
-  path(['cache', 'hasCloudBackup'], state)
-export const getLastLogoutTimestamp = (state): boolean | undefined =>
-  path(['cache', 'lastLogoutTimestamp'], state)
-export const getUnifiedAccountStatus = (state): boolean | undefined =>
-  path(['cache', 'unifiedAccount'], state)
-export const getLastUnusedAmounts = (state): boolean | undefined =>
-  path(['cache', 'lastUnusedAmounts'], state)
+export const getLastAnnouncementState = (state: RootState) => state.cache.announcements
+export const getCache = (state: RootState) => state.cache
+export const getEmail = (state: RootState) => state.cache.lastEmail
+export const getExchangeEmail = (state: RootState) => state.cache.exchangeEmail
+export const getExchangeWalletGuid = (state: RootState) => state.cache.exchangeWalletGuid
+export const getStoredGuid = (state: RootState) => state.cache.guidStored
+export const getMobileConnected = (state: RootState) => state.cache.mobileConnected
+export const getLastGuid = (state: RootState) => state.cache.lastGuid
+export const getChannelChannelId = (state: RootState) => state.cache.channelChannelId
+export const getChannelPrivKey = (state: RootState) => state.cache.channelPrivKey
+export const getPhonePubkey = (state: RootState) => state.cache.channelPhonePubkey
+export const getHasCloudBackup = (state: RootState) => state.cache.hasCloudBackup
+export const getLastLogoutTimestamp = (state: RootState) => state.cache.lastLogoutTimestamp
+export const getUnifiedAccountStatus = (state: RootState) => state.cache.unifiedAccount
+export const getLastUnusedAmounts = (state: RootState) => state.cache.lastUnusedAmounts
 
-export const getNoActionRequiredSweep = (state): { guid: string; seen: boolean } | undefined =>
-  path(['cache', 'noActionRequiredSweep'], state)
+export const getNoActionRequiredSweep = (state: RootState) => state.cache.noActionRequiredSweep
 
-export const getChannelPrivKeyForQrData = (state) => {
+export const getChannelPrivKeyForQrData = (state: RootState) => {
   const channelPrivKey = getChannelPrivKey(state)
 
-  if (channelPrivKey) {
-    return JSON.stringify({
-      channelId: getChannelChannelId(state),
-      pubkey: wCrypto.derivePubFromPriv(Buffer.from(channelPrivKey, 'hex')).toString('hex'),
-      type: 'login_wallet'
-    })
-  }
-  return ''
+  if (!channelPrivKey) return ''
+
+  return JSON.stringify({
+    channelId: getChannelChannelId(state),
+    pubkey: wCrypto.derivePubFromPriv(Buffer.from(channelPrivKey, 'hex')).toString('hex'),
+    type: 'login_wallet'
+  })
 }
