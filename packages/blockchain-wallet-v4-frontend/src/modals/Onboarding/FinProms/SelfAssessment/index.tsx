e66d8e8f730b779f7585db249c45ae382c3e47df
Milan Vasic <milan@blockchain.com>
2024-01-16 11:10:27 +0100

    feat(hand hold): fixed issue after rebase

--- packages/blockchain-wallet-v4-frontend/src/modals/Onboarding/FinProms/SelfAssessment/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/modals/Onboarding/FinProms/SelfAssessment/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import { ModalPropsType } from 'blockchain-wallet-v4-frontend/src/modals/types'
 
