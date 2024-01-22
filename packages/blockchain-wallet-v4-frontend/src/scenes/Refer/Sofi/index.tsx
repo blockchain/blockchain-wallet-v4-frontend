05c0a682eb3d871880fcd2150179e7084fbeaee5
mrodriguez-bc <mrodriguez@blockchain.com>
2024-01-22 15:45:58 -0500

    feat(sofi-referral): create redirect handler for migration + anon referral

---
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Refer/Sofi/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Redirect, RouteComponentProps } from 'react-router-dom'
+
+const SofiReferralComponent: React.FC<RouteComponentProps> = ({ location }) => {
+  const searchParams = new URLSearchParams(location.search)
+  const hasAllRequiredJwtParams = ['aesCiphertext', 'aesIV', 'aesTag', 'aesKeyCiphertext'].every(
+    (param) => searchParams.has(param)
+  )
+
+  let redirectTo = ''
+  if (hasAllRequiredJwtParams) {
+    redirectTo = '/sofi'
+  } else {
+    redirectTo = 'https://blockchainwallet.page.link/buysofi'
+    const viewParam = searchParams.get('buy')
+    if (viewParam) {
+      redirectTo += `?code=${viewParam}`
+    }
+  }
+
+  redirectTo += location.search
+
+  return <Redirect to={redirectTo} />
+}
+
+export default SofiReferralComponent
