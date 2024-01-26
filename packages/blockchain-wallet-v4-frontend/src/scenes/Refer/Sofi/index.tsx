000617034a6ba1f7a03158c3f37daf42cad9ea4a
mrodriguez-bc <mrodriguez@blockchain.com>
2024-01-26 08:12:18 -0500

    feat(sofi-referral): remove migration handler

--- packages/blockchain-wallet-v4-frontend/src/scenes/Refer/Sofi/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Refer/Sofi/index.tsx
@@ -3,27 +3,11 @@ import { Redirect, RouteComponentProps } from 'react-router-dom'
 
 const SofiReferralComponent: React.FC<RouteComponentProps> = ({ location }) => {
   const searchParams = new URLSearchParams(location.search)
-  const hasAllRequiredJwtParams = ['aesCiphertext', 'aesIV', 'aesTag', 'aesKeyCiphertext'].every(
-    (param) => searchParams.has(param)
-  )
+  const viewParam = searchParams.get('buy')
+  const queryString = viewParam ? `?code=${viewParam}` : '?ref=sofi'
+  const redirectTo = `https://blockchainwallet.page.link/buysofi${queryString}`
 
-  let redirectTo = ''
-  let queryString = ''
-
-  if (hasAllRequiredJwtParams) {
-    redirectTo = '/sofi'
-    queryString = location.search
-  } else {
-    redirectTo = 'https://blockchainwallet.page.link/buysofi'
-    const viewParam = searchParams.get('buy')
-    if (viewParam) {
-      queryString = `?code=${viewParam}`
-    } else {
-      queryString = '?ref=sofi'
-    }
-  }
-
-  return <Redirect to={redirectTo + queryString} />
+  return <Redirect to={redirectTo} />
 }
 
 export default SofiReferralComponent
