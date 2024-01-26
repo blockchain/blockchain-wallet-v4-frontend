b336376a46aa8f58f980acd828de322b8a60b4f9
mperdomo-bc <97296851+mperdomo-bc@users.noreply.github.com>
2024-01-26 14:26:49 -0300

    chore(coinbox): do not rerender the coin box unnecessarily (#6252)

--- packages/blockchain-wallet-v4-frontend/src/components/Form/SelectBoxCoinPriceChart/selectors.js
+++
@@ -1,8 +0,0 @@
-export const getCoins = () => [
-  { text: 'Bitcoin', value: 'BTC' },
-  { text: 'Ether', value: 'ETH' },
-  { text: 'Bitcoin Cash', value: 'BCH' },
-  { text: 'Stellar', value: 'XLM' }
-]
-
-export default getCoins
