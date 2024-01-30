1981859afc05c7eb2525da2eb04c62b03a56629f
Leora Brody <leora@blockchain.com>
2024-01-30 15:43:28 -0700

    feat(sofi-referral): redirect based on migration status

--- packages/blockchain-wallet-v4-frontend/src/scenes/Refer/Sofi/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Refer/Sofi/index.tsx
@@ -1,28 +1,77 @@
-import React, { useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+
+import { getDomains } from '@core/redux/walletOptions/selectors'
+import { actions, selectors } from 'data'
+import { SofiUserMigrationStatus } from 'data/types'
 
 const SofiReferralComponent: React.FC = () => {
-  const location = useLocation()
-  const history = useHistory()
+  const dispatch = useDispatch()
 
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search)
+  const {
+    data: { api }
+  } = useSelector(getDomains)
+
+  const search = useSelector(selectors.router.getSearch) as string
+
+  const url = window.location.href
+  const getQueryParamCaseInsensitive = (url, paramName) => {
+    const searchParams = new URLSearchParams(search)
+    // Create URLSearchParams object from the URL
+    const normalizedParamName = paramName.toLowerCase()
+    // Iterate over all query parameters
+    // eslint-disable-next-line
+    for (const [key, value] of searchParams) {
+      if (key.toLowerCase() === normalizedParamName) {
+        return value
+      }
+    }
+    // If the parameter is not found, return null or undefined
+    return null
+  }
+
+  const sofiReferralRedirect = async () => {
+    const searchParams = new URLSearchParams(search)
     const hasAllRequiredJwtParams = ['aesCiphertext', 'aesIV', 'aesTag', 'aesKeyCiphertext'].every(
       (param) => searchParams.has(param)
     )
 
     // if jwt is present, check the migration status
-
-    // if (hasAllRequiredJwtParams && migrationPending) {
+    let migrationStatus
     if (hasAllRequiredJwtParams) {
-      history.push(`/sofi${location.search}`)
+      const aesCiphertext = getQueryParamCaseInsensitive(url, 'aesCiphertext') as string
+      const aesIV = getQueryParamCaseInsensitive(url, 'aesIV') as string
+      const aesTag = getQueryParamCaseInsensitive(url, 'aesTag') as string
+      const aesKeyCiphertext = getQueryParamCaseInsensitive(url, 'aesKeyCipherText') as string
+      const response = await axios.get(
+        `${api}/nabu-gateway/sofi/user-migration-status?api_code=1770d5d9-bcea-4d28-ad21-6cbd5be018a8`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-SoFi-AES-IV': aesIV,
+            'X-SoFi-AES-Key-Ciphertext': aesKeyCiphertext,
+            'X-SoFi-AES-Tag': aesTag,
+            'X-SoFi-JWT-AES-Ciphertext': aesCiphertext
+          }
+        }
+      )
+      migrationStatus = response.data.migrationStatus
+    }
+
+    if (hasAllRequiredJwtParams && migrationStatus === SofiUserMigrationStatus.AWAITING_USER) {
+      dispatch(actions.router.push(`/sofi${search}`))
     } else {
       const viewParam = searchParams.get('buy')
       const queryString = viewParam ? `?code=${viewParam}&ref=sofi` : '?ref=sofi'
 
       window.location.href = `https://blockchainwallet.page.link/buysofi${queryString}`
     }
-  }, [location, history])
+  }
+
+  useEffect(() => {
+    sofiReferralRedirect()
+  }, [])
 
   return null
 }
