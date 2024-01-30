769799093f9fccb3f0cb0f5e69817b59c2f7fbfa
mrodriguez-bc <mrodriguez@blockchain.com>
2024-01-30 12:43:57 -0500

    feat(sofi-referral): add migration handler back

--- packages/blockchain-wallet-v4-frontend/src/scenes/Refer/Sofi/index.tsx
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Refer/Sofi/index.tsx
@@ -1,14 +1,28 @@
 import React, { useEffect } from 'react'
-import { RouteComponentProps } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
-const SofiReferralComponent: React.FC<RouteComponentProps> = ({ location }) => {
+const SofiReferralComponent: React.FC = () => {
+  const location = useLocation()
+  const history = useHistory()
+
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search)
-    const viewParam = searchParams.get('buy')
-    const queryString = viewParam ? `?code=${viewParam}` : '?ref=sofi'
+    const hasAllRequiredJwtParams = ['aesCiphertext', 'aesIV', 'aesTag', 'aesKeyCiphertext'].every(
+      (param) => searchParams.has(param)
+    )
 
-    window.location.href = `https://blockchainwallet.page.link/buysofi${queryString}`
-  }, [location])
+    // if jwt is present, check the migration status
+
+    // if (hasAllRequiredJwtParams && migrationPending) {
+    if (hasAllRequiredJwtParams) {
+      history.push(`/sofi${location.search}`)
+    } else {
+      const viewParam = searchParams.get('buy')
+      const queryString = viewParam ? `?code=${viewParam}&ref=sofi` : '?ref=sofi'
+
+      window.location.href = `https://blockchainwallet.page.link/buysofi${queryString}`
+    }
+  }, [location, history])
 
   return null
 }
