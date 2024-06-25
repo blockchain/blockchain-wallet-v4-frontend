e202e047cb129ef7539a62aed7a7be2955d82cc1
jjBlockchain <jj@blockchain.com>
2024-06-25 12:03:25 -0500

    chore(spelling): fix spelling in webpack builder

--- packages/blockchain-wallet-v4-frontend/webpackBuilder.js
+++ packages/blockchain-wallet-v4-frontend/webpackBuilder.js
@@ -150,9 +150,11 @@ const buildWebpackConfig = (envConfig, extraPluginsList) => ({
       }),
       new HtmlReplaceWebpackPlugin([
         {
-          patten: '**WALLET_V5_LINK**',
+          pattern: '**WALLET_V5_LINK**',
           // Note: using an obvious redirection issue
-          replacement: process.env.WALLET_V5_LINK ? process.env.WALLET_V5_LINK : 'https://google.com'
+          replacement: process.env.WALLET_V5_LINK
+            ? process.env.WALLET_V5_LINK
+            : 'https://google.com'
         },
         {
           pattern: '**APP_VERSION**',
