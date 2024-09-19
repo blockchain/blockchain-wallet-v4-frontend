373dfe246b976358e00c29c77f12d6a0240272f1
John Hagerman <jhagerman@blockchain.com>
2024-09-19 13:05:04 -0400

    feat/bitpay-tracking: added events to track bitpay usage

--- packages/blockchain-wallet-v4-frontend/src/data/analytics/types/misc.ts
+++ packages/blockchain-wallet-v4-frontend/src/data/analytics/types/misc.ts
@@ -3,7 +3,8 @@ export enum MiscEvents {
   CUSTOMER_SUPPORT_CLICKED = 'Customer Support Clicked',
   SIGNUP_VIEWED = 'Signup Viewed',
   WRONG_CHANGE_CACHE = 'Wrong Change Cache',
-  WRONG_RECEIVE_CACHE = 'Wrong Receive Cache'
+  WRONG_RECEIVE_CACHE = 'Wrong Receive Cache',
+  BITPAY_LINK = 'Bitpay Deeplink detected'
 }
 
 export type MiscActions = {
